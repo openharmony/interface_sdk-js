c4507d3f4a83bb07d38da1cad804213f4ea5475e
lyj_love_code <liangyujian2@huawei.com>
2021-08-31 09:48:46 +0800

    move @ohos.hiAppEvent.d.ts
    Signed-off-by: lyj_love_code <liangyujian2@huawei.com>

--- api/phone/@ohos.hiAppEvent.d.ts
+++ api/phone/@ohos.hiAppEvent.d.ts
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-import { AsyncCallback } from './phone/basic';
+import { AsyncCallback } from './basic';
 
 /**
  * Provides the event logging function for applications to log the fault, statistical, security,
