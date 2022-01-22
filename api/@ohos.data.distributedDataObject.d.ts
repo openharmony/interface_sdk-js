a5be5e438641fd80737fd4229e04a833ed4eb8c8
hanlu <hanlu1@huawei.com>
2022-01-22 16:00:49 +0800

    objectstore
    Signed-off-by: hanlu <hanlu1@huawei.com>

--- api/@ohos.data.distributedDataObject.d.ts
+++ api/@ohos.data.distributedDataObject.d.ts
@@ -72,14 +72,15 @@ declare namespace distributedDataObject {
          * @since 8
          */
         off(type: 'change', callback?: Callback<{ sessionId: string, fields: Array<string> }>): void;
-		/**
-		 * On watch of status
-		 *
-		 * @param callback The callback of status, sessionId is the sessionId of changed object
-		 * deviceId is the device that object changed, status is the status of object
-		 * @since 8
-		 */
-        on(type: 'status', callback: Callback<{ sessionId: string, deviceId: string, status: 'online'|'offline' }>): void;
+
+        /**
+         * On watch of status
+         *
+         * @param callback The callback of status, sessionId is the sessionId of changed object
+         * deviceId is the device that object changed, status is the status of object
+         * @since 8
+         */
+        on(type: 'status', callback: Callback<{ sessionId: string, deviceId: string, status: 'online' | 'offline' }>): void;
 
         /**
          * Off watch of status
@@ -87,7 +88,7 @@ declare namespace distributedDataObject {
          * @param callback If not null, off the callback, if undefined, off all callbacks
          * @since 8
          */
-        off(type: 'status', callback?: Callback<{ sessionId: string, deviceId: string, status: 'online'|'offline' }>): void;
+        off(type: 'status', callback?: Callback<{ sessionId: string, deviceId: string, status: 'online' | 'offline' }>): void;
     }
 }
 
