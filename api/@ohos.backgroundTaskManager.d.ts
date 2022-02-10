d5bdb763b73ddb6bdffdbeba3047581ceae0eef8
廖康康 <liaokangkang@huawei.com>
2022-02-10 09:51:42 +0800

    modify syscap
    Signed-off-by: 廖康康 <liaokangkang@huawei.com>

--- api/@ohos.backgroundTaskManager.d.ts
+++ api/@ohos.backgroundTaskManager.d.ts
@@ -19,7 +19,7 @@ import { AsyncCallback , Callback} from './basic';
  * Manages background tasks.
  *
  * @since 7
- * @sysCap SystemCapability.Ressched.BackgroundTaskMgr
+ * @sysCap SystemCapability.ResourceSchedule.BackgroundTaskMgr.TransientTask
  */
 declare namespace backgroundTaskManager {
     /**
@@ -27,7 +27,7 @@ declare namespace backgroundTaskManager {
      *
      * @name DelaySuspendInfo
      * @since 7
-     * @sysCap SystemCapability.Ressched.BackgroundTaskMgr
+     * @sysCap SystemCapability.ResourceSchedule.BackgroundTaskMgr.TransientTask
      */
     interface DelaySuspendInfo {
         /**
@@ -44,7 +44,7 @@ declare namespace backgroundTaskManager {
      * Cancels delayed transition to the suspended state.
      *
      * @since 7
-     * @sysCap SystemCapability.Ressched.BackgroundTaskMgr
+     * @sysCap SystemCapability.ResourceSchedule.BackgroundTaskMgr.TransientTask
      * @param requestId Indicates the identifier of the delay request.
      */
     function cancelSuspendDelay(requestId: number): void;
@@ -53,7 +53,7 @@ declare namespace backgroundTaskManager {
      * Obtains the remaining time before an application enters the suspended state.
      *
      * @since 7
-     * @sysCap SystemCapability.Ressched.BackgroundTaskMgr
+     * @sysCap SystemCapability.ResourceSchedule.BackgroundTaskMgr.TransientTask
      * @param requestId Indicates the identifier of the delay request.
      * @return The remaining delay time
      */
@@ -64,7 +64,7 @@ declare namespace backgroundTaskManager {
      * Requests delayed transition to the suspended state.
      *
      * @since 7
-     * @sysCap SystemCapability.Ressched.BackgroundTaskMgr
+     * @sysCap SystemCapability.ResourceSchedule.BackgroundTaskMgr.TransientTask
      * @param reason Indicates the reason for delayed transition to the suspended state.
      * @param callback The callback delay time expired.
      * @return Info of delay request
