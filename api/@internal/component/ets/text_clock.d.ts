ef84a3ebf0addcb996cdc707f0fa813e99f50f51
yaoyuchi <yaoyuchi@huawei.com>
2022-02-17 14:45:49 +0800

    add_textclock_controller
    Signed-off-by: yaoyuchi <yaoyuchi@huawei.com>

--- api/@internal/component/ets/text_clock.d.ts
+++ api/@internal/component/ets/text_clock.d.ts
@@ -14,6 +14,28 @@
  */
 
 /**
+ * Provides a way to control the textclock status.
+ * @since 8
+ */
+export declare class TextClockController {
+  /**
+   * constructor.
+   * @since 8
+   */
+  constructor();
+  /**
+   * Provides a start event for textclock.
+   * @since 8
+   */
+  start();
+  /**
+   *  Provides a stop event for textclock.
+   *  @since 8
+   */
+  stop();
+}
+
+/**
  * TextClock component, which provides the text clock capability.
  * @since 8
  */
@@ -25,7 +47,7 @@ interface TextClockInterface {
    * Where a negative value indicates the eastern time zone, for example, -8.
    * @since 8
    */
-  (options?: {hourswest?: number}): TextClockAttribute;
+  (options?: {timeZoneOffset?: number, controller?: TextClockController}): TextClockAttribute;
 }
 
 declare class TextClockAttribute extends CommonMethod<TextClockAttribute> {
@@ -48,21 +70,13 @@ declare class TextClockAttribute extends CommonMethod<TextClockAttribute> {
   debugLine(value: string): TextClockAttribute;
 
   /**
-   * set display time format，such as "yyyy/mm/dd"、“yyyy-mm-dd".
+   * set display time format,such as "yyyy/mm/dd","yyyy-mm-dd".
    * support time format：yyyy,mm,mmm(English month abbreviation),mmmm(Full name of the month in English),
    * dd,ddd(English Week abbreviation),dddd(Full name of the week in English),
-   * HH/hh(24-hour clock/12-hour clock),MM/mm(minute),SS/ss(second)
+   * HH/hh(24-hour clock/12-hour clock),MM/mm(minute),SS/ss(second).
    * @since 8
    */
   format(value: string): TextClockAttribute;
-  /**
-   * sets the start and stop of the text clock. You can customize the start and stop of the text clock.
-   * when the value is true, the time of the current custom style is displayed.
-   * the time is recalled every second.
-   * if the value is false, the clock stops updating the time and stops updating the callback.
-   * @since 8
-   */
-  status(value: boolean): TextClockAttribute;
   /**
    * Provides a date change callback.
    * The callback parameter is Unix Time Stamp,
