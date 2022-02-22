cefa003aded3ee7ac8263b0d0be4b47161e1b283
clevercong <lichunlin2@huawei.com>
2022-02-22 09:47:58 +0800

    add hasVoiceCap api.
    Signed-off-by: clevercong <lichunlin2@huawei.com>

--- api/@ohos.telephony.call.d.ts
+++ api/@ohos.telephony.call.d.ts
@@ -73,6 +73,18 @@ declare namespace call {
   function muteRinger(): Promise<void>;
 
   /**
+   * Checks whether a device supports voice calls.
+   *
+   * <p>The system checks whether the device has the capability to initiate a circuit switching (CS) or IP multimedia
+   * subsystem domain (IMS) call on a telephone service network. If the device supports only packet switching
+   * (even if the device supports OTT calls), {@code false} is returned.
+   *
+   * @return Returns {@code true} if the device supports voice calls; returns {@code false} otherwise.
+   * @since 7
+   */
+  function hasVoiceCapability(): boolean;
+
+  /**
    * Checks whether a phone number is on the emergency number list.
    *
    * @param phoneNumber Indicates the phone number to check.
