ad9df692e1b566802c0e5e9f1df583ba6fde7de5
youqijing <youqijing@huawei.com>
2022-02-26 10:42:05 +0800

    Update screenshot.d.ts file.
    Signed-off-by: youqijing <youqijing@huawei.com>
    Change-Id: I41937bd8bf1c9613cc1acdbb06406640491e8b8a

--- api/@ohos.screenshot.d.ts
+++ api/@ohos.screenshot.d.ts
@@ -29,6 +29,14 @@ declare namespace screenshot {
      * @permission ohos.permission.CAPTURE_SCREEN
      * @since 7
      */
+    function save(options?: ScreenshotOptions, callback: AsyncCallback<image.PixelMap>): void;
+
+    /**
+     * Takes a screenshot and saves it as a PixelMap object
+     * @param options Screenshot options, which consist of screenRect, imageSize, and rotation. You need to set these parameters
+     * @permission ohos.permission.CAPTURE_SCREEN
+     * @since 7
+     */
     function save(options?: ScreenshotOptions): Promise<image.PixelMap>;
 
     /**
