eac2ebb4aa703ea6daf32f08f0be379b8bb764c8
zhouyongfei <zhouyongfei@huawei.com>
2022-03-28 06:49:30 +0000

    更新文件
    Signed-off-by: zhouyongfei <zhouyongfei@huawei.com>

--- api/@ohos.inputmethod.d.ts
+++ api/@ohos.inputmethod.d.ts
@@ -19,7 +19,7 @@ import {AsyncCallback} from './basic';
  * inputmethod
  *
  * @since 6
- * @syscap SystemCapability.Miscservices.InputMethodFramework
+ * @syscap SystemCapability.MiscServices.InputMethodFramework
  */
 declare namespace inputMethod {
     const MAX_TYPE_NUM: number
