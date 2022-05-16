8436565f82bc2877daeab0c570273faa081c99ca
zhouke <zhouke25@huawe.com>
2022-05-16 15:30:16 +0800

    @ohos.uitest.d.ts增加接口声明. Signed-off-by: <zhouke25@huawei.com>.
    Signed-off-by: zhouke <zhouke25@huawe.com>

--- api/@ohos.uitest.d.ts
+++ api/@ohos.uitest.d.ts
@@ -50,6 +50,18 @@
 }
 
 /**
+ * Represents the rectangle area on the device screen.
+ *
+ * @since 9
+ */
+declare interface Rect {
+    readonly  leftX: number;
+    readonly  topY: number;
+    readonly  rightX: number;
+    readonly  bottomY: number;
+}
+
+/**
  * Describes the attribute requirements for the target UiComponents.
  *
  * @since 8
@@ -108,6 +120,16 @@
    clickable(b?:bool):By;
 
    /**
+    * Specifies the longClickable status of the target UiComponent.
+    * @syscap SystemCapability.Test.UiTest
+    * @param b the clickable status,default to true.
+    * @return Returns this {@link By} object.
+    * @since 9
+    * @test
+    */
+   longClickable(b?: bool): By;
+
+   /**
     * Specifies the scrollable status of the target UiComponent.
     * @syscap SystemCapability.Test.UiTest
     * @param b the scrollable status,default to true.
@@ -146,6 +168,26 @@
     * @test
     */
    selected(b?:bool):By;
+
+   /**
+    * Specifies the checked status of the target UiComponent.
+    * @syscap SystemCapability.Test.UiTest
+    * @param b the checked status,default to false.
+    * @return Returns this {@link By} object.
+    * @since 9
+    * @test
+    */
+   checked(b?: bool): By;
+
+   /**
+    * Specifies the checkable status of the target UiComponent.
+    * @syscap SystemCapability.Test.UiTest
+    * @param b the checkable status,default to false.
+    * @return Returns this {@link By} object.
+    * @since 9
+    * @test
+    */
+   checkable(b?: bool): By;
 
    /**
     * Requires that the target UiComponent which is before another UiComponent that specified by the given {@link By}
@@ -247,6 +289,15 @@
       isClickable():Promise<bool>;
 
       /**
+       * Get the longClickable status of this {@link UiComponent}.
+       * @syscap SystemCapability.Test.UiTest
+       * @returns the longClickable status.
+       * @since 9
+       * @test
+       */
+      isLongClickable(): Promise<bool>;
+
+      /**
        * Get the scrollable status of this {@link UiComponent}.
        * @syscap SystemCapability.Test.UiTest
        * @returns the scrollable status.
@@ -283,6 +334,24 @@
       isSelected():Promise<bool>;
 
       /**
+       * Get the checked status of this {@link UiComponent}.
+       * @syscap SystemCapability.Test.UiTest
+       * @returns the checked status.
+       * @since 9
+       * @test
+       */
+      isChecked(): Promise<bool>;
+
+      /**
+       * Get the checkable status of this {@link UiComponent}.
+       * @syscap SystemCapability.Test.UiTest
+       * @returns the checkable status.
+       * @since 9
+       * @test
+       */
+      isCheckable(): Promise<bool>;
+
+      /**
        * Inject text to this {@link UiComponent},applicable to TextInput.
        * @syscap SystemCapability.Test.UiTest
        * @param text the text to inject.
@@ -290,6 +359,30 @@
        * @test
        */
       inputText(text: string):Promise<void>;
+
+      /**
+       * Clear text of this {@link UiComponent},applicable to TextInput.
+       * @syscap SystemCapability.Test.UiTest
+       * @since 9
+       * @test
+       */
+      clearText(): Promise<void>;
+
+      /**
+       * Scroll on this {@link UiComponent} to the top,applicable to scrollable one.
+       * @syscap SystemCapability.Test.UiTest
+       * @since 9
+       * @test
+       */
+      scrollToTop(): Promise<void>;
+
+      /**
+       * Scroll on this {@link UiComponent} to the bottom,applicable to scrollable one.
+       * @syscap SystemCapability.Test.UiTest
+       * @since 9
+       * @test
+       */
+      scrollToBottom(): Promise<void>;
 
       /**
        * Scroll on this {@link UiComponent}to find matched {@link UiComponent},applicable to scrollable one.
@@ -300,6 +393,24 @@
        * @test
        */
       scrollSearch(by:By):Promise<UiComponent>;
+
+	  /**
+       * Get the bounds rect of this {@link UiComponent}.
+       * @syscap SystemCapability.Test.UiTest
+       * @return the bounds rect object.
+       * @since 9
+       * @test
+       */
+      getBounds(): Promise<Rect>;
+
+      /**
+       * Drag this {@link UiComponent} to the bounds rect of target UiComponent.
+       * @syscap SystemCapability.Test.UiTest
+       * @param target the target {@link UiComponent}.
+       * @since 9
+       * @test
+       */
+      dragTo(target: UiComponent): Promise<void>;
 }
 
 /**
@@ -338,6 +449,17 @@
    * @test
    */
   findComponent(by:By):Promise<UiComponent>;
+
+  /**
+   * Find the first matched {@link UiComponent} on current UI during the time given.
+   * @syscap SystemCapability.Test.UiTest
+   * @param by the attribute requirements of the target {@link UiComponent}.
+   * @param time duration of finding in milliseconds
+   * @returns the first matched {@link UiComponent} or undefined.
+   * @since 9
+   * @test
+   */
+  waitForComponent(by: By, time: number): Promise<UiComponent>;
 
   /**
    * Find all the matched {@link UiComponent}s on current UI.
@@ -417,6 +539,18 @@
    * @test
    */
   swipe(startx:number,starty:number,endx:number,endy:number):Promise<void>;
+
+/**
+   * Drag on the screen between the specified points.
+   * @syscap SystemCapability.Test.UiTest
+   * @param startx the x-coordinate of the starting point.
+   * @param starty the y-coordinate of the starting point.
+   * @param endx the x-coordinate of the ending point.
+   * @param endy the y-coordinate of the ending point.
+   * @since 9
+   * @test
+   */
+  drag(startx: number, starty: number, endx: number, endy: number): Promise<void>;
 
   /**
    * Capture current screen and save as picture which PNG format.
