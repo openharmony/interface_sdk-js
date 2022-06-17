cb972e9c4ec1ca33a3c72c35911ff5eb3639b9ff
lovechinamo <wangdongqi2@huawei.com>
2022-06-17 16:18:17 +0800

    Signed-off-by: lovechinamo <wangdongqi2@huawei.com>
     Changes to be committed:

--- api/@ohos.request.d.ts
+++ api/@ohos.request.d.ts
@@ -691,14 +691,14 @@ declare namespace request {
      * @since 6
      * @permission ohos.permission.INTERNET
      */
-    header?: Object;
+    header: Object;
     /**
      * Request method: POST, PUT. The default POST.
      *
      * @since 6
      * @permission ohos.permission.INTERNET
      */
-    method?: string;
+    method: string;
     /**
      * A list of files to be uploaded. Please use multipart/form-data to submit.
      *
@@ -712,7 +712,7 @@ declare namespace request {
      * @since 6
      * @permission ohos.permission.INTERNET
      */
-    data?: Array<RequestData>;
+    data: Array<RequestData>;
   }
 
   interface UploadTask {
