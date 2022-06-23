d782a2a203eace32b3701fdbebdc588a6778d41c
Yao yuchi <yaoyuchi@huawei.com>
2022-06-23 20:08:30 +0800

    change version to 9, and add system annonation to plugin
    Signed-off-by: Yao yuchi <yaoyuchi@huawei.com>

--- api/@internal/component/ets/plugin_component.d.ts
+++ api/@internal/component/ets/plugin_component.d.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,53 +15,55 @@
 
 /**
  * PluginComponentTemplate
- * @since 8
+ * @since 9
+ * @systemapi Hide this for inner system use.
  */
 interface PluginComponentTemplate {
   /**
    * Defines the plugin source name.
-   * @since 8
+   * @since 9
+   * @systemapi Hide this for inner system use.
    */
   source: string;
   /**
    * Defines the ability name.
-   * @since 8
+   * @since 9
+   * @systemapi Hide this for inner system use.
    */
   ability: string;
 }
 
 /**
  * Provides plugin component.
- * @since 8
+ * @since 9
+ * @systemapi Hide this for inner system use.
  */
 interface PluginComponentInterface {
   /**
    * Called when setting the plugin.
-   * @since 8
+   * @since 9
+   * @systemapi Hide this for inner system use.
    */
   (value: { template: PluginComponentTemplate; data: any }): PluginComponentAttribute;
 }
 
 /**
  * Defines the plugin component attibute functions.
- * @since 8
+ * @since 9
+ * @systemapi Hide this for inner system use.
  */
 declare class PluginComponentAttribute extends CommonMethod<PluginComponentAttribute> {
   /**
-   * Set pluginComponent size,
-   * @since 8
-   */
-  size(value: { width: number; height: number }): PluginComponentAttribute;
-
-  /**
    * pluginComponent onComplete callback,
-   * @since 8
+   * @since 9
+   * @systemapi Hide this for inner system use.
    */
   onComplete(callback: () => void): PluginComponentAttribute;
 
   /**
    * pluginComponent onError callback,
-   * @since 8
+   * @since 9
+   * @systemapi Hide this for inner system use.
    */
   onError(callback: (info: { errcode: number; msg: string }) => void): PluginComponentAttribute;
 }
--- api/@ohos.pluginComponent.d.ts
+++ api/@ohos.pluginComponent.d.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -19,7 +19,8 @@ import Want from './@ohos.application.want';
 /**
  * Plugin component template property.
  * @syscap SystemCapability.ArkUI.ArkUI.Full
- * @since 8
+ * @since 9
+ * @systemapi Hide this for inner system use.
  */
 interface PluginComponentTemplate {
   source: string;
@@ -29,37 +30,95 @@ interface PluginComponentTemplate {
 /**
  * Plugin component manager interface.
  * @syscap SystemCapability.ArkUI.ArkUI.Full
- * @since 8
+ * @since 9
+ * @systemapi Hide this for inner system use.
  */
 declare namespace pluginComponentManager {
   type KVObject = {[key: string]: number | string | boolean | [] | KVObject}
 
   /**
+   * Plugin component event listener type
+   * @since 9
+   * @systemapi Hide this for inner system use.
+   */
+  export enum EventType {
+    /**
+     * Indicates the event type of push.
+     * @since 9
+     */
+    EVENT_TYPE_PUSH = "push",
+
+    /**
+     * Indicates the event type of request.
+     * @since 9
+     */
+    EVENT_TYPE_REQUEST = "request"
+  }
+
+  /**
    * Plugin component push parameters.
-   * @since 8
+   * @since 9
+   * @systemapi Hide this for inner system use.
    */
   interface PushParameters {
+    /**
+     * Indicates the want of the caller.
+     */
+    owner: Want;
+    /**
+     * Indicates the want of the plugin template App.
+     */
     want: Want;
+    /**
+     * Indicates the name of the template.
+     */
     name: string;
+    /**
+     * Represents the data passed to the template.
+     */
     data: KVObject;
+    /**
+     * Represents extended data passed to the template.
+     */
     extraData: KVObject;
+    /**
+     * Represents the path to the template json configuration file.
+     */
     jsonPath?: string;
   }
 
   /**
    * Plugin component request parameters.
-   * @since 8
+   * @since 9
+   * @systemapi Hide this for inner system use.
    */
   interface RequestParameters {
+    /**
+     * Indicates the want of the caller.
+     */
+    owner: Want;
+    /**
+     * Indicates the want of the plugin template App.
+     */
     want: Want;
+    /**
+     * Indicates the name of the template.
+     */
     name: string;
+    /**
+     * Represents the data passed to the template.
+     */
     data: KVObject;
+    /**
+     * Represents the path to the template's json configuration file.
+     */
     jsonPath?: string;
   }
 
   /**
    * Plugin component request callback parameters.
-   * @since 8
+   * @since 9
+   * @systemapi Hide this for inner system use.
    */
   interface RequestCallbackParameters {
     componentTemplate: PluginComponentTemplate;
@@ -69,7 +128,8 @@ declare namespace pluginComponentManager {
 
   /**
    * Plugin component request event result value.
-   * @since 8
+   * @since 9
+   * @systemapi Hide this for inner system use.
    */
   interface RequestEventResult {
     template?: string;
@@ -79,35 +139,69 @@ declare namespace pluginComponentManager {
 
   /**
    * Plugin component push event callback.
-   * @since 8
+   * @since 9
+   * @systemapi Hide this for inner system use.
    */
   type OnPushEventCallback = (source: Want, template: PluginComponentTemplate, data: KVObject,
     extraData: KVObject) => void;
 
   /**
    * Plugin component request event callback.
-   * @since 8
+   * @since 9
+   * @systemapi Hide this for inner system use.
    */
-
   type OnRequestEventCallback = (source: Want, name: string, data: KVObject) => RequestEventResult;
 
   /**
    * Plugin component push method.
-   * @since 8
+   * @since 9
+   * @systemapi Hide this for inner system use.
    */
   function push(param: PushParameters, callback: AsyncCallback<void>): void;
 
   /**
+   * Plugin component push method.
+   * @since 9
+   * @systemapi Hide this for inner system use.
+   */
+  function push(param: PushParameters): Promise<void>;
+
+  /**
    * Plugin component request method.
-   * @since 8
+   * @since 9
+   * @systemapi Hide this for inner system use.
    */
   function request(param: RequestParameters, callback: AsyncCallback<RequestCallbackParameters>): void;
 
   /**
-   * Plugin component event listener.
-   * @since 8
+   * Plugin component request method.
+   * @since 9
+   * @systemapi Hide this for inner system use.
+   */
+  function request(param: RequestParameters): Promise<RequestCallbackParameters>;
+
+  /**
+   * Plugin component on event listener.
+   * @since 9
+   * @systemapi Hide this for inner system use.
    */
   function on(eventType: string, callback: OnPushEventCallback | OnRequestEventCallback): void;
+
+  /**
+   * Plugin component on event listener.
+   * @param owner Indicates the want of the caller.
+   * @since 9
+   * @systemapi Hide this for inner system use.
+   */
+  function on(owner: Want, eventType: EventType, callback: OnPushEventCallback | OnRequestEventCallback): void;
+
+  /**
+   * Plugin component cancel event listener.
+   * @param owner Indicates the want of the caller.
+   * @since 9
+   * @systemapi Hide this for inner system use.
+   */
+  function off(owner: Want, callback: AsyncCallback<void>): void;
 }
 
 export default pluginComponentManager;
