8a6db0d35cfe1c663e05b30eda5e44d077e7d5c8
h00514358 <huiyuehong@huawei.com>
2022-07-26 15:02:02 +0800

    Modify sensor js api
    Signed-off-by: h00514358 <huiyuehong@huawei.com>

--- api/@ohos.vibrator.d.ts
+++ api/@ohos.vibrator.d.ts
@@ -20,6 +20,14 @@ import { AsyncCallback } from './basic';
  * @since 8
  * @syscap SystemCapability.Sensors.MiscDevice
  * @import import vibrator from '@ohos.vibrator'
+ * @permission ohos.permission.VIBRATE
+ */
+/**
+ * This module provides the capability to control motor vibration.
+ *
+ * @since 9
+ * @syscap SystemCapability.Sensors.MiscDevice
+ * @import import vibrator from '@ohos.vibrator'
  */
 declare namespace vibrator {
     /**
