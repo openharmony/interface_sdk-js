2a1f7d0cc7b743003e8991d0c67ba7ff80bb0575
li_juntao <lijuntao9@huawei.com>
2022-07-27 10:20:44 +0800

    修改remoteQuery接口描述
    Signed-off-by: li_juntao <lijuntao9@huawei.com>

--- api/@ohos.data.rdb.d.ts
+++ api/@ohos.data.rdb.d.ts
@@ -195,14 +195,14 @@ declare namespace rdb {
          *
          * @since 9
          * @syscap SystemCapability.DistributedDataManager.RelationalStore.Core
-		 * @param devices Indicates specified remote devices.
+		 * @param device Indicates specified remote device.
 		 * @param table Indicates the target table.
          * @param rdbPredicates Indicates the specified remote query condition by the instance object of RdbPredicates or DataSharePredicates.
          * @param columns Indicates the columns to remote query. If the value is null, the remote query applies to all columns.
          * @return Returns a ResultSet object if the operation is successful;
          */
-        remoteQuery(devices: Array<string>, table: string, rdbPredicates: RdbPredicates | dataSharePredicates.DataSharePredicates, columns?: Array<string>, callback: AsyncCallback<ResultSet>): void;
-        remoteQuery(devices: Array<string>, table: string, rdbPredicates: RdbPredicates | dataSharePredicates.DataSharePredicates, columns?: Array<string>): Promise<ResultSet>;
+        remoteQuery(device: string, table: string, rdbPredicates: RdbPredicates | dataSharePredicates.DataSharePredicates, columns?: Array<string>, callback: AsyncCallback<ResultSet>): void;
+        remoteQuery(device: string, table: string, rdbPredicates: RdbPredicates | dataSharePredicates.DataSharePredicates, columns?: Array<string>): Promise<ResultSet>;
 
 
         /**
