598d494a914498ed0a842996bc8e2ac0ec2cc504
li_juntao <lijuntao9@huawei.com>
2022-08-01 14:06:12 +0800

    修改remoteQuery接口描述 Signed-off-by: li_juntao <lijuntao9@huawei.com>

--- api/@ohos.data.rdb.d.ts
+++ api/@ohos.data.rdb.d.ts
@@ -198,11 +198,11 @@ declare namespace rdb {
          * @param device Indicates specified remote device.
          * @param table Indicates the target table.
          * @param predicates Indicates the specified remote query condition by the instance object of RdbPredicates.
-         * @param columns Indicates the columns to remote query. If the value is undefined or null, the remote query applies to all columns.
+         * @param columns Indicates the columns to remote query. If the value is null, the remote query applies to all columns.
          * @return Returns a ResultSet object if the operation is successful;
          */
         remoteQuery(device: string, table: string, predicates: RdbPredicates, columns: Array<string>, callback: AsyncCallback<ResultSet>): void;
-        remoteQuery(device: string, table: string, predicates: RdbPredicates, columns?: Array<string>): Promise<ResultSet>;
+        remoteQuery(device: string, table: string, predicates: RdbPredicates, columns: Array<string>): Promise<ResultSet>;
 
 
         /**
