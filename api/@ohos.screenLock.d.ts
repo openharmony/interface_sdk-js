0cd7c606661e4c1370287c8aedb9907041a58598
qixiao-meng <qimeng11@huawei.com>
2022-09-21 09:05:11 +0800

    Signed-off-by: qixiao-meng <qimeng11@huawei.com> fix: API modify

--- api/@ohos.screenLock.d.ts
+++ api/@ohos.screenLock.d.ts
@@ -58,42 +58,21 @@ declare namespace screenLock {
   function lockScreen(callback: AsyncCallback<boolean>): void;
   function lockScreen():Promise<boolean>;
 
+  type EventType = 'beginWakeUp' | 'endWakeUp' | 'beginScreenOn' | 'endScreenOn' | 'beginScreenOff' | 'endScreenOff' | 'unlockScreen' | 'lockScreen' | 'beginExitAnimation' | 'beginSleep' | 'endSleep' | 'changeUser' | 'screenlockEnabled'
+
+  interface SystemEvent {
+    eventType: EventType, // event type of the system event
+    params: string // event params of the system event
+  }
+  
   /**
-   * Receives {beginWakeUp|endWakeUp|beginScreenOn|endScreenOn|beginScreenOff|endScreenOff|unlockScreen|beginExitAnimation} called. 
-   * This callback is invoked when {beginWakeUp|endWakeUp|beginScreenOn|endScreenOn|beginScreenOff|endScreenOff|unlockScreen|beginExitAnimation}
-   * is called by runtime
-   *
+   * Register system event related to syscreen lock 
+   * @params callback The callback function for indcating the system event related screen lock
+   * @throws {BusinessError} Parameter error
    * @systemapi Hide this for inner system use.
    * @since 9
    */
-  function on(type: 'beginWakeUp' | 'endWakeUp' | 'beginScreenOn' | 'endScreenOn' | 'beginScreenOff' | 'endScreenOff' | 'unlockScreen' | 'beginExitAnimation', callback: Callback<void>): void;
-
-  /**
-    * Receives {beginSleep | endSleep | changeUser} called. This callback is invoked when {beginSleep | endSleep | changeUser} is called by runtime 
-    *
-    * @systemapi Hide this for inner system use.
-    * @since 9
-    */
-  function on(type: 'beginSleep' | 'endSleep' | 'changeUser', callback: Callback<number>): void;
-
-  /**
-   * Receives screenlockEnabled change. This callback is invoked when screenlockEnabled is called by runtime 
-   *
-   * @systemapi Hide this for inner system use.
-   * @since 9
-   */
-  function on(type: 'screenlockEnabled', callback: Callback<boolean>): void;
-
-  /**
-   * Remove the receives of {beginWakeUp | endWakeUp | beginScreenOn | endScreenOn | beginScreenOff | endScreenOff | unlockScreen 
-   * | beginExitAnimation | screenlockEnabled | beginSleep | endSleep | changeUser}.
-   *
-   * @systemapi Hide this for inner system use.
-   * @since 9
-   */
-   function off(type: 'beginWakeUp' | 'endWakeUp' | 'beginScreenOn' | 'endScreenOn' | 'beginScreenOff' | 'endScreenOff' 
-      | 'unlockScreen' | 'beginExitAnimation' | 'screenlockEnabled' | 'beginSleep' | 'endSleep' | 'changeUser', callback: Callback<void>): void;
-
+  function onSystemEvent(callback: Callback<SystemEvent>): boolean;
 
   /**
    * screenlockAPP send event to screenlockSA
