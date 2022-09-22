7fb738661630b585924fd80c06d2b9ee484f2fb6
@shi-xiaoxiao-iris <shixiaoxiao2@huawei.com>
2022-09-22 20:41:23 +0800

    [需求]api异常处理
    Signed-off-by: @shi-xiaoxiao-iris <shixiaoxiao2@huawei.com>

--- api/@ohos.distributedHardware.deviceManager.d.ts
+++ api/@ohos.distributedHardware.deviceManager.d.ts
@@ -290,40 +290,6 @@ declare namespace deviceManager {
   }
 
   /**
-   * @name DMErrorCode
-   * @since 9
-   * @syscap SystemCapability.DistributedHardware.DeviceManager
-   * @import NA
-   * @permission NA
-   */
-  export enum DMErrorCode{
-    /**
-     * Failed to execute the function.
-     */
-    DM_ERR_FAILED = 11600101,
-
-    /**
-     * Failed to obtain the service.
-     */
-    DM_ERR_OBTAIN_SERVICE = 11600102,
-
-    /**
-     * Authentication invalid.
-     */
-    DM_ERR_AUTHENTICATION_INVALID = 11600103,
-
-    /**
-     * Discovery invalid.
-     */
-    DM_ERR_DISCOVERY_INVALID = 11600104,
-
-    /**
-     * Publish invalid.
-     */
-    DM_ERR_PUBLISH_INVALID = 11600105,
-  }
-
-  /**
    * Device Authentication param
    *
    * @systemapi this method can be used only by system applications
