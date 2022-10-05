739e069412ab89ac8de01ebe02f2e7b4eec46530
caiminggang <caiminggang1@huawei.com>
2022-10-05 16:51:42 +0800

    Enterprise Device Manager 错误码整改
    Signed-off-by: caiminggang <caiminggang1@huawei.com>

--- api/@ohos.enterpriseDeviceManager.d.ts
+++ api/@ohos.enterpriseDeviceManager.d.ts
@@ -54,98 +54,267 @@ declare namespace enterpriseDeviceManager {
 
   /**
    * Enables the given ability as a administrator of the device.
-   * 
    * Only apps with the ohos.permission.MANAGE_ENTERPRISE_DEVICE_ADMIN permission or the shell uid can call this method.
-   *
-   * @since 9
-   * @syscap SystemCapability.Customization.EnterpriseDeviceManager
-   * @param admin Indicates the administrator ability information.
-   * @param enterpriseInfo Indicates the enterprise information of the calling application.
-   * @param type Indicates the type of administrator to set.
-   * @param userId Indicates the user ID or do not pass user ID.
-   * @return {@code true} if enables administrator success.
    * @permission ohos.permission.MANAGE_ENTERPRISE_DEVICE_ADMIN
+   * @param { Want } admin - admin indicates the administrator ability information.
+   * @param { EnterpriseInfo } enterpriseInfo - enterpriseInfo indicates the enterprise information of the calling application.
+   * @param { AdminType } type - type indicates the type of administrator to set.
+   * @param { AsyncCallback<void> } callback - the callback of enableAdmin.
+   * @throws { BusinessError } 9200003 - the administrator ability component is invalid.
+   * @throws { BusinessError } 9200004 - failed to enable the adminstrator application of the device.
+   * @throws { BusinessError } 9200007 - the system ability work abnormally.
+   * @throws { BusinessError } 201 - the application does not have permission to call this function.
+   * @throws { BusinessError } 401 - invalid input parameter.
+   * @syscap SystemCapability.Customization.EnterpriseDeviceManager
+   * @systemapi
+   * @stagemodelonly
+   * @since 9
    */
-  function enableAdmin(admin: Want, enterpriseInfo: EnterpriseInfo, type: AdminType, callback: AsyncCallback<boolean>): void;
-  function enableAdmin(admin: Want, enterpriseInfo: EnterpriseInfo, type: AdminType, userId: number, callback: AsyncCallback<boolean>): void;
-  function enableAdmin(admin: Want, enterpriseInfo: EnterpriseInfo, type: AdminType, userId?: number): Promise<boolean>;
+  function enableAdmin(admin: Want, enterpriseInfo: EnterpriseInfo, type: AdminType, callback: AsyncCallback<void>): void;
 
   /**
-   * Disables a current normal administrator ability. 
-   * 
+   * Enables the given ability as a administrator of the device.
    * Only apps with the ohos.permission.MANAGE_ENTERPRISE_DEVICE_ADMIN permission or the shell uid can call this method.
-   *
-   * @since 9
-   * @syscap SystemCapability.Customization.EnterpriseDeviceManager
-   * @param admin Indicates the administrator ability information.
-   * @param userId Indicates the user ID or do not pass user ID.
-   * @return {@code true} if disables administrator success.
    * @permission ohos.permission.MANAGE_ENTERPRISE_DEVICE_ADMIN
+   * @param { Want } admin - admin indicates the administrator ability information.
+   * @param { EnterpriseInfo } enterpriseInfo - enterpriseInfo indicates the enterprise information of the calling application.
+   * @param { AdminType } type - type indicates the type of administrator to set.
+   * @param { number } userId - userId indicates the user ID.
+   * @param { AsyncCallback<void> } callback - the callback of enableAdmin.
+   * @throws { BusinessError } 9200003 - the administrator ability component is invalid.
+   * @throws { BusinessError } 9200004 - failed to enable the adminstrator application of the device.
+   * @throws { BusinessError } 9200007 - the system ability work abnormally.
+   * @throws { BusinessError } 201 - the application does not have permission to call this function.
+   * @throws { BusinessError } 401 - invalid input parameter.
+   * @syscap SystemCapability.Customization.EnterpriseDeviceManager
+   * @systemapi
+   * @stagemodelonly
+   * @since 9
    */
-  function disableAdmin(admin: Want, callback: AsyncCallback<boolean>): void;
-  function disableAdmin(admin: Want, userId: number, callback: AsyncCallback<boolean>): void;
-  function disableAdmin(admin: Want, userId?: number): Promise<boolean>;
+  function enableAdmin(admin: Want, enterpriseInfo: EnterpriseInfo, type: AdminType, userId: number, callback: AsyncCallback<void>): void;
 
   /**
-   * Disables a current super administrator ability. 
-   * 
+   * Enables the given ability as a administrator of the device.
+   * Only apps with the ohos.permission.MANAGE_ENTERPRISE_DEVICE_ADMIN permission or the shell uid can call this method.
+   * @permission ohos.permission.MANAGE_ENTERPRISE_DEVICE_ADMIN
+   * @param { Want } admin - admin indicates the administrator ability information.
+   * @param { EnterpriseInfo } enterpriseInfo - enterpriseInfo indicates the enterprise information of the calling application.
+   * @param { AdminType } type - type indicates the type of administrator to set.
+   * @param { number } [userId] - userId indicates the user ID or do not pass user ID.
+   * @returns { Promise<void> } the promise returned by the enableAdmin.
+   * @throws { BusinessError } 9200003 - the administrator ability component is invalid.
+   * @throws { BusinessError } 9200004 - failed to enable the adminstrator application of the device.
+   * @throws { BusinessError } 9200007 - the system ability work abnormally.
+   * @throws { BusinessError } 201 - the application does not have permission to call this function.
+   * @throws { BusinessError } 401 - invalid input parameter.
+   * @syscap SystemCapability.Customization.EnterpriseDeviceManager
+   * @systemapi
+   * @stagemodelonly
+   * @since 9
+   */
+  function enableAdmin(admin: Want, enterpriseInfo: EnterpriseInfo, type: AdminType, userId?: number): Promise<void>;
+
+  /**
+   * Disables a current normal administrator ability.
+   * Only apps with the ohos.permission.MANAGE_ENTERPRISE_DEVICE_ADMIN permission or the shell uid can call this method.
+   * @permission ohos.permission.MANAGE_ENTERPRISE_DEVICE_ADMIN
+   * @param { Want } admin - admin indicates the administrator ability information.
+   * @param { AsyncCallback<void> } callback - the callback of disableAdmin.
+   * @throws { BusinessError } 9200005 - failed to disable the administrator application of the device.
+   * @throws { BusinessError } 201 - the application does not have permission to call this function.
+   * @throws { BusinessError } 401 - invalid input parameter.
+   * @syscap SystemCapability.Customization.EnterpriseDeviceManager
+   * @systemapi
+   * @stagemodelonly
+   * @since 9
+   */
+  function disableAdmin(admin: Want, callback: AsyncCallback<void>): void;
+
+  /**
+   * Disables a current normal administrator ability.
+   * Only apps with the ohos.permission.MANAGE_ENTERPRISE_DEVICE_ADMIN permission or the shell uid can call this method.
+   * @permission ohos.permission.MANAGE_ENTERPRISE_DEVICE_ADMIN
+   * @param { Want } admin - admin indicates the administrator ability information.
+   * @param { number } userId - userId indicates the user ID.
+   * @param { AsyncCallback<void> } callback - the callback of disableAdmin.
+   * @throws { BusinessError } 9200005 - failed to disable the administrator application of the device.
+   * @throws { BusinessError } 201 - the application does not have permission to call this function.
+   * @throws { BusinessError } 401 - invalid input parameter.
+   * @syscap SystemCapability.Customization.EnterpriseDeviceManager
+   * @systemapi
+   * @stagemodelonly
+   * @since 9
+   */
+  function disableAdmin(admin: Want, userId: number, callback: AsyncCallback<void>): void;
+
+  /**
+   * Disables a current normal administrator ability.
+   * Only apps with the ohos.permission.MANAGE_ENTERPRISE_DEVICE_ADMIN permission or the shell uid can call this method.
+   * @permission ohos.permission.MANAGE_ENTERPRISE_DEVICE_ADMIN
+   * @param { Want } admin - admin indicates the administrator ability information.
+   * @param { number } [userId] - userId indicates the user ID or do not pass user ID.
+   * @returns { Promise<void> } the promise returned by the disableAdmin.
+   * @throws { BusinessError } 9200005 - failed to disable the administrator application of the device.
+   * @throws { BusinessError } 201 - the application does not have permission to call this function.
+   * @throws { BusinessError } 401 - invalid input parameter.
+   * @syscap SystemCapability.Customization.EnterpriseDeviceManager
+   * @systemapi
+   * @stagemodelonly
+   * @since 9
+   */
+  function disableAdmin(admin: Want, userId?: number): Promise<void>;
+
+  /**
+   * Disables a current super administrator ability.
    * Only the administrator app or apps with the shell uid can call this method.
-   *
-   * @since 9
+   * @permission ohos.permission.MANAGE_ENTERPRISE_DEVICE_ADMIN
+   * @param { string } bundleName - bundleName indicates the administrator bundle information.
+   * @param { AsyncCallback<void> } callback - the callback of disableSuperAdmin.
+   * @throws { BusinessError } 9200005 - failed to disable the administrator application of the device.
+   * @throws { BusinessError } 201 - the application does not have permission to call this function.
+   * @throws { BusinessError } 401 - invalid input parameter.
    * @syscap SystemCapability.Customization.EnterpriseDeviceManager
-   * @param bundleName Indicates the administrator bundle information.
-   * @return {@code true} if disables super administrator success.
+   * @systemapi
+   * @stagemodelonly
+   * @since 9
    */
-  function disableSuperAdmin(bundleName: String, callback: AsyncCallback<boolean>): void;
-  function disableSuperAdmin(bundleName: String): Promise<boolean>;
+  function disableSuperAdmin(bundleName: String, callback: AsyncCallback<void>): void;
+
+  /**
+   * Disables a current super administrator ability.
+   * Only the administrator app or apps with the shell uid can call this method.
+   * @permission ohos.permission.MANAGE_ENTERPRISE_DEVICE_ADMIN
+   * @param { string } bundleName - bundleName indicates the administrator bundle information.
+   * @returns { Promise<void> } the promise returned by the disableSuperAdmin.
+   * @throws { BusinessError } 9200005 - failed to disable the administrator application of the device.
+   * @throws { BusinessError } 201 - the application does not have permission to call this function.
+   * @throws { BusinessError } 401 - invalid input parameter.
+   * @syscap SystemCapability.Customization.EnterpriseDeviceManager
+   * @systemapi
+   * @stagemodelonly
+   * @since 9
+   */
+  function disableSuperAdmin(bundleName: String): Promise<void>;
 
   /**
    * Get whether the ability is enabled as device administrator.
-   *
-   * @since 9
+   * @param { Want } admin - admin indicates the administrator ability information.
+   * @param { AsyncCallback<boolean> } callback - callback contained true if the administrator is enabled.
+   * @throws { BusinessError } 401 - invalid input parameter.
    * @syscap SystemCapability.Customization.EnterpriseDeviceManager
-   * @param admin Indicates the administrator ability information.
-   * @param userId Indicates the user ID or do not pass user ID.
-   * @return {@code true} if the administrator is enabled.
+   * @systemapi
+   * @stagemodelonly
+   * @since 9
    */
   function isAdminEnabled(admin: Want, callback: AsyncCallback<boolean>): void;
+
+  /**
+   * Get whether the ability is enabled as device administrator.
+   * @param { Want } admin - admin indicates the administrator ability information.
+   * @param { number } userId - userId indicates the user ID.
+   * @param { AsyncCallback<boolean> } callback - callback contained true if the administrator is enabled.
+   * @throws { BusinessError } 401 - invalid input parameter.
+   * @syscap SystemCapability.Customization.EnterpriseDeviceManager
+   * @systemapi
+   * @stagemodelonly
+   * @since 9
+   */
   function isAdminEnabled(admin: Want, userId: number, callback: AsyncCallback<boolean>): void;
+
+  /**
+   * Get whether the ability is enabled as device administrator.
+   * @param { Want } admin - admin indicates the administrator ability information.
+   * @param { number } [userId] - userId indicates the user ID or do not pass user ID.
+   * @returns { Promise<boolean> } promise contained true if the administrator is enabled.
+   * @throws { BusinessError } 401 - invalid input parameter.
+   * @syscap SystemCapability.Customization.EnterpriseDeviceManager
+   * @systemapi
+   * @stagemodelonly
+   * @since 9
+   */
   function isAdminEnabled(admin: Want, userId?: number): Promise<boolean>;
 
   /**
    * Get information of the administrator's enterprise.
-   *
-   * @since 9
+   * @param { Want } admin - admin indicates the administrator ability information.
+   * @param { AsyncCallback<EnterpriseInfo> } callback - callback contained the enterprise info of administrator.
+   * @throws { BusinessError } 9200001 - the applicayion is not an administrator of the device.
+   * @throws { BusinessError } 401 - invalid input parameter.
    * @syscap SystemCapability.Customization.EnterpriseDeviceManager
-   * @param admin Indicates the administrator ability information.
-   * @return Returns the enterprise information of the administrator.
+   * @systemapi
+   * @stagemodelonly
+   * @since 9
    */
   function getEnterpriseInfo(admin: Want, callback: AsyncCallback<EnterpriseInfo>): void;
+
+  /**
+   * Get information of the administrator's enterprise.
+   * @param { Want } admin - admin indicates the administrator ability information.
+   * @returns { Promise<EnterpriseInfo> } promise contained the enterprise info of administrator.
+   * @throws { BusinessError } 9200001 - the applicayion is not an administrator of the device.
+   * @throws { BusinessError } 401 - invalid input parameter.
+   * @syscap SystemCapability.Customization.EnterpriseDeviceManager
+   * @systemapi
+   * @stagemodelonly
+   * @since 9
+   */
   function getEnterpriseInfo(admin: Want): Promise<EnterpriseInfo>;
 
   /**
    * Set the information of the administrator's enterprise.
-   * 
    * Only the administrator app can call this method.
-   *
-   * @since 9
+   * @param { Want } admin - admin indicates the administrator ability information.
+   * @param { EnterpriseInfo } enterpriseInfo - enterpriseInfo indicates the enterprise information of the calling application.
+   * @param { AsyncCallback<void> } callback - the callback of setEnterpriseInfo.
+   * @throws { BusinessError } 9200001 - the applicayion is not an administrator of the device.
+   * @throws { BusinessError } 201 - the application does not have permission to call this function.
+   * @throws { BusinessError } 401 - invalid input parameter.
    * @syscap SystemCapability.Customization.EnterpriseDeviceManager
-   * @param admin Indicates the administrator ability information.
-   * @param enterpriseInfo Indicates the enterprise information of the calling application.
-   * @return {@code true} if sets enterprise information success.
+   * @systemapi
+   * @stagemodelonly
+   * @since 9
    */
-  function setEnterpriseInfo(admin: Want, enterpriseInfo: EnterpriseInfo, callback: AsyncCallback<boolean>): void;
-  function setEnterpriseInfo(admin: Want, enterpriseInfo: EnterpriseInfo): Promise<boolean>;
+  function setEnterpriseInfo(admin: Want, enterpriseInfo: EnterpriseInfo, callback: AsyncCallback<void>): void;
+
+  /**
+   * Set the information of the administrator's enterprise.
+   * Only the administrator app can call this method.
+   * @permission ohos.permission.SET_ENTERPRISE_INFO
+   * @param { Want } admin - admin indicates the administrator ability information.
+   * @param { EnterpriseInfo } enterpriseInfo - enterpriseInfo indicates the enterprise information of the calling application.
+   * @returns { Promise<void> } the promise returned by the setEnterpriseInfo.
+   * @throws { BusinessError } 9200001 - the applicayion is not an administrator of the device.
+   * @throws { BusinessError } 201 - the application does not have permission to call this function.
+   * @throws { BusinessError } 401 - invalid input parameter.
+   * @syscap SystemCapability.Customization.EnterpriseDeviceManager
+   * @systemapi
+   * @stagemodelonly
+   * @since 9
+   */
+  function setEnterpriseInfo(admin: Want, enterpriseInfo: EnterpriseInfo): Promise<void>;
 
   /**
    * Get whether the ability is enabled as super device administrator.
-   *
-   * @since 9
+   * @param { string } bundleName - bundleName indicates the administrator bundle information.
+   * @param { AsyncCallback<boolean> } callback - callback contained true if the administrator is super administrator.
+   * @throws { BusinessError } 401 - invalid input parameter.
    * @syscap SystemCapability.Customization.EnterpriseDeviceManager
-   * @param bundleName Indicates the administrator bundle information.
-   * @return {@code true} if the administrator is enabled as super device administrator.
+   * @systemapi
+   * @stagemodelonly
+   * @since 9
    */
   function isSuperAdmin(bundleName: String, callback: AsyncCallback<boolean>): void;
+
+  /**
+   * Get whether the ability is enabled as super device administrator.
+   * @param { string } bundleName - bundleName indicates the administrator bundle information.
+   * @returns { Promise<boolean> } promise contained true if the administrator is super administrator.
+   * @throws { BusinessError } 401 - invalid input parameter.
+   * @syscap SystemCapability.Customization.EnterpriseDeviceManager
+   * @systemapi
+   * @stagemodelonly
+   * @since 9
+   */
   function isSuperAdmin(bundleName: String): Promise<boolean>;
 
   /**
