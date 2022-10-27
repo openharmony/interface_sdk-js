88c43aa6b688dfc05e1bf2377337d9c9a1742ef1
panqiangbiao <panqiangbiao@huawei.com>
2022-10-27 14:47:05 +0800

    fix context
    Signed-off-by: panqiangbiao <panqiangbiao@huawei.com>

--- api/@ohos.multimedia.mediaLibrary.d.ts
+++ api/@ohos.multimedia.mediaLibrary.d.ts
@@ -14,7 +14,7 @@
  */
 
 import { AsyncCallback, Callback } from './basic';
-import Context from './application/Context';
+import { Context } from './app/context';
 import image from './@ohos.multimedia.image';
 
 /**
