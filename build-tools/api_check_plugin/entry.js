36ac9508303fee5168b7ae219635257ff4e999a1
wangqing <wangqing136@huawei.com>
2022-10-29 18:47:49 +0800

    api_check联调验证
    Signed-off-by: wangqing <wangqing136@huawei.com>

--- build-tools/api_check_plugin/entry.js
+++ build-tools/api_check_plugin/entry.js
@@ -19,6 +19,7 @@ const { writeResultFile } = require('./src/utils');
 
 function checkEntry(url) {
   let result = "";
+  result += 'line:22,first:::::::::'
   fs.access(url, fs.constants.R_OK | fs.constants.W_OK, (err) => {
     const checkResult1 = err ? 'no access!' : 'can read/write';
     result += `checkResult1 = ${checkResult1} ||| ${url} ||| \n`;
@@ -29,14 +30,16 @@ function checkEntry(url) {
     const execSync = require("child_process").execSync;
     execSync("cd interface/sdk-js/build-tools/api_check_plugin && npm install");
     const path2 = path.resolve(sourceDirname, '../../../../', "ci_tool/ci_build/readme_file.txt");
+    result += 'line:33,second:::::::::'
     fs.access(path2, fs.constants.R_OK | fs.constants.W_OK, (err) => {
       const checkResult2 = err ? 'no access!' : 'can read/write';
       result += `checkResult2 = ${checkResult2} ||| ${path2} ||| \n`;
     });
     const path3 = path.resolve(__dirname, '../../../../', "ci_tool/ci_build/readme_file.txt");
+    result += 'line:40,third::::::::::'
     fs.access(path3, fs.constants.R_OK | fs.constants.W_OK, (err) => {
       const checkResult3 = err ? 'no access!' : 'can read/write';
-      result += `checkResult2 = ${checkResult3} ||| ${path3} ||| \n`;
+      result += `checkResult3 = ${checkResult3} ||| ${path3} ||| \n`;
     });
     const { scanEntry } = require(path.resolve(__dirname, "./src/api_check_plugin"));
     result += scanEntry(url);
