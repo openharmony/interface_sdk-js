4cf2bebf56c8b8255503bd44bed1e93aeefa81f3
zhuwenchao <zhuwenchao@huawei.com>
2022-11-01 21:54:53 +0800

    update api of net
    Signed-off-by: zhuwenchao <zhuwenchao@huawei.com>

--- api/@ohos.net.connection.d.ts
+++ api/@ohos.net.connection.d.ts
@@ -56,6 +56,7 @@ declare namespace connection {
    *      returns {@code null} if the default network is not activated.
    * @permission ohos.permission.GET_NETWORK_INFO
    * @since 9
+   * @throws {BusinessError} 201 - Permission denied.
    */
   function getDefaultNetSync(): NetHandle;
 
@@ -87,6 +88,18 @@ declare namespace connection {
    */
   function getNetCapabilities(netHandle: NetHandle, callback: AsyncCallback<NetCapabilities>): void;
   function getNetCapabilities(netHandle: NetHandle): Promise<NetCapabilities>;
+
+  /**
+   * Checks whether data traffic usage on the current network is metered.
+   *
+   * @param callback Returns {@code true} if data traffic usage on the current network is metered;
+   *      returns {@code false} otherwise.
+   * @permission ohos.permission.GET_NETWORK_INFO
+   * @since 9
+   * @throws {BusinessError} 201 - Permission denied.
+   */
+  function isDefaultNetMetered(callback: AsyncCallback<boolean>): void;
+  function isDefaultNetMetered(): Promise<boolean>;
 
   /**
    * Checks whether the default data network is activated.
