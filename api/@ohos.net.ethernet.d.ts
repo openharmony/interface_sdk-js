6d097461bbadc6fe8396947715fd679f1746eb1d
liyufan123 <liyufan5@huawei.com>
2022-11-26 15:31:28 +0800

    fix API description
    Signed-off-by: liyufan123 <liyufan5@huawei.com>

--- api/@ohos.net.ethernet.d.ts
+++ api/@ohos.net.ethernet.d.ts
@@ -19,7 +19,7 @@ import {AsyncCallback, Callback} from "./basic";
  * Provides interfaces to manage ethernet.
  *
  * @since 9
- * @syscap SystemCapability.Communication.NetManager.Ethernet
+ * @syscap SystemCapability.Communication.NetManager.Extension
  */
 declare namespace ethernet {
   /**
