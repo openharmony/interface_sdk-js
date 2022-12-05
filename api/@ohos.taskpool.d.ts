d09b9d6b9ef039675485a03dfb59005de4f055b2
wengchangcheng <wengchangcheng@huawei.com>
2022-12-05 20:10:30 +0800

    Descriptor: Add d.ts for taskpool
    Details: Add d.ts for taskpool
    
    Issue: https://gitee.com/openharmony/interface_sdk-js/issues/I64QIR
    
    Signed-off-by: wengchangcheng <wengchangcheng@huawei.com>
    Change-Id: I26929fec6a2bd0f1e246458f73d6162c874f04e4

---
+++ api/@ohos.taskpool.d.ts
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * JS cross-thread task executor
+ * experimental
+ * @since 9
+ * @syscap SystemCapability.Utils.Lang
+ */
+declare namespace taskpool {
+  /**
+   * The Priority define the task priority.
+   * experimental
+   * @since 9
+   * @syscap SystemCapability.Utils.Lang
+   */
+  enum Priority {
+    HIGH,
+    MEDIUM,
+    LOW
+  }
+
+  /**
+   * The Task class provide interface to create task.
+   * experimental
+   * @since 9
+   * @syscap SystemCapability.Utils.Lang
+   */
+  class Task {
+    /**
+     * Creates a Task instance.
+     * @param func Concurrent function to execute in taskpool
+     * @param args The concurrent function arguments
+     * @throws {BusinessError} 401 - if the input parameters are invalid.
+     * @throws {BusinessError} 10200014 - if the function is not mark as concurrent.
+     * experimental
+     * @since 9
+     * @syscap SystemCapability.Utils.Lang
+     */
+    constructor(func: Function, ...args: unknown[]);
+
+    /**
+     * concurrent function to execute in taskpool.
+     */
+    function: Function;
+
+    /**
+     * function arguments.
+     */
+    arguments?: unknown[];
+  }
+
+  /**
+   * Execute a concurrent function
+   * @param func concurrent function want to execute
+   * @param args function arguments
+   * @throws {BusinessError} 401 - if the input parameters are invalid.
+   * @throws {BusinessError} 10200003 - Worker initialization failure.
+   * @throws {BusinessError} 10200006 - Serializing an uncaught exception failed.
+   * @throws {BusinessError} 10200014 - if the function is not mark as concurrent.
+   * experimental
+   * @since 9
+   * @syscap SystemCapability.Utils.Lang
+   */
+  function execute(func: Function, ...args: unknown[]): Promise<unknown>;
+
+  /**
+   * Execute a concurrent task.
+   * @param task Task want to execute
+   * @param priority Task priority, MEDIUM is default
+   * @throws {BusinessError} 401 - if the input parameters are invalid.
+   * @throws {BusinessError} 10200003 - Worker initialization failure.
+   * @throws {BusinessError} 10200006 - Serializing an uncaught exception failed.
+   * @throws {BusinessError} 10200014 - if the function in task is not mark as concurrent.
+   * experimental
+   * @since 9
+   * @syscap SystemCapability.Utils.Lang
+   */
+  function execute(task: Task, priority?: Priority): Promise<unknown>;
+
+  /**
+   * Cancel a concurrent task
+   * @param task Task want to cancel
+   * @throws {BusinessError} 401 - if the input parameters are invalid.
+   * @throws {BusinessError} 10200015 - if the task is not exist.
+   * @throws {BusinessError} 10200016 - if the task is running.
+   * experimental
+   * @since 9
+   * @syscap SystemCapability.Utils.Lang
+   */
+  function cancel(task: Task): void;
+}
+
+export default taskpool
