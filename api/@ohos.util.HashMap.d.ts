dc88bfed1389fb37058e6be1e055851bf47fbb5b
liu-ganlin <liuganlin@huawei.com>
2022-12-14 10:09:26 +0800

    merge trunk to branch
    Signed-off-by: liu-ganlin <liuganlin@huawei.com>

--- api/@ohos.util.HashMap.d.ts
+++ api/@ohos.util.HashMap.d.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,9 +12,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 declare class HashMap<K, V> {
   /**
    * A constructor used to create a HashMap object.
+   * @throws { BusinessError } 10200012 - The HashMap's constructor cannot be directly invoked.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -28,6 +30,7 @@ declare class HashMap<K, V> {
   /**
    * Returns whether the Map object contains elements
    * @returns the boolean type
+   * @throws { BusinessError } 10200011 - The isEmpty method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -36,6 +39,7 @@ declare class HashMap<K, V> {
    * Returns whether a key is contained in this map
    * @param key need to determine whether to include the key
    * @returns the boolean type
+   * @throws { BusinessError } 10200011 - The hasKey method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -44,6 +48,7 @@ declare class HashMap<K, V> {
    * Returns whether a value is contained in this map
    * @param value need to determine whether to include the value
    * @returns the boolean type
+   * @throws { BusinessError } 10200011 - The hasValue method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -52,6 +57,7 @@ declare class HashMap<K, V> {
    * Returns a specified element in a Map object, or null if there is no corresponding element
    * @param key the index in HashMap
    * @returns value or null
+   * @throws { BusinessError } 10200011 - The get method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -59,6 +65,8 @@ declare class HashMap<K, V> {
   /**
    * Adds all element groups in one map to another map
    * @param map the Map object to add members
+   * @throws { BusinessError } 10200011 - The setAll method cannot be bound.
+   * @throws { BusinessError } 401 - The type of parameters are invalid.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -68,6 +76,8 @@ declare class HashMap<K, V> {
    * @param key Added or updated targets
    * @param value Added or updated value
    * @returns the map object after set
+   * @throws { BusinessError } 10200011 - The set method cannot be bound.
+   * @throws { BusinessError } 401 - The type of parameters are invalid.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -76,24 +86,28 @@ declare class HashMap<K, V> {
    * Remove a specified element from a Map object
    * @param key  Target to be deleted
    * @returns Target mapped value
+   * @throws { BusinessError } 10200011 - The remove method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
   remove(key: K): V;
   /**
    * Clear all element groups in the map
+   * @throws { BusinessError } 10200011 - The clear method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
   clear(): void;
   /**
    * Returns a new Iterator object that contains the keys contained in this map
+   * @throws { BusinessError } 10200011 - The keys method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
   keys(): IterableIterator<K>;
   /**
    * Returns a new Iterator object that contains the values contained in this map
+   * @throws { BusinessError } 10200011 - The values method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -103,6 +117,7 @@ declare class HashMap<K, V> {
    * @param key Updated targets
    * @param newValue Updated the target mapped value
    * @returns the boolean type(Is there a target pointed to by the key)
+   * @throws { BusinessError } 10200011 - The replace method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -110,6 +125,8 @@ declare class HashMap<K, V> {
   /**
    * Executes the given callback function once for each real key in the map.
    * It does not perform functions on deleted keys
+   * @throws { BusinessError } 10200011 - The forEach method cannot be bound.
+   * @throws { BusinessError } 401 - The type of parameters are invalid.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -117,12 +134,14 @@ declare class HashMap<K, V> {
   thisArg?: Object): void;
   /**
    * Returns a new Iterator object that contains the [key, value] pairs for each element in the Map object in insertion order
+   * @throws { BusinessError } 10200011 - The entries method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
   entries(): IterableIterator<[K, V]>;
   /**
    * returns an iterator.Each item of the iterator is a Javascript Object
+   * @throws { BusinessError } 10200011 - The Symbol.iterator method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
--- api/@ohos.util.LightWeightMap.d.ts
+++ api/@ohos.util.LightWeightMap.d.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,9 +12,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 declare class LightWeightMap<K, V> {
   /**
    * A constructor used to create a LightWeightMap object.
+   * @throws { BusinessError } 10200012 - The LightWeightMap's constructor cannot be directly invoked.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -29,6 +31,8 @@ declare class LightWeightMap<K, V> {
    * Returns whether this map has all the object in a specified map
    * @param map the Map object to compare
    * @returns the boolean type
+   * @throws { BusinessError } 401 - The type of parameters are invalid.
+   * @throws { BusinessError } 10200011 - The hasAll method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -37,6 +41,7 @@ declare class LightWeightMap<K, V> {
    * Returns whether a key is contained in this map
    * @param key need to determine whether to include the key
    * @returns the boolean type
+   * @throws { BusinessError } 10200011 - The hasKey method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -45,6 +50,7 @@ declare class LightWeightMap<K, V> {
    * Returns whether a value is contained in this map
    * @param value need to determine whether to include the value
    * @returns the boolean type
+   * @throws { BusinessError } 10200011 - The hasValue method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -53,12 +59,15 @@ declare class LightWeightMap<K, V> {
    * Ensures that the capacity of an LightWeightMap container is greater than or equal to a specified value,
    * and that the container has all the original objects after capacity expansion
    * @param minimumCapacity Minimum capacity to be reserved
+   * @throws { BusinessError } 10200011 - The increaseCapacityTo method cannot be bound.
+   * @throws { BusinessError } 401 - The type of parameters are invalid.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
   increaseCapacityTo(minimumCapacity: number): void;
   /**
    * Returns a new Iterator object that contains the [key, value] pairs for each element in the Map object in insertion order
+   * @throws { BusinessError } 10200011 - The entries method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -67,6 +76,7 @@ declare class LightWeightMap<K, V> {
    * Returns the value to which the specified key is mapped, or undefined if this map contains no mapping for the key
    * @param key the index in LightWeightMap
    * @returns value or undefined
+   * @throws { BusinessError } 10200011 - The get method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -75,6 +85,7 @@ declare class LightWeightMap<K, V> {
    * Obtains the index of the key equal to a specified key in an LightWeightMap container
    * @param key Looking for goals
    * @returns Subscript corresponding to target
+   * @throws { BusinessError } 10200011 - The getIndexOfKey method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -83,6 +94,7 @@ declare class LightWeightMap<K, V> {
    * Obtains the index of the value equal to a specified value in an LightWeightMap container
    * @param value Looking for goals
    * @returns Subscript corresponding to target
+   * @throws { BusinessError } 10200011 - The getIndexOfValue method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -90,6 +102,7 @@ declare class LightWeightMap<K, V> {
   /**
    * Returns whether the Map object contains elements
    * @returns the boolean type
+   * @throws { BusinessError } 10200011 - The isEmpty method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -98,12 +111,16 @@ declare class LightWeightMap<K, V> {
    * Obtains the key at the location identified by index in an LightWeightMap container
    * @param index Target subscript for search
    * @returns the key of key-value pairs
+   * @throws { BusinessError } 10200011 - The getKeyAt method cannot be bound.
+   * @throws { BusinessError } 10200001 - The value of index is out of range.
+   * @throws { BusinessError } 401 - The type of parameters are invalid.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
   getKeyAt(index: number): K;
   /**
    * Obtains a ES6 iterator that contains all the keys of an LightWeightMap container
+   * @throws { BusinessError } 10200011 - The keys method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -111,6 +128,8 @@ declare class LightWeightMap<K, V> {
   /**
    * Adds all element groups in one map to another map
    * @param map the Map object to add members
+   * @throws { BusinessError } 10200011 - The setAll method cannot be bound.
+   * @throws { BusinessError } 401 - The type of parameters are invalid.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -120,6 +139,7 @@ declare class LightWeightMap<K, V> {
    * @param key Added or updated targets
    * @param value Added or updated value
    * @returns the map object after set
+   * @throws { BusinessError } 10200011 - The set method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -128,6 +148,7 @@ declare class LightWeightMap<K, V> {
    * Remove the mapping for this key from this map if present
    * @param key  Target to be deleted
    * @returns Target mapped value
+   * @throws { BusinessError } 10200011 - The remove method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -136,6 +157,8 @@ declare class LightWeightMap<K, V> {
    * Deletes a key-value pair at the location identified by index from an LightWeightMap container
    * @param index  Target subscript for search
    * @returns the boolean type(Is there a delete value)
+   * @throws { BusinessError } 10200011 - The removeAt method cannot be bound.
+   * @throws { BusinessError } 401 - The type of parameters are invalid.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -143,6 +166,7 @@ declare class LightWeightMap<K, V> {
   /**
    * Removes all of the mapping from this map
    * The map will be empty after this call returns
+   * @throws { BusinessError } 10200011 - The clear method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -152,6 +176,9 @@ declare class LightWeightMap<K, V> {
    * @param index Target subscript for search
    * @param value Updated the target mapped value
    * @returns the boolean type(Is there a value corresponding to the subscript)
+   * @throws { BusinessError } 10200011 - The setValueAt method cannot be bound.
+   * @throws { BusinessError } 10200001 - The value of index is out of range.
+   * @throws { BusinessError } 401 - The type of parameters are invalid.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -159,6 +186,8 @@ declare class LightWeightMap<K, V> {
   /**
    * Executes the given callback function once for each real key in the map.
    * It does not perform functions on deleted keys
+   * @throws { BusinessError } 10200011 - The forEach method cannot be bound.
+   * @throws { BusinessError } 401 - The type of parameters are invalid.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -166,12 +195,14 @@ declare class LightWeightMap<K, V> {
   thisArg?: Object): void;
   /**
    * returns an ES6 iterator.Each item of the iterator is a Javascript Object
+   * @throws { BusinessError } 10200011 - The Symbol.iterator method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
   [Symbol.iterator](): IterableIterator<[K, V]>;
   /**
    * Obtains a string that contains all the keys and values in an LightWeightMap container
+   * @throws { BusinessError } 10200011 - The toString method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
@@ -180,12 +211,16 @@ declare class LightWeightMap<K, V> {
    * Obtains the value identified by index in an LightWeightMap container
    * @param index Target subscript for search
    * @returns the value of key-value pairs
+   * @throws { BusinessError } 10200011 - The getValueAt method cannot be bound.
+   * @throws { BusinessError } 10200001 - The value of index is out of range.
+   * @throws { BusinessError } 401 - The type of parameters are invalid.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
   getValueAt(index: number): V;
   /**
    * Returns an iterator of the values contained in this map
+   * @throws { BusinessError } 10200011 - The values method cannot be bound.
    * @since 8
    * @syscap SystemCapability.Utils.Lang
    */
