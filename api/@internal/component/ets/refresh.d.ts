3e3fcd400c88387380eecb2f4699b692ee5043a0
limeng <limeng208@huawei.com>
2023-02-21 11:13:59 +0800

    Refresh组件能力增强
    Signed-off-by: limeng <limeng208@huawei.com>

--- api/@internal/component/ets/refresh.d.ts
+++ api/@internal/component/ets/refresh.d.ts
@@ -58,7 +58,37 @@ interface RefreshInterface {
    * Called when the drop-down refresh is set.
    * @since 8
    */
-  (value: { refreshing: boolean; offset?: number | string; friction?: number | string }): RefreshAttribute;
+  (value: {
+    /**
+     * Whether the current component is being refreshed.
+     * This parameter supports $$ for two-way binding of variables.
+     * @type {boolean}
+     * @since 8
+     */
+    refreshing: boolean;
+    /**
+     * Distance to the top of the parent component from the <Refresh> component that
+     * comes to rest after a successful pull-down gesture. Default value: 16, in vp
+     * @type {number | string}
+     * @since 8
+     */
+    offset?: number | string;
+    /**
+     * Coefficient of friction, which indicates the <Refresh> component's sensitivity to the pull-down gesture.
+     * The value ranges from 0 to 100. Default value: 62
+     *   - 0 indicates that the <Refresh> component is not sensitive to the pull-down gesture.
+     *   - 100 indicates that the <Refresh> component is highly sensitive to the pull-down gesture.
+     *   - A larger value indicates a more sensitive response of the <Refresh> component to the pull-down gesture.
+     * @type {number | string}
+     * @since 
+8     */
+    friction?: number | string;
+    /**
+     * Custom component to display during dragging.
+     * @type {CustomBuilder}
+     * @since 10
+     */
+    builder?: CustomBuilder }): RefreshAttribute;
 }
 
 /**
