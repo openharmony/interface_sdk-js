04844480d202d2e5a82811277107f1881a625379
zhangxiao150 <zhangxiao150@huawei.com>
2023-02-21 11:17:03 +0000

    支持设置文本输入框选择区域并高亮
    Signed-off-by: zhangxiao150 <zhangxiao150@huawei.com>
    Change-Id: Ie5d4ef9cbe2b98c44a6107e956484cca47bd3fa2

--- api/@internal/component/ets/text_input.d.ts
+++ api/@internal/component/ets/text_input.d.ts
@@ -100,6 +100,14 @@ declare class TextInputController {
    * @since 8
    */
   caretPosition(value: number): void;
+
+  /**
+   * Text selection is achieved by specifying the start and end positions of the text.
+   * @param { number } selectionStart - The start position of the selected text.
+   * @param { number } selectionEnd - The end position of the selected text.
+   * @since 10
+   */
+  setTextSelection(selectionStart: number, selectionEnd: number): void;
 }
 
 /**
