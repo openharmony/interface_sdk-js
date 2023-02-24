1c737d74b3f616ec11728bdf39445ecbdc5635f8
gongyuechen <gongyuechen@huawei.com>
2023-02-24 08:08:56 +0000

    add grantUriPermission API add grantUriPermission API
    Signed-off-by: gongyuechen <gongyuechen@huawei.com>

---
+++ api/@ohos.application.uriPermissionManager.d.ts
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License"),
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { AsyncCallback } from './basic';
+import wantConstant from "./@ohos.ability.wantConstant";
+
+/**
+ * The management class for uri of file.
+ *
+ * @since 9
+ * @syscap SystemCapability.Ability.AbilityRuntime.Core
+ * @permission N/A
+ */
+declare namespace uriPermissionManager {
+    /**
+     * Grant URI to another application
+     *
+     * @since 9
+     * @syscap SystemCapability.Ability.AbilityRuntime.Core
+     * @param uri File URI.
+     * @param flag wantConstant.Flags.FLAG_AUTH_READ_URI_PERMISSION or wantConstant.Flags.FLAG_AUTH_WRITE_URI_PERMISSION
+     * @param fromTokenId Indicates the access token of the application which URI belong to.
+     * @param targetTokenId Indicates the access token of the target application.
+     * @return Returns 0 if the verification is successful, otherwise returns -1.
+     */
+    function grantUriPermission(uri: string, flag: wantConstant.Flags, fromTokenId: number, targetTokenId: number, callback: AsyncCallback<number>): void;
+    function grantUriPermission(uri: string, flag: wantConstant.Flags, fromTokenId: number, targetTokenId: number): Promise<number>;
+
+    /**
+     * Grant URI from self to another application
+     *
+     * @since 9
+     * @syscap SystemCapability.Ability.AbilityRuntime.Core
+     * @param uri File URI.
+     * @param flag wantConstant.Flags.FLAG_AUTH_READ_URI_PERMISSION or wantConstant.Flags.FLAG_AUTH_WRITE_URI_PERMISSION
+     * @param targetTokenId Indicates the access token of the target application.
+     * @return Returns 0 if the verification is successful, otherwise returns -1.
+     */
+	function grantUriPermissionFromSelf(uri: string, flag: wantConstant.Flags, targetTokenId: number, callback: AsyncCallback<number>): void;
+    function grantUriPermissionFromSelf(uri: string, flag: wantConstant.Flags, targetTokenId: number): Promise<number>;
+}
+
+export default uriPermissionManager;
