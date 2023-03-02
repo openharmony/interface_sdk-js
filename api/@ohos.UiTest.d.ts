89250f1a6373f5c21cb259d7a8fd57ee476113e1
zhouke <zhouke35@huawei.com>
2023-03-02 19:37:43 +0800

    告警修改.Signed-off-by: <zhouke35@huawei.com>.
    Signed-off-by: zhouke <zhouke35@huawei.com>

--- api/@ohos.UiTest.d.ts
+++ api/@ohos.UiTest.d.ts
@@ -1854,4 +1854,5 @@ declare const BY: By;
  */
 declare const ON: On;
 
-export {UiComponent, UiDriver, Component, Driver, UiWindow, ON, BY, MatchPattern, DisplayRotation, ResizeDirection, WindowMode, PointerMatrix, UiDirection, MouseButton};
+export {UiComponent, UiDriver, Component, Driver, UiWindow, ON, BY, MatchPattern, DisplayRotation, ResizeDirection, WindowMode, PointerMatrix,
+  UiDirection, MouseButton};
