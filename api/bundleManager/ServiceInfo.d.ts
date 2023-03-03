78530b7b6fa191b15af483c48ee12e05d62570a1
xucheng <xucheng2@huawei.com>
2023-03-03 03:42:31 +0000

    add servicerouter query api
    Signed-off-by: xucheng <xucheng2@huawei.com>
    Change-Id: I954307dc774ca882b2f824bbb640689501a830f2

---
+++ api/bundleManager/ServiceInfo.d.ts
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { ApplicationInfo } from './ApplicationInfo';
+import serviceRouter from './../@ohos.serviceRouter';
+
+  /**
+   * Service information about a bundle
+   * @typedef ServiceInfo
+   * @syscap SystemCapability.BundleManager.BundleFramework.Core
+   * @systemapi
+   * @since 10
+   */
+  export interface ServiceInfo {
+    /**
+     * Indicates the name of the bundle
+     * @type {string}
+     * @syscap SystemCapability.BundleManager.BundleFramework.Core
+     * @since 10
+     */
+    readonly bundleName: string;
+
+    /**
+     * Indicates the name of the module
+     * @type {string}
+     * @syscap SystemCapability.BundleManager.BundleFramework.Core
+     * @since 10
+     */
+    readonly moduleName: string;
+
+    /**
+     * Indicates the name of the service info
+     * @type {string}
+     * @syscap SystemCapability.BundleManager.BundleFramework.Core
+     * @since 10
+     */
+    readonly name: string;
+
+    /**
+     * Indicates the label id of the service info
+     * @type {number}
+     * @syscap SystemCapability.BundleManager.BundleFramework.Core
+     * @since 10
+     */
+    readonly labelId: number;
+
+    /**
+     * Indicates the description id of the service info
+     * @type {number}
+     * @syscap SystemCapability.BundleManager.BundleFramework.Core
+     * @since 10
+     */
+    readonly descriptionId: number;
+
+    /**
+     * Indicates the icon id of the service info
+     * @type {number}
+     * @syscap SystemCapability.BundleManager.BundleFramework.Core
+     * @since 10
+     */
+    readonly iconId: number;
+
+    /**
+     * Enumerates types of the service info
+     * @type {serviceRouter.ExtensionServiceType}
+     * @syscap SystemCapability.BundleManager.BundleFramework.Core
+     * @since 10
+     */
+    readonly serviceType: serviceRouter.ExtensionServiceType;
+
+    /**
+     * The permissions that others need to use this service info
+     * @type {Array<string>}
+     * @syscap SystemCapability.BundleManager.BundleFramework.Core
+     * @since 10
+     */
+    readonly permissions: Array<string>;
+
+    /**
+     * Obtains configuration information about an application
+     * @type {ApplicationInfo}
+     * @syscap SystemCapability.BundleManager.BundleFramework.Core
+     * @since 10
+     */
+    readonly applicationInfo: ApplicationInfo;
+  }
