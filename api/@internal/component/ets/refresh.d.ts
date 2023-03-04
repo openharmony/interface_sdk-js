a53509f98bf6e4edc20862dfb836a52dd9b87aa2
limeng <limeng208@huawei.com>
2023-03-04 10:23:02 +0800

    Refresh组件能力增强
    Signed-off-by: limeng <limeng208@huawei.com>

--- api/@internal/component/ets/refresh.d.ts
+++ api/@internal/component/ets/refresh.d.ts
@@ -56,13 +56,22 @@ declare enum RefreshStatus {
 interface RefreshInterface {
   /**
    * Called when the drop-down refresh is set.
+   * @param { { refreshing: boolean; offset?: number | string; friction?: number | string } }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @since 8
+   */
+  /**
+   * Called when the drop-down refresh is set.
+   * @param { { refreshing: boolean; offset?: number | string; friction?: number | string; builder?: CustomBuilder } }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @since 10
    */
   (value: {
     /**
      * Whether the current component is being refreshed.
      * This parameter supports $$ for two-way binding of variables.
      * @type {boolean}
+     * @syscap SystemCapability.ArkUI.ArkUI.Full
      * @since 8
      */
     refreshing: boolean;
@@ -70,6 +79,7 @@ interface RefreshInterface {
      * Distance to the top of the parent component from the <Refresh> component that
      * comes to rest after a successful pull-down gesture. Default value: 16, in vp
      * @type {number | string}
+     * @syscap SystemCapability.ArkUI.ArkUI.Full
      * @since 8
      */
     offset?: number | string;
@@ -80,12 +90,14 @@ interface RefreshInterface {
      *   - 100 indicates that the <Refresh> component is highly sensitive to the pull-down gesture.
      *   - A larger value indicates a more sensitive response of the <Refresh> component to the pull-down gesture.
      * @type {number | string}
-     * @since 
-8     */
+     * @syscap SystemCapability.ArkUI.ArkUI.Full
+     * @since 8
+     */
     friction?: number | string;
     /**
      * Custom component to display during dragging.
      * @type {CustomBuilder}
+     * @syscap SystemCapability.ArkUI.ArkUI.Full
      * @since 10
      */
     builder?: CustomBuilder }): RefreshAttribute;
