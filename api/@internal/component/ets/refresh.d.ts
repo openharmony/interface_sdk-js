6f78272a8b53a1a0c0a72fb7d652e193ac2a91de
limeng <limeng208@huawei.com>
2023-03-07 05:31:36 +0000

    modified by comments
    Signed-off-by: limeng <limeng208@huawei.com>

--- api/@internal/component/ets/refresh.d.ts
+++ api/@internal/component/ets/refresh.d.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -50,6 +50,48 @@ declare enum RefreshStatus {
 }
 
 /**
+ * Defines the options of refresh component.
+ * @syscap SystemCapability.ArkUI.ArkUI.Full
+ * @since 10
+ */
+interface RefreshOptions {
+  /**
+   * Whether the current component is being refreshed.
+   * This parameter supports $$ for two-way binding of variables.
+   * @type {boolean}
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @since 8
+   */
+  refreshing: boolean;
+  /**
+   * Distance to the top of the parent component from the component that
+   * comes to rest after a successful pull-down gesture. Default value: 16, in vp
+   * @type {number | string}
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @since 8
+   */
+  offset?: number | string;
+  /**
+   * Coefficient of friction, which indicates the component's sensitivity to the pull-down gesture.
+   * The value ranges from 0 to 100. Default value: 62
+   *   - 0 indicates that the component is not sensitive to the pull-down gesture.
+   *   - 100 indicates that the component is highly sensitive to the pull-down gesture.
+   *   - A larger value indicates a more sensitive response of the component to the pull-down gesture.
+   * @type {number | string}
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @since 8
+   */
+  friction?: number | string;
+  /**
+   * Custom component to display during dragging.
+   * @type {CustomBuilder}
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @since 10
+   */
+  builder?: CustomBuilder;
+}
+
+/**
  * Provides a pull-down refresh interface.
  * @since 8
  */
@@ -60,47 +102,7 @@ interface RefreshInterface {
    * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @since 8
    */
-  /**
-   * Called when the drop-down refresh is set.
-   * @param { { refreshing: boolean; offset?: number | string; friction?: number | string; builder?: CustomBuilder } }
-   * @syscap SystemCapability.ArkUI.ArkUI.Full
-   * @since 10
-   */
-  (value: {
-    /**
-     * Whether the current component is being refreshed.
-     * This parameter supports $$ for two-way binding of variables.
-     * @type {boolean}
-     * @syscap SystemCapability.ArkUI.ArkUI.Full
-     * @since 8
-     */
-    refreshing: boolean;
-    /**
-     * Distance to the top of the parent component from the <Refresh> component that
-     * comes to rest after a successful pull-down gesture. Default value: 16, in vp
-     * @type {number | string}
-     * @syscap SystemCapability.ArkUI.ArkUI.Full
-     * @since 8
-     */
-    offset?: number | string;
-    /**
-     * Coefficient of friction, which indicates the <Refresh> component's sensitivity to the pull-down gesture.
-     * The value ranges from 0 to 100. Default value: 62
-     *   - 0 indicates that the <Refresh> component is not sensitive to the pull-down gesture.
-     *   - 100 indicates that the <Refresh> component is highly sensitive to the pull-down gesture.
-     *   - A larger value indicates a more sensitive response of the <Refresh> component to the pull-down gesture.
-     * @type {number | string}
-     * @syscap SystemCapability.ArkUI.ArkUI.Full
-     * @since 8
-     */
-    friction?: number | string;
-    /**
-     * Custom component to display during dragging.
-     * @type {CustomBuilder}
-     * @syscap SystemCapability.ArkUI.ArkUI.Full
-     * @since 10
-     */
-    builder?: CustomBuilder }): RefreshAttribute;
+  (value: RefreshOptions): RefreshAttribute;
 }
 
 /**
