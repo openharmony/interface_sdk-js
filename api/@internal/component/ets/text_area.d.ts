d74a9af43837a6e5da95b684f055cd8322d45497
chensi10 <chensi52@huawei.com>
2023-03-09 16:33:36 +0800

    add api to text_area.d.ts
    Signed-off-by: chensi10 <chensi52@huawei.com>

--- api/@internal/component/ets/text_area.d.ts
+++ api/@internal/component/ets/text_area.d.ts
@@ -28,6 +28,15 @@ declare class TextAreaController {
    * @since 8
    */
   caretPosition(value: number): void;
+
+  /**
+   * Text selection is achieved by specifying the start and end positions of the text.
+   * @param { number } selectionStart - The start position of the selected text.
+   * @param { number } selectionEnd - The end position of the selected text.
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @since 10
+   */
+  setTextSelection(selectionStart: number, selectionEnd: number): void;
 }
 
 /**
@@ -160,6 +169,14 @@ declare class TextAreaAttribute extends CommonMethod<TextAreaAttribute> {
    * @since 9
    */
   copyOption(value: CopyOptions): TextAreaAttribute;
+
+  /**
+   * Define the max height of the text area.
+   * @param { Length }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @since 10
+   */
+  maxHeight(value: Length): TextAreaAttribute;
 }
 
 declare const TextArea: TextAreaInterface;
