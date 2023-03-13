f116fa4a7ebecc5369012555c01ae3ce0b32d739
朱天怡 <zhutianyi2@huawei.com>
2023-03-13 07:25:51 +0000

    修改错误码
    Signed-off-by: 朱天怡 <zhutianyi2@huawei.com>

--- api/@ohos.resourceschedule.workScheduler.d.ts
+++ api/@ohos.resourceschedule.workScheduler.d.ts
@@ -215,7 +215,6 @@ declare namespace workScheduler {
      * @throws { BusinessError } 9700001 - Memory operation failed.
      * @throws { BusinessError } 9700002 - Parcel operation failed.
      * @throws { BusinessError } 9700003 - System service operation failed.
-     * @throws { BusinessError } 9700004 - Check workInfo failed.
      * @syscap SystemCapability.ResourceSchedule.WorkScheduler
      * @StageModelOnly
      * @since 9
@@ -231,7 +230,6 @@ declare namespace workScheduler {
      * @throws { BusinessError } 9700001 - Memory operation failed.
      * @throws { BusinessError } 9700002 - Parcel operation failed.
      * @throws { BusinessError } 9700003 - System service operation failed.
-     * @throws { BusinessError } 9700004 - Check workInfo failed.
      * @syscap SystemCapability.ResourceSchedule.WorkScheduler
      * @StageModelOnly
      * @since 9
