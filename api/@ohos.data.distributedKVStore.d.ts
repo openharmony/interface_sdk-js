ebbdccd9d2a53cbc1414f5a1c7351d635a5f93a5
Wangkai <wangkai424@huawei.com>
2023-03-14 08:43:16 +0000

    update api/@ohos.data.distributedKVStore.d.ts.
    Signed-off-by: Wangkai <wangkai424@huawei.com>

--- api/@ohos.data.distributedKVStore.d.ts
+++ api/@ohos.data.distributedKVStore.d.ts
@@ -41,7 +41,7 @@ declare namespace distributedKVStore {
         /**
          * Indicates the ability or hap context
          * @syscap SystemCapability.DistributedDataManager.KVStore.Core
-         * if swap the area, you should close all the KV store and use the new Context to create the KVManager
+         * if swap the area, you should close all the KV store and use the new BaseContext to create the KVManager
          * @since 9
          */
         context: BaseContext;
