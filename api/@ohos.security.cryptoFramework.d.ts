1e2eae5fc568e13e6048aaa9df593678ec4d8cda
xwb <xuwanbing1@huawei.com>
2023-04-06 07:43:11 +0000

    update api/@ohos.security.cryptoFramework.d.ts.
    Signed-off-by: xwb <xuwanbing1@huawei.com>

--- api/@ohos.security.cryptoFramework.d.ts
+++ api/@ohos.security.cryptoFramework.d.ts
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-import type { AsyncCallback, Callback } from './basic';
+import type { AsyncCallback, Callback } from './@ohos.base';
 
 /**
  * Provides a set of encryption and decryption algorithm library framework, shields the underlying differences,
