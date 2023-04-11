14eebf869ec8e94b3702a78766f8ce95741c05f4
houchengyu <houchengyu@huawei.com>
2023-04-11 14:09:57 +0000

    update api/@ohos.multimodalInput.pointer.d.ts.
    Signed-off-by: houchengyu <houchengyu@huawei.com>

--- api/@ohos.multimodalInput.pointer.d.ts
+++ api/@ohos.multimodalInput.pointer.d.ts
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-import { AsyncCallback } from './basic';
+import type { AsyncCallback } from './basic';
 
 /**
  * Declares interfaces related to mouse pointer attributes.
