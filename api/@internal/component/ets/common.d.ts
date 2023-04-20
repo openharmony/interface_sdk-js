8c801e452184663ac02e75f01347a2b7c346727d
zhongjianfei <zhongjianfei@huawei.com>
2023-04-20 16:53:36 +0800

    fixed 8f24355 from https://gitee.com/raulnaruto/interface_sdk-js/pulls/5161 fix SDK bugs for ArkTS card.
    Signed-off-by: zhongjianfei <zhongjianfei@huawei.com>
    Change-Id: I4c015a1b21959c2ba9c217146b6da09b88c0a682

--- api/@internal/component/ets/common.d.ts
+++ api/@internal/component/ets/common.d.ts
@@ -649,6 +649,15 @@ declare interface AlignRuleOption {
   center?: { anchor: string, align: VerticalAlign };
 }
 
+/**
+ * The param of rotate.
+ * @since 7
+ */
+/**
+ * The param of rotate.
+ * @form
+ * @since 9
+ */
 declare interface RotateOptions {
   /**
    * The param of x direction.
@@ -3829,8 +3838,25 @@ declare interface LinearGradient {
  * @since 9
  */
 declare interface LayoutBorderInfo {
+  /**
+   * Sub component borderWidth info.
+   * @form
+   * @since 9
+   */
   borderWidth: EdgeWidths,
+
+  /**
+   * Sub component margin info.
+   * @form
+   * @since 9
+   */
   margin: Margin,
+
+  /**
+   * Sub component padding info.
+   * @form
+   * @since 9
+   */
   padding: Padding,
 }
 
@@ -3840,7 +3866,18 @@ declare interface LayoutBorderInfo {
  * @since 9
  */
 declare interface LayoutInfo {
+  /**
+   * Sub component position info.
+   * @form
+   * @since 9
+   */
   position: Position,
+
+  /**
+   * Sub component constraint info.
+   * @form
+   * @since 9
+   */
   constraint: ConstraintSizeOptions,
 }
 
@@ -4003,6 +4040,13 @@ declare class View {
    * @ignore ide should ignore this attribute
    * @systemapi
    * @since 7
+   */
+  /**
+   * Just use for generate tsbundle
+   * @ignore ide should ignore this attribute
+   * @systemapi
+   * @form
+   * @since 9
    */
   create(value: any): any;
 }
