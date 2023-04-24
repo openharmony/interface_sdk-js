09818a6de5a73e21a005de9f41e5f823ce02fae2
magekkkk <lixin113@huawei.com>
2023-04-24 08:29:16 +0000

    define audio apis for audio volume manager new features
    Signed-off-by: magekkkk <lixin113@huawei.com>

--- api/@ohos.multimedia.audio.d.ts
+++ api/@ohos.multimedia.audio.d.ts
@@ -1138,6 +1138,29 @@ declare namespace audio {
   }
 
   /**
+   * Enumerates volume adjustment types.
+   * @since 10
+   * @syscap SystemCapability.Multimedia.Audio.Volume
+   * @systemapi
+   */
+  enum VolumeAdjustType {
+    /**
+     * Adjust volume up
+     * @since 10
+     * @syscap SystemCapability.Multimedia.Audio.Volume
+     * @systemapi
+     */
+    VOLUME_ADJUST_TURN_UP = 0,
+    /**
+     * Adjust volume down
+     * @since 10
+     * @syscap SystemCapability.Multimedia.Audio.Volume
+     * @systemapi
+     */
+    VOLUME_ADJUST_TURN_DOWN = 1,
+  }
+
+  /**
    * Implements audio volume and audio device management.
    * @typedef AudioManager
    * @syscap SystemCapability.Multimedia.Audio.Core
@@ -2233,6 +2256,84 @@ declare namespace audio {
      * @since 9
      */
     on(type: 'micStateChange', callback: Callback<MicStateChangeEvent>): void;
+
+    /**
+     * Gets if this volume group is volume unadjustable.
+     * @returns Whether it is volume unadjustable.
+     * @since 10
+     * @syscap SystemCapability.Multimedia.Audio.Volume
+     */
+    isVolumeUnadjustable(): boolean;
+
+    /**
+     * Adjusts system volume by step, volume type is decided by system.
+     * This method uses an asynchronous callback to return the result.
+     * @param adjustType Volume adjustment type.
+     * @param callback Callback used to return the result.
+     * @since 10
+     * @syscap SystemCapability.Multimedia.Audio.Volume
+     * @permission ohos.permission.ACCESS_NOTIFICATION_POLICY
+     * @systemapi
+     */
+    adjustVolumeByStep(adjustType: VolumeAdjustType, callback: AsyncCallback<void>): void;
+    /**
+     * Adjusts system volume by step, volume type is decided by system.
+     * @param adjustType Volume adjustment type.
+     * @returns Promise used to return the result.
+     * @since 10
+     * @syscap SystemCapability.Multimedia.Audio.Volume
+     * @permission ohos.permission.ACCESS_NOTIFICATION_POLICY
+     * @systemapi
+     */
+    adjustVolumeByStep(adjustType: VolumeAdjustType): Promise<void>;
+
+    /**
+     * Adjusts system volume by step for target volume type.
+     * This method uses an asynchronous callback to return the result.
+     * @param volumeType Audio volume type.
+     * @param adjustType Volume adjustment type.
+     * @param callback Callback used to return the result.
+     * @since 10
+     * @syscap SystemCapability.Multimedia.Audio.Volume
+     * @permission ohos.permission.ACCESS_NOTIFICATION_POLICY
+     * @systemapi
+     */
+    adjustSystemVolumeByStep(volumeType: AudioVolumeType, adjustType: VolumeAdjustType, callback: AsyncCallback<void>): void;
+    /**
+     * Adjusts system volume by step for target volume type.
+     * This method uses a promise to return the result.
+     * @param volumeType Audio volume type.
+     * @param adjustType Volume adjustment type.
+     * @returns Promise used to return the result.
+     * @since 10
+     * @syscap SystemCapability.Multimedia.Audio.Volume
+     * @permission ohos.permission.ACCESS_NOTIFICATION_POLICY
+     * @systemapi
+     */
+    adjustSystemVolumeByStep(volumeType: AudioVolumeType, adjustType: VolumeAdjustType): Promise<void>;
+
+    /**
+     * Gets the volume db value that system calculate by volume type, volume level and device type.
+     * This method uses an asynchronous callback to return the result.
+     * @param volumeType Audio volume type.
+     * @param volumeLevel Volume level to set.
+     * @param device Output device type.
+     * @param callback Callback used to return the result.
+     * @since 10
+     * @syscap SystemCapability.Multimedia.Audio.Volume
+     */
+    getSystemVolumeInDb(volumeType: AudioVolumeType, volumeLevel: number, device: DeviceType, callback: AsyncCallback<number>): void;
+    /**
+     * Gets the volume db value that system calculate by volume type, volume level and device type.
+     * This method uses a promise to return the result.
+     * @param volumeType Audio volume type.
+     * @param volumeLevel Volume level to set.
+     * @param device Output device type.
+     * @returns Promise used to return the result.
+     * @since 10
+     * @syscap SystemCapability.Multimedia.Audio.Volume
+     */
+    getSystemVolumeInDb(volumeType: AudioVolumeType, volumeLevel: number, device: DeviceType): Promise<number>;
   }
 
   /**
