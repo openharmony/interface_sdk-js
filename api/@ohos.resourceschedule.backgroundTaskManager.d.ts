0e267522a3363952ff7d10d557fd089014482a65
xuhanyang2 <xuhanyang2@huawei.com>
2023-05-15 15:10:50 +0800

    update
    Signed-off-by: xuhanyang2 <xuhanyang2@huawei.com>

--- api/@ohos.resourceschedule.backgroundTaskManager.d.ts
+++ api/@ohos.resourceschedule.backgroundTaskManager.d.ts
@@ -394,7 +394,7 @@ declare namespace backgroundTaskManager {
     AUDIO = 1 << 6,
 
     /**
-     * The resource for not being proxyed running_lock.
+     * The resource for not being proxyed running lock.
      *
      * @syscap SystemCapability.ResourceSchedule.BackgroundTaskManager.EfficiencyResourcesApply
      * @systemapi Hide this for inner system use.
@@ -403,7 +403,7 @@ declare namespace backgroundTaskManager {
     RUNNING_LOCK = 1 << 7,
 
     /**
-     * The resource for not being proxyed Sensor.
+     * The resource for not being proxyed sensor.
      *
      * @syscap SystemCapability.ResourceSchedule.BackgroundTaskManager.EfficiencyResourcesApply
      * @systemapi Hide this for inner system use.
