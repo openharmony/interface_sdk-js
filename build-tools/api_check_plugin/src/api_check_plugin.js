3c41efdf552b5cdab7bddd22c0987b3757b1b502
fanjiaojiao <fanjiaojiao@huawei.com>
2023-05-15 15:24:26 +0800

    fix:api_check门禁报错修复
    Signed-off-by: fanjiaojiao <fanjiaojiao@huawei.com>

--- build-tools/api_check_plugin/src/api_check_plugin.js
+++ build-tools/api_check_plugin/src/api_check_plugin.js
@@ -84,8 +84,7 @@ function checkAllNode(node, sourcefile, fileName) {
     checkPermission(node, sourcefile, fileName);
 
     if (commentNodeWhiteList.includes(node.kind)) {
-      const permissionConfigPath = require('../config/config.json');
-      checkJSDoc(node, sourcefile, permissionConfigPath, fileName);
+      checkJSDoc(node, sourcefile, fileName);
     }
 
   }
