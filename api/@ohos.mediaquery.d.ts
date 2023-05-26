d83b2b36a87c86c4391464fc9f77d524f7a5c584
quguiren <quguiren@huawei.com>
2023-05-26 12:01:21 +0800

    API加跨平台标签
    Signed-off-by: quguiren <quguiren@huawei.com>

--- api/@ohos.mediaquery.d.ts
+++ api/@ohos.mediaquery.d.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,53 +18,80 @@ import { Callback } from './@ohos.base';
 /**
  * Used to do mediaquery operations.
  * @syscap SystemCapability.ArkUI.ArkUI.Full
- * @crossplatform
  * @since 7
+ */
+/**
+ * Used to do mediaquery operations.
+ * @syscap SystemCapability.ArkUI.ArkUI.Full
+ * @crossplatform
+ * @since 10
  */
 declare namespace mediaquery {
 
   interface MediaQueryResult {
-
+    /**
+     * Whether the match condition is met.
+     * This parameter is read-only.
+     * @since 7
+     */
     /**
      * Whether the match condition is met.
      * This parameter is read-only.
      * @crossplatform
-     * @since 7
+     * @since 10
      */
     readonly matches: boolean;
 
     /**
      * Matching condition of a media event.
      * This parameter is read-only.
-     * @crossplatform
      * @since 7
+     */
+    /**
+     * Matching condition of a media event.
+     * This parameter is read-only.
+     * @crossplatform
+     * @since 10
      */
     readonly media: string;
   }
 
   interface MediaQueryListener extends MediaQueryResult {
-
+    /**
+     * Registers a callback with the corresponding query condition by using the handle.
+     * This callback is triggered when the media attributes change.
+     * @since 7
+     */
     /**
      * Registers a callback with the corresponding query condition by using the handle.
      * This callback is triggered when the media attributes change.
      * @crossplatform
-     * @since 7
+     * @since 10
      */
     on(type: 'change', callback: Callback<MediaQueryResult>): void;
 
     /**
      * Deregisters a callback with the corresponding query condition by using the handle.
      * This callback is not triggered when the media attributes chang.
-     * @crossplatform
      * @since 7
+     */
+    /**
+     * Deregisters a callback with the corresponding query condition by using the handle.
+     * This callback is not triggered when the media attributes chang.
+     * @crossplatform
+     * @since 10
      */
     off(type: 'change', callback?: Callback<MediaQueryResult>): void;
   }
 
   /**
    * Sets the media query criteria and returns the corresponding listening handle
-   * @crossplatform
    * @since 7
+   */
+  /**
+   * Sets the media query criteria and returns the corresponding listening handle
+   * @crossplatform
+   * @since 10
    */
   function matchMediaSync(condition: string): MediaQueryListener;
 }
