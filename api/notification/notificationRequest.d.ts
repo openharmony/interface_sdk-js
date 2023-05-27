16d3041424bcad475c1436f2a6ecb99ca1a6c6a7
xinking129 <xinxin13@huawei.com>
2023-05-27 14:22:29 +0800

    JSDoc changes
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- api/notification/notificationRequest.d.ts
+++ api/notification/notificationRequest.d.ts
@@ -281,6 +281,7 @@ export interface NotificationRequest {
    *
    * @type { ?string }
    * @syscap SystemCapability.Notification.Notification
+   * @systemapi Hide this for inner system use.
    * @since 7
    */
   classification?: string;
