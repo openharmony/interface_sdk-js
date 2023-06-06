97198390a413a8e2c369874dcb58729194f28db1
xixian <xixian@huawei.com>
2023-06-06 07:02:03 +0000

    update api/@ohos.multimodalInput.pointer.d.ts.
    Signed-off-by: xixian <xixian@huawei.com>

--- api/@ohos.multimodalInput.pointer.d.ts
+++ api/@ohos.multimodalInput.pointer.d.ts
@@ -848,7 +848,7 @@ declare namespace pointer {
   /**
    * Get touchpad pointer speed.
    *
-   * @returns { Promise<void> } Returns the result through a promise.
+   * @returns { Promise<number> } Returns the result through a promise.
    * @throws { BusinessError } 202 - SystemAPI permission error.
    * @throws { BusinessError } 401 - Parameter error.
    * @syscap SystemCapability.MultimodalInput.Input.Pointer
