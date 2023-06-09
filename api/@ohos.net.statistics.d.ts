31ff5ebbd13006605eb26239b1cbaf221ce76734
xujie <xujie223@huawei.com>
2023-06-09 17:35:20 +0800

    update
    Change-Id: I093cb8573ad5e4ea1695287d401e7c9e0eb9225d
    Signed-off-by: xujie <xujie223@huawei.com>

--- api/@ohos.net.statistics.d.ts
+++ api/@ohos.net.statistics.d.ts
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-import { AsyncCallback, Callback } from "./basic";
+import { AsyncCallback, Callback } from './basic';
 
 /**
  * Obtains traffic statistics.
@@ -24,8 +24,8 @@ import { AsyncCallback, Callback } from "./basic";
 declare namespace statistics {
   /**
    * Queries the data traffic (including all TCP and UDP data packets) received through a specified NIC.
-   * @param { string } nic Indicates the NIC name.
-   * @param { AsyncCallback<number> } callback Returns the data traffic received through the specified NIC.
+   * @param { string } nic - Indicates the NIC name.
+   * @param { AsyncCallback<number> } callback - Returns the data traffic received through the specified NIC.
    * @throws { BusinessError } 401 - Parameter error.
    * @throws { BusinessError } 2100002 - Operation failed. Cannot connect to service.
    * @throws { BusinessError } 2100003 - System internal error.
@@ -39,7 +39,7 @@ declare namespace statistics {
 
   /**
    * Queries the data traffic (including all TCP and UDP data packets) received through a specified NIC.
-   * @param { string } nic Indicates the NIC name.
+   * @param { string } nic - Indicates the NIC name.
    * @returns { Promise<number> } The promise returned by the function.
    * @throws { BusinessError } 401 - Parameter error.
    * @throws { BusinessError } 2100002 - Operation failed. Cannot connect to service.
@@ -54,8 +54,8 @@ declare namespace statistics {
 
   /**
    * Queries the data traffic (including all TCP and UDP data packets) sent through a specified NIC.
-   * @param { string } nic Indicates the NIC name.
-   * @param { AsyncCallback<number> } callback Returns the data traffic sent through the specified NIC.
+   * @param { string } nic - Indicates the NIC name.
+   * @param { AsyncCallback<number> } callback - Returns the data traffic sent through the specified NIC.
    * @throws { BusinessError } 401 - Parameter error.
    * @throws { BusinessError } 2100002 - Operation failed. Cannot connect to service.
    * @throws { BusinessError } 2100003 - System internal error.
@@ -69,7 +69,7 @@ declare namespace statistics {
 
   /**
    * Queries the data traffic (including all TCP and UDP data packets) sent through a specified NIC.
-   * @param { string } nic Indicates the NIC name.
+   * @param { string } nic - Indicates the NIC name.
    * @returns { Promise<number> } The promise returned by the function.
    * @throws { BusinessError } 401 - Parameter error.
    * @throws { BusinessError } 2100002 - Operation failed. Cannot connect to service.
@@ -84,7 +84,7 @@ declare namespace statistics {
 
   /**
    * Queries the data traffic (including all TCP and UDP data packets) received through the cellular network.
-   * @param { AsyncCallback<number> } callback Returns the data traffic received through the cellular network.
+   * @param { AsyncCallback<number> } callback - Returns the data traffic received through the cellular network.
    * @throws { BusinessError } 2100002 - Operation failed. Cannot connect to service.
    * @throws { BusinessError } 2100003 - System internal error.
    * @throws { BusinessError } 2103005 - Failed to read map.
@@ -110,7 +110,7 @@ declare namespace statistics {
 
   /**
    * Queries the data traffic (including all TCP and UDP data packets) sent through the cellular network.
-   * @param { AsyncCallback<number> } callback Returns the data traffic sent through the cellular network.
+   * @param { AsyncCallback<number> } callback - Returns the data traffic sent through the cellular network.
    * @throws { BusinessError } 2100002 - Operation failed. Cannot connect to service.
    * @throws { BusinessError } 2100003 - System internal error.
    * @throws { BusinessError } 2103005 - Failed to read map.
@@ -136,7 +136,7 @@ declare namespace statistics {
 
   /**
    * Queries the data traffic (including all TCP and UDP data packets) received through all NICs.
-   * @param { AsyncCallback<number> } callback Returns the data traffic received through all NICs.
+   * @param { AsyncCallback<number> } callback - Returns the data traffic received through all NICs.
    * @throws { BusinessError } 2100002 - Operation failed. Cannot connect to service.
    * @throws { BusinessError } 2100003 - System internal error.
    * @throws { BusinessError } 2103005 - Failed to read map.
@@ -160,7 +160,7 @@ declare namespace statistics {
 
   /**
    * Queries the data traffic (including all TCP and UDP data packets) sent through all NICs.
-   * @param { AsyncCallback<number> } callback Returns the data traffic sent through all NICs.
+   * @param { AsyncCallback<number> } callback - Returns the data traffic sent through all NICs.
    * @throws { BusinessError } 2100002 - Operation failed. Cannot connect to service.
    * @throws { BusinessError } 2100003 - System internal error.
    * @throws { BusinessError } 2103005 - Failed to read map.
@@ -185,8 +185,8 @@ declare namespace statistics {
   /**
    * Queries the data traffic (including all TCP and UDP data packets) received by a specified application.
    * This method applies only to system applications and your own applications.
-   * @param { number } uid Indicates the process ID of the application.
-   * @param { AsyncCallback<number> } callback Returns the data traffic received by the specified application.
+   * @param { number } uid - Indicates the process ID of the application.
+   * @param { AsyncCallback<number> } callback - Returns the data traffic received by the specified application.
    * @throws { BusinessError } 401 - Parameter error.
    * @throws { BusinessError } 2100002 - Operation failed. Cannot connect to service.
    * @throws { BusinessError } 2100003 - System internal error.
@@ -200,7 +200,7 @@ declare namespace statistics {
   /**
    * Queries the data traffic (including all TCP and UDP data packets) received by a specified application.
    * This method applies only to system applications and your own applications.
-   * @param { number } uid Indicates the process ID of the application.
+   * @param { number } uid - Indicates the process ID of the application.
    * @returns { Promise<number> } The promise returned by the function.
    * @throws { BusinessError } 401 - Parameter error.
    * @throws { BusinessError } 2100002 - Operation failed. Cannot connect to service.
@@ -215,8 +215,8 @@ declare namespace statistics {
   /**
    * Queries the data traffic (including all TCP and UDP data packets) sent by a specified application.
    * This method applies only to system applications and your own applications.
-   * @param { number } uid Indicates the process ID of the application.
-   * @param { AsyncCallback<number> } callback Returns the data traffic sent by the specified application.
+   * @param { number } uid - Indicates the process ID of the application.
+   * @param { AsyncCallback<number> } callback - Returns the data traffic sent by the specified application.
    * @throws { BusinessError } 401 - Parameter error.
    * @throws { BusinessError } 2100002 - Operation failed. Cannot connect to service.
    * @throws { BusinessError } 2100003 - System internal error.
@@ -230,7 +230,7 @@ declare namespace statistics {
   /**
    * Queries the data traffic (including all TCP and UDP data packets) sent by a specified application.
    * This method applies only to system applications and your own applications.
-   * @param { number } uid Indicates the process ID of the application.
+   * @param { number } uid - Indicates the process ID of the application.
    * @returns { Promise<number> } The promise returned by the function.
    * @throws { BusinessError } 401 - Parameter error.
    * @throws { BusinessError } 2100002 - Operation failed. Cannot connect to service.
@@ -245,7 +245,7 @@ declare namespace statistics {
   /**
    * Register notifications of network traffic updates.
    * @permission ohos.permission.GET_NETWORK_STATS
-   * @param { 'netStatsChange' } type Indicates Event name.
+   * @param { 'netStatsChange' } type - Indicates Event name.
    * @throws { BusinessError } 201 - Permission denied.
    * @throws { BusinessError } 202 - Non-system applications use system APIs.
    * @throws { BusinessError } 401 - Parameter error.
@@ -260,7 +260,7 @@ declare namespace statistics {
   /**
    * Unregister notifications of network traffic updates.
    * @permission ohos.permission.GET_NETWORK_STATS
-   * @param { 'netStatsChange' } type Indicates Event name.
+   * @param { 'netStatsChange' } type - Indicates Event name.
    * @throws { BusinessError } 201 - Permission denied.
    * @throws { BusinessError } 202 - Non-system applications use system APIs.
    * @throws { BusinessError } 401 - Parameter error.
@@ -275,8 +275,8 @@ declare namespace statistics {
   /**
    * Get the traffic usage details of the network interface in the specified time period.
    * @permission ohos.permission.GET_NETWORK_STATS
-   * @param { IfaceInfo } ifaceInfo Indicates the handle. See {@link IfaceInfo}.
-   * @param { AsyncCallback<NetStatsInfo> } callback Returns the {@link NetStatsInfo} object;
+   * @param { IfaceInfo } ifaceInfo - Indicates the handle. See {@link IfaceInfo}.
+   * @param { AsyncCallback<NetStatsInfo> } callback - Returns the {@link NetStatsInfo} object;
    * @throws { BusinessError } 201 - Permission denied.
    * @throws { BusinessError } 202 - Non-system applications use system APIs.
    * @throws { BusinessError } 401 - Parameter error.
@@ -293,7 +293,7 @@ declare namespace statistics {
   /**
    * Get the traffic usage details of the network interface in the specified time period.
    * @permission ohos.permission.GET_NETWORK_STATS
-   * @param { IfaceInfo } ifaceInfo Indicates the handle. See {@link IfaceInfo}.
+   * @param { IfaceInfo } ifaceInfo - Indicates the handle. See {@link IfaceInfo}.
    * @returns { Promise<NetStatsInfo> } The promise returned by the function.
    * @throws { BusinessError } 201 - Permission denied.
    * @throws { BusinessError } 202 - Non-system applications use system APIs.
@@ -311,8 +311,8 @@ declare namespace statistics {
   /**
    * Get the traffic usage details of the specified time period of the application.
    * @permission ohos.permission.GET_NETWORK_STATS
-   * @param { UidStatsInfo } uidStatsInfo Indicates the handle. See {@link UidStatsInfo}.
-   * @param { AsyncCallback<NetStatsInfo> } callback Returns the {@link NetStatsInfo} object;
+   * @param { UidStatsInfo } uidStatsInfo - Indicates the handle. See {@link UidStatsInfo}.
+   * @param { AsyncCallback<NetStatsInfo> } callback - Returns the {@link NetStatsInfo} object;
    * @throws { BusinessError } 201 - Permission denied.
    * @throws { BusinessError } 202 - Non-system applications use system APIs.
    * @throws { BusinessError } 401 - Parameter error.
@@ -329,7 +329,7 @@ declare namespace statistics {
   /**
    * Get the traffic usage details of the specified time period of the application.
    * @permission ohos.permission.GET_NETWORK_STATS
-   * @param { UidStatsInfo } uidStatsInfo Indicates the handle. See {@link UidStatsInfo}.
+   * @param { UidStatsInfo } uidStatsInfo - Indicates the handle. See {@link UidStatsInfo}.
    * @returns { Promise<NetStatsInfo> } The promise returned by the function.
    * @throws { BusinessError } 201 - Permission denied.
    * @throws { BusinessError } 202 - Non-system applications use system APIs.
