6b4663ea36206bb53672f54c2aea6950dc92e5c1
pengpenglottie <pengpeng35@huawei.com>
2023-07-11 09:31:55 +0800

    range
    Signed-off-by: pengpenglottie <pengpeng35@huawei.com>
    Change-Id: I94089c839b9eedccc218c1a0aaf943e293bc08f5

--- api/@ohos.effectKit.d.ts
+++ api/@ohos.effectKit.d.ts
@@ -175,7 +175,7 @@ declare namespace effectKit {
    * Create a color picker to get color of an image.
    * @param { image.PixelMap } source - the source pixelmap.
    * @param { Array<number> } region - at least 4 elements, represents the region's left, top, right, bottom coordinates,
-   * its range is [0, 1], default is [0, 0, 1, 1], represents the region of color picker is the whole pixelMap.
+   * default is [0, 0, 1, 1], represents the region of color picker is the whole pixelMap.
    * @returns { Promise<ColorPicker> } - returns the ColorPicker generated.
    * @syscap SystemCapability.Multimedia.Image.Core
    * @since 10
@@ -194,7 +194,7 @@ declare namespace effectKit {
    * @param { image.PixelMap } source - the source pixelmap.
    * @param { AsyncCallback<ColorPicker> } callback - the callback of createColorPicker.
    * @param { Array<number> } region - at least 4 elements, represents the region's left, top, right, bottom coordinates,
-   * its range is [0, 1], default is [0, 0, 1, 1], represents the region of color picker is the whole pixelMap.
+   * default is [0, 0, 1, 1], represents the region of color picker is the whole pixelMap.
    * @syscap SystemCapability.Multimedia.Image.Core
    * @since 10
    */
