e385fd7aceb3dfdae8bd24b52d2a88c80b2ee201
xujie <xujie223@huawei.com>
2023-09-05 15:26:29 +0800

    Modify createVpnConnection to system api
    Change-Id: I27cb913aec3c3faacaa579591b081d883b807a4b
    Signed-off-by: xujie <xujie223@huawei.com>

--- api/@ohos.net.vpn.d.ts
+++ api/@ohos.net.vpn.d.ts
@@ -31,8 +31,10 @@ declare namespace vpn {
    * Create a VPN connection using the AbilityContext.
    * @param { AbilityContext } context - Indicates the context of application or capability.
    * @returns { VpnConnection } the VpnConnection of the construct VpnConnection instance.
+   * @throws { BusinessError } 202 - Non-system applications use system APIs.
    * @throws { BusinessError } 401 - Parameter error.
    * @syscap SystemCapability.Communication.NetManager.Vpn
+   * @systemapi Hide this for inner system use.
    * @since 10
    */
   function createVpnConnection(context: AbilityContext): VpnConnection;
