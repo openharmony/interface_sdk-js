1586f727df177f5e537ff6b6a50652487f482673
葛尚华 <geshanghua@h-partners.com>
2023-09-12 10:21:10 +0000

    update api/@ohos.app.ability.DriverExtensionAbility.d.ts.
    Signed-off-by: 葛尚华 <geshanghua@h-partners.com>

--- api/@ohos.app.ability.DriverExtensionAbility.d.ts
+++ api/@ohos.app.ability.DriverExtensionAbility.d.ts
@@ -17,6 +17,7 @@ import type rpc from './@ohos.rpc';
 import type Want from './@ohos.app.ability.Want';
 import _DriverExtensionContext from './application/DriverExtensionContext';
 
+
 export type DriverExtensionContext = _DriverExtensionContext;
 
 /**
