cbeb1d35b9cf79551f38fc4318935ea6f7d80090
mayunteng_1 <mayunteng@huawei.com>
2023-11-07 13:17:27 +0000

    穿越js接口增加权限校验
    Signed-off-by: mayunteng_1 <mayunteng@huawei.com>
    Change-Id: I7527e2fb618615342e5329c62e522dcf8b4f2021

--- api/@ohos.cooperate.d.ts
+++ api/@ohos.cooperate.d.ts
@@ -32,6 +32,8 @@ declare namespace cooperate {
    * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
    * @systemapi Hide this for inner system use.
    * @since 10
+   * @deprecated since 11
+   * @useinstead cooperate#CooperateMessage
    */
   enum CooperateMsg {
     /**
@@ -108,6 +110,88 @@ declare namespace cooperate {
   }
 
   /**
+   * Enumerates screen hopping message notifications.
+   *
+   * @enum { number }
+   * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
+   * @systemapi Hide this for inner system use.
+   * @since 11
+   */
+  enum CooperateMessage {
+    /**
+     * Preparing for screen hopping.
+     *
+     * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
+     * @systemapi Hide this for inner system use.
+     * @since 11
+     */
+    COOPERATE_PREPARE = 0,
+
+    /**
+     * Canceling the preparation for screen hopping.
+     *
+     * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
+     * @systemapi Hide this for inner system use.
+     * @since 11
+     */
+    COOPERATE_UNPREPARE = 1,
+
+    /**
+     * Starting screen hopping.
+     *
+     * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
+     * @systemapi Hide this for inner system use.
+     * @since 11
+     */
+    COOPERATE_ACTIVATE = 2,
+
+    /**
+     * Success in starting screen hopping.
+     *
+     * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
+     * @systemapi Hide this for inner system use.
+     * @since 11
+     */
+    COOPERATE_ACTIVATE_SUCCESS = 3,
+
+    /**
+     * Failure to start screen hopping.
+     *
+     * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
+     * @systemapi Hide this for inner system use.
+     * @since 11
+     */
+    COOPERATE_ACTIVATE_FAILURE = 4,
+
+    /**
+     * Success in stopping screen hopping.
+     *
+     * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
+     * @systemapi Hide this for inner system use.
+     * @since 11
+     */
+    COOPERATE_DEACTIVATE_SUCCESS = 5,
+
+    /**
+     * Failure to stop screen hopping.
+     *
+     * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
+     * @systemapi Hide this for inner system use.
+     * @since 11
+     */
+    COOPERATE_DEACTIVATE_FAILURE = 6,
+
+    /**
+     * Inter-device session disconnected.
+     *
+     * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
+     * @systemapi Hide this for inner system use.
+     * @since 11
+     */
+    COOPERATE_SESSION_DISCONNECT = 7,
+  }
+
+  /**
    * Prepares for screen hopping.
    *
    * @param { AsyncCallback<void> } callback Asynchronous callback used to return the operation result.
@@ -115,6 +199,8 @@ declare namespace cooperate {
    * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
    * @systemapi Hide this for inner system use.
    * @since 10
+   * @deprecated since 11
+   * @useinstead cooperate#prepareCooperate
    */
   function prepare(callback: AsyncCallback<void>): void;
 
@@ -126,8 +212,32 @@ declare namespace cooperate {
    * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
    * @systemapi Hide this for inner system use.
    * @since 10
+   * @deprecated since 11
+   * @useinstead cooperate#prepareCooperate
    */
   function prepare(): Promise<void>;
+
+  /**
+   * Prepares for screen hopping.
+   *
+   * @param { AsyncCallback<void> } callback Asynchronous callback used to return the operation result.
+   * @throws {BusinessError} 401 - Parameter error.
+   * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
+   * @systemapi Hide this for inner system use.
+   * @since 11
+   */
+  function prepareCooperate(callback: AsyncCallback<void>): void;
+
+  /**
+   * Prepares for screen hopping.
+   *
+   * @returns { Promise<void> } the promise returned by the function.
+   * @throws {BusinessError} 401 - Parameter error.
+   * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
+   * @systemapi Hide this for inner system use.
+   * @since 11
+   */
+  function prepareCooperate(): Promise<void>;
 
   /**
    * Cancels the preparation for screen hopping.
@@ -137,6 +247,8 @@ declare namespace cooperate {
    * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
    * @systemapi Hide this for inner system use.
    * @since 10
+   * @deprecated since 11
+   * @useinstead cooperate#unprepareCooperate
    */
   function unprepare(callback: AsyncCallback<void>): void;
 
@@ -148,8 +260,32 @@ declare namespace cooperate {
    * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
    * @systemapi Hide this for inner system use.
    * @since 10
+   * @deprecated since 11
+   * @useinstead cooperate#unprepareCooperate
    */
   function unprepare(): Promise<void>;
+
+  /**
+   * Cancels the preparation for screen hopping.
+   *
+   * @param { AsyncCallback<void> } callback Asynchronous callback used to return the operation result.
+   * @throws {BusinessError} 401 - Parameter error.
+   * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
+   * @systemapi Hide this for inner system use.
+   * @since 11
+   */
+  function unprepareCooperate(callback: AsyncCallback<void>): void;
+
+  /**
+   * Cancels the preparation for screen hopping.
+   *
+   * @returns { Promise<void> } the promise returned by the function.
+   * @throws {BusinessError} 401 - Parameter error.
+   * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
+   * @systemapi Hide this for inner system use.
+   * @since 11
+   */
+  function unprepareCooperate(): Promise<void>;
 
   /**
    * Starts screen hopping.
@@ -162,6 +298,8 @@ declare namespace cooperate {
    * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
    * @systemapi Hide this for inner system use.
    * @since 10
+   * @deprecated since 11
+   * @useinstead cooperate#activateCooperate
    */
   function activate(targetNetworkId: string, inputDeviceId: number, callback: AsyncCallback<void>): void;
 
@@ -176,8 +314,38 @@ declare namespace cooperate {
    * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
    * @systemapi Hide this for inner system use.
    * @since 10
+   * @deprecated since 11
+   * @useinstead cooperate#activateCooperate
    */
   function activate(targetNetworkId: string, inputDeviceId: number): Promise<void>;
+
+  /**
+   * Starts screen hopping.
+   *
+   * @param { string } targetNetworkId Descriptor of the target device for screen hopping.
+   * @param { number } inputDeviceId Identifier of the input device for screen hopping.
+   * @param { AsyncCallback<void> } callback Asynchronous callback used to return the operation result.
+   * @throws {BusinessError} 401 - Parameter error.
+   * @throws {BusinessError} 20900001 - Operation failed.
+   * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
+   * @systemapi Hide this for inner system use.
+   * @since 11
+   */
+  function activateCooperate(targetNetworkId: string, inputDeviceId: number, callback: AsyncCallback<void>): void;
+
+  /**
+   * Starts screen hopping.
+   *
+   * @param { string } targetNetworkId Descriptor of the target device for screen hopping.
+   * @param { number }inputDeviceId Identifier of the input device for screen hopping.
+   * @returns { Promise<void> } the promise returned by the function.
+   * @throws {BusinessError} 401 - Parameter error.
+   * @throws {BusinessError} 20900001 - Operation failed.
+   * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
+   * @systemapi Hide this for inner system use.
+   * @since 11
+   */
+  function activateCooperate(targetNetworkId: string, inputDeviceId: number): Promise<void>;
 
   /**
    * Stops screen hopping.
@@ -188,6 +356,8 @@ declare namespace cooperate {
    * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
    * @systemapi Hide this for inner system use.
    * @since 10
+   * @deprecated since 11
+   * @useinstead cooperate#deactivateCooperate
    */
   function deactivate(isUnchained: boolean, callback: AsyncCallback<void>): void;
 
@@ -199,8 +369,33 @@ declare namespace cooperate {
    * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
    * @systemapi Hide this for inner system use.
    * @since 10
+   * @deprecated since 11
+   * @useinstead cooperate#deactivateCooperate
    */
   function deactivate(isUnchained: boolean): Promise<void>;
+
+  /**
+   * Stops screen hopping.
+   *
+   * @param { boolean } isUnchained Whether the cross-device link is unchained.
+   * @param { AsyncCallback<void> } callback Asynchronous callback used to return the operation result.
+   * @throws {BusinessError} 401 - Parameter error.
+   * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
+   * @systemapi Hide this for inner system use.
+   * @since 11
+   */
+  function deactivateCooperate(isUnchained: boolean, callback: AsyncCallback<void>): void;
+
+  /**
+   * Stops screen hopping.
+   *
+   * @param { boolean } isUnchained Whether the cross-device link is unchained.
+   * @returns { Promise<void> } the promise returned by the function.
+   * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
+   * @systemapi Hide this for inner system use.
+   * @since 11
+   */
+  function deactivateCooperate(isUnchained: boolean): Promise<void>;
 
   /**
    * Obtains the screen hopping status.
@@ -211,6 +406,8 @@ declare namespace cooperate {
    * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
    * @systemapi Hide this for inner system use.
    * @since 10
+   * @deprecated since 11
+   * @useinstead cooperate#getCooperateSwitchState
    */
   function getCrossingSwitchState(networkId: string, callback: AsyncCallback<boolean>): void;
 
@@ -224,8 +421,36 @@ declare namespace cooperate {
    * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
    * @systemapi Hide this for inner system use.
    * @since 10
+   * @deprecated since 11
+   * @useinstead cooperate#getCooperateSwitchState
    */
   function getCrossingSwitchState(networkId: string): Promise<boolean>;
+
+
+  /**
+   * Obtains the screen hopping status.
+   *
+   * @param { string } networkId Descriptor of the target device for screen hopping.
+   * @param { AsyncCallback<boolean> } callback Asynchronous callback used to return the screen hopping status.
+   * @throws {BusinessError} 401 - Parameter error.
+   * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
+   * @systemapi Hide this for inner system use.
+   * @since 11
+   */
+  function getCooperateSwitchState(networkId: string, callback: AsyncCallback<boolean>): void;
+
+  /**
+   * Obtains the status of the screen hopping switch.
+   *
+   * @param { string } networkId Descriptor of the target device for screen hopping.
+   * @returns { Promise<boolean> } Returns {@code true} the screen hopping status;
+   *                               returns {@code false} otherwise.
+   * @throws {BusinessError} 401 - Parameter error.
+   * @syscap SystemCapability.Msdp.DeviceStatus.Cooperate
+   * @systemapi Hide this for inner system use.
+   * @since 11
+   */
+  function getCooperateSwitchState(networkId: string): Promise<boolean>;
 
   /**
    * Enables listening for screen hopping status change events.
