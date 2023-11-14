123dfd4b434cb32b9cd679c693a47c8e096d144a
yanxiaotao <yanxiaotao@huawei.com>
2023-11-14 14:29:00 +0000

    ok
    Signed-off-by: yanxiaotao <yanxiaotao@huawei.com>

--- api/@ohos.wifiManager.d.ts
+++ api/@ohos.wifiManager.d.ts
@@ -793,7 +793,7 @@ declare namespace wifiManager {
 
     /**
    * Get all the stations in the block list.
-   * @permission ohos.permission.SET_WIFI_INFO and ohos.permission.MANAGE_WIFI_HOTSPOT
+   * @permission ohos.permission.GET_WIFI_INFO and ohos.permission.MANAGE_WIFI_HOTSPOT
    * @throws {BusinessError} 201 - Permission denied.
    * @throws {BusinessError} 202 - System API is not allowed called by Non-system application.
    * @throws {BusinessError} 801 - Capability not supported.
