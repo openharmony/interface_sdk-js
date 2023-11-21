ec930eee9190c490af88b248c12c04e9a61aa622
penghongxing <penghongxing@huawei.com>
2023-11-21 12:59:39 +0000

    add dragPreviewMode interface
    Signed-off-by: penghongxing <penghongxing@huawei.com>

--- api/@internal/component/ets/common.d.ts
+++ api/@internal/component/ets/common.d.ts
@@ -12941,6 +12941,58 @@ declare class CommonMethod<T> {
    */
   draggable(value: boolean): T;
 
+/**
+ * Defines the drag preview mode.
+ *
+ * @enum { number }
+ * @syscap SystemCapability.ArkUI.ArkUI.Full
+ * @since 11
+ */
+  declare enum DragPreviewMode {
+    /**
+     * Default preview mode, let system process preview scale.
+     *
+     * @syscap SystemCapability.ArkUI.ArkUI.Full
+     * @since 11
+     */
+    AUTO = 1,
+    /**
+     * Disable system scale to preview pannel
+     *
+     * @syscap SystemCapability.ArkUI.ArkUI.Full
+     * @since 11
+     */
+    DISABLE_SCALE = 2,
+  }
+
+  /**
+   * Defines the preview options.
+   *
+   * @interface DragPreviewOptions
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @since 11
+   */
+  declare interface DragPreviewOptions {
+    /**
+    * Drag preview mode.
+    *
+    * @type { DragPreviewMode }
+    * @syscap SystemCapability.ArkUI.ArkUI.Full
+    * @since 11
+    */
+    mode?: DragPreviewMode;
+  }
+
+  /**
+   * Set the selectable area drag preview option.
+   *
+   * @param { DragPreviewMode } value - preview mode enum value.
+   * @returns { T } property value of type T.
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @since 11
+   */
+    dragPreviewOption(value:DragPreviewOptions): T;
+
   /**
    * Add mask text to the current component. The layout is the same as that of the current component.
    *
@@ -15938,7 +15990,7 @@ declare module "AnimateToParam" {
 declare module 'DragControllerParam' {
   module 'DragControllerParam' {
     // @ts-ignore
-    export type { CustomBuilder, DragItemInfo, DragEvent };
+    export type { CustomBuilder, DragItemInfo, DragEvent, DragPreviewMode, DragPreviewOptions };
   }
 }
 
