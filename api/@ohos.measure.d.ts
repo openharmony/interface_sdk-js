99f96f5d693772b54608285836363692a8da4fd4
dlbben <2861452198@qq.com>
2023-11-29 07:54:20 +0000

    Revert "Merge branch 'master' of https://gitee.com/dlbben/interface_sdk-js"
    This reverts commit 7c82b95dc78884a780efa62afc999df8dc60e62c.
    Signed-off-by: d00373225 <donglinbin1@huawei.com>

--- api/@ohos.measure.d.ts
+++ api/@ohos.measure.d.ts
@@ -154,6 +154,7 @@ export interface MeasureOptions {
    *
    * @type { ?(number | string) }
    * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
    * @since 11
    */
   textIndent?: number | string;
@@ -163,6 +164,7 @@ export interface MeasureOptions {
    *
    * @type { ?WordBreak }
    * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
    * @since 11
    */
   wordBreak?: WordBreak;
