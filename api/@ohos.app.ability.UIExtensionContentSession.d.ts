c504fc59abbd50277a1495ca5273729c418c7291
xia <xialiangwei1@huawei.com>
2023-12-01 14:29:08 +0800

    增加系统API错误码
    Signed-off-by: xia <xialiangwei1@huawei.com>

--- api/@ohos.app.ability.UIExtensionContentSession.d.ts
+++ api/@ohos.app.ability.UIExtensionContentSession.d.ts
@@ -222,7 +222,7 @@ export default class UIExtensionContentSession {
    * @since 11
    */
   startAbilityAsCaller(want: Want, callback: AsyncCallback<void>): void;
-  
+
   /**
    * Starts a new ability using the original caller information. If the caller application is in foreground,
    * you can use this method to start ability; If the caller application is in the background,
@@ -291,7 +291,7 @@ export default class UIExtensionContentSession {
    * @StageModelOnly
    * @since 11
    */
-  startAbilityAsCaller(want: Want, options?: StartOptions): Promise<void>;  
+  startAbilityAsCaller(want: Want, options?: StartOptions): Promise<void>;
 
   /**
    * Starts an ability and returns the execution result when the ability is destroyed.
@@ -497,6 +497,7 @@ export default class UIExtensionContentSession {
    * @param { AbilityStartCallback } abilityStartCallback - Indicates the abilityStartCallback.
    * @param { AsyncCallback<void> } callback - The callback of startAbility.
    * @throws { BusinessError } 201 - The application does not have permission to call the interface.
+   * @throws { BusinessError } 202 - The application is not system-app, can not use system-api.
    * @throws { BusinessError } 401 - If the input parameter is not valid parameter.
    * @throws { BusinessError } 16000001 - The specified ability does not exist.
    * @throws { BusinessError } 16000002 - Incorrect ability type.
@@ -521,6 +522,7 @@ export default class UIExtensionContentSession {
    * @param { AbilityStartCallback } abilityStartCallback - Indicates the abilityStartCallback.
    * @returns { Promise<void> } The promise returned by the function.
    * @throws { BusinessError } 201 - The application does not have permission to call the interface.
+   * @throws { BusinessError } 202 - The application is not system-app, can not use system-api.
    * @throws { BusinessError } 401 - If the input parameter is not valid parameter.
    * @throws { BusinessError } 16000001 - The specified ability does not exist.
    * @throws { BusinessError } 16000002 - Incorrect ability type.
