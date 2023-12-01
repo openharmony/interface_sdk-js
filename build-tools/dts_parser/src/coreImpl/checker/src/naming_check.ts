a9d96f9d352ff192531a9722b55fcba39b0e447a
fanjiaojiao0729 <fanjiaojiao@huawei.com>
2023-12-01 15:42:53 +0800

    api check工具重构代码-功能代码
    Signed-off-by: fanjiaojiao0729 <fanjiaojiao@huawei.com>

---
+++ build-tools/dts_parser/src/coreImpl/checker/src/naming_check.ts
@@ -0,0 +1,164 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {
+  ErrorTagFormat,
+  NameDictionaryType,
+  NameScenarioType,
+  ErrorMessage,
+} from '../../../typedef/checker/result_type';
+import { ApiInfo } from '../../../typedef/parser/ApiInfoDefination';
+import { CommonFunctions } from '../../../utils/checkUtils';
+const nameDictionary = require('../config/name_dictionary.json');
+const nameScenarioScope = require('../config/name_scenario_scope.json');
+
+export class ApiNamingCheck {
+  /**
+   * Check if the Api name is correct.
+   * @param { Comment.JsDocInfo } apiJsdoc -api jsdoc all infomation
+   * @returns { boolean }
+   */
+  static namingCheck(singleApi: ApiInfo): ErrorTagFormat {
+    const tagNameCheckResult: ErrorTagFormat = {
+      state: true,
+      errorInfo: '',
+    };
+    const apiVersionToBeVerified: number = CommonFunctions.getCheckApiVersion();
+    const lowIdentifier: string = singleApi.getDefinedText().toLowerCase();
+    ApiNamingCheck.checkApiNamingWords(lowIdentifier, tagNameCheckResult);
+    ApiNamingCheck.checkApiNamingScenario(lowIdentifier, tagNameCheckResult, singleApi);
+    console.log(apiVersionToBeVerified);
+    return tagNameCheckResult;
+  }
+
+  /**
+   *
+   * @param { string } lowIdentifier
+   * @param { ErrorTagFormat } tagNameCheckResult
+   */
+  static checkApiNamingWords(lowIdentifier: string, tagNameCheckResult: ErrorTagFormat): void {
+    const lowercaseNamingMap: Map<string, NameDictionaryType> = ApiNamingCheck.getlowercaseNamingMap();
+    for (const [key, value] of lowercaseNamingMap) {
+      const prohibitedWordIndex: number = lowIdentifier.indexOf(key);
+      if (prohibitedWordIndex === -1) {
+        continue;
+      }
+      const lowercaseIgnoreWordArr: string[] = value.ignore.map((word: string) => word.toLowerCase());
+      const internalWord: string = lowIdentifier.substr(prohibitedWordIndex, key.length);
+      if (lowercaseIgnoreWordArr.length === 0) {
+        tagNameCheckResult.state = false;
+        tagNameCheckResult.errorInfo = CommonFunctions.createErrorInfo(ErrorMessage.ERROR_NAMING, [
+          lowIdentifier,
+          internalWord,
+          value.suggestion,
+        ]);
+        break;
+      } else {
+        const isIgnoreWord: boolean = ApiNamingCheck.checkIgnoreWord(lowercaseIgnoreWordArr, lowIdentifier);
+        if (isIgnoreWord === false) {
+          tagNameCheckResult.state = false;
+          tagNameCheckResult.errorInfo = CommonFunctions.createErrorInfo(ErrorMessage.ERROR_NAMING, [
+            lowIdentifier,
+            internalWord,
+            value.suggestion,
+          ]);
+        }
+      }
+    }
+  }
+
+  /**
+   *
+   * @param {string} lowIdentifier
+   * @param {ErrorTagFormat} tagNameCheckResult
+   * @param {ApiInfo} singleApi
+   */
+  static checkApiNamingScenario(lowIdentifier: string, tagNameCheckResult: ErrorTagFormat, singleApi: ApiInfo): void {
+    const lowercaseNamingScenarioMap = ApiNamingCheck.getlowercaseNamingScenarioMap();
+    for (const [key, value] of lowercaseNamingScenarioMap) {
+      const prohibitedWordIndex: number = lowIdentifier.indexOf(key);
+      if (prohibitedWordIndex !== -1 && !ApiNamingCheck.isInAllowedFiles(value.files, singleApi.getFilePath())) {
+        const internalWord = lowIdentifier.substr(prohibitedWordIndex, key.length);
+        tagNameCheckResult.state = false;
+        tagNameCheckResult.errorInfo = CommonFunctions.createErrorInfo(ErrorMessage.ERROR_SCENARIO, [
+          lowIdentifier,
+          internalWord,
+          singleApi.getFilePath(),
+        ]);
+      }
+    }
+  }
+
+  /**
+   *
+   * @returns
+   */
+  static getlowercaseNamingMap(): Map<string, NameDictionaryType> {
+    const lowercaseNamingMap: Map<string, NameDictionaryType> = new Map();
+    for (const item of nameDictionary) {
+      const namingKey: string = item.badWord.toLowerCase();
+      const namingValue: NameDictionaryType = item;
+      lowercaseNamingMap.set(namingKey, namingValue);
+    }
+    return lowercaseNamingMap;
+  }
+
+  /**
+   *
+   * @param { string[] } lowercaseIgnoreWordArr
+   * @param { string }lowIdentifier
+   * @returns { boolean }
+   */
+  static checkIgnoreWord(lowercaseIgnoreWordArr: string[], lowIdentifier: string): boolean {
+    let isIgnoreWord: boolean = false;
+    for (let i = 0; i < lowercaseIgnoreWordArr.length; i++) {
+      if (lowercaseIgnoreWordArr[i] && lowIdentifier.indexOf(lowercaseIgnoreWordArr[i]) !== -1) {
+        isIgnoreWord = true;
+        break;
+      }
+    }
+    return isIgnoreWord;
+  }
+
+  /**
+   *
+   * @returns { Map<string, NameScenarioType> }
+   */
+  static getlowercaseNamingScenarioMap(): Map<string, NameScenarioType> {
+    const lowercaseNamingScenarioMap: Map<string, NameScenarioType> = new Map();
+    for (const item of nameScenarioScope) {
+      const scenarioKey = item.word.toLowerCase();
+      const scenarioValue: NameScenarioType = item;
+      lowercaseNamingScenarioMap.set(scenarioKey, scenarioValue);
+    }
+    return lowercaseNamingScenarioMap;
+  }
+
+  /**
+   *
+   * @param { string[] } files
+   * @param { string } fileName
+   * @returns { boolean }
+   */
+  static isInAllowedFiles(files: string[], fileName: string): boolean {
+    for (const item of files) {
+      const pattern: RegExp = new RegExp(item);
+      pattern.test(fileName);
+      if (pattern.test(fileName)) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
---
+++ build-tools/dts_parser/src/coreImpl/checker/src/tag_legality_check.ts
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import ts from 'typescript';
+import { ApiInfo, ApiType, ClassInfo, MethodInfo } from '../../../typedef/parser/ApiInfoDefination';
+import {
+  tagsArrayOfOrder,
+  optionalTags,
+  apiLegalityCheckTypeMap,
+  permissionOptionalTags,
+} from '../../../utils/checkUtils';
+import { Comment } from '../../../typedef/parser/Comment';
+import { ErrorTagFormat, ErrorMessage } from '../../../typedef/checker/result_type';
+import { CommonFunctions, conditionalOptionalTags } from '../../../utils/checkUtils';
+
+export class LegalityCheck {
+  /**
+   * Tag's legality check
+   * @param { ApiInfo } singleApi -Individual node information.
+   * @param { Comment.JsDocInfo } apiJsdoc -Individual node JsDoc.
+   * @returns { ErrorTagFormat[] }
+   */
+  static apiLegalityCheck(singleApi: ApiInfo, apiJsdoc: Comment.JsDocInfo): ErrorTagFormat[] {
+    const apiLegalityCheckResult: ErrorTagFormat[] = [];
+    const nodeInfo: ts.Node = singleApi.getNode() as ts.Node;
+    const apiLegalityTagsArray: string[] = apiLegalityCheckTypeMap.get(nodeInfo.kind) as string[];
+
+    // 判断api的jsdoc中是否存在非法标签，是否缺失必选标签
+    if (Array.isArray(apiLegalityTagsArray)) {
+      const apiLegalityTagsSet: Set<string> = new Set(apiLegalityTagsArray);
+      const illegalTagsArray = LegalityCheck.getIllegalTagsArray(apiLegalityTagsArray);
+      const apiTags: Comment.CommentTag[] = apiJsdoc.tags as Comment.CommentTag[];
+      let paramTagNumber: number = 0;
+      let paramApiNumber: number =
+        singleApi.getApiType() === ApiType.METHOD ? (singleApi as MethodInfo).getParams().length : 0;
+      apiTags.forEach((apiTag) => {
+        paramTagNumber = apiTag.tag === 'param' ? paramTagNumber + 1 : paramTagNumber;
+        const isUseinsteadLegalSituation: boolean = apiTag.tag === 'useinstead' && apiJsdoc.deprecatedVersion !== '-1';
+        if (illegalTagsArray.includes(apiTag.tag)) {
+          if (apiTag.tag !== 'useinstead' || !isUseinsteadLegalSituation) {
+            const apiRedundantResultFormat: ErrorTagFormat = {
+              state: false,
+              errorInfo: CommonFunctions.createErrorInfo(ErrorMessage.ERROR_USE, [apiTag.tag]),
+            };
+            apiLegalityCheckResult.push(apiRedundantResultFormat);
+          }
+        }
+        apiLegalityTagsSet.delete('param');
+        if (apiLegalityTagsSet.has(apiTag.tag)) {
+          apiLegalityTagsSet.delete(apiTag.tag);
+          if (singleApi.getApiType() === ApiType.INTERFACE) {
+            apiLegalityTagsSet.delete('typedef');
+          }
+        }
+      });
+      // param合法性单独进行校验
+      LegalityCheck.paramLegalityCheck(paramTagNumber, paramApiNumber, apiLegalityCheckResult);
+      // 缺失标签set合集
+      apiLegalityTagsSet.forEach((apiLegalityTag) => {
+        const isSyacapOptionalSituation: boolean =
+          apiLegalityTag === 'syscap' &&
+          (nodeInfo.kind === ts.SyntaxKind.ClassDeclaration || nodeInfo.kind === ts.SyntaxKind.ModuleDeclaration);
+        const isPermissionOptionalSituation: boolean =
+          apiLegalityTag === 'permission' && permissionOptionalTags.includes(nodeInfo.kind);
+        const isExtendsRequireSituation: boolean =
+          apiLegalityTag === 'extends' && (singleApi as ClassInfo).getParentClasses().length > 0;
+
+        if (
+          !conditionalOptionalTags.includes(apiLegalityTag) &&
+          (!isSyacapOptionalSituation || !isPermissionOptionalSituation || isExtendsRequireSituation)
+        ) {
+          const apiLostResultFormat: ErrorTagFormat = {
+            state: false,
+            errorInfo: CommonFunctions.createErrorInfo(ErrorMessage.ERROR_LOST_LABEL, [apiLegalityTag]),
+          };
+          apiLegalityCheckResult.push(apiLostResultFormat);
+        }
+      });
+    }
+    return apiLegalityCheckResult;
+  }
+
+  /**
+   * param tag  legality check
+   * @param { number } paramTagNumber
+   * @param { number } paramApiNumber
+   * @param { ErrorTagFormat[] } apiLegalityCheckResult
+   */
+  static paramLegalityCheck(
+    paramTagNumber: number,
+    paramApiNumber: number,
+    apiLegalityCheckResult: ErrorTagFormat[]
+  ): void {
+    if (paramTagNumber > paramApiNumber) {
+      const apiRedundantResultFormat: ErrorTagFormat = {
+        state: false,
+        errorInfo: CommonFunctions.createErrorInfo(ErrorMessage.ERROR_MORELABEL, [
+          JSON.stringify(paramTagNumber - paramApiNumber),
+          'param',
+        ]),
+      };
+      apiLegalityCheckResult.push(apiRedundantResultFormat);
+    } else if (paramTagNumber < paramApiNumber) {
+      const apiLostResultFormat: ErrorTagFormat = {
+        state: false,
+        errorInfo: CommonFunctions.createErrorInfo(ErrorMessage.ERROR_LOST_LABEL, ['param']),
+      };
+      apiLegalityCheckResult.push(apiLostResultFormat);
+    }
+  }
+
+  /**
+   * Gets all illegal tags for the api.
+   * @param { string[] } RequiredTagsArray
+   * @returns { string[] }
+   */
+  static getIllegalTagsArray(requiredTagsArray: string[]): string[] {
+    const illegalTagsArray: string[] = [];
+    tagsArrayOfOrder.forEach((tag) => {
+      if (!optionalTags.includes(tag) && !requiredTagsArray.includes(tag)) {
+        illegalTagsArray.push(tag);
+      }
+    });
+    return illegalTagsArray;
+  }
+}
