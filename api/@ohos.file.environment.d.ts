8590e08d88d9af68a5a781ab54f46dcf93fb6399
LVB8189 <lvbai1@huawei.com>
2023-12-01 09:11:05 +0800

    modify
    Signed-off-by: LVB8189 <lvbai1@huawei.com>

--- api/@ohos.file.environment.d.ts
+++ api/@ohos.file.environment.d.ts
@@ -20,7 +20,6 @@ import type { AsyncCallback } from './@ohos.base';
  *
  * @namespace Environment
  * @syscap SystemCapability.FileManagement.File.Environment
- * @systemapi
  * @since 8
  */
 declare namespace Environment {
