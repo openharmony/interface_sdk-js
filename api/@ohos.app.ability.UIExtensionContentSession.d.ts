03c182d3e209bfdd9d9f345d5ed53291de4848f8
xia <xialiangwei1@huawei.com>
2023-12-01 14:35:49 +0800

    增加系统API错误码
    Signed-off-by: xia <xialiangwei1@huawei.com>

--- api/@ohos.app.ability.UIExtensionContentSession.d.ts
+++ api/@ohos.app.ability.UIExtensionContentSession.d.ts
@@ -497,7 +497,6 @@ export default class UIExtensionContentSession {
    * @param { AbilityStartCallback } abilityStartCallback - Indicates the abilityStartCallback.
    * @param { AsyncCallback<void> } callback - The callback of startAbility.
    * @throws { BusinessError } 201 - The application does not have permission to call the interface.
-   * @throws { BusinessError } 202 - The application is not system-app, can not use system-api.
    * @throws { BusinessError } 401 - If the input parameter is not valid parameter.
    * @throws { BusinessError } 16000001 - The specified ability does not exist.
    * @throws { BusinessError } 16000002 - Incorrect ability type.
@@ -522,7 +521,6 @@ export default class UIExtensionContentSession {
    * @param { AbilityStartCallback } abilityStartCallback - Indicates the abilityStartCallback.
    * @returns { Promise<void> } The promise returned by the function.
    * @throws { BusinessError } 201 - The application does not have permission to call the interface.
-   * @throws { BusinessError } 202 - The application is not system-app, can not use system-api.
    * @throws { BusinessError } 401 - If the input parameter is not valid parameter.
    * @throws { BusinessError } 16000001 - The specified ability does not exist.
    * @throws { BusinessError } 16000002 - Incorrect ability type.
