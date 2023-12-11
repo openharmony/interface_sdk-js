fa5995f6f40ed3ea91cf382cce11efc238e5ab81
任倩 <renqian12@huawei.com>
2023-12-11 06:31:13 +0000

    update api/@ohos.data.preferences.d.ts.
    Signed-off-by: 任倩 <renqian12@huawei.com>

--- api/@ohos.data.preferences.d.ts
+++ api/@ohos.data.preferences.d.ts
@@ -1586,7 +1586,7 @@ declare namespace preferences {
      * @atomicservice
      * @since 11
      */
-    off(type: 'change', callback: Callback<string>): void;
+    off(type: 'change', callback?: Callback<string>): void;
 
     /**
      * Unregisters an existing observer.
@@ -1607,7 +1607,7 @@ declare namespace preferences {
      * @atomicservice
      * @since 11
      */
-    off(type: 'multiProcessChange', callback: Callback<string>): void;
+    off(type: 'multiProcessChange', callback?: Callback<string>): void;
   }
 }
 
