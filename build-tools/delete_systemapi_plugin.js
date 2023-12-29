dc0cdc8b35d86fc762a29e874ef31b7c393819a3
fanjiaojiao0729 <fanjiaojiao@huawei.com>
2023-12-29 15:56:06 +0800

    解决文件过滤导致的bug
    Signed-off-by: fanjiaojiao0729 <fanjiaojiao@huawei.com>

--- build-tools/delete_systemapi_plugin.js
+++ build-tools/delete_systemapi_plugin.js
@@ -202,11 +202,12 @@ function processFileNameWithoutExt(filePath) {
  */
 function tsTransform(utFiles, callback) {
   utFiles.forEach((url) => {
-    if (/\.json/.test(url) || /index\-full\.d\.ts/.test(url) || /common\.d\.ts/.test(url)) {
+    const apiBaseName = path.basename(url);
+    if (/\.json/.test(url) || apiBaseName === 'index-full.d.ts' || apiBaseName === 'common.d.ts') {
       // 特殊类型文件处理
       const content = fs.readFileSync(url, 'utf-8');
       writeFile(url, content);
-    } else if (/\.d\.ts/.test(url) || /\.d\.ets/.test(url)) {
+    } else if (/\.d\.ts/.test(apiBaseName) || /\.d\.ets/.test(apiBaseName)) {
       // dts文件处理
       let content = fs.readFileSync(url, 'utf-8'); // 文件内容
       const fileName = processFileName(url);
