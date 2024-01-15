877597eeb2eaf1cf93736ecc5f0ae4531ade0696
wuXin <wuxintao5@huawei.com>
2024-01-15 01:30:28 +0000

    update api/@ohos.security.huks.d.ts. 新增Attest_key 权限说明
    Signed-off-by: wuXin <wuxintao5@huawei.com>

--- api/@ohos.security.huks.d.ts
+++ api/@ohos.security.huks.d.ts
@@ -1206,6 +1206,7 @@ declare namespace huks {
   /**
    * Key Attestation.
    *
+   * @permission ohos.permission.ATTEST_KEY
    * @param { string } keyAlias - keyAlias indicates the key's name.
    * @param { HuksOptions } options - options indicates the properties of the key attestation operation.
    * @param { AsyncCallback<HuksReturnResult> } callback - the callback of attestKeyItem.
@@ -1229,6 +1230,7 @@ declare namespace huks {
   /**
    * Key Attestation.
    *
+   * @permission ohos.permission.ATTEST_KEY
    * @param { string } keyAlias - keyAlias indicates the key's name.
    * @param { HuksOptions } options - options indicates the properties of the key attestation operation.
    * @returns { Promise<HuksReturnResult> } the promise returned by the function.
