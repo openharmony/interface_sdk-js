dbe0f0ad0eafb667b73ee5877a0059f7126cbd88
liuxiyao223 <liuxiyao223@huawei.com>
2024-01-18 01:54:46 +0000

    add interface js kit
    Signed-off-by: liuxiyao223 <liuxiyao223@huawei.com>

--- api/@ohos.app.ability.VpnExtensionAbility.d.ts
+++ api/@ohos.app.ability.VpnExtensionAbility.d.ts
@@ -12,6 +12,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/**
+ * @file
+ * @kit NetworkKit
+ */
+
 import type VpnExtensionContext from './application/VpnExtensionContext';
 import type Want from './@ohos.app.ability.Want';
 
