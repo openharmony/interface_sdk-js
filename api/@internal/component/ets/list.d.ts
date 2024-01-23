5b159938e289c532fe01b35450f2300445ec945d
yeyinglong <yeyinglong@hisilicon.com>
2024-01-23 09:15:13 +0000

    fixed e9d2e77 from https://gitee.com/yeyinglong_admin/interface_sdk-js/pulls/8878 修复List lanes属性接口注释问题
    Signed-off-by: yeyinglong <yeyinglong@hisilicon.com>

--- api/@internal/component/ets/list.d.ts
+++ api/@internal/component/ets/list.d.ts
@@ -733,7 +733,6 @@ declare class ListAttribute extends ScrollableCommonMethod<ListAttribute> {
    * Called when need to decide how much lanes the list will show.
    *
    * @param { number | LengthConstrain } value
-   * @param { Dimension } gutter
    * @returns { ListAttribute }
    * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @since 9
