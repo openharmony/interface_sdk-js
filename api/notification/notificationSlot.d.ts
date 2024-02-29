787490bcdfbb7edc0576d00d2bd0c001079afd14
z30053788 <zhaobaoxin1@huawei.com>
2024-02-29 16:35:21 +0800

    5.0新增接口
    Signed-off-by: z30053788 <zhaobaoxin1@huawei.com>
    Change-Id: I8f3e1be61faa1c1d483e7a611ae4f8ee413c02a5

--- api/notification/notificationSlot.d.ts
+++ api/notification/notificationSlot.d.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License"),
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -153,4 +153,15 @@ export interface NotificationSlot {
    * @since 11
    */
   readonly reminderMode?: number;
+
+  /**
+   * Obtains channel information is authorized by the user.
+   *
+   * @type { ?number }
+   * @readonly
+   * @syscap SystemCapability.Notification.Notification
+   * @systemapi
+   * @since 12
+   */
+  readonly authorizedStatus?: number;
 }
--- api/notification/notificationSubscribeInfo.d.ts
+++ api/notification/notificationSubscribeInfo.d.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License"),
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -41,4 +41,14 @@ export interface NotificationSubscribeInfo {
    * @since 7
    */
   userId?: number;
+
+  /**
+   * Subscribing to Notifications Synchronized to Devices of a Specified Type.
+   *
+   * @type { ?string }
+   * @syscap SystemCapability.Notification.Notification
+   * @systemapi
+   * @since 12
+   */
+  deviceType?: string;
 }
