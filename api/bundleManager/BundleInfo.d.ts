5cd95b1cfada79a1503b42ae97c9d82a471dbcfc
zhaogan <zhaogan2@huawei.com>
2024-03-07 11:27:43 +0800

    navigation合并
    Signed-off-by: zhaogan <zhaogan2@huawei.com>

--- api/bundleManager/BundleInfo.d.ts
+++ api/bundleManager/BundleInfo.d.ts
@@ -14,7 +14,7 @@
  */
 
 import { ApplicationInfo } from './ApplicationInfo';
-import { HapModuleInfo } from './HapModuleInfo';
+import { HapModuleInfo, RouterItem } from './HapModuleInfo';
 import bundleManager from './../@ohos.bundle.bundleManager';
 
 /**
@@ -253,6 +253,17 @@ export interface BundleInfo {
    * @since 11
    */
   readonly updateTime: number;
+
+  /**
+   * Indicates the router information of the application
+   *
+   * @type { Array<RouterItem> }
+   * @readonly
+   * @syscap SystemCapability.BundleManager.BundleFramework.Core
+   * @atomicservice
+   * @since 12
+   */
+  readonly routerMap: Array<RouterItem>;
 }
 
 /**
