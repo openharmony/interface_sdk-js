6cfb2a4e92b95d183255a76735653dbf0ce6a6c2
zhaoleyi <zhaoleyi@huawei.com>
2024-03-15 10:43:52 +0800

    add preload UIExtension api
    Signed-off-by: zhaoleyi <zhaoleyi@huawei.com>
    Change-Id: I524c2a214d837fe1ef03473d3aa0759f17c55f16

--- api/application/ApplicationContext.d.ts
+++ api/application/ApplicationContext.d.ts
@@ -464,4 +464,25 @@ export default class ApplicationContext extends Context {
    * @since 12
    */
   restartApp(want: Want): void;
+  
+  /**
+   * Preload UIExtensionAbility.
+   *
+   * @permission ohos.permission.PRELOAD_UI_EXTENSION_ABILITY
+   * @param { Want } want - Indicates the want of target UIExtensionAbility.
+   * @returns { Promise<void> } The promise returned by the function.
+   * @throws { BusinessError } 201 - The application does not have permission to call the interface.
+   * @throws { BusinessError } 202 - The application is not system-app, can not use system-api.
+   * @throws { BusinessError } 401 - If the input parameter is not valid parameter.
+   * @throws { BusinessError } 16000001 - The specified ability does not exist.
+   * @throws { BusinessError } 16000002 - Incorrect ability type.
+   * @throws { BusinessError } 16000004 - Can not start invisible component.
+   * @throws { BusinessError } 16000011 - The context does not exist.
+   * @throws { BusinessError } 16000050 - Internal error.
+   * @syscap SystemCapability.Ability.AbilityRuntime.Core
+   * @systemapi
+   * @stagemodelonly
+   * @since 12
+   */
+  preloadUIExtensionAbility(want: Want): Promise<void>;
 }
