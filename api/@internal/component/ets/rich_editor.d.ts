f18f87d32582e1c962cdf3513efb170003a7edbd
AnBetter <wangguan27@huawei.com>
2024-03-19 12:22:18 +0800

    RichEditor的自定义键盘支持避让功能
    Signed-off-by: AnBetter <wangguan27@huawei.com>

--- api/@internal/component/ets/rich_editor.d.ts
+++ api/@internal/component/ets/rich_editor.d.ts
@@ -1531,6 +1531,26 @@ declare interface RichEditorTextSpanOptions {
 }
 
 /**
+ * Defines the Keyboard options of RichEditor.
+ *
+ * @interface KeyboardOptions
+ * @syscap SystemCapability.ArkUI.ArkUI.Full
+ * @crossplatform
+ * @since 12
+ */
+declare interface KeyboardOptions {
+  /**
+   * Indicates whether to support keyboard avoidance.
+   *
+   * @type { ?boolean }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @since 12
+   */
+  supportAvoidance?: boolean;
+}
+
+/**
  * Defines the selection options of RichEditor.
  *
  * @interface SelectionOptions
@@ -2600,7 +2620,18 @@ declare class RichEditorAttribute extends CommonMethod<RichEditorAttribute> {
    * @atomicservice
    * @since 11
    */
-  customKeyboard(value: CustomBuilder): RichEditorAttribute;
+  /**
+   * Define custom keyboard.
+   *
+   * @param { CustomBuilder } value - Set up a custom keyboard of RichEditor
+   * @param { KeyboardOptions } [options] - Indicates the Keyboard options of RichEditor
+   * @returns { RichEditorAttribute } returns the instance of the RichEditorAttribute.
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 12
+   */
+  customKeyboard(value: CustomBuilder, options?: KeyboardOptions): RichEditorAttribute;
 
   /**
     * Defines onPaste callback.
