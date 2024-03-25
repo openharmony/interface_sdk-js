dee11451a7961b642ef72800e3a2c7d72a9d5f02
openharmony_ci <120357966@qq.com>
2024-03-25 02:56:04 +0000

    fixed 7a631b7 from https://gitee.com/openharmony_ci/interface_sdk-js/pulls/10198 回退 'Pull Request !10095 : Support arkts directory in sdk'

--- kits/@kit.ArkTS.d.ts
+++ kits/@kit.ArkTS.d.ts
@@ -46,14 +46,11 @@ import worker, {
 } from '@ohos.worker';
 import xml from '@ohos.xml';
 import JSON from '@ohos.util.json';
-import lang from '@arkts.lang';
-import ArkTSUtils from '@arkts.utils';
-import collections from '@arkts.collections';
 
 export {
   ArrayList, convertxml, DedicatedWorkerGlobalScope, Deque, ErrorEvent, Event, EventListener,
   EventTarget, HashMap, HashSet, LightWeightMap, LightWeightSet, LinkedList, List, MessageEvent,
   MessageEvents, PlainArray, PostMessageOptions, Queue, Stack, ThreadWorkerGlobalScope, TreeMap,
   TreeSet, Vector, WorkerEventListener, WorkerEventTarget, WorkerOptions, buffer, process, taskpool,
-  uri, url, util, worker, xml, JSON, lang, ArkTSUtils, collections
+  uri, url, util, worker, xml, JSON
 };
