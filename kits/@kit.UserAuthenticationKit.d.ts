cea8e2b3053e320bf1685d191120c6cc725ed8e3
李蜜 <limi24@huawei.com>
2024-03-30 16:22:24 +0800

    add user auth icon interface
    Signed-off-by: 李蜜 <limi24@huawei.com>

--- kits/@kit.UserAuthenticationKit.d.ts
+++ kits/@kit.UserAuthenticationKit.d.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -20,5 +20,6 @@
 
 import faceAuth from '@ohos.userIAM.faceAuth';
 import userAuth from '@ohos.userIAM.userAuth';
+import userAuthIcon from '@ohos.userIAM.userAuthIcon';
 
-export { faceAuth, userAuth };
+export { faceAuth, userAuth, userAuthIcon };
