27cba4f379c8a18087df6dc52bfeac1743065f8e
xuqinfeng1@huawei.com <xuqinfeng1@huawei.com>
2024-04-03 11:49:58 +0800

    nextMargin与prevMargin添加ignoreBlank参数
    Signed-off-by: xuqinfeng1@huawei.com <xuqinfeng1@huawei.com>
    Change-Id: I4a748e760a5539c7971517463c93a773a855cdb0

--- api/@internal/component/ets/swiper.d.ts
+++ api/@internal/component/ets/swiper.d.ts
@@ -1726,7 +1726,17 @@ declare class SwiperAttribute extends CommonMethod<SwiperAttribute> {
    * @atomicservice
    * @since 11
    */
-  prevMargin(value: Length): SwiperAttribute;
+  /**
+   * When the previous item is empty, do not display blank space.
+   *
+   * @param { boolean } ignoreBlank - Ignoring blank space at the end of the page.
+   * @returns { SwiperAttribute } The attribute of the swiper.
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 12
+   */
+  prevMargin(value: Length, ignoreBlank?: boolean): SwiperAttribute;
 
   /**
    * The next margin which can be used to expose a small portion of the latter item.
@@ -1746,7 +1756,17 @@ declare class SwiperAttribute extends CommonMethod<SwiperAttribute> {
    * @atomicservice
    * @since 11
    */
-  nextMargin(value: Length): SwiperAttribute;
+  /**
+   * When the next item is empty, do not display blank space.
+   *
+   * @param { boolean } ignoreBlank - Ignoring blank space on homepage.
+   * @returns { SwiperAttribute } The attribute of the swiper.
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 12
+   */
+  nextMargin(value: Length, ignoreBlank?: boolean): SwiperAttribute;
 
   /**
    * Called when the swiper animation start.
