85611307282b5d697c060ece596e98ccd8e8e63b
lixiangpeng5 <lixiangpeng5@huawei.com>
2024-04-18 14:58:50 +0000

    add vibrator effect and parameters
    Signed-off-by: lixiangpeng5 <lixiangpeng5@huawei.com>
    Change-Id: Id61c8bcb19ef77e1fc44cab7ed1e404c1a83b9fa

--- api/@ohos.vibrator.d.ts
+++ api/@ohos.vibrator.d.ts
@@ -286,6 +286,39 @@ declare namespace vibrator {
   }
 
   /**
+   * Simple and universal vibration effects.
+   * 
+   * @enum { string }
+   * @syscap SystemCapability.Sensors.MiscDevice
+   * @since 12
+   */
+  enum HapticFeedback {
+    /**
+     * Describes the soft vibration effect of the vibrator.
+     *
+     * @syscap SystemCapability.Sensors.MiscDevice
+     * @since 12
+     */
+    EFFECT_SOFT = 'haptic.effect.soft',
+
+    /**
+     * Describes the hard vibration effect of the vibrator.
+     *
+     * @syscap SystemCapability.Sensors.MiscDevice
+     * @since 12
+     */
+    EFFECT_HARD = 'haptic.effect.hard',
+
+    /**
+     * Describes the sharp vibration effect of the vibrator.
+     *
+     * @syscap SystemCapability.Sensors.MiscDevice
+     * @since 12
+     */
+    EFFECT_SHARP = 'haptic.effect.sharp'
+  }
+
+  /**
    * Vibrator vibration stop mode.
    *
    * @enum { string }
@@ -473,7 +506,23 @@ declare namespace vibrator {
      * @syscap SystemCapability.Sensors.MiscDevice
      * @since 9
      */
-    count: number;
+    /**
+     * The number of vibration repetitions.
+     *
+     * @type { ?number }
+     * @syscap SystemCapability.Sensors.MiscDevice
+     * @since 12
+     */
+    count?: number;
+
+    /**
+     * The intensity of vibration effect.
+     *
+     * @type { ?number }
+     * @syscap SystemCapability.Sensors.MiscDevice
+     * @since 12
+     */
+    intensity?: number;
   }
 
   /**
