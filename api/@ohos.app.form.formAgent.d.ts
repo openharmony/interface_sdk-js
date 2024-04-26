7089f0d29561c42b38673e6d1e4d88c23652824c
jiangzhijun7@huawei.com <jiangzhijun7@huawei.com>
2024-04-26 16:41:09 +0800

    【基础能力】卡片加桌结果返回
    Issue:https://gitee.com/openharmony/interface_sdk-js/issues/I91YJJ
    
    Signed-off-by: jiangzhijun7@huawei.com <jiangzhijun7@huawei.com>

--- api/@ohos.app.form.formAgent.d.ts
+++ api/@ohos.app.form.formAgent.d.ts
@@ -46,6 +46,22 @@ declare namespace formAgent {
    * @systemapi
    * @since 11
    */
+  /**
+   * Request to publish a form to the form host.
+   *
+   * @permission ohos.permission.AGENT_REQUIRE_FORM
+   * @param { Want } want - The want of the form to publish.
+   * @param { AsyncCallback<string> } callback - The callback is used to return the form id.
+   * @throws { BusinessError } 202 - The application is not a system application.
+   * @throws { BusinessError } 401 - If the input parameter is not valid parameter.
+   * @throws { BusinessError } 16500050 - An IPC connection error happened.
+   * @throws { BusinessError } 16500100 - Failed to obtain the configuration information.
+   * @throws { BusinessError } 16501000 - An internal functional error occurred.
+   * @throws { BusinessError } 16501008 - Waiting for the form addition to the desktop timeout.
+   * @syscap SystemCapability.Ability.Form
+   * @systemapi
+   * @since 12
+   */
   function requestPublishForm(want: Want, callback: AsyncCallback<string>): void;
 
   /**
@@ -62,6 +78,22 @@ declare namespace formAgent {
    * @syscap SystemCapability.Ability.Form
    * @systemapi
    * @since 11
+   */
+  /**
+   * Request to publish a form to the form host.
+   *
+   * @permission ohos.permission.AGENT_REQUIRE_FORM
+   * @param { Want } want - The want of the form to publish.
+   * @returns { Promise<string> } Returns the form id.
+   * @throws { BusinessError } 202 - The application is not a system application.
+   * @throws { BusinessError } 401 - If the input parameter is not valid parameter.
+   * @throws { BusinessError } 16500050 - An IPC connection error happened.
+   * @throws { BusinessError } 16500100 - Failed to obtain the configuration information.
+   * @throws { BusinessError } 16501000 - An internal functional error occurred.
+   * @throws { BusinessError } 16501008 - Waiting for the form addition to the desktop timeout.
+   * @syscap SystemCapability.Ability.Form
+   * @systemapi
+   * @since 12
    */
   function requestPublishForm(want: Want): Promise<string>;
 }
