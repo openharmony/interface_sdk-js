3e6148db23dc86d22d1938c1036f73fee1fb8eda
hobbycao <caohui10@huawei.com>
2024-04-28 16:22:31 +0800

    feat: add dfs interface
    Signed-off-by: hobbycao <caohui10@huawei.com>

--- api/@ohos.file.fs.d.ts
+++ api/@ohos.file.fs.d.ts
@@ -113,9 +113,12 @@ declare namespace fileIo {
   export { Stream };
   export { Watcher };
   export { WhenceType };
+  export { connectDfs };
+  export { disconnectDfs };
   export type { Progress };
   export type { CopyOptions };
   export type { ProgressListener };
+  export type { DfsListeners };
 
   /**
    * Mode Indicates the open flags.
@@ -5741,6 +5744,33 @@ declare function writeSync(
 ): number;
 
 /**
+ * Connect Distributed File System.
+ *
+ * @param { string } networkId - The networkId of device.
+ * @param { DfsListeners } listeners - The listeners of Distributed File System.
+ * @returns { Promise<void> } The promise returned by the function.
+ * @throws { BusinessError } 201 - Permission denied.
+ * @throws { BusinessError } 401 - The parameter check failed.
+ * @throws { BusinessError } 13900045 - Connection failed.
+ * @throws { BusinessError } 13900046 - Software caused connection abort.
+ * @syscap SystemCapability.FileManagement.File.FileIO
+ * @since 12
+ */
+declare function connectDfs(networkId: string, listeners: DfsListeners): Promise<void>;
+
+/**
+ * Disconnect Distributed File System.
+ *
+ * @param { string } networkId - The networkId of device.
+ * @returns { Promise<void> } The promise returned by the function.
+ * @throws { BusinessError } 201 - Permission denied.
+ * @throws { BusinessError } 401 - The parameter check failed.
+ * @syscap SystemCapability.FileManagement.File.FileIO
+ * @since 12
+ */
+declare function disconnectDfs(networkId: string): Promise<void>;
+
+/**
  * Progress data of copyFile
  *
  * @typedef Progress
@@ -7585,6 +7615,25 @@ export interface ListFileOptions {
    * @since 11
    */
   filter?: Filter;
+}
+
+/**
+ * The listeners of Distributed File System.
+ *
+ * @typedef DfsListeners
+ * @syscap SystemCapability.FileManagement.File.FileIO
+ * @since 12
+ */
+interface DfsListeners {
+  /**
+   * The Listener of Distributed File System status
+   *
+   * @param { string } networkId - The networkId of device.
+   * @param { number } status - The status code of Distributed File System.
+   * @syscap SystemCapability.FileManagement.File.FileIO
+   * @since 12
+   */
+  onStatus(networkId: string, status: number): void;
 }
 
 /**
