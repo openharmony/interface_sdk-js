e9b0422624a874ea8cfa64f4d37626195a6b8141
ma-shaoyin <mashaoyin1@huawei.com>
2024-04-28 19:28:25 +0800

    Signed-off-by: ma-shaoyin <mashaoyin1@huawei.com>
     Changes to be committed:

--- api/@ohos.InputMethodExtensionContext.d.ts
+++ api/@ohos.InputMethodExtensionContext.d.ts
@@ -95,6 +95,7 @@ export default class InputMethodExtensionContext extends ExtensionContext {
    * @throws { BusinessError } 16000050 - Internal error.
    * @throws { BusinessError } 16000053 - The ability is not on the top of the UI.
    * @throws { BusinessError } 16000055 - Installation-free timed out.
+   * @throws { BusinessError } 16000061 - Can not start component belongs to other bundle.
    * @throws { BusinessError } 16200001 - The caller has been released.
    * @syscap SystemCapability.MiscServices.InputMethodFramework
    * @StageModelOnly
