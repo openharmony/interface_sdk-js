29d73bb8f33ce127c51b89984b3b1225a5d10594
瓜瓜地雷 <zhangzhi80@huawei.com>
2024-05-09 11:49:44 +0800

    支持模态窗可移动缩放
    Signed-off-by: 瓜瓜地雷 <zhangzhi80@huawei.com>

--- api/@ohos.window.d.ts
+++ api/@ohos.window.d.ts
@@ -1597,6 +1597,23 @@ declare namespace window {
      * @since 9
      */
     parentId?: number;
+
+    /**
+     * Indicates whether enable window decor, only support dialog, The default value is false.
+     *
+     * @type { ?boolean }
+     * @syscap SystemCapability.Window.SessionManager
+     * @since 12
+     */
+    decorEnabled?: boolean;
+
+    /**
+     * Indicates dialog window title when decor enabled.
+     *
+     * @syscap SystemCapability.Window.SessionManager
+     * @since 12
+     */
+    title?: string;
   }
 
   /**
