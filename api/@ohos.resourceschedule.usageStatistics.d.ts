4c3012786338e6b05ae81944cb49d66161ab03b7
fengyang <fengyang44@huawei.com>
2024-05-09 11:19:12 +0800

    告警清理
    Signed-off-by: fengyang <fengyang44@huawei.com>

--- api/@ohos.resourceschedule.usageStatistics.d.ts
+++ api/@ohos.resourceschedule.usageStatistics.d.ts
@@ -680,6 +680,7 @@ declare namespace usageStatistics {
   function queryAppGroupSync(bundleName: string): number;
 
   /**
+   * @typedef BundleStatsMap
    * @syscap SystemCapability.ResourceSchedule.UsageStatistics.App
    * @systemapi Hide this for inner system use.
    * @since 9
