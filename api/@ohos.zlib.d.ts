008e79138cb59fc516228efa5dcc91858aabd306
junyi233 <zhengjun29@huawei.com>
2024-05-15 12:57:04 +0800

    update
    Signed-off-by: junyi233 <zhengjun29@huawei.com>

--- api/@ohos.zlib.d.ts
+++ api/@ohos.zlib.d.ts
@@ -1225,7 +1225,7 @@ declare namespace zlib {
      * @param { number } adler - Initial value of Adler32 checksum.
      * @param { ArrayBuffer } buf - Calculate checksum data buffer.
      * @returns { Promise<number> } Return the updated checksum.
-     * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified; 2. Incorrect parameter types. Possible causes: 1. Mandatory parameters are left unspecified;
+     * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified;
      * 2. Incorrect parameter types; 3. Parameter verification failed.
      * @syscap SystemCapability.BundleManager.Zlib
      * @atomicservice
@@ -1240,7 +1240,7 @@ declare namespace zlib {
      * @param { number } adler2 - The second Adler32 checksum.
      * @param { number } len2 - The length of the data block associated with the second Adler32 checksum.
      * @returns { Promise<number> } Returns the Adler-32 checksum.
-     * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified; 2. Incorrect parameter types. Possible causes: 1. Mandatory parameters are left unspecified;
+     * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified;
      * 2. Incorrect parameter types; 3. Parameter verification failed.
      * @syscap SystemCapability.BundleManager.Zlib
      * @atomicservice
@@ -1254,7 +1254,7 @@ declare namespace zlib {
      * @param { number } crc - Initial value of CRC-32 checksum.
      * @param { ArrayBuffer } buf - Calculate checksum data buffer.
      * @returns { Promise<number> } Return the updated CRC-32.
-     * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified; 2. Incorrect parameter types. Possible causes: 1. Mandatory parameters are left unspecified;
+     * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified;
      * 2. Incorrect parameter types; 3. Parameter verification failed.
      * @syscap SystemCapability.BundleManager.Zlib
      * @atomicservice
@@ -1269,7 +1269,7 @@ declare namespace zlib {
      * @param { number } crc2 - The second CRC-32 checksum.
      * @param { number } len2 - The length of the data block associated with the second CRC-32 checksum.
      * @returns { Promise<number> } Returns the CRC-32 check value.
-     * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified; 2. Incorrect parameter types. Possible causes: 1. Mandatory parameters are left unspecified;
+     * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified;
      * 2. Incorrect parameter types; 3. Parameter verification failed.
      * @syscap SystemCapability.BundleManager.Zlib
      * @atomicservice
@@ -1283,7 +1283,7 @@ declare namespace zlib {
      * @param { number } crc - Initial value of CRC-64 checksum.
      * @param { ArrayBuffer } buf - Calculate checksum data buffer.
      * @returns { Promise<number> } Return the updated CRC-64.
-     * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified; 2. Incorrect parameter types. Possible causes: 1. Mandatory parameters are left unspecified;
+     * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified;
      * 2. Incorrect parameter types; 3. Parameter verification failed.
      * @syscap SystemCapability.BundleManager.Zlib
      * @atomicservice
@@ -1816,7 +1816,8 @@ declare namespace zlib {
      * @atomicservice
      * @since 12
      */
-    deflateInit2(strm: ZStream, level: CompressLevel, method: CompressMethod, windowBits: number, memLevel: MemLevel, strategy: CompressStrategy): Promise<ReturnStatus>;
+    deflateInit2(strm: ZStream, level: CompressLevel, method: CompressMethod, windowBits: number,
+        memLevel: MemLevel, strategy: CompressStrategy): Promise<ReturnStatus>;
 
     /**
      * Compresses as much data as possible, and stops when the input buffer becomes empty or the output buffer becomes full.
