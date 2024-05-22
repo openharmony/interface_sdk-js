50e4e51e1d06a5a3cd02c9b78b5faa0daed64d5a
jiangdayuan <jiangdayuan@huawei.com>
2024-05-22 17:57:11 +0800

    add interface of navigation
    Signed-off-by: jiangdayuan <jiangdayuan@huawei.com>
    Change-Id: Ib26693d360ac441d2e9ec11d75ba61f5b5d42da2

--- api/@internal/component/ets/navigation.d.ts
+++ api/@internal/component/ets/navigation.d.ts
@@ -2498,6 +2498,17 @@ declare interface NavigationAnimatedTransition {
   timeout?: number;
 
   /**
+   * Indicates whether it is an interactive transition.
+   *
+   * @type { ?boolean }
+   * @default false
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @since 12
+   */
+  isInteractive?: boolean;
+
+  /**
    * Configure the animations associated with custom transition.
    *
    * @type { function }
@@ -2574,6 +2585,17 @@ declare interface NavigationTransitionProxy {
   to: NavContentInfo;
 
   /**
+   * Indicates whether it is an interactive transition.
+   *
+   * @type { ?boolean }
+   * @default false
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @since 12
+   */
+  isInteractive?: boolean;
+
+  /**
    * Notification system transition animation completed.
    *
    * @syscap SystemCapability.ArkUI.ArkUI.Full
@@ -2589,6 +2611,15 @@ declare interface NavigationTransitionProxy {
    * @since 12
    */
   finishTransition(): void;
+
+  /**
+   * Notification system transition animation canceled.
+   *
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @since 12
+   */
+  cancelTransition?(): void;
 }
 
 /**
@@ -2665,6 +2696,16 @@ declare interface NavContentInfo {
    * @since 12
    */
   mode?: NavDestinationMode;
+
+  /**
+   * Navigation content param.
+   *
+   * @type { ?Object }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @since 12
+   */
+  param?: Object;
 }
 
 /**
