303b832f62b33e750832642c04287b246844774b
xqq879988 <xieqiqi2@huawei.com>
2024-05-23 20:51:08 +0800

    新增线性导航条组件LinearIndicator
    Signed-off-by: xqq879988 <xieqiqi2@huawei.com>

--- api/@internal/component/ets/index-full.d.ts
+++ api/@internal/component/ets/index-full.d.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -147,3 +147,4 @@
 /// <reference path="./embedded_component.d.ts" />
 /// <reference path="./styled_string.d.ts" />
 /// <reference path="./content_slot.d.ts" />
+/// <reference path="./linearindicator.d.ts" />
