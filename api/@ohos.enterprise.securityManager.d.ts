2a3a125fe96836becbabfbbdb16601c4b6926ca3
h30034439 <hanyiyichn@163.com>
2024-05-24 16:40:26 +0800

    部分接口回退
    Signed-off-by: h30034439 <hanyiyichn@163.com>

--- api/@ohos.enterprise.securityManager.d.ts
+++ api/@ohos.enterprise.securityManager.d.ts
@@ -222,40 +222,6 @@ declare namespace securityManager {
   function getPasswordPolicy(admin: Want): PasswordPolicy;
 
   /**
-   * Sets the application's clipboard policy of the device.
-   * 
-   * @permission ohos.permission.ENTERPRISE_MANAGE_SECURITY
-   * @param { Want } admin - admin indicates the administrator ability information.
-   * @param { number } tokenId - tokenId indicates the token id of the application.
-   * @param { ClipboardPolicy } policy - clipboard policy to be set.
-   * @throws { BusinessError } 9200001 - The application is not an administrator application of the device.
-   * @throws { BusinessError } 9200002 - the administrator application does not have permission to manage the device.
-   * @throws { BusinessError } 201 - Permission verification failed. The application does not have the permission required to call the API.
-   * @throws { BusinessError } 401 - invalid input parameter.
-   * @syscap SystemCapability.Customization.EnterpriseDeviceManager
-   * @stagemodelonly
-   * @since 12
-   */
-  function setAppClipboardPolicy(admin: Want, tokenId: number, policy: ClipboardPolicy): void;
-
-  /**
-   * Gets the application's clipboard policy of the device.
-   * 
-   * @permission ohos.permission.ENTERPRISE_MANAGE_SECURITY
-   * @param { Want } admin - admin indicates the administrator ability information.
-   * @param { number } [tokenId] - tokenId indicates the token id of the application.
-   * @returns { string } the json string of clipboard policy for each application of the device.
-   * @throws { BusinessError } 9200001 - The application is not an administrator application of the device.
-   * @throws { BusinessError } 9200002 - the administrator application does not have permission to manage the device.
-   * @throws { BusinessError } 201 - Permission verification failed. The application does not have the permission required to call the API.
-   * @throws { BusinessError } 401 - invalid input parameter.
-   * @syscap SystemCapability.Customization.EnterpriseDeviceManager
-   * @stagemodelonly
-   * @since 12
-   */
-  function getAppClipboardPolicy(admin: Want, tokenId?: number): string;
-
-  /**
    * Password policy.
    * 
    * @typedef PasswordPolicy
@@ -293,52 +259,6 @@ declare namespace securityManager {
      * @since 12
      */
     additionalDescription?: string;
-  }
-
-  /**
-   * Clipboard policy.
-   * 
-   * @enum { number } ClipboardPolicy
-   * @syscap SystemCapability.Customization.EnterpriseDeviceManager
-   * @stagemodelonly
-   * @since 12
-   */
-  export enum ClipboardPolicy {
-    /**
-     * Policy default
-     * 
-     * @syscap SystemCapability.Customization.EnterpriseDeviceManager
-     * @stagemodelonly
-     * @since 12
-     */
-    DEFAULT = 0,
-
-    /**
-     * Policy indicates that the clipboard can be used on the same application
-     * 
-     * @syscap SystemCapability.Customization.EnterpriseDeviceManager
-     * @stagemodelonly
-     * @since 12
-     */
-    IN_APP = 1,
-
-    /**
-     * Policy indicates that the clipboard can be used on the same device
-     * 
-     * @syscap SystemCapability.Customization.EnterpriseDeviceManager
-     * @stagemodelonly
-     * @since 12
-     */
-    LOCAL_DEVICE = 2,
-
-    /**
-     * Policy indicates that the clipboard can be used across device
-     * 
-     * @syscap SystemCapability.Customization.EnterpriseDeviceManager
-     * @stagemodelonly
-     * @since 12
-     */
-    CROSS_DEVICE = 3,
   }
 }
 
