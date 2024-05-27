0f040d5ba11f654c026a4abdd305f666ae8517ef
duao oh <duao@huawei.com>
2024-05-27 16:27:12 +0800

    formKit interface 401错误码描述整改
    Signed-off-by: duao oh <duao@huawei.com>

--- api/@ohos.app.form.formAgent.d.ts
+++ api/@ohos.app.form.formAgent.d.ts
@@ -38,8 +38,8 @@ declare namespace formAgent {
    * @param { Want } want - The want of the form to publish.
    * @param { AsyncCallback<string> } callback - The callback is used to return the form id.
    * @throws { BusinessError } 202 - The application is not a system application.
-   * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified;
-   * 2. Incorrect parameter types; 3. Parameter verification failed.
+   * @throws { BusinessError } 401 - Parameter error. Possible causes:
+   *     1.Mandatory parameters are left unspecified; 2.Incorrect parameter types; 3.Parameter verification failed.
    * @throws { BusinessError } 16500050 - An IPC connection error happened.
    * @throws { BusinessError } 16500100 - Failed to obtain the configuration information.
    * @throws { BusinessError } 16501000 - An internal functional error occurred.
@@ -73,8 +73,8 @@ declare namespace formAgent {
    * @param { Want } want - The want of the form to publish.
    * @returns { Promise<string> } Returns the form id.
    * @throws { BusinessError } 202 - The application is not a system application.
-   * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified;
-   * 2. Incorrect parameter types; 3. Parameter verification failed.
+   * @throws { BusinessError } 401 - Parameter error. Possible causes:
+   *     1.Mandatory parameters are left unspecified; 2.Incorrect parameter types; 3.Parameter verification failed.
    * @throws { BusinessError } 16500050 - An IPC connection error happened.
    * @throws { BusinessError } 16500100 - Failed to obtain the configuration information.
    * @throws { BusinessError } 16501000 - An internal functional error occurred.
