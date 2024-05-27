2a093b9e0c6faad2a25b4cc0b3732de59e38a377
yaozhupeng <yaozhupeng@huawei.com>
2024-05-27 11:29:41 +0800

    add create pixelmap from surface sync interface
    Signed-off-by: yaozhupeng <yaozhupeng@huawei.com>

--- api/@ohos.multimedia.image.d.ts
+++ api/@ohos.multimedia.image.d.ts
@@ -4207,6 +4207,21 @@ function createUnpremultipliedPixelMap(src: PixelMap, dst: PixelMap): Promise<vo
   function createPixelMapFromSurface(surfaceId: string, region: Region): Promise<PixelMap>;
 
   /**
+   * Creates a PixelMap object from surface id.
+   *
+   * @param { string } surfaceId - surface id.
+   * @param { Region } region - The region to surface.
+   * @returns { PixelMap } Returns the instance if the operation is successful;Otherwise, an exception will be thrown.
+   * @throws { BusinessError } 401 - Parameter error. Possible causes: 1.Mandatory parameters are left unspecified.
+   *     2.Incorrect parameter types. 3.Parameter verification failed.
+   * @throws { BusinessError } 62980105 - Failed to get the data.
+   * @throws { BusinessError } 62980178 - Failed to create the PixelMap.
+   * @syscap SystemCapability.Multimedia.Image.Core
+   * @since 12
+   */
+  function createPixelMapFromSurfaceSync(surfaceId: string, region: Region): PixelMap;
+
+  /**
    * Creates an ImageSource instance based on the URI.
    *
    * @param { string } uri Image source URI.
