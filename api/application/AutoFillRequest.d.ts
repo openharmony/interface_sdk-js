b05d02029a3e32360c8ffb32c177c279ff219281
xinking129 <xinxin13@huawei.com>
2024-05-28 18:49:30 +0800

    auto fill request 401 parameter modification
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- api/application/AutoFillRequest.d.ts
+++ api/application/AutoFillRequest.d.ts
@@ -148,7 +148,7 @@ export interface FillRequestCallback {
    * @param { FillResponse } response - Indicates the fill response.
    * @throws { BusinessError } 202 - Permission denied, non-system app called system api.
    * @throws { BusinessError } 401 - The parameter check failed. Possible causes: 1. Mandatory parameters are left unspecified;
-   * 2. Incorrect parameters types.
+   * <br>2. Incorrect parameters types.
    * @throws { BusinessError } 16000050 - Internal error.
    * @syscap SystemCapability.Ability.AbilityRuntime.AbilityCore
    * @systemapi
@@ -185,7 +185,7 @@ export interface FillRequestCallback {
    * @param { string } [fillContent] - Indicates the content to be filled in.
    * @throws { BusinessError } 202 - Permission denied, non-system app called system api.
    * @throws { BusinessError } 401 - Incorrect parameter types. Possible causes: 1. Mandatory parameters are left unspecified;
-   * 2. Incorrect parameters types.
+   * <br>2. Incorrect parameters types.
    * @throws { BusinessError } 16000050 - Internal error.
    * @syscap SystemCapability.Ability.AbilityRuntime.AbilityCore
    * @systemapi
@@ -200,7 +200,7 @@ export interface FillRequestCallback {
    * @param { AutoFillPopupConfig } autoFillPopupConfig - Indicates the autofill popup config.
    * @throws { BusinessError } 202 - Permission denied, non-system app called system api.
    * @throws { BusinessError } 401 - The input parameter is not valid parameter. Possible causes: 1. Mandatory parameters are left unspecified;
-   * 2. Incorrect parameters types.
+   * <br>2. Incorrect parameters types.
    * @throws { BusinessError } 16000050 - Internal error.
    * @syscap SystemCapability.Ability.AbilityRuntime.AbilityCore
    * @systemapi
