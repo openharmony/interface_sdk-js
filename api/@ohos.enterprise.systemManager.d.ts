544fc735eb776d622b0d53961a66582b371d8a4e
h30034439 <hanyiyichn@163.com>
2024-05-31 20:23:35 +0800

    修正部分错误码描述-挑单beta1
    Signed-off-by: h30034439 <hanyiyichn@163.com>

--- api/@ohos.enterprise.systemManager.d.ts
+++ api/@ohos.enterprise.systemManager.d.ts
@@ -78,7 +78,7 @@ declare namespace systemManager {
    *                         The admin must have the corresponding permission.
    * @param { string } server - the address of NTP server.
    * @throws { BusinessError } 9200001 - The application is not an administrator application of the device.
-   * @throws { BusinessError } 9200002 - the administrator application does not have permission to manage the device.
+   * @throws { BusinessError } 9200002 - The administrator application does not have permission to manage the device.
    * @throws { BusinessError } 201 - Permission verification failed. The application does not have the permission required to call the API.
    * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified;
    *                                 2. Incorrect parameter types; 3. Parameter verification failed.
@@ -97,7 +97,7 @@ declare namespace systemManager {
    *                         The admin must have the corresponding permission.
    * @returns { string } the address of NTP server.
    * @throws { BusinessError } 9200001 - The application is not an administrator application of the device.
-   * @throws { BusinessError } 9200002 - the administrator application does not have permission to manage the device.
+   * @throws { BusinessError } 9200002 - The administrator application does not have permission to manage the device.
    * @throws { BusinessError } 201 - Permission verification failed. The application does not have the permission required to call the API.
    * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified;
    *                                 2. Incorrect parameter types; 3. Parameter verification failed.
