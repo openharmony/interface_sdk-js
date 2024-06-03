9980ba13daaa060f94b9ce1cce5a2baab71f10d1
fanjiaojiao0729 <fanjiaojiao@huawei.com>
2024-06-03 10:54:53 +0800

    适配form标签顺序，修复命名校验，去除interface合法性校验，修复逃生通道
    Signed-off-by: fanjiaojiao0729 <fanjiaojiao@huawei.com>

--- build-tools/api_check_plugin/entry.js
+++ build-tools/api_check_plugin/entry.js
@@ -39,13 +39,14 @@ function checkEntry(prId) {
     if (!execute) {
       throw 'npm install timeout';
     }
-    const { getMdFiles } = require(path.resolve(__dirname, './src/api_check_plugin'));
+    const { reqGitApi, getMdFiles } = require(path.resolve(__dirname, './src/api_check_plugin'));
     const { ruleArr } = require(path.resolve(__dirname, './src/utils'));
 
     const filePathArr = getMdFiles(mdFilesPath, false);
     const filePath = filePathArr.join(',');
     const resultPath = path.resolve(__dirname, './newResult.json');
     const ruleInfo = ruleArr.join(',');
+    let ApiCheckResult = true;
     buffer = execSync(`cd interface/sdk-js/build-tools/dts_parser/package && node ./JS_API_CHECK.js -N checkOnline --path ${filePath} --checker ${ruleInfo} --prId ${prId} --output ${resultPath} --excel false`, {
       timeout: 120000,
     });
@@ -61,8 +62,9 @@ function checkEntry(prId) {
         newToolResult.push(newToolResultInfo);
       });
       newToolResult.push('api_check: false');
+      ApiCheckResult = false;
     }
-
+    newToolResult = reqGitApi(newToolResult, prId, ApiCheckResult);
     removeDir(path.resolve(__dirname, '../api_diff/node_modules'));
     removeDir(path.resolve(__dirname, 'node_modules'));
   } catch (error) {
