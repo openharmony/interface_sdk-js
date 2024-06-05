b67c1c0de7ccc4129c38a271f16dcafd33100235
DaTeng <fengdateng@huawei.com>
2024-06-05 23:53:29 +0000

    fixed 4f14ab2 from https://gitee.com/fengdateng/interface_sdk-js/pulls/11894 整改的clip mask接口需要支持元服务
    Signed-off-by: DaTeng <fengdateng@huawei.com>

--- api/@internal/component/ets/common.d.ts
+++ api/@internal/component/ets/common.d.ts
@@ -17873,6 +17873,7 @@ declare class CommonMethod<T> {
   * @returns { T }
   * @syscap SystemCapability.ArkUI.ArkUI.Full
   * @crossplatform
+  * @atomicservice
   * @since 12
   */
   clipShape(value: CircleShape | EllipseShape | PathShape | RectShape): T;
@@ -17937,6 +17938,7 @@ declare class CommonMethod<T> {
    * @returns { T }
    * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @crossplatform
+   * @atomicservice
    * @since 12
    */
   maskShape(value: CircleShape | EllipseShape | PathShape | RectShape): T;
