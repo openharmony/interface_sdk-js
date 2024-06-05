bdb6b07673669d6f31ef9b85556a426356a3f8f9
DaTeng <fengdateng@huawei.com>
2024-06-05 11:55:25 +0000

    fixed dad6bc7 from https://gitee.com/fengdateng/interface_sdk-js/pulls/11894 整改的clip mask接口需要支持元服务
    Signed-off-by: DaTeng <fengdateng@huawei.com>

--- api/@internal/component/ets/common.d.ts
+++ api/@internal/component/ets/common.d.ts
@@ -17816,6 +17816,7 @@ declare class CommonMethod<T> {
    * @returns { T }
    * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @crossplatform
+   * @atomicservice
    * @since 12
    */
   clip(value: boolean): T;
@@ -17885,6 +17886,7 @@ declare class CommonMethod<T> {
    * @returns { T }
    * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @crossplatform
+   * @atomicservice
    * @since 12
    */
   mask(value: ProgressMask): T;
