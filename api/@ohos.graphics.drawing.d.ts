d845b57b16464f9aa16a7cb4bdaaea69a0e16ab7
lw19901203 <liuwei793@h-partners.com>
2024-06-06 11:29:32 +0800

    drawing ts interface font 0428
    Signed-off-by: lw19901203 <liuwei793@h-partners.com>

--- api/@ohos.graphics.drawing.d.ts
+++ api/@ohos.graphics.drawing.d.ts
@@ -894,6 +894,24 @@ declare namespace drawing {
      * @since 11
      */
     measureText(text: string, encoding: TextEncoding): number;
+    /**
+     * Sets text scale on x-axis to font.
+     * @param { number } scaleX - Text scaleX.
+     * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified;
+     * <br>2. Incorrect parameter types.
+     * @syscap SystemCapability.Graphics.Drawing
+     * @since 12
+     */
+    setScaleX(scaleX: number): void;
+    /**
+     * Sets text skew on x-axis to font.
+     * @param { number } skewX - Text skewX.
+     * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified;
+     * <br>2. Incorrect parameter types.
+     * @syscap SystemCapability.Graphics.Drawing
+     * @since 12
+     */
+    setSkewX(skewX: number): void;
   }
 
   /**
