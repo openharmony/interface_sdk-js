c8c0234b443055332344d26189548484df829f12
Your Name <you@example.com>
2024-06-07 12:34:40 +0800

    drawing add makefromfile napi
    Signed-off-by: Your Name <you@example.com>

--- api/@ohos.graphics.drawing.d.ts
+++ api/@ohos.graphics.drawing.d.ts
@@ -808,6 +808,17 @@ declare namespace drawing {
      * @since 11
      */
     getFamilyName(): string;
+
+    /**
+     * Generate typeface from file.
+     * @param { string } filePath - file path for typeface.
+     * @returns { Typeface } Typeface.
+     * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified;
+     * <br>2. Incorrect parameter types.
+     * @syscap SystemCapability.Graphics.Drawing
+     * @since 12
+     */
+     static makeFromFile(filePath: string): Typeface;
   }
 
   /**
