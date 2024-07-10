d32dedc377f3d52967970ee10dc09ff6ccd9259b
t30052775 <tianlanqing1@huawei.com>
2024-07-10 17:10:33 +0800

    导出下载控件到arkUi Kit
    Signed-off-by: t30052775 <tianlanqing1@huawei.com>

--- kits/@kit.ArkUI.d.ts
+++ kits/@kit.ArkUI.d.ts
@@ -140,7 +140,14 @@ import {
   HyperlinkModifier, MenuModifier, MenuItemModifier, PanelModifier, SymbolGlyphModifier, AttributeUpdater,
   ContainerSpanModifier
 } from "@ohos.arkui.modifier";
-import { DownloadFileButton } from '@ohos.arkui.DownloadFileButton';
+import {
+  DownloadFileButton,
+  DownloadLayoutDirection,
+  DownloadIconStyle,
+  DownloadDescription,
+  DownloadContentOptions,
+  DownloadStyleOptions
+} from '@ohos.arkui.advanced.DownloadFileButton';
 
 export {
   AddFormMenuItem, AddFormOptions, AlertDialog, Animator, AnimatorOptions, AnimatorResult, App, AppResponse, AtomicServiceBar,
@@ -186,7 +193,8 @@ export {
   MarqueeModifier, ProgressModifier, QRCodeModifier, TextClockModifier, TextTimerModifier, LineModifier, PathModifier,
   PolygonModifier, PolylineModifier, RectModifier, ShapeModifier, AlphabetIndexerModifier, FormComponentModifier,
   HyperlinkModifier, MenuModifier, MenuItemModifier, PanelModifier, SymbolGlyphModifier, AttributeUpdater,
-  ContainerSpanModifier, DownloadFileButton, SegmentButtonTextItem, SegmentButtonIconItem, SegmentButtonIconTextItem, DimensionNoPercentage,
+  ContainerSpanModifier, SegmentButtonTextItem, SegmentButtonIconItem, SegmentButtonIconTextItem, DimensionNoPercentage,
   CommonSegmentButtonOptions, ItemRestriction, SegmentButtonItemTuple, SegmentButtonItemArray, SegmentButtonItemOptionsConstructorOptions, SegmentButtonItemOptions,
-  PixelMapDrawableDescriptor, AnimationOptions, AnimatedDrawableDescriptor, NodeAdapter
+  PixelMapDrawableDescriptor, AnimationOptions, AnimatedDrawableDescriptor, NodeAdapter, DownloadFileButton,
+  DownloadLayoutDirection, DownloadIconStyle, DownloadDescription, DownloadContentOptions, DownloadStyleOptions
 };
