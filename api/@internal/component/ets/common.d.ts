d1293cea2efa1cfcf8795648972e2d54d51c67e6
189******51 <lipeicheng5@huawei.com>
2024-07-19 03:15:54 +0000

    update api/@internal/component/ets/common.d.ts.
    Signed-off-by: 189******51 <lipeicheng5@huawei.com>

--- api/@internal/component/ets/common.d.ts
+++ api/@internal/component/ets/common.d.ts
@@ -21713,7 +21713,7 @@ declare interface MeasureResult extends SizeResult {
 /**
  * The navigation destination information.
  *
- * @typedef import('../api/@ohos.arkui.observer').default.NavDestinationInfo NavDestinationInfo
+ * @typedef {import('../api/@ohos.arkui.observer').default.NavDestinationInfo} NavDestinationInfo
  * @syscap SystemCapability.ArkUI.ArkUI.Full
  * @crossplatform
  * @atomicservice
