f8770cbbca8723c7397b71e03740a8e927b42541
tangjie <1402602435@qq.com>
2024-07-22 16:38:29 +0800

    media.SubtitleInfo打标签
    Signed-off-by: tangjie <1402602435@qq.com>

--- api/@ohos.multimedia.media.d.ts
+++ api/@ohos.multimedia.media.d.ts
@@ -3196,6 +3196,7 @@ declare namespace media {
    *
    * @typedef SubtitleInfo
    * @syscap SystemCapability.Multimedia.Media.Core
+   * @atomicservice
    * @since 12
    */
   interface SubtitleInfo {
@@ -3203,6 +3204,7 @@ declare namespace media {
      * Duration of the text to be displayed, as milliseconds.
      * @type { ?number }
      * @syscap SystemCapability.Multimedia.Media.Core
+     * @atomicservice
      * @since 12
      */
     duration?: number;
@@ -3210,6 +3212,7 @@ declare namespace media {
      * Display start time of the text, as milliseconds.
      * @type { ?number }
      * @syscap SystemCapability.Multimedia.Media.Core
+     * @atomicservice
      * @since 12
      */
     startTime?: number;
@@ -3217,6 +3220,7 @@ declare namespace media {
      * Text information of current update event.
      * @type { ?string }
      * @syscap SystemCapability.Multimedia.Media.Core
+     * @atomicservice
      * @since 12
      */
     text?: string;
