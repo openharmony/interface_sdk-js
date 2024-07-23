89fe041cbc0a4e46c7d068b0a6ea3f7e63e1e70e
CoderAdo <duliqun1@h-partners.com>
2024-07-23 01:50:30 +0000

    update api/@ohos.wifiManager.d.ts.
    Signed-off-by: CoderAdo <duliqun1@h-partners.com>

--- api/@ohos.wifiManager.d.ts
+++ api/@ohos.wifiManager.d.ts
@@ -2724,6 +2724,15 @@ declare namespace wifiManager {
      * @since 12
      */
     wapiConfig?: WifiWapiConfig;
+
+    /**
+     * Device config status: 0 - enabled, 1 - diabled, 2 - perminate diabled, 3 - unknown.
+     * @type { ?number }
+     * @syscap SystemCapability.Communication.WiFi.STA
+     * @systemapi Hide this for inner system use.
+     * @since 12
+     */
+    configStatus?: number;
   }
 
   /**
@@ -3609,7 +3618,7 @@ declare namespace wifiManager {
      * @syscap SystemCapability.Communication.WiFi.STA
      * @since 12
      */
-    uniqueIpv6Address: string;
+    uniqueIpv6Address?: string;
 
     /**
      * The rand unique IPv6 address of the Wi-Fi connection
@@ -3617,7 +3626,7 @@ declare namespace wifiManager {
      * @syscap SystemCapability.Communication.WiFi.STA
      * @since 12
      */
-    randomUniqueIpv6Address: string;
+    randomUniqueIpv6Address?: string;
 
     /**
      * The gateway of the Wi-Fi connection
