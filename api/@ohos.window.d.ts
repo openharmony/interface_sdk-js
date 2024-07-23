6ff000dedcb85fb279aa6aaf2051c37710ebb239
钟林杰 <1163437528@qq.com>
2024-07-23 06:43:36 +0000

    update api/@ohos.window.d.ts.
    Signed-off-by: 钟林杰 <1163437528@qq.com>

--- api/@ohos.window.d.ts
+++ api/@ohos.window.d.ts
@@ -7081,7 +7081,6 @@ declare namespace window {
      * @throws { BusinessError } 401 - Parameter error. Possible cause: 1. Mandatory parameters are left unspecified; 
      *                                                                  2. Incorrect parameter types; 
      * @throws { BusinessError } 801 - Capability not supported. Failed to call the API due to limited device capabilities.
-     * @throws { BusinessError } 1300001 - Repeated operation.
      * @throws { BusinessError } 1300002 - This window state is abnormal.
      * @throws { BusinessError } 1300003 - This window manager service works abnormally.
      * @throws { BusinessError } 1300004 - Unauthorized operation.
