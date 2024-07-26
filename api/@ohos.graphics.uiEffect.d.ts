80dedfb953e14e7105e6f0df2e4fbdf1f23c3f34
王禹臻 <wangyuzhen5@huawei.com>
2024-07-26 14:23:04 +0800

    Feature Fly In Fly Out Api
    Signed-off-by: 王禹臻 <wangyuzhen5@huawei.com>

--- api/@ohos.graphics.uiEffect.d.ts
+++ api/@ohos.graphics.uiEffect.d.ts
@@ -71,6 +71,20 @@ declare namespace uiEffect {
      * @since 12
      */
     waterRipple(progress: number, waveCount: number, x: number, y: number): Filter;
+
+    /**
+     * Set the fly in or fly out effect of the component.
+     *
+     * @param { number } degree - set the degree of fly in or fly out effect, value range [0, 1].
+     * @param { FlyMode } flyMode - set the location of stretching when fly in or out
+     * If the value is 0, the component keep same, else the value is 1, component are fully fly out or fly in.
+     * @returns { Filter } - Returns  fly in fly out Filter.
+     * @throws { BusinessError } 202 - Permission verification failed. A non-system application calls a system API.
+     * @syscap SystemCapability.Graphics.Drawing
+     * @systemapi
+     * @since 12
+     */
+    flyInFlyOutEffect(degree: number, flyMode: FlyMode ): Filter;
   }
 
   /**
@@ -117,6 +131,34 @@ declare namespace uiEffect {
      * @since 12
      */
     DECAL = 3,
+  }
+
+  /**
+   * FlyMode enumeration description
+   *
+   * @enum { number }
+   * @syscap SystemCapability.Graphics.Drawing
+   * @systemapi
+   * @since 12
+   */
+  enum FlyMode {
+    /**
+     * BOTTOM fly mode.
+     *
+     * @syscap SystemCapability.Graphics.Drawing
+     * @systemapi
+     * @since 12
+     */
+    BOTTOM = 0,
+
+    /**
+     * TOP fly mode.
+     *
+     * @syscap SystemCapability.Graphics.Drawing
+     * @systemapi
+     * @since 12
+     */
+    TOP = 1,
   }
   
   /**
