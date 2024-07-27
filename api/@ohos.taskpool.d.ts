55d44ff0aac79f2a0d4b35692bd5ee75d05dbb95
zhuruigan <zhuruigan@huawei.com>
2024-07-27 10:24:55 +0800

    remove taskpool 10200003 code
    Signed-off-by: zhuruigan <zhuruigan@huawei.com>
    Change-Id: I110fa4245d306528a66fbb47453ddecd2296f100

--- api/@ohos.taskpool.d.ts
+++ api/@ohos.taskpool.d.ts
@@ -746,7 +746,6 @@ declare namespace taskpool {
      * @throws { BusinessError } 401 - Parameter error. Possible causes:
      * <br>1. Mandatory parameters are left unspecified;
      * <br>2. Incorrect parameter types;
-     * @throws { BusinessError } 10200003 - Worker initialization failed.
      * @throws { BusinessError } 10200006 - An exception occurred during serialization.
      * @throws { BusinessError } 10200025 - A dependent task cannot be added to SequenceRunner.
      * @throws { BusinessError } 10200051 - The periodic task cannot be executed again.
@@ -1111,6 +1110,23 @@ declare namespace taskpool {
    * @atomicservice
    * @since 11
    */
+  /**
+   * Execute a concurrent function.
+   *
+   * @param { Function } func - func func Concurrent function want to execute.
+   * @param { Object[] } args - args args The concurrent function arguments.
+   * @returns { Promise<Object> }
+   * @throws { BusinessError } 401 - Parameter error. Possible causes:
+   * 1.Mandatory parameters are left unspecified;
+   * 2.Incorrect parameter types;
+   * 3.Parameter verification failed.
+   * @throws { BusinessError } 10200006 - An exception occurred during serialization.
+   * @throws { BusinessError } 10200014 - The function is not marked as concurrent.
+   * @syscap SystemCapability.Utils.Lang
+   * @crossplatform
+   * @atomicservice
+   * @since 12
+   */
   function execute(func: Function, ...args: Object[]): Promise<Object>;
 
   /**
@@ -1174,7 +1190,6 @@ declare namespace taskpool {
    * <br>1. Mandatory parameters are left unspecified;
    * <br>2. Incorrect parameter types;
    * <br>3. Parameter verification failed.
-   * @throws { BusinessError } 10200003 - Worker initialization failed.
    * @throws { BusinessError } 10200006 - An exception occurred during serialization.
    * @throws { BusinessError } 10200014 - The function is not marked as concurrent.
    * @throws { BusinessError } 10200051 - The periodic task cannot be executed again.
@@ -1265,7 +1280,6 @@ declare namespace taskpool {
    * <br>1. Mandatory parameters are left unspecified;
    * <br>2. Incorrect parameter types;
    * <br>3. Parameter verification failed.
-   * @throws { BusinessError } 10200003 - Worker initialization failed.
    * @throws { BusinessError } 10200006 - An exception occurred during serialization.
    * @throws { BusinessError } 10200014 - The function is not marked as concurrent.
    * @throws { BusinessError } 10200028 - The period is less than zero.
