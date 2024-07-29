64b8e2be4120641ef814c72a29ededa902b11c04
sunqinglong <sunqinglong@huawei.com>
2024-07-29 11:21:00 +0000

    update api/@internal/component/ets/styled_string.d.ts.
    Signed-off-by: sunqinglong <sunqinglong@huawei.com>

--- api/@internal/component/ets/styled_string.d.ts
+++ api/@internal/component/ets/styled_string.d.ts
@@ -107,8 +107,8 @@ declare class StyledString {
      * @atomicservice
      * @since 12
      */
-
     subStyledString(start: number, length?: number): StyledString;
+
     /**
      * Returns StyledString from the provided HTML string.
      *
