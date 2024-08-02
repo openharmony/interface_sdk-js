5c55356b1a33fffebbef23090a42325c27d59ac0
fanjiaojiao0729 <fanjiaojiao@huawei.com>
2024-08-02 14:32:41 +0800

    修复extends值校验bug
    Signed-off-by: fanjiaojiao0729 <fanjiaojiao@huawei.com>

--- build-tools/dts_parser/src/coreImpl/checker/src/tag_value_check.ts
+++ build-tools/dts_parser/src/coreImpl/checker/src/tag_value_check.ts
@@ -133,11 +133,11 @@ export class TagValueCheck {
       state: true,
       errorInfo: '',
     };
-    let extendsTagValue: string = tag.name;
+    let extendsTagValue: string = tag.name + tag.description;
     if (singleApi.getApiType() === ApiType.CLASS || singleApi.getApiType() === ApiType.INTERFACE) {
       const extendsApiValue: string = CommonFunctions.getExtendsApiValue(singleApi);
       const ImplementsApiValue: string = CommonFunctions.getImplementsApiValue(singleApi);
-      if (tag.tag === 'extends' && extendsTagValue !== extendsApiValue) {
+      if (tag.tag === 'extends' && extendsTagValue.replace(/\s/g, '') !== extendsApiValue) {
         extendsValueCheckResult.state = false;
         extendsValueCheckResult.errorInfo = ErrorMessage.ERROR_INFO_VALUE_EXTENDS;
       }
