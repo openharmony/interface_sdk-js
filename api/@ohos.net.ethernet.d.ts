0a47c038274739542f3b6406bb9be33b358f3a98
mayongzhi <mayongzhi@h-partners.com>
2024-08-14 12:03:24 +0000

    update api/@ohos.net.ethernet.d.ts.
    Signed-off-by: mayongzhi <mayongzhi@h-partners.com>

--- api/@ohos.net.ethernet.d.ts
+++ api/@ohos.net.ethernet.d.ts
@@ -29,7 +29,7 @@ import type connection from './@ohos.net.connection';
  */
 declare namespace ethernet {
   /**
-   * @typedef connection.HttpProxy
+   * @typedef { connection.HttpProxy }
    * @syscap SystemCapability.Communication.NetManager.Ethernet
    * @since 10
    */
