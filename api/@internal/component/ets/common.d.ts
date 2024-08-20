b0a593d76479a7a0f674faebf76eebb15d39cff8
b30058220 <baodi1@huawei.com>
2024-08-20 14:18:00 +0000

    fix overlay从10开始支持CustomBuilder
    Signed-off-by: b30058220 <baodi1@huawei.com>
    Change-Id: Ie75c19bf1c5545cce9df792089983d66de6fbfbc

--- api/@internal/component/ets/common.d.ts
+++ api/@internal/component/ets/common.d.ts
@@ -18691,7 +18691,7 @@ declare class CommonMethod<T> {
   /**
    * Add mask text to the current component. The layout is the same as that of the current component.
    *
-   * @param { string | CustomBuilder } value
+   * @param { string } value
    * @param { object } options
    * @returns { T }
    * @syscap SystemCapability.ArkUI.ArkUI.Full
@@ -18700,7 +18700,7 @@ declare class CommonMethod<T> {
   /**
    * Add mask text to the current component. The layout is the same as that of the current component.
    *
-   * @param { string | CustomBuilder } value
+   * @param { string } value
    * @param { object } options
    * @returns { T }
    * @syscap SystemCapability.ArkUI.ArkUI.Full
