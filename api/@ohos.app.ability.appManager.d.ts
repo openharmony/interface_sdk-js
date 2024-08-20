92d4d7ac6541bfefc7b40b05948fe7ffcc79a3cf
yuwenze <yuwenze1@huawei.com>
2024-08-20 10:56:34 +0800

    offline Signed-off-by: yuwenze <yuwenze1@huawei.com> （cherry picked commit from <gitee.com//openharmony/interface_sdk-js/commit/0cd988704e794f5f7da4684390aa317b4c6d18e6>

--- api/@ohos.app.ability.appManager.d.ts
+++ api/@ohos.app.ability.appManager.d.ts
@@ -905,25 +905,6 @@ declare namespace appManager {
   function isAppRunning(bundleName: string, appCloneIndex?: number): Promise<boolean>;
 
   /**
-   * Clear up application data by bundle name
-   *
-   * @permission ohos.permission.CLEAN_APPLICATION_DATA
-   * @param { string } bundleName - bundle name.
-   * @param { number } [appCloneIndex] - app clone index
-   * @returns { Promise<void> } The promise returned by the function.
-   * @throws { BusinessError } 201 - Permission denied.
-   * @throws { BusinessError } 202 - Not system application.
-   * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified;
-   *    2. Incorrect parameter types; 3. Parameter verification failed.
-   * @throws { BusinessError } 16000050 - Internal error.
-   * @throws { BusinessError } 16000073 - The app clone index is invalid.
-   * @syscap SystemCapability.Ability.AbilityRuntime.Core
-   * @systemapi
-   * @since 12
-   */
-  function clearUpAppData(bundleName: string, appCloneIndex?: number): Promise<void>;
-
-  /**
    * Terminate the mission of a UIAbility and back to caller.
    *
    * @permission ohos.permission.KILL_APP_PROCESSES
