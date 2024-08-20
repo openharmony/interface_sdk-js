f015fee3042614b2bdd21ac1debdeb63e10c664f
geraltxu <xufanghan@huawei.com>
2024-08-20 17:07:21 +0800

    暂时删除窗口优化接口
    Signed-off-by: xufanghan <xufanghan@huawei.com>

--- api/@internal/component/ets/web.d.ts
+++ api/@internal/component/ets/web.d.ts
@@ -8371,16 +8371,16 @@ declare class WebAttribute extends CommonMethod<WebAttribute> {
    */
   enableNativeMediaPlayer(config: NativeMediaPlayerConfig): WebAttribute;
 
-  /**
-   * Sets the enable window drag smooth for web.
-   *
-   * @param { boolean } mode - True if it needs to enable window drag smooth.
-   * @returns { WebAttribute }
-   * @syscap SystemCapability.Web.Webview.Core
-   * @atomicservice
-   * @since 12
-   */
-   enableSmoothDragResize(mode: boolean): WebAttribute;
+  // /**
+  //  * Sets the enable window drag smooth for web.
+  //  *
+  //  * @param { boolean } mode - True if it needs to enable window drag smooth.
+  //  * @returns { WebAttribute }
+  //  * @syscap SystemCapability.Web.Webview.Core
+  //  * @atomicservice
+  //  * @since 12
+  //  */
+  //  enableSmoothDragResize(mode: boolean): WebAttribute;
 
   /**
    * Triggered when render process not responding.
