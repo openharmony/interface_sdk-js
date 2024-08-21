1e42e5272d218f38514800408813dd93ce7c5e5f
tantingting <tantingting5@huawei.com>
2024-08-21 18:07:39 +0800

    modify old
    Signed-off-by: tantingting <tantingting5@huawei.com>

--- api/application/AutoFillExtensionContext.d.ts
+++ api/application/AutoFillExtensionContext.d.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License"),
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -26,7 +26,7 @@ import ExtensionContext from './ExtensionContext';
  * @extends ExtensionContext
  * @syscap SystemCapability.Ability.AbilityRuntime.AbilityCore
  * @systemapi
- * @stagemodelonly
+ * @StageModelOnly
  * @since 11
  */
 export default class AutoFillExtensionContext extends ExtensionContext {
