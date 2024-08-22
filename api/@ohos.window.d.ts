2df3cf46ee19884563ce00c17850882106a6e08e
zhangkai <zhangkai324@huawei.com>
2024-08-22 11:34:19 +0800

    add code for createWindow
    Signed-off-by: zhangkai <zhangkai324@huawei.com>
    Change-Id: I2a07bf28d81632b6bfd350a5027ffca352e6962a

--- api/@ohos.window.d.ts
+++ api/@ohos.window.d.ts
@@ -2298,6 +2298,7 @@ declare namespace window {
    *                                                                  2. Incorrect parameter types.
    * @throws { BusinessError } 801 - Capability not supported. Failed to call the API due to limited device capabilities.
    * @throws { BusinessError } 1300001 - Repeated operation.
+   * @throws { BusinessError } 1300002 - This window state is abnormal.
    * @throws { BusinessError } 1300004 - Unauthorized operation.
    * @throws { BusinessError } 1300006 - This window context is abnormal.
    * @throws { BusinessError } 1300008 - The display device is abnormal.
@@ -2335,6 +2336,7 @@ declare namespace window {
    *                                                                  2. Incorrect parameter types.
    * @throws { BusinessError } 801 - Capability not supported. Failed to call the API due to limited device capabilities.
    * @throws { BusinessError } 1300001 - Repeated operation.
+   * @throws { BusinessError } 1300002 - This window state is abnormal.
    * @throws { BusinessError } 1300004 - Unauthorized operation.
    * @throws { BusinessError } 1300006 - This window context is abnormal.
    * @throws { BusinessError } 1300008 - The display device is abnormal.
