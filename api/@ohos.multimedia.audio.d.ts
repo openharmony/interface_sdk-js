2b928650f0bfb6bdc8253c9b235ede4038a3fae7
libliang <libliang@163.com>
2024-08-23 20:56:05 +0800

    add new funtion of setDefaultOutputDevice
    Signed-off-by: libliang <libliang@163.com>

--- api/@ohos.multimedia.audio.d.ts
+++ api/@ohos.multimedia.audio.d.ts
@@ -7416,6 +7416,25 @@ declare namespace audio {
     getSilentModeAndMixWithOthers(): boolean;
 
     /**
+     * Temporarily changes the current audio device
+     * This function applys on audiorenderers whose StreamUsage are
+     * STREAM_USAGE_VOICE_COMMUNICATIN/STREAM_USAGE_VIDEO_COMMUNICATION/STREAM_USAGE_VOICE_MESSAGE.
+     * Setting the device will ony takes effect if no other accessory such as headphoes are in use
+     * @param { DeviceType } devicetype - the available deviceTypes are
+     *     EARPIECE: Built-in earpiece
+     *     SPEAKER: Built-in speaker
+     *     DEFAULT: System default output device
+     * @throws { BusinessError } 401 - Parameter error. Possible causes:
+     *                                 1.Mandatory parameters are left unspecified;
+     *                                 2.Incorrect parameter types.
+     * @throws { BusinessError } 6800101 - Parameter verification failed.
+     * @throws { BusinessError } 6800103 - Operation not permit at current state.
+     * @syscap SystemCapability.Multimedia.Audio.Renderer
+     * @since 13
+     */
+    setDefaultOutputDevice(deviceType: DeviceType): void
+
+    /**
      * Listens for audio interrupt events. This method uses a callback to get interrupt events. The interrupt event is
      * triggered when audio playback is interrupted.
      * @param { 'audioInterrupt' } type - Type of the event to listen for. Only the audioInterrupt event is supported.
