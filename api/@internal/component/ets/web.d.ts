9e7b4db2f404311a510c7a318df2313ebd3205a4
panliujun <panliujun3@h-partners.com>
2024-08-26 11:18:15 +0800

    web新增文本选择扩展接口
    Signed-off-by: panliujun <panliujun3@h-partners.com>

--- api/@internal/component/ets/web.d.ts
+++ api/@internal/component/ets/web.d.ts
@@ -8438,6 +8438,16 @@ declare class WebAttribute extends CommonMethod<WebAttribute> {
    * @since 12
    */
   keyboardAvoidMode(mode: WebKeyboardAvoidMode): WebAttribute;
+  
+  /**
+   * Set the custom text menu.
+   *
+   * @param { EditMenuOptions } editMenu - Customize text menu options.
+   * @returns { WebAttribute }
+   * @syscap SystemCapability.Web.Webview.Core
+   * @since 12
+   */
+  editMenuOptions(editMenu: EditMenuOptions): WebAttribute;
 }
 
 /**
