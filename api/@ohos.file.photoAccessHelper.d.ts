958dcd386b72e80227b647b302236eed13af7e1e
liu-zhengji <liuzhengji@huawei.com>
2024-08-27 15:07:52 +0800

    add multi crop to 5.0
    Signed-off-by: liu-zhengji <liuzhengji@huawei.com>

--- api/@ohos.file.photoAccessHelper.d.ts
+++ api/@ohos.file.photoAccessHelper.d.ts
@@ -395,7 +395,15 @@ declare namespace photoAccessHelper {
      * @systemapi
      * @since 12
      */
-    ANALYSIS_HIGHLIGHT
+    ANALYSIS_HIGHLIGHT,
+    /**
+     * Analysis of multi crop
+     *
+     * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+     * @systemapi
+     * @since 12
+     */
+    ANALYSIS_MULTI_CROP
   }
 
   /**
