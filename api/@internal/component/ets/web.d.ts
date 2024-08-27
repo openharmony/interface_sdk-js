47f5282d62a6a69d4ea22f20c1eebc0057a36593
lanshouren <lanshouren@huawei.com>
2024-08-27 19:01:23 +0800

    cherry pick abf2bac from https://gitee.com/lanshouren/interface_sdk-js/pulls/13693 add haptic feedback switch for web
    Signed-off-by: lanshouren <lanshouren@huawei.com>
    Change-Id: I2feabf833afd7b47a59706ea21b094fecbc0875b

--- api/@internal/component/ets/web.d.ts
+++ api/@internal/component/ets/web.d.ts
@@ -8516,6 +8516,16 @@ declare class WebAttribute extends CommonMethod<WebAttribute> {
    * @since 12
    */
   editMenuOptions(editMenu: EditMenuOptions): WebAttribute;
+
+  /**
+   * Enable or disable haptic feedback.
+   *
+   * @param { boolean } enable - Default value is true, set false to disable haptic feedback.
+   * @returns { WebAttribute }
+   * @syscap SystemCapability.Web.Webview.Core
+   * @since 13
+   */
+  enableHapticFeedback(enable: boolean): WebAttribute;
 }
 
 /**
