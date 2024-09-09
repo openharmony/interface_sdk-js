45d906f779c45572cc4f74ad3a99ec3d05d14fe9
liyu <liyu312@huawei.com>
2024-09-09 10:58:47 +0800

    TextInput适配symbol
    Signed-off-by: liyu <liyu312@huawei.com>

--- api/@internal/component/ets/text_input.d.ts
+++ api/@internal/component/ets/text_input.d.ts
@@ -2203,7 +2203,17 @@ declare class TextInputAttribute extends CommonMethod<TextInputAttribute> {
    * @atomicservice
    * @since 12
    */
-  cancelButton(value: { style?: CancelButtonStyle, icon?: IconOptions }): TextInputAttribute;
+  /**
+   * Set the cancel button style
+   *
+   * @param { CancelButtonOptions | CancelButtonSymbolOptions } value - indicates the style of the cancel button.
+   * @returns { TextInputAttribute }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 14
+   */
+  cancelButton(value: CancelButtonOptions | CancelButtonSymbolOptions): TextInputAttribute;
 
   /**
    * Sets selection when on focus.
