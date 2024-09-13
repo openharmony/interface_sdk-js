0322313f3d7f86b756c1bab71ebac9aac6fcddb2
SQ <sunqi57@huawei.com>
2024-09-13 15:36:16 +0800

    Tabs支持配置barBackgroundEffect和BackgroundBlurStyle
    Signed-off-by: SQ <sunqi57@huawei.com>

--- api/@internal/component/ets/tabs.d.ts
+++ api/@internal/component/ets/tabs.d.ts
@@ -1362,6 +1362,31 @@ declare class TabsAttribute extends CommonMethod<TabsAttribute> {
   barBackgroundBlurStyle(value: BlurStyle): TabsAttribute;
 
   /**
+   * Set the BlurStyle of the tab bar.
+   *
+   * @param { BlurStyle } style - style indicate the blur style for the tab bar
+   * @param { BackgroundBlurStyleOptions } options - options indicate the options for the tab bar
+   * @returns { TabsAttribute } the attribute of the tabs
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 13
+   */
+  barBackgroundBlurStyle(style: BlurStyle, options: BackgroundBlurStyleOptions): TabsAttribute;
+
+  /**
+   * Set the BackgroundEffect of the tab bar.
+   *
+   * @param { BackgroundEffectOptions } options - options indicate the options for the tab bar
+   * @returns { TabsAttribute } the attribute of the tabs
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 13
+   */
+  barBackgroundEffect(options: BackgroundEffectOptions): TabsAttribute;
+
+  /**
    * Called when content will change.
    *
    * @param { function } handler
