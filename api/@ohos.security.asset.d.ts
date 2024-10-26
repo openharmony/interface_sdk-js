9e85d24fc9ef10bef268374b62d5af3f698ffdfc
Cai Xincheng <caixincheng@huawei.com>
2024-10-26 15:36:13 +0800

    correct asset atomic service tag
    Signed-off-by: Cai Xincheng <caixincheng@huawei.com>
    Change-Id: I5b47922a9cf6975e3e2b05c84e8358ef24b7eb9e

--- api/@ohos.security.asset.d.ts
+++ api/@ohos.security.asset.d.ts
@@ -66,12 +66,9 @@ declare namespace asset {
    */
   /**
    * Add an Asset.
-   * Permission ohos.permission.STORE_PERSISTENT_DATA is required when the Asset needs to be stored persistently
-   *     by setting {@link Tag.IS_PERSISTENT} tag.
    *
    * @param { AssetMap } attributes - a map object containing attributes of the Asset to be added.
    * @returns { Promise<void> } the promise object returned by the function.
-   * @throws { BusinessError } 201 - The caller doesn't have the permission.
    * @throws { BusinessError } 401 - Parameter error. Possible causes:
    *     1. Mandatory parameters are left unspecified.
    *     2. Incorrect parameter types.
@@ -158,11 +155,8 @@ declare namespace asset {
    */
   /**
    * Add an Asset.
-   * Permission ohos.permission.STORE_PERSISTENT_DATA is required when the Asset needs to be stored persistently
-   *     by setting {@link Tag.IS_PERSISTENT} tag.
    *
    * @param { AssetMap } attributes - a map object containing attributes of the Asset to be added.
-   * @throws { BusinessError } 201 - The caller doesn't have the permission.
    * @throws { BusinessError } 401 - Parameter error. Possible causes:
    *     1. Mandatory parameters are left unspecified.
    *     2. Incorrect parameter types.
@@ -1684,26 +1678,12 @@ declare namespace asset {
      * @syscap SystemCapability.Security.Asset
      * @since 11
      */
-    /**
-     * The error code indicates that the caller doesn't have the permission.
-     *
-     * @syscap SystemCapability.Security.Asset
-     * @atomicservice
-     * @since 14
-     */
     PERMISSION_DENIED = 201,
     /**
      * The error code indicates that the caller is not a system application.
      *
      * @syscap SystemCapability.Security.Asset
      * @since 12
-     */
-    /**
-     * The error code indicates that the caller is not a system application.
-     *
-     * @syscap SystemCapability.Security.Asset
-     * @atomicservice
-     * @since 14
      */
     NOT_SYSTEM_APPLICATION = 202,
     /**
