905992c0e5059d8b61872f027c5d297186f04abd
tanchenghao <tanchenghao@h-partners.com>
2024-11-04 09:37:42 +0800

    删除 startMoving 系统注释
    Signed-off-by: tanchenghao <tanchenghao@h-partners.com>

--- api/@ohos.window.d.ts
+++ api/@ohos.window.d.ts
@@ -7784,20 +7784,6 @@ declare namespace window {
      * Start moving window.
      *
      * @returns { Promise<void> } Promise that returns no value.
-     * @throws { BusinessError } 202 - Permission verification failed. A non-system application calls a system API.
-     * @throws { BusinessError } 801 - Capability not supported. Failed to call the API due to limited device capabilities.
-     * @throws { BusinessError } 1300001 - Repeated operation.
-     * @throws { BusinessError } 1300002 - This window state is abnormal.
-     * @throws { BusinessError } 1300003 - This window manager service works abnormally.
-     * @throws { BusinessError } 1300004 - Unauthorized operation.
-     * @syscap SystemCapability.Window.SessionManager
-     * @systemapi Hide this for inner system use.
-     * @since 13
-     */
-    /**
-     * Start moving window.
-     *
-     * @returns { Promise<void> } Promise that returns no value.
      * @throws { BusinessError } 801 - Capability not supported. Failed to call the API due to limited device capabilities.
      * @throws { BusinessError } 1300001 - Repeated operation.
      * @throws { BusinessError } 1300002 - This window state is abnormal.
