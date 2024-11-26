d7885d6f5190a36378a0499eccfffa59054444aa
zyx0121 <zhangyingxiang2@huawei.com>
2024-11-26 20:37:41 +0800

    增加富文本获取光标位置接口
    Signed-off-by: zyx0121 <zhangyingxiang2@huawei.com>

--- api/@internal/component/ets/rich_editor.d.ts
+++ api/@internal/component/ets/rich_editor.d.ts
@@ -2932,6 +2932,17 @@ declare class RichEditorBaseController implements TextEditControllerEx {
    * @since 12
    */
   getPreviewText(): PreviewText;
+
+  /**
+   * Get CaretRect.
+   *
+   * @returns { RectResult } - Return the caret rect.
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 16
+   */
+  getCaretRect(): RectResult;
 }
 
 /**
