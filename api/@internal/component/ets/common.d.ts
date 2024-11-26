832674b62e80a4cd9d5f54f91c7d485acb44221b
fenglinbailu <liubin249@huawei.com>
2024-11-26 16:12:59 +0800

    cherry pick 7a2a237 from https://gitee.com/fenglinbailu/interface_sdk-js/pulls/15034 add LayoutWeightOptions for relativecontainer chainmode layoutweight.
    Signed-off-by: fenglinbailu <liubin249@huawei.com>

--- api/@internal/component/ets/common.d.ts
+++ api/@internal/component/ets/common.d.ts
@@ -15263,6 +15263,18 @@ declare class CommonMethod<T> {
   layoutWeight(value: number | string): T;
 
   /**
+   * chain Weight
+   *
+   * @param { ChainWeightOptions } chainWeight
+   * @returns { T }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 14
+   */
+  chainWeight(chainWeight: ChainWeightOptions): T;
+
+  /**
    * Inner margin.
    *
    * @param { Padding | Length } value
