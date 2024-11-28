c8de7597f840d5a037b967f201e176894edf1d0f
lwx1281857 <linnanmu@h-partners.com>
2024-11-28 09:53:30 +0800

    Description:repair api description Feature or Bugfix:Bugfix Binary Source: No
    Signed-off-by: lwx1281857 <linnanmu@h-partners.com>

--- api/@ohos.deviceInfo.d.ts
+++ api/@ohos.deviceInfo.d.ts
@@ -216,7 +216,6 @@ declare namespace deviceInfo {
 
   /**
    * Obtains the device serial number represented by a string.
-   * This API can be called only by system applications.
    *
    * @permission ohos.permission.sec.ACCESS_UDID
    * 
@@ -579,9 +578,9 @@ declare namespace deviceInfo {
    * @since 10
    */
   const buildRootHash: string;
+
   /**
    * Obtains the device udid.
-   * This API can be called only by system applications.
    *
    * @permission ohos.permission.sec.ACCESS_UDID
    * 
