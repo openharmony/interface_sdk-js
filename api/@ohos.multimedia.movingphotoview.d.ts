efa365030138ca8e16dd47d95959d685d5e45cac
qinlida <qinlida1@h-partners.com>
2024-12-03 14:54:27 +0800

    [增加range mode]
    Signed-off-by: qinlida <qinlida1@h-partners.com>

--- api/@ohos.multimedia.movingphotoview.d.ts
+++ api/@ohos.multimedia.movingphotoview.d.ts
@@ -83,6 +83,40 @@ export declare enum PixelMapFormat {
     YCRCB_P010 = 5
 }
 /**
+ * dynamic range mode of moving photo.
+ *
+ * @enum { number }
+ * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+ * @systemapi
+ * @since 14
+ */
+export declare enum DynamicRangeMode {
+    /**
+     * Allow image content to use an unrestricted extended range
+     *
+     * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+     * @systemapi
+     * @since 14
+     */
+    HIGH = 0,
+    /**
+     * Allow image content to use some extended range
+     *
+     * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+     * @systemapi
+     * @since 14
+     */
+    CONSTRAINT = 1,
+    /**
+     * Restrict the image content to dynamic range to the standard range
+     *
+     * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+     * @systemapi
+     * @since 14
+     */
+    STANDARD = 2,
+}
+/**
  * Defines the moving photo view options.
  *
  * @interface MovingPhotoViewOptions
@@ -132,6 +166,15 @@ declare interface MovingPhotoViewOptions {
      * @since 14
      */
     movingPhotoFormat?: PixelMapFormat;
+    /**
+     * range mode of MovingPhotoView.
+     *
+     * @type { ?DynamicRangeMode }
+     * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+     * @systemapi
+     * @since 14
+     */
+    dynamicRangeMode?: DynamicRangeMode;
 }
 /**
  * Defines the moving photo view interface.
