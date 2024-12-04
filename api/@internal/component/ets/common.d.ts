6056c0ef79ab1bc9ce5dce81f1f67a7f358f2fe0
sun-xinyan <sunxinyan2@huawei.com>
2024-12-04 16:21:38 +0800

    回退延期需求接口代码
    Signed-off-by: sun-xinyan <sunxinyan2@huawei.com>
    Change-Id: I8569f16c420e3405801b90ccb1c18f66d783af5b

--- api/@internal/component/ets/common.d.ts
+++ api/@internal/component/ets/common.d.ts
@@ -20042,19 +20042,7 @@ declare class CommonMethod<T> {
    * @atomicservice
    * @since 11
    */
-  /**
-   * Bind content cover
-   *
-   * @param { Optional<boolean> } isShow - true means display content, false means hide content.
-   * @param { CustomBuilder } builder - the content to be displayed.
-   * @param { ModalTransition } [type] - transition type.
-   * @returns { T }
-   * @syscap SystemCapability.ArkUI.ArkUI.Full
-   * @crossplatform
-   * @atomicservice
-   * @since 12
-   */
-  bindContentCover(isShow: Optional<boolean>, builder: CustomBuilder, type?: ModalTransition): T;
+  bindContentCover(isShow: boolean, builder: CustomBuilder, type?: ModalTransition): T;
 
   /**
    * Bind content cover
@@ -20079,19 +20067,7 @@ declare class CommonMethod<T> {
    * @atomicservice
    * @since 11
    */
-  /**
-   * Bind content cover
-   *
-   * @param { Optional<boolean> } isShow - true means display content, false means hide content.
-   * @param { CustomBuilder } builder - the content to be displayed.
-   * @param { ContentCoverOptions } [options] - options of content cover.
-   * @returns { T }
-   * @syscap SystemCapability.ArkUI.ArkUI.Full
-   * @crossplatform
-   * @atomicservice
-   * @since 12
-   */
-  bindContentCover(isShow: Optional<boolean>, builder: CustomBuilder, options?: ContentCoverOptions): T;
+  bindContentCover(isShow: boolean, builder: CustomBuilder, options?: ContentCoverOptions): T;
 
   /**
    * Bind sheet
@@ -20116,19 +20092,7 @@ declare class CommonMethod<T> {
    * @atomicservice
    * @since 11
    */
-  /**
-   * Bind sheet
-   *
-   * @param { Optional<boolean> } isShow - true means display sheet, false means hide sheet.
-   * @param { CustomBuilder } builder - the sheet to be displayed.
-   * @param { SheetOptions } [options] - options of sheet.
-   * @returns { T } - template type
-   * @syscap SystemCapability.ArkUI.ArkUI.Full
-   * @crossplatform
-   * @atomicservice
-   * @since 12
-   */
-  bindSheet(isShow: Optional<boolean>, builder: CustomBuilder, options?: SheetOptions): T;
+  bindSheet(isShow: boolean, builder: CustomBuilder, options?: SheetOptions): T;
 
   /**
    * Sets styles for component state.
