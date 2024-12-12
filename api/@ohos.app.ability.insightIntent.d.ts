c75cd4e10c96aaec2aa1605420726f0f45b38f5a
孙旭辉 <sunxuhui7@huawei.com>
2024-12-12 09:12:13 +0000

    update api/@ohos.app.ability.insightIntent.d.ts.
    Signed-off-by: 孙旭辉 <sunxuhui7@huawei.com>

--- api/@ohos.app.ability.insightIntent.d.ts
+++ api/@ohos.app.ability.insightIntent.d.ts
@@ -118,7 +118,7 @@ declare namespace insightIntent {
      *
      * @type { ?Array<string> }
      * @syscap SystemCapability.Ability.AbilityRuntime.Core
-     * @StageModelOnly
+     * @stagemodelonly
      * @atomicservice
      * @since 16
      */
@@ -133,7 +133,7 @@ declare namespace insightIntent {
      *
      * @type { ?number }
      * @syscap SystemCapability.Ability.AbilityRuntime.Core
-     * @StageModelOnly
+     * @stagemodelonly
      * @atomicservice
      * @since 16
      */
