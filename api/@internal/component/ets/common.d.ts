832d019cf156ce53590a15ba5fae63388c881468
Zhang Jinyu <zhangjinyu101@huawei.com>
2024-12-20 11:02:37 +0800

    bindSheet支持配置圆角
    Signed-off-by: Zhang Jinyu <zhangjinyu101@huawei.com>
    Change-Id: I31f48bc5a0ad5cadb343bd480fce66e3ae0f279e

--- api/@internal/component/ets/common.d.ts
+++ api/@internal/component/ets/common.d.ts
@@ -11872,6 +11872,17 @@ declare interface SheetOptions extends BindOptions {
    * @since 14
    */
     offset?: Position
+
+  /**
+   * Defines sheet radius
+   * 
+   * @type { ?(LengthMetrics | BorderRadiuses | LocalizedBorderRadiuses) }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 16
+   */
+  radius?: LengthMetrics | BorderRadiuses | LocalizedBorderRadiuses;
 }
 
 /**
