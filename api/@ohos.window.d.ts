473700128e26e7dad738d0764305a03a98152f10
韦国宁 <weiguoning2@huawei.com>
2024-12-22 11:57:53 +0800

    新增终止窗口拖动接口
    Signed-off-by: 韦国宁 <weiguoning2@huawei.com>

--- api/@ohos.window.d.ts
+++ api/@ohos.window.d.ts
@@ -8020,6 +8020,20 @@ declare namespace window {
     startMoving(): Promise<void>;
 
     /**
+     * Stop moving window.
+     *
+     * @returns { Promise<void> } Promise that returns no value.
+     * @throws { BusinessError } 801 - Capability not supported. Failed to call the API due to limited device capabilities.
+     * @throws { BusinessError } 1300002 - This window state is abnormal.
+     * @throws { BusinessError } 1300003 - This window manager service works abnormally.
+     * @throws { BusinessError } 1300004 - Unauthorized operation.
+     * @syscap SystemCapability.Window.SessionManager
+     * @atomicservice
+     * @since 15
+     */
+    stopMoving(): Promise<void>;
+
+    /**
      * Enable drag window.
      *
      * @param { boolean } enable - The value true means to enable window dragging, and false means the opposite.
