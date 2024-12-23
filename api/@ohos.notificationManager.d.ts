ea4a193727ddac728bdaa7fda4a7d7c492cb538b
markYao <yaozhiyuan@huawei.com>
2024-12-23 16:57:05 +0800

    publish接口新增SEND_NOTIFICATION_CROSS_USER权限相关注释
    Signed-off-by: markYao <yaozhiyuan@huawei.com>

--- api/@ohos.notificationManager.d.ts
+++ api/@ohos.notificationManager.d.ts
@@ -268,6 +268,34 @@ declare namespace notificationManager {
    * @systemapi
    * @since 11
    */
+  /**
+   * Publishes a notification to the specified user.
+   *
+   * @permission ohos.permission.NOTIFICATION_CONTROLLER or ohos.permission.SEND_NOTIFICATION_CROSS_USER
+   * @param { NotificationRequest } request - a notification.
+   * @param { number } userId - of subscriber receiving the notification.
+   * @param { AsyncCallback<void> } callback - The callback of publish.
+   * @throws { BusinessError } 201 - Permission denied.
+   * @throws { BusinessError } 202 - Not system application to call the interface.
+   * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified.
+   * <br>2. Incorrect parameter types. 3. Parameter verification failed.
+   * @throws { BusinessError } 1600001 - Internal error.
+   * @throws { BusinessError } 1600002 - Marshalling or unmarshalling error.
+   * @throws { BusinessError } 1600003 - Failed to connect to the service.
+   * @throws { BusinessError } 1600004 - Notification disabled.
+   * @throws { BusinessError } 1600005 - Notification slot disabled.
+   * @throws { BusinessError } 1600007 - The notification does not exist.
+   * @throws { BusinessError } 1600008 - The user does not exist.
+   * @throws { BusinessError } 1600009 - The notification sending frequency reaches the upper limit.
+   * @throws { BusinessError } 1600012 - No memory space.
+   * @throws { BusinessError } 1600014 - No permission.
+   * @throws { BusinessError } 1600015 - The current notification status does not support duplicate configurations.
+   * @throws { BusinessError } 1600016 - The notification version for this update is too low.
+   * @throws { BusinessError } 2300007 - Network unreachable.
+   * @syscap SystemCapability.Notification.Notification
+   * @systemapi
+   * @since 16
+   */
   function publish(request: NotificationRequest, userId: number, callback: AsyncCallback<void>): void;
 
   /**
@@ -320,6 +348,34 @@ declare namespace notificationManager {
    * @syscap SystemCapability.Notification.Notification
    * @systemapi
    * @since 11
+   */
+  /**
+   * Publishes a notification to the specified user.
+   *
+   * @permission ohos.permission.NOTIFICATION_CONTROLLER or ohos.permission.SEND_NOTIFICATION_CROSS_USER
+   * @param { NotificationRequest } request - a notification.
+   * @param { number } userId - of subscriber receiving the notification.
+   * @returns { Promise<void> } The promise returned by the function.
+   * @throws { BusinessError } 201 - Permission denied.
+   * @throws { BusinessError } 202 - Not system application to call the interface.
+   * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified.
+   * <br>2. Incorrect parameter types. 3. Parameter verification failed.
+   * @throws { BusinessError } 1600001 - Internal error.
+   * @throws { BusinessError } 1600002 - Marshalling or unmarshalling error.
+   * @throws { BusinessError } 1600003 - Failed to connect to the service.
+   * @throws { BusinessError } 1600004 - Notification disabled.
+   * @throws { BusinessError } 1600005 - Notification slot disabled.
+   * @throws { BusinessError } 1600007 - The notification does not exist.
+   * @throws { BusinessError } 1600008 - The user does not exist.
+   * @throws { BusinessError } 1600009 - The notification sending frequency reaches the upper limit.
+   * @throws { BusinessError } 1600012 - No memory space.
+   * @throws { BusinessError } 1600014 - No permission.
+   * @throws { BusinessError } 1600015 - The current notification status does not support duplicate configurations.
+   * @throws { BusinessError } 1600016 - The notification version for this update is too low.
+   * @throws { BusinessError } 2300007 - Network unreachable.
+   * @syscap SystemCapability.Notification.Notification
+   * @systemapi
+   * @since 16
    */
   function publish(request: NotificationRequest, userId: number): Promise<void>;
 
