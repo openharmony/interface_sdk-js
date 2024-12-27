df1eb97b2dcaeac9fc8b7b9bc3e5d50a57badaa3
wangxiuxiu96 <wangxiuxiu9@huawei.com>
2024-12-27 10:26:28 +0800

    navdestinationinfo inner
    Signed-off-by: wangxiuxiu96 <wangxiuxiu9@huawei.com>
    Change-Id: Iffb00e4807a80a2fdb1acc92228fe66e00d9a50d

--- api/@internal/component/ets/common.d.ts
+++ api/@internal/component/ets/common.d.ts
@@ -23033,6 +23033,18 @@ declare class CustomComponent extends CommonAttribute {
    * @since 12
    */
   queryNavDestinationInfo(): NavDestinationInfo | undefined;
+  
+  /**
+   * Queries the navigation destination information.
+   *
+   * @param { Optional<boolean> } [isInner]
+   * @returns { NavDestinationInfo | undefined } The navigation destination information, or undefined if it is not available.
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 16
+   */
+  queryNavDestinationInfo(isInner: Optional<boolean>): NavDestinationInfo | undefined;
 
   /**
    * Query the navigation information of the current custom component.
