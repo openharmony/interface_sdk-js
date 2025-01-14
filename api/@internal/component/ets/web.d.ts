0216eca9d2dddb3a405326dd0844139d72fbb63b
zhufenghao <zhufenghao2@huawei.com>
2025-01-14 11:39:24 +0800

    新增SelectionMenuOptionsExt可选参数PreviewMenuOptions
    Signed-off-by: zhufenghao <zhufenghao2@huawei.com>

--- api/@internal/component/ets/web.d.ts
+++ api/@internal/component/ets/web.d.ts
@@ -6304,6 +6304,24 @@ declare enum WebResponseType {
 }
 
 /**
+ * Defines the options of preview menu
+ *
+ * @interface PreviewMenuOptions
+ * @syscap SystemCapability.Web.Webview.Core
+ * @since 18
+ */
+declare interface PreviewMenuOptions {
+  /**
+   * Defines the haptic feedback mode of preview menu.
+   *
+   * @type { ?HapticFeedbackMode }
+   * @syscap SystemCapability.Web.Webview.Core
+   * @since 18
+   */
+  hapticFeedbackMode?: HapticFeedbackMode;
+}
+
+/**
  * Defines the selection menu options.
  *
  * @typedef SelectionMenuOptionsExt
@@ -6346,6 +6364,15 @@ declare interface SelectionMenuOptionsExt {
    * @since 13
    */
   menuType?: MenuType;
+
+  /**
+   * Defines the options of preview menu.
+   *
+   * @type { ?PreviewMenuOptions }
+   * @syscap SystemCapability.Web.Webview.Core
+   * @since 18
+   */
+  previewMenuOptions?: PreviewMenuOptions;
 }
 
 /**
@@ -8867,7 +8894,7 @@ declare class WebAttribute extends CommonMethod<WebAttribute> {
 
   /**
    * Sets whether to optimize parser budget to reduce FCP time
-   * 
+   *
    * @param { boolean} optimizeParserBudget Default value is false, set true to enable optimize parser budget.
    * @returns { WebAttribute }
    * @syscap SystemCapability.Web.Webview.Core
