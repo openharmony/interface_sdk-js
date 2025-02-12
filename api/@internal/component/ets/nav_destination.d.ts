16314010235e611925f29f3c385df5376884508e
tsj_2020 <tongshijia@huawei.com>
2025-02-12 09:56:50 +0800

    fix onResult api version
    Signed-off-by: tsj_2020 <tongshijia@huawei.com>

--- api/@internal/component/ets/nav_destination.d.ts
+++ api/@internal/component/ets/nav_destination.d.ts
@@ -853,7 +853,7 @@ declare class NavDestinationAttribute extends CommonMethod<NavDestinationAttribu
    * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @crossplatform
    * @atomicservice
-   * @since 16
+   * @since 15
    */
   onResult(callback: Optional<Callback<ESObject>>): NavDestinationAttribute;
 
