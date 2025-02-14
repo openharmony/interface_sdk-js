426ae3a735d4676c55c57edd937b57adeaf51ee7
yangbiao199318 <yangbiao63@huawei-partners.com>
2025-02-14 18:34:25 +0800

    Select下拉菜单避让规则优化
    Signed-off-by: yangbiao199318 <yangbiao63@huawei-partners.com>

--- api/@internal/component/ets/select.d.ts
+++ api/@internal/component/ets/select.d.ts
@@ -279,6 +279,35 @@ declare enum MenuAlignType {
 }
 
 /**
+ * Enumeration of avoidance modes for the Select dropdown menu
+ * @enum { number }
+ * @syscap SystemCapability.ArkUI.ArkUI.Full
+ * @crossplatform
+ * @atomicservice
+ * @since 18
+ */
+declare enum AvoidanceMode {
+  /**
+   * The value covers the target component.
+   *
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 18
+   */
+  COVER_TARGET,
+  /**
+   * This value is displayed in the maximum space.(Compress the scrollbar)
+   *
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 18
+   */
+  AVOID_AROUND_TARGET
+}
+
+/**
  * Callback of selecting an item from the select event.
  * 
  * @typedef {function} OnSelectCallback
@@ -1118,6 +1147,18 @@ declare class SelectAttribute extends CommonMethod<SelectAttribute> {
    * @since 18
    */
   dividerStyle(style: Optional<DividerStyleOptions>): SelectAttribute;
+
+  /**
+   * Set the select menu avoidance mode
+   *
+   * @param { AvoidanceMode } mode - Enumeration value of the avoidance mode
+   * @returns { SelectAttribute } Returns the chained object of Select component attributes
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 18
+   */
+  avoidance(mode: AvoidanceMode): SelectAttribute;
 }
 
 /**
