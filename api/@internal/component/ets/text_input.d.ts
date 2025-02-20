5075fbdb2d88e7029c9514d10ff00000a379d5e9
zhouyan <zhouyan162@huawei.com>
2025-02-20 09:58:40 +0800

    [挑单5.0.3]onWillChange需求相关api
    Signed-off-by: zhouyan <zhouyan162@huawei.com>

--- api/@internal/component/ets/text_input.d.ts
+++ api/@internal/component/ets/text_input.d.ts
@@ -2481,6 +2481,18 @@ declare class TextInputAttribute extends CommonMethod<TextInputAttribute> {
    * @since 15
    */
   keyboardAppearance(appearance: Optional<KeyboardAppearance>): TextInputAttribute;
+
+  /**
+   * Get text value information when about to change.
+   *
+   * @param { Callback<EditableTextChangeValue, boolean> } callback - The triggered function when text content is about to change.
+   * @returns { TextInputAttribute }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 15
+   */
+  onWillChange(callback: Callback<EditableTextChangeValue, boolean>): TextInputAttribute;
 }
 
 /**
