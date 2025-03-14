8e466da1c5f34a71e8dfd68bd4317f87a8eb5cb1
刘波 <liubo419@huawei.com>
2025-03-14 07:03:52 +0000

    update api/@ohos.display.d.ts.
    Signed-off-by: 刘波 <liubo419@huawei.com>

--- api/@ohos.display.d.ts
+++ api/@ohos.display.d.ts
@@ -576,7 +576,7 @@ declare namespace display {
    * Create virtual screen.
    *
    * @permission ohos.permission.ACCESS_VIRTUAL_SCREEN
-   * @param { VirtualScreenOption } options Indicates the options of the virtual screen.
+   * @param { VirtualScreenOptions } options Indicates the options of the virtual screen.
    * @returns { Promise<number> } Promise used to return the created virtual screen id
    * @throws { BusinessError } 201 - Permission verification failed. The application does not have the permission required to call the API.
    * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified.
