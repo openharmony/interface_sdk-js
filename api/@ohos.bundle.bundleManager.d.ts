e272a61c0a7e73b3fcdef51530159b910a3e1ada
liumingyue <liumingyue12@huawei.com>
2025-03-18 14:45:10 +0800

    新增插件查询接口
    Signed-off-by: liumingyue <liumingyue12@huawei.com>

--- api/@ohos.bundle.bundleManager.d.ts
+++ api/@ohos.bundle.bundleManager.d.ts
@@ -23,6 +23,7 @@ import type { ApplicationInfo as _ApplicationInfo, ModuleMetadata as _ModuleMeta
   PreinstalledApplicationInfo as _PreinstalledApplicationInfo } from './bundleManager/ApplicationInfo';
 import { Metadata as _Metadata } from './bundleManager/Metadata';
 import { PermissionDef as _PermissionDef } from './bundleManager/PermissionDef';
+import { PluginBundleInfo as _PluginBundleInfo, PluginModuleInfo as _PluginModuleInfo} from './bundleManager/PluginBundleInfo';
 import { ElementName as _ElementName } from './bundleManager/ElementName';
 import { SharedBundleInfo as _SharedBundleInfo } from './bundleManager/SharedBundleInfo';
 import type { RecoverableApplicationInfo as _RecoverableApplicationInfo } from './bundleManager/RecoverableApplicationInfo';
@@ -3470,6 +3471,24 @@ declare namespace bundleManager {
   function getAppCloneIdentity(uid: number): Promise<AppCloneIdentity>;
 
   /**
+   * Get all plugin info of host application.
+   *
+   * @permission ohos.permission.GET_BUNDLE_INFO_PRIVILEGED
+   * @param { string } hostBundleName - Indicates the host application bundle name to be queried.
+   * @param { number } [userId] - Indicates the user ID or do not pass user ID.
+   * @returns { Promise<Array<PluginBundleInfo>> } Returns PluginBundleInfo Arrays.
+   * @throws { BusinessError } 201 - Permission denied.
+   * @throws { BusinessError } 202 - Permission denied, non-system app called system api.
+   * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified; 2. Incorrect parameter types.
+   * @throws { BusinessError } 17700001 - The specified bundleName is not found.
+   * @throws { BusinessError } 17700004 - The specified user ID is not found.
+   * @syscap SystemCapability.BundleManager.BundleFramework.Core
+   * @systemapi
+   * @since 18
+   */
+  function getAllPluginInfo(hostBundleName: string, userId?: number): Promise<Array<PluginBundleInfo>>;
+
+  /**
    * Migrate files from the source paths to the destination path.
    * 
    * @permission ohos.permission.MIGRATE_DATA
@@ -3838,6 +3857,26 @@ declare namespace bundleManager {
    * @since 12
    */
   export type PreinstalledApplicationInfo = _PreinstalledApplicationInfo;
+
+  /**
+   * Indicates the information about a plugin.
+   *
+   * @typedef { _PluginBundleInfo  }
+   * @syscap SystemCapability.BundleManager.BundleFramework.Core
+   * @systemapi
+   * @since 18
+   */
+  export type PluginBundleInfo = _PluginBundleInfo;
+
+  /**
+   * Indicates the plugin module info.
+   *
+   * @typedef { _PluginModuleInfo  }
+   * @syscap SystemCapability.BundleManager.BundleFramework.Core
+   * @systemapi
+   * @since 18
+   */
+  export type PluginModuleInfo = _PluginModuleInfo;
 }
 
 export default bundleManager;
