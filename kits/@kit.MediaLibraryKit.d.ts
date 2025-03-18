cf24a0b5d6e7ef8aeeeb1f6b27ab6659a07e78d5
pengpeng7872 <pengxieyu@huawei.com>
2025-03-18 14:35:07 +0000

    新增COLOR_STYTLE_PHOTO
    Signed-off-by: pengpeng7872 <pengxieyu@huawei.com>

--- kits/@kit.MediaLibraryKit.d.ts
+++ kits/@kit.MediaLibraryKit.d.ts
@@ -21,7 +21,7 @@
 import photoAccessHelper from '@ohos.file.photoAccessHelper';
 import sendablePhotoAccessHelper from '@ohos.file.sendablePhotoAccessHelper';
 import { MovingPhotoView, MovingPhotoViewController, MovingPhotoViewAttribute } from '@ohos.multimedia.movingphotoview';
-import { AlbumPickerComponent, AlbumPickerOptions, AlbumInfo, EmptyAreaClickCallback } from '@ohos.file.AlbumPickerComponent';
+import { AlbumPickerComponent, AlbumPickerOptions, AlbumInfo } from '@ohos.file.AlbumPickerComponent';
 import {
     PhotoPickerComponent, PickerController, PickerOptions,
     DataType, BaseItemInfo, ItemInfo, PhotoBrowserInfo, AnimatorParams,
@@ -39,7 +39,7 @@ export {
     DataType, BaseItemInfo, ItemInfo, PhotoBrowserInfo, AnimatorParams,
     MaxSelected, ItemType, ClickType, PickerOrientation,
     SelectMode, PickerColorMode, ReminderMode, MaxCountType, PhotoBrowserRange,
-    AlbumPickerComponent, AlbumPickerOptions, AlbumInfo, EmptyAreaClickCallback,
+    AlbumPickerComponent, AlbumPickerOptions, AlbumInfo,
     RecentPhotoComponent, RecentPhotoCheckResultCallback,
     RecentPhotoClickCallback, RecentPhotoOptions, PhotoSource
 };
