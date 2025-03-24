92ed2befdce497956f48bd9c8c171f1a7ccbe420
张珂 <zhangke172@huawei.com>
2025-03-24 07:05:24 +0000

    update api/@ohos.deviceInfo.d.ts.
    Signed-off-by: 张珂 <zhangke172@huawei.com>

--- api/@ohos.deviceInfo.d.ts
+++ api/@ohos.deviceInfo.d.ts
@@ -713,7 +713,6 @@ declare namespace deviceInfo {
    * Obtaining the performance class.
    *
    * @syscap SystemCapability.Startup.SystemInfo
-   * @crossplatform
    * @since 18
    */
   const performanceClass: PerformanceClassLevel;
