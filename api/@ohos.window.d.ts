37250702b7e64143ed139a7a492060adbe9e8406
zzz <zhengleran@huawei.com>
2025-03-29 06:58:21 +0000

    update api/@ohos.window.d.ts.
    Signed-off-by: zzz <zhengleran@huawei.com>

--- api/@ohos.window.d.ts
+++ api/@ohos.window.d.ts
@@ -3078,7 +3078,7 @@ declare namespace window {
    * @permission ohos.permission.VISIBLE_WINDOW_INFO
    * @returns { Promise<Array<WindowInfo>> } - Promise that returns windowInfo list.
    * @throws { BusinessError } 201 - Permission verification failed. The application does not have the permission required to call the API.
-   * @throws { BusinessError } 801 - Capability not supported. Failed to call the API due to limited device capabilities.
+   * @throws { BusinessError } 801 - Capability not supported. Function getVisibleWindowInfo can not work correctly due to limited device capabilities.
    * @throws { BusinessError } 1300003 - This window manager service works abnormally.
    * @syscap SystemCapability.Window.SessionManager
    * @since 18
