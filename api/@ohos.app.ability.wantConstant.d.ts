cb919a78d68d6c4e10a73cfbc3070f612e568628
wangzhen <wangzhen416@huawei.com>
2025-04-01 17:52:31 +0800

    Add flag to indicate starting a plugin ability
    Signed-off-by: wangzhen <wangzhen416@huawei.com>
    Change-Id: Id502d98484ad8984f7365e1471462bc89b5517fd

--- api/@ohos.app.ability.wantConstant.d.ts
+++ api/@ohos.app.ability.wantConstant.d.ts
@@ -346,6 +346,14 @@ declare namespace wantConstant {
     LAUNCH_REASON_MESSAGE = 'ohos.params.launchReasonMessage',
 
     /**
+    * Indicates the dest ability refers to a plugin ability.
+    *
+    * @syscap SystemCapability.Ability.AbilityBase
+    * @since 18
+    */
+    DESTINATION_PLUGIN_ABILITY = 'ohos.params.pluginAbility',
+
+    /**
     * Indicates the param of the bundleName Trust list when implicitly launching applications.
     *
     * @syscap SystemCapability.Ability.AbilityBase
