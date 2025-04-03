7b8055a05f2fc756b56ab4568fde4d288a6b3f65
guozejun <guozejun@huawei.com>
2025-04-03 17:05:18 +0800

    ArkUI SDK converter plugin
    Signed-off-by: guozejun <guozejun@huawei.com>
    Change-Id: I3169388b304ec00e1e855065bb05d490b98098f8

---
+++ build-tools/arkui_transformer/src/arkui_config_util.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as fs from "fs"
+
+export interface ArkUIConfig {
+    components: Array<string>
+}
+
+export class ArkUIConfigUtil {
+    static instance: ArkUIConfigUtil = new ArkUIConfigUtil
+    constructor() {
+        this.config = JSON.parse(fs.readFileSync("./config/arkui_config.json", 'utf-8'))
+        this.config.components.forEach(c => {
+            this.componentSet.add(c)
+        })
+    }
+    private config: ArkUIConfig
+    public componentSet: Set<string> = new Set
+}
+
+export default ArkUIConfigUtil.instance
