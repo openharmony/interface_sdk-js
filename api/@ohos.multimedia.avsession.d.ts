a2f30773b0eb43b9ea9d9370a30db427c7f6ba31
ccfriend <chengcheng14@huawei.com>
2025-04-09 10:57:04 +0800

    modify lyric to 17
    Signed-off-by: ccfriend <chengcheng14@huawei.com>

--- api/@ohos.multimedia.avsession.d.ts
+++ api/@ohos.multimedia.avsession.d.ts
@@ -4345,7 +4345,7 @@ declare namespace avSession {
      * @type { ?string }
      * @syscap SystemCapability.Multimedia.AVSession.Core
      * @atomicservice
-     * @since 18
+     * @since 17
      */
     singleLyricText?: string;
 
