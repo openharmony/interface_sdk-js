7fa31b8610b325a2e5037cc73ffe2db1a86ebc6b
guoyi <guoyi39@huawei.com>
2025-04-15 23:30:24 +0800

    修复since
    Signed-off-by: guoyi <guoyi39@huawei.com>

--- api/@ohos.distributedDeviceManager.d.ts
+++ api/@ohos.distributedDeviceManager.d.ts
@@ -1124,7 +1124,7 @@ declare namespace distributedDeviceManager {
      * @throws { BusinessError } 11600102 - Failed to obtain service.
      * @syscap SystemCapability.DistributedHardware.DeviceManager
      * @systemapi
-     * @since 15
+     * @since 18
      */
     putDeviceProfileInfoList(deviceProfileInfoList: Array<DeviceProfileInfo>): Promise<number>;
 
