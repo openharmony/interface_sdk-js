50047bd5de633dbb4f890eb91321f84324966547
yyuanche <yuancheng25@huawei.com>
2025-04-16 15:49:01 +0800

    函数一致性整改 Signed-off-by: yyuanche <yuancheng25@huawei.com>

--- api/@internal/component/ets/common.d.ts
+++ api/@internal/component/ets/common.d.ts
@@ -15000,6 +15000,7 @@ declare interface DismissPopupAction {
 declare interface PopupStateChangeParam {
   /**
    * is Visible.
+   * Anonymous Object Rectification.
    *
    * @type { boolean }
    * @syscap SystemCapability.ArkUI.ArkUI.Full
@@ -15034,6 +15035,7 @@ declare type PopupStateChangeCallback = (event: PopupStateChangeParam) => void;
 declare interface PopupMaskType {
   /**
    * Color.
+   * Anonymous Object Rectification.
    *
    * @type { ResourceColor }
    * @syscap SystemCapability.ArkUI.ArkUI.Full
