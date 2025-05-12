3f335a8d4cbae334a11b972766dea00e15f7fda0
liangxinghui <liangxinghui1@h-partners.com>
2025-05-12 08:49:51 +0000

    fix code 2
    Signed-off-by: liangxinghui <liangxinghui1@h-partners.com>
    Change-Id: I5650babc75d2a3edfa853e1e3b600a0585afa14a

--- api/@ohos.window.d.ts
+++ api/@ohos.window.d.ts
@@ -8941,6 +8941,8 @@ declare namespace window {
     /**
      * Set window transition animation
      *
+     * @param { WindowTransitionType } transitionType - Transition animation type.
+     * @param { TransitionAnimation } animation - Transition animation config.
      * @returns { Promise<void> } Promise that returns no value.
      * @throws { BusinessError } 801 - Capability not supported. Failed to call the API due to limited device capabilities.
      * @throws { BusinessError } 1300002 - This window state is abnormal.
@@ -8958,7 +8960,8 @@ declare namespace window {
     /**
      * Get window transition animation configuration
      *
-     * @returns { TransitionAnimation | undefined } Window transition animation configuration. Undefined means no configuration.
+     * @param { WindowTransitionType } transitionType - Transition animation type.
+     * @returns { TransitionAnimation | undefined } Transition animation with transition type, or undefined if it has not been set.
      * @throws { BusinessError } 801 - Capability not supported. Failed to call the API due to limited device capabilities. 
      * @throws { BusinessError } 1300002 - This window state is abnormal.
      * @throws { BusinessError } 1300003 - This window manager service works abnormally.
