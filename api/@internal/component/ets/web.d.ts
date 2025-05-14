6bd6a272804b715234ba062442abb4fcd58b5e37
icaozhengfei <caozhengfei@huawei-partners.com>
2025-05-14 10:24:06 +0800

    add onActivateContent interface
    Signed-off-by: icaozhengfei <caozhengfei@huawei-partners.com>

--- api/@internal/component/ets/web.d.ts
+++ api/@internal/component/ets/web.d.ts
@@ -9584,6 +9584,16 @@ declare class WebAttribute extends CommonMethod<WebAttribute> {
    * @since 20
    */
   dataDetectorConfig(config: TextDataDetectorConfig): WebAttribute;
+
+  /**
+   * Triggered when the web page is activated for window.open called by other web component.
+   *
+   * @param { Callback<void> } callback the triggered function when the web page is activated for window.open called by other web component.
+   * @returns { WebAttribute }
+   * @syscap SystemCapability.Web.Webview.Core
+   * @since 20
+   */
+  onActivateContent(callback: Callback<void>): WebAttribute;
 }
 
 /**
