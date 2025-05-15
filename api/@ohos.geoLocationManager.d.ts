2b79bfa9b99c62d7e52c709ecd27d87d1faab70c
WangTianhe8 <wangtianhe8@huawei.com>
2025-05-15 15:09:12 +0800

    POI-API20
    Signed-off-by: WangTianhe8 <wangtianhe8@huawei.com>

--- api/@ohos.geoLocationManager.d.ts
+++ api/@ohos.geoLocationManager.d.ts
@@ -1305,6 +1305,32 @@ declare namespace geoLocationManager {
   function getDistanceBetweenLocations(location1: Location, location2: Location): number;
 
   /**
+   * Check whether the POI service is supported.
+   *
+   * @returns { boolean } Returns {@code true} if POI service is available, returns {@code false} otherwise.
+   * @syscap SystemCapability.Location.Location.Core
+   * @atomicservice
+   * @since 20
+   */
+  function isPoiServiceSupported(): boolean;
+
+  /**
+   * Obtaining POI Information.
+   *
+   * @permission ohos.permission.LOCATION and ohos.permission.APPROXIMATELY_LOCATION
+   * @returns { Promise<PoiInfo> } The promise returned by the function, for reporting POI info.
+   * 
+   * @throws { BusinessError } 201 - Permission verification failed. The application does not have the permission required to call the API.
+   * @throws { BusinessError } 801 - Capability not supported. Failed to call ${geoLocationManager.getPoiInfo} due to limited device capabilities.
+   * @throws { BusinessError } 3301000 - The location service is unavailable.
+   * @throws { BusinessError } 3301100 - The location switch is off.
+   * @syscap SystemCapability.Location.Location.Core
+   * @atomicservice
+   * @since 20
+   */
+  function getPoiInfo(): Promise<PoiInfo>;
+
+  /**
    * Configuration parameters for simulating reverse geocoding.
    *
    * @typedef ReverseGeocodingMockInfo
