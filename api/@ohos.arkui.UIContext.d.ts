2493c4c5bd8215ea7702fd05cf711c3bf037f37b
wangweiyuan <wangweiyuan2@huawei-partners.com>
2025-05-20 17:46:10 +0800

    uicontext有效性接口
    Signed-off-by: wangweiyuan <wangweiyuan2@huawei-partners.com>

--- api/@ohos.arkui.UIContext.d.ts
+++ api/@ohos.arkui.UIContext.d.ts
@@ -3188,6 +3188,17 @@ export class ComponentSnapshot {
  * @since 11
  */
 export class UIContext {
+      /**
+   * Checks whether the UiContext object ia available.
+   *
+   * @returns { boolean } Returns true if the UIConetxt object is available.
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 20
+   */
+  isAvailable(): boolean;
+
   /**
    * get object font.
    *
