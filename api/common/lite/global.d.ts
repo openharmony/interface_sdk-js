4d931e82814712207902dcaae9b3c773ffcb9e7b
zzz701 <lihaima1@huawei.com>
2025-05-24 11:33:27 +0800

    修改接口描述
    Signed-off-by: zzz701 <lihaima1@huawei.com>

--- api/common/lite/global.d.ts
+++ api/common/lite/global.d.ts
@@ -109,14 +109,22 @@ export declare const LITE: string;
 export declare function canIUse(syscap: string): boolean;
 
 /**
- * Defining apiversion function.
+ * determine whether the current operating system version is greater than or equal to the given value.
  *
- * @param { string } apiversion
- * @returns { boolean }
- * @syscap SystemCapability.ArkUI.ArkUI.Full
- * @crossplatform
- * @atomicservice
+ * @param { string } apiVersion - Only major version can be passed in, such as "19";
+ *   major and minor version can be passed in, such as "19.1"; major minor and patch
+ *   version can be passed in, such as "19.1.2"
+ * @returns { boolean } true - operating system version is greater than or equal to the given value
+ *                      false - operating system version is less than the given value or invalid api version
+ * @syscap SystemCapability.Startup.SystemInfo.Lite
  * @since 19
+ * @example
+ * if (isApiVersionGreaterOrEqual("19.1")) {
+ *    // Use 19.1 APIs.
+ * } else {
+ *    // Alternative code for earlier versions.
+ * }
+ *
  */
 export declare function isApiVersionGreaterOrEqual(apiversion: string): boolean;
 
