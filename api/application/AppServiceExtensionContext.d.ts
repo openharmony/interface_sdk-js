35dea4d8fa4c090afa933835822b19e8197c83cf
zhangzezhong <zhangzezhong8@huawei-partners.com>
2025-05-27 11:40:21 +0800

    startAbility interface 0526
    Signed-off-by: zhangzezhong <zhangzezhong8@huawei-partners.com>

--- api/application/AppServiceExtensionContext.d.ts
+++ api/application/AppServiceExtensionContext.d.ts
@@ -21,6 +21,7 @@
 import ExtensionContext from './ExtensionContext';
 import { ConnectOptions } from '../ability/connectOptions';
 import Want from '../@ohos.app.ability.Want';
+import StartOptions from '../@ohos.app.ability.StartOptions';
 
 /**
  * The context of app service extension. It allows access to AppServiceExtension-specific resources.
@@ -66,6 +67,43 @@ export default class AppServiceExtensionContext extends ExtensionContext {
    * @since 20
    */
   disconnectServiceExtensionAbility(connection: number): Promise<void>;
+
+  /**
+   * Start a UIAbility.
+   * If the target ability is visible, you can start the target ability: If the target ability is invisible,
+   * you need to apply for permission:ohos.pernission.START_INVISIBLE_ABILITY to start target invisible ability.
+   * If the target ability is in cross-device, you need to appply for permission:ohos.pernission.DISTRIBUTED_DATASYNC.
+   *
+   * @param { Want } want - Indicates the ability to start.
+   * @param { StartOptions } [options] - Indicates the start options.
+   * @returns { Promise<void> } The promise returned by the function.
+   * @throws { BusinessError } 201 - The application does not have permission to call the interface.
+   * @throws { BusinessError } 16000001 - The specified ability does not exist.
+   * @throws { BusinessError } 16000002 - Incorrect ability type.
+   * @throws { BusinessError } 16000004 - Failed to start theinvisible ability.
+   * @throws { BusinessError } 16000005 - The specified process does not have the permission.
+   * @throws { BusinessError } 16000008 - The crowdtesting application expires.
+   * @throws { BusinessError } 16000009 - An ability cannot be started or stopped in Wukong mode.
+   * @throws { BusinessError } 16000010 - The call with the continuation and prepare continuation flag is forbidden.
+   * @throws { BusinessError } 16000011 - The context does not exist.
+   * @throws { BusinessError } 16000012 - The application is controlled.
+   * @throws { BusinessError } 16000013 - The application is controlled by EDM.
+   * @throws { BusinessError } 16000019 - No natching abiliity is found.
+   * @throws { BusinessError } 16000050 - Internal error.
+   * @throws { BusinessError } 16000055 - Installation-free timed out.
+   * @throws { BusinessError } 16000071 - App clone is not supported.
+   * @throws { BusinessError } 16000072 - App clone or multi-instance is not supported.
+   * @throws { BusinessError } 16000073 - The app clone index is invalid.
+   * @throws { BusinessError } 16000076 - The app instance key is invalid.
+   * @throws { BusinessError } 16000077 - The number of app instances reaches the limit.
+   * @throws { BusinessError } 16000078 - The multi-instance is not supported.
+   * @throws { BusinessError } 16000079 - The APP_INSTANCE_KEY cannot be specified.
+   * @throws { BusinessError } 16000080 - Creating a new instance is not supported.
+   * @syscap SystemCapability.Ability.AbilityRuntime.Core
+   * @stagemodelonly
+   * @since 20
+   */
+  startAbility(want: Want, options?: StartOptions): Promise<void>;
 
   /**
    * Destroys this app service extension.
