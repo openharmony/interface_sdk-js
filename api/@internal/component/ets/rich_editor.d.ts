1ab366c0ac1884971e18dfed3f61c92c2f5d4a59
zyx0121 <zhangyingxiang2@huawei.com>
2025-06-03 14:47:43 +0800

    RichEditor 1.2 Callback to VoidCallback
    Signed-off-by: zyx0121 <zhangyingxiang2@huawei.com>

--- api/@internal/component/ets/rich_editor.d.ts
+++ api/@internal/component/ets/rich_editor.d.ts
@@ -21,7 +21,7 @@
 /*** if arkts 1.2 */
 import {  ShadowOptions, ClickEvent, SelectionOptions, CommonMethod, Callback, HapticFeedbackMode, RectResult, HoverEvent, PixelMap } from './common';
 import { CustomBuilder } from './builder';
-import { ResourceColor, Length, ResourceStr, Dimension, Margin, BorderRadiuses, Font } from './units';
+import { ResourceColor, Length, ResourceStr, Dimension, Margin, BorderRadiuses, Font, VoidCallback } from './units';
 import { FontStyle, FontWeight, TextAlign, WordBreak, LineBreakStrategy, ImageSpanAlignment, ImageFit, ResponseType, CopyOptions, BarState } from './enums';
 import { DecorationStyleInterface, StyledString, MutableStyledString } from './styledString';
 import { Resource } from './../../../api/global/resource';
@@ -731,10 +731,21 @@ declare interface PasteEvent {
    * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @crossplatform
    * @atomicservice
-   * @since arkts {'1.1':'12','1.2':'20'}
-   * @arkts 1.1&1.2
+   * @since 12
    */
   preventDefault?: Callback<void>;
+
+  /**
+   * Override system paste event.
+   *
+   * @type { ?VoidCallback }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 20
+   * @arkts 1.2
+   */
+  preventDefault?: VoidCallback;
 }
 
 /**
@@ -3062,10 +3073,21 @@ declare interface SelectionMenuOptions {
    * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @crossplatform
    * @atomicservice
-   * @since arkts {'1.1':'12','1.2':'20'}
-   * @arkts 1.1&1.2
+   * @since 12
    */
   onDisappear?: Callback<void>;
+
+  /**
+   * Callback function when the selection menu disappears.
+   * 
+   * @type { ?VoidCallback }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 20
+   * @arkts 1.2
+   */
+  onDisappear?: VoidCallback;
 
   /**
    * Menu type, default value is MenuType.SELECTION_MENU.
@@ -3715,10 +3737,22 @@ declare class RichEditorAttribute extends CommonMethod<RichEditorAttribute> {
    * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @crossplatform
    * @atomicservice
-   * @since arkts {'1.1':'12','1.2':'20'}
-   * @arkts 1.1&1.2
+   * @since 12
    */
   onReady(callback: Callback<void>): RichEditorAttribute;
+
+  /**
+   * Called when on ready.
+   *
+   * @param { VoidCallback } callback - The triggered function when rich editor is ready.
+   * @returns { RichEditorAttribute }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 20
+   * @arkts 1.2
+   */
+  onReady(callback: VoidCallback): RichEditorAttribute;
 
   /**
    * Called when the content is selected.
@@ -3896,10 +3930,22 @@ declare class RichEditorAttribute extends CommonMethod<RichEditorAttribute> {
    * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @crossplatform
    * @atomicservice
-   * @since arkts {'1.1':'12','1.2':'20'}
-   * @arkts 1.1&1.2
+   * @since 12
    */
   onDeleteComplete(callback: Callback<void>): RichEditorAttribute;
+
+    /**
+   * Notify that the deletion has been completed
+   *
+   * @param { VoidCallback } callback - The triggered function when text content has been deleted.
+   * @returns { RichEditorAttribute }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 20
+   * @arkts 1.2
+   */
+  onDeleteComplete(callback: VoidCallback): RichEditorAttribute;
 
   /**
    * Allow replication.
@@ -4314,10 +4360,21 @@ declare interface CutEvent {
    * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @crossplatform
    * @atomicservice
-   * @since arkts {'1.1':'12','1.2':'20'}
-   * @arkts 1.1&1.2
+   * @since 12
    */
   preventDefault?: Callback<void>;
+
+  /**
+   * Prevent system cut event.
+   *
+   * @type { ?VoidCallback }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 20
+   * @arkts 1.2
+   */
+  preventDefault?: VoidCallback;
 }
 
 /**
@@ -4337,10 +4394,21 @@ declare interface CopyEvent {
    * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @crossplatform
    * @atomicservice
-   * @since arkts {'1.1':'12','1.2':'20'}
-   * @arkts 1.1&1.2
+   * @since 12
    */
   preventDefault?: Callback<void>;
+
+  /**
+   * Prevent system cut event.
+   *
+   * @type { ?VoidCallback }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 20
+   * @arkts 1.2
+   */
+  preventDefault?: VoidCallback;
 }
 
 /**
