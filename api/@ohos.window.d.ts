a86e3251de6aab952d44039830d66ce6766a3222
ZihaoWU <wuzihao11@huawei.com>
2025-06-04 12:04:34 +0800

    add code
    Signed-off-by: ZihaoWU <wuzihao11@huawei.com>

--- api/@ohos.window.d.ts
+++ api/@ohos.window.d.ts
@@ -3673,6 +3673,20 @@ declare namespace window {
   function setStartWindowBackgroundColor(moduleName: string, abilityName: string, color: ColorMetrics): Promise<void>;
 
   /**
+   * Notify screenshot event
+   *
+   * @param { ScreenshotEventType } eventType - Screenshot event name.
+   * @returns { Promise<void> } Promise that returns no value.
+   * @throws { BusinessError } 202 - Permission verification failed. A non-system application calls a system API.
+   * @throws { BusinessError } 1300003 - This window manager service works abnormally.
+   * @throws { BusinessError } 1300016 - Parameter error. Possible cause: 1. Invalid parameter range.
+   * @syscap SystemCapability.WindowManager.WindowManager.Core
+   * @systemapi Hide this for inner system use.
+   * @since 20
+   */
+  function notifyScreenshotEvent(eventType: ScreenshotEventType): Promise<void>;
+
+  /**
    * Display orientation
    *
    * @enum { number }
@@ -6839,6 +6853,30 @@ declare namespace window {
      * @since 12
      */
     off(type: 'screenshot', callback?: Callback<void>): void;
+
+    /**
+     * Register the callback of screenshot app event
+     *
+     * @param { 'screenshotAppEvent' } type - The value is fixed at 'screenshotAppEvent', indicating the screenshot app event.
+     * @param { Callback<ScreenshotEventType> } callback - Callback invoked when a screenshot app event occurs.
+     * @throws { BusinessError } 1300002 - This window state is abnormal.
+     * @throws { BusinessError } 1300003 - This window manager service works abnormally.
+     * @syscap SystemCapability.WindowManager.WindowManager.Core
+     * @since 20
+     */
+    on(type: 'screenshotAppEvent', callback: Callback<ScreenshotEventType>): void;
+
+    /**
+     * Unregister the callback of screenshot app event
+     *
+     * @param { 'screenshotAppEvent' } type - The value is fixed at 'screenshotAppEvent', indicating the screenshot app event.
+     * @param { Callback<ScreenshotEventType> } callback - Callback invoked when a screenshot app event occurs.
+     * @throws { BusinessError } 1300002 - This window state is abnormal.
+     * @throws { BusinessError } 1300003 - This window manager service works abnormally.
+     * @syscap SystemCapability.WindowManager.WindowManager.Core
+     * @since 20
+     */
+    off(type: 'screenshotAppEvent', callback?: Callback<ScreenshotEventType>): void;
 
     /**
      * Register the callback of dialogTargetTouch
@@ -11561,6 +11599,51 @@ declare namespace window {
      * @since 19
      */
     RELATIVE_TO_PARENT_WINDOW = 1
+  }
+
+  /**
+   * Screenshot event type
+   *
+   * @enum { number }
+   * @syscap SystemCapability.WindowManager.WindowManager.Core
+   * @since 20
+   */
+  enum ScreenshotEventType {
+    /**
+     * System screenshot
+     *
+     * @syscap SystemCapability.WindowManager.WindowManager.Core
+     * @since 20
+     */
+    SYSTEM_SCREENSHOT = 0,
+    /**
+     * System screenshot abort
+     *
+     * @syscap SystemCapability.WindowManager.WindowManager.Core
+     * @since 20
+     */
+    SYSTEM_SCREENSHOT_ABORT = 1,
+    /**
+     * Scroll shot start
+     *
+     * @syscap SystemCapability.WindowManager.WindowManager.Core
+     * @since 20
+     */
+    SCROLL_SHOT_START = 2,
+    /**
+     * Scroll shot end
+     *
+     * @syscap SystemCapability.WindowManager.WindowManager.Core
+     * @since 20
+     */
+    SCROLL_SHOT_END = 3,
+    /**
+     * Scroll shot abort
+     *
+     * @syscap SystemCapability.WindowManager.WindowManager.Core
+     * @since 20
+     */
+    SCROLL_SHOT_ABORT = 4,
   }
 
   /**
