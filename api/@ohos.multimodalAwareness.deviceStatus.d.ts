d98fb7ca3c3bd9093ef142eb76d334ed24962614
就是铸币捏 <tianruifeng@huawei.com>
2025-06-07 13:40:25 +0000

    update api/@ohos.multimodalAwareness.deviceStatus.d.ts.
    Signed-off-by: 就是铸币捏 <tianruifeng@huawei.com>

--- api/@ohos.multimodalAwareness.deviceStatus.d.ts
+++ api/@ohos.multimodalAwareness.deviceStatus.d.ts
@@ -118,21 +118,6 @@ declare namespace deviceStatus {
   function off(type: 'steadyStandingDetect', callback?: Callback<SteadyStandingStatus>): void;
 
   /**
-   * Unsubscribe to detect the steady standing status
-   * @param { 'steadyStandingDetect' } type - Indicates the event type.
-   * @param { Callback<SteadyStandingStatus> } callback - Indicates the callback for getting the event data.
-   * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified. 
-   * <br> 2. Incorrect parameter types. 3.Parameter verification failed.
-   * @throws { BusinessError } 801 - Capability not supported. Function can not work correctly due to limited
-   * <br> device capabilities.
-   * @throws { BusinessError } 32500001 - Service exception.
-   * @throws { BusinessError } 32500003 - Unsubscribe Failed.
-   * @syscap SystemCapability.MultimodalAwareness.DeviceStatus
-   * @since 18
-   */
-  function off(type: 'steadyStandingDetect', callback?: Callback<SteadyStandingStatus>): void;
-
-  /**
    * Get the device rotation radian
    * @returns { Promise<DeviceRotationRadian> } The result of device roatation radian.
    * @throws { BusinessError } 202 - Permission check failed. A non-system application uses the system API.
