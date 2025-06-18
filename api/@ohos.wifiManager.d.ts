e43ae3b17b009e7b85289f47b535584a0919d500
xionglei <xionglei3@huawei.com>
2025-06-18 09:13:13 +0000

    update api/@ohos.wifiManager.d.ts.
    Signed-off-by: xionglei <xionglei3@huawei.com>

--- api/@ohos.wifiManager.d.ts
+++ api/@ohos.wifiManager.d.ts
@@ -55,13 +55,15 @@ declare namespace wifiManager {
 
   /**
    * Disable Wi-Fi.
-   * @permission ohos.permission.SET_WIFI_INFO and (ohos.permission.MANAGE_WIFI_CONNECTION or ohos.permission.MANAGE_ENTERPRISE_WIFI_CONNECTION)
+   * @permission ohos.permission.SET_WIFI_INFO and ohos.permission.MANAGE_WIFI_CONNECTION
    * @throws {BusinessError} 201 - Permission denied.
+   * @throws {BusinessError} 202 - System API is not allowed called by Non-system application.
    * @throws {BusinessError} 801 - Capability not supported.
    * @throws {BusinessError} 2501000 - Operation failed.
    * @throws {BusinessError} 2501004 - Operation failed because the service is being opened.
    * @syscap SystemCapability.Communication.WiFi.STA
-   * @since 20
+   * @systemapi Hide this for inner system use.
+   * @since 9
    */
   function disableWifi(): void;
 
