921837156715906c49f29ce68ae9f0ba9ceb0484
hanKF <hanhu3@h-partners.com>
2025-06-18 02:58:00 +0000

    update api/@ohos.enterprise.systemManager.d.ts.
    Signed-off-by: hanKF <hanhu3@h-partners.com>

--- api/@ohos.enterprise.systemManager.d.ts
+++ api/@ohos.enterprise.systemManager.d.ts
@@ -663,8 +663,6 @@ declare namespace systemManager {
    * @throws { BusinessError } 9200001 - The application is not an administrator application of the device.
    * @throws { BusinessError } 9200002 - The administrator application does not have permission to manage the device.
    * @throws { BusinessError } 201 - Permission verification failed. The application does not have the permission required to call the API.
-   * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified;
-   *                                 2. Incorrect parameter types; 3. Parameter verification failed.
    * @syscap SystemCapability.Customization.EnterpriseDeviceManager
    * @stagemodelonly
    * @since 20
@@ -683,8 +681,6 @@ declare namespace systemManager {
    * @throws { BusinessError } 9200001 - The application is not an administrator application of the device.
    * @throws { BusinessError } 9200002 - The administrator application does not have permission to manage the device.
    * @throws { BusinessError } 201 - Permission verification failed. The application does not have the permission required to call the API.
-   * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified;
-   *                                 2. Incorrect parameter types; 3. Parameter verification failed.
    * @syscap SystemCapability.Customization.EnterpriseDeviceManager
    * @stagemodelonly
    * @since 20
