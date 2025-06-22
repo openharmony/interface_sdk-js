9d6d5e68f1699c50f4a0808835fbaef18abaeade
qianyong325 <qianyong15@h-partners.com>
2025-06-22 10:15:47 +0000

    update api/@ohos.inputMethod.d.ts.
    Signed-off-by: qianyong325 <qianyong15@h-partners.com>

--- api/@ohos.inputMethod.d.ts
+++ api/@ohos.inputMethod.d.ts
@@ -372,7 +372,7 @@ declare namespace inputMethod {
    * @since 20
    * @arkts 1.1&1.2
    */
-  function SetSimpleKeyboardEnabledSync(enable: boolean): void;
+  function SetSimpleKeyboardEnabled(enable: boolean): void;
 
   /**
    * @interface InputMethodSetting
