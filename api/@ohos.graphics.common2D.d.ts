a04f62876e616f0df67d0ba8700b551a415ab63f
yp9522 <liuwei793@h-partners.com>
2025-06-25 17:05:43 +0800

    添加2d的跨平台支持
    Signed-off-by: yp9522 <liuwei793@h-partners.com>
    Change-Id: I26870db7f046eaf9ae81b2276c7b3ee5bad7d4da

--- api/@ohos.graphics.common2D.d.ts
+++ api/@ohos.graphics.common2D.d.ts
@@ -24,12 +24,27 @@
  * @syscap SystemCapability.Graphics.Drawing
  * @since 11
  */
+/**
+ * The date structure that provides the basis for graphics.
+ *
+ * @namespace common2D
+ * @syscap SystemCapability.Graphics.Drawing
+ * @crossplatform
+ * @since 20
+ */
 declare namespace common2D {
   /**
    * Describes a color in ARGB format.
    * @typedef Color
    * @syscap SystemCapability.Graphics.Drawing
    * @since 11
+   */
+  /**
+   * Describes a color in ARGB format.
+   * @typedef Color
+   * @syscap SystemCapability.Graphics.Drawing
+   * @crossplatform
+   * @since 20
    */
   interface Color {
     /**
@@ -38,12 +53,26 @@ declare namespace common2D {
      * @syscap SystemCapability.Graphics.Drawing
      * @since 11
      */
+    /**
+     * Alpha component of the color. The value is an integer ranging from 0 to 255.
+     * @type { number }
+     * @syscap SystemCapability.Graphics.Drawing
+     * @crossplatform
+     * @since 20
+     */
     alpha: number;
     /**
      * Red component of the color. The value is an integer ranging from 0 to 255.
      * @type { number }
      * @syscap SystemCapability.Graphics.Drawing
      * @since 11
+     */
+    /**
+     * Red component of the color. The value is an integer ranging from 0 to 255.
+     * @type { number }
+     * @syscap SystemCapability.Graphics.Drawing
+     * @crossplatform
+     * @since 20
      */
     red: number;
     /**
@@ -52,12 +81,26 @@ declare namespace common2D {
      * @syscap SystemCapability.Graphics.Drawing
      * @since 11
      */
+    /**
+     * Green component of the color. The value is an integer ranging from 0 to 255.
+     * @type { number }
+     * @syscap SystemCapability.Graphics.Drawing
+     * @crossplatform
+     * @since 20
+     */
     green: number;
     /**
      * Blue component of the color. The value is an integer ranging from 0 to 255.
      * @type { number }
      * @syscap SystemCapability.Graphics.Drawing
      * @since 11
+     */
+    /**
+     * Blue component of the color. The value is an integer ranging from 0 to 255.
+     * @type { number }
+     * @syscap SystemCapability.Graphics.Drawing
+     * @crossplatform
+     * @since 20
      */
     blue: number;
   }
@@ -68,12 +111,26 @@ declare namespace common2D {
    * @syscap SystemCapability.Graphics.Drawing
    * @since 11
    */
+  /**
+   * Describes a rectangle, which can be defined by two coordinate points: upper left corner point and lower right corner point.
+   * @typedef Rect
+   * @syscap SystemCapability.Graphics.Drawing
+   * @crossplatform
+   * @since 20
+   */
   interface Rect {
     /**
      * X coordinate of the upper left corner of the rectangle. The value is a floating point number.
      * @type { number }
      * @syscap SystemCapability.Graphics.Drawing
      * @since 11
+     */
+    /**
+     * X coordinate of the upper left corner of the rectangle. The value is a floating point number.
+     * @type { number }
+     * @syscap SystemCapability.Graphics.Drawing
+     * @crossplatform
+     * @since 20
      */
     left: number;
     /**
@@ -82,6 +139,13 @@ declare namespace common2D {
      * @syscap SystemCapability.Graphics.Drawing
      * @since 11
      */
+    /**
+     * Y coordinate of the upper left corner of the rectangle. The value is a floating point number.
+     * @type { number }
+     * @syscap SystemCapability.Graphics.Drawing
+     * @crossplatform
+     * @since 20
+     */
     top: number;
     /**
      * X coordinate of the lower right corner of the rectangle. The value is a floating point number.
@@ -89,12 +153,26 @@ declare namespace common2D {
      * @syscap SystemCapability.Graphics.Drawing
      * @since 11
      */
+    /**
+     * X coordinate of the lower right corner of the rectangle. The value is a floating point number.
+     * @type { number }
+     * @syscap SystemCapability.Graphics.Drawing
+     * @crossplatform
+     * @since 20
+     */
     right: number;
     /**
      * Y coordinate of the lower right corner of the rectangle. The value is a floating point number.
      * @type { number }
      * @syscap SystemCapability.Graphics.Drawing
      * @since 11
+     */
+    /**
+     * Y coordinate of the lower right corner of the rectangle. The value is a floating point number.
+     * @type { number }
+     * @syscap SystemCapability.Graphics.Drawing
+     * @crossplatform
+     * @since 20
      */
     bottom: number;
   }
@@ -105,12 +183,26 @@ declare namespace common2D {
    * @syscap SystemCapability.Graphics.Drawing
    * @since 12
    */
+  /**
+   * Describes a coordinate point.
+   * @typedef Point
+   * @syscap SystemCapability.Graphics.Drawing
+   * @crossplatform
+   * @since 20
+   */
   interface Point {
     /**
      * Horizontal coordinate. The value is a floating point number.
      * @type { number }
      * @syscap SystemCapability.Graphics.Drawing
      * @since 12
+     */
+    /**
+     * Horizontal coordinate. The value is a floating point number.
+     * @type { number }
+     * @syscap SystemCapability.Graphics.Drawing
+     * @crossplatform
+     * @since 20
      */
     x: number;
 
@@ -119,6 +211,12 @@ declare namespace common2D {
      * @type { number }
      * @syscap SystemCapability.Graphics.Drawing
      * @since 12
+     */
+    /**
+     * Vertical coordinate. The value is a floating point number.
+     * @type { number }
+     * @crossplatform
+     * @since 20
      */
     y: number;
   }
@@ -131,12 +229,28 @@ declare namespace common2D {
    * @syscap SystemCapability.Graphics.Drawing
    * @since 12
    */
+  /**
+   * Describes a 3D coordinate point.
+   *
+   * @typedef Point3d
+   * @extends Point
+   * @syscap SystemCapability.Graphics.Drawing
+   * @crossplatform
+   * @since 20
+   */
   interface Point3d extends Point {
     /**
      * Z-axis coordinate. The value is a floating point number.
      * @type { number }
      * @syscap SystemCapability.Graphics.Drawing
      * @since 12
+     */
+    /**
+     * Z-axis coordinate. The value is a floating point number.
+     * @type { number }
+     * @syscap SystemCapability.Graphics.Drawing
+     * @crossplatform
+     * @since 20
      */
     z: number;
   }
