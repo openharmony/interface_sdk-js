86b0b9984b3080f3c1bb0d71bf8711524b2ff046
cq_0418 <chenqian73@huawei.com>
2025-07-02 15:31:38 +0800

    优化setRelativePositionToParentWindowEnabled接口描述
    Signed-off-by: cq_0418 <chenqian73@huawei.com>

--- api/@ohos.window.d.ts
+++ api/@ohos.window.d.ts
@@ -5571,15 +5571,13 @@ declare namespace window {
      * @param { number } offsetX - The x-axis offset between the anchor point of the first level sub window and the anchor point of the main window.
      * @param { number } offsetY - The y-axis offset between the anchor point of the first level sub window and the anchor point of the main window.
      * @returns { Promise<void> } Promise that returns no value.
-     * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified; 
-     *                                                                   2. Incorrect parameter types.
      * @throws { BusinessError } 801 - Capability not supported.
      *                                 Function setRelativePositionToParentWindowEnabled can not work correctly due to limited device capabilities.
      * @throws { BusinessError } 1300002 - This window state is abnormal.
      * @throws { BusinessError } 1300003 - This window manager service works abnormally.
      * @throws { BusinessError } 1300004 - Unauthorized operation.
+     * @throws { BusinessError } 1300016 - Parameter error. Possible cause: 1. Invalid parameter range.
      * @syscap SystemCapability.Window.SessionManager
-     * @atomicservice
      * @since 20
      */
     setRelativePositionToParentWindowEnabled(enabled: boolean, anchor?: WindowAnchor, offsetX?: number, offsetY?: number): Promise<void>;
