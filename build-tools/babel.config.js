54c26c351884df88cd3ad302448d34edb23bb2cc
yangbo_404 <yangbo198@huawei.com>
2025-07-04 14:27:44 +0800

    update common utils
    Signed-off-by: yangbo_404 <yangbo198@huawei.com>

--- build-tools/babel.config.js
+++ build-tools/babel.config.js
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,5 +13,28 @@
  * limitations under the License.
  */
 
-export * from './utils/api_check_wrapper_typedef';
-export * from './utils/api_check_wrapper_enums';
+module.exports = function(api) {
+  api.cache(true);
+
+  const presets = ['@babel/typescript'];
+  const plugins = [
+    '@babel/plugin-transform-modules-commonjs',
+    '@babel/plugin-proposal-class-properties',
+    [
+      '@babel/plugin-transform-arrow-functions',
+      {
+        spec: true
+      }
+    ]
+  ];
+  const ignore = [
+    '**/test/**', 
+    '**/node_modules/**'
+  ];
+
+  return {
+    presets,
+    plugins,
+    ignore
+  };
+};
