872d343926e3d2a93f3f83b18ee5106d6895410e
xulei123 <xulei272@huawei.com>
2025-07-07 07:30:24 +0000

    add errorCode
    Signed-off-by: xulei123 <xulei272@huawei.com>

--- api/@ohos.multimedia.media.d.ts
+++ api/@ohos.multimedia.media.d.ts
@@ -1779,6 +1779,24 @@ declare namespace media {
      * @since 18
      */
     AVERR_SUPER_RESOLUTION_NOT_ENABLED = 5410004,
+
+    /**
+     * Clear text traffic not permitted.
+     * @syscap SystemCapability.Multimedia.Media.Core
+     * @atomicservice
+     * @since 20
+     * @arkts 1.1&1.2
+     */
+    AVERR_IO_CLEARTEXT_NOT_PERMITTED = 5411012,
+
+    /**
+     * The parameter value is out of the specified range.
+     * @syscap SystemCapability.Multimedia.Media.Core
+     * @atomicservice
+     * @since 20
+     * @arkts 1.1&1.2
+     */
+    AVERR_PARAMETER_OUT_OF_RANGE = 5400108,
   }
 
   /**
