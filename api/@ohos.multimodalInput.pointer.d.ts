cca539a545610ced243998a22f450fe62aa2dc77
liyaoyao <liyaoyao7@huawei.com>
2025-07-17 03:16:18 +0000

    update api/@ohos.multimodalInput.pointer.d.ts.
    Signed-off-by: liyaoyao <liyaoyao7@huawei.com>

--- api/@ohos.multimodalInput.pointer.d.ts
+++ api/@ohos.multimodalInput.pointer.d.ts
@@ -740,7 +740,7 @@ declare namespace pointer {
   /**
    * Sets the mouse pointer style. This API uses an asynchronous callback to return the result.
    *
-   * @param { number } windowId - Window ID.
+   * @param { int } windowId - Window ID.
    * @param { PointerStyle } pointerStyle - Pointer style.
    * @param { AsyncCallback<void> } callback - Callback used to return the result.
    * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified;
@@ -749,12 +749,12 @@ declare namespace pointer {
    * @since arkts {'1.1':'9', '1.2':'20'}
    * @arkts 1.1&1.2
    */
-  function setPointerStyle(windowId: number, pointerStyle: PointerStyle, callback: AsyncCallback<void>): void;
+  function setPointerStyle(windowId: int, pointerStyle: PointerStyle, callback: AsyncCallback<void>): void;
 
   /**
    * Sets the mouse pointer style. This API uses a promise to return the result.
    *
-   * @param { number } windowId - Window ID.
+   * @param { int } windowId - Window ID.
    * @param { PointerStyle } pointerStyle - Pointer style.
    * @returns { Promise<void> } Promise that returns no value.
    * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified;
@@ -763,19 +763,20 @@ declare namespace pointer {
    * @since arkts {'1.1':'9', '1.2':'20'}
    * @arkts 1.1&1.2
    */
-  function setPointerStyle(windowId: number, pointerStyle: PointerStyle): Promise<void>;
+  function setPointerStyle(windowId: int, pointerStyle: PointerStyle): Promise<void>;
 
   /**
    * Sets the mouse pointer style. This API returns the result synchronously.
    *
-   * @param { number } windowId - Window ID.
+   * @param { int } windowId - Window ID.
    * @param { PointerStyle } pointerStyle - Pointer style.
    * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified;
    * <br>2. Incorrect parameter types; 3. Parameter verification failed.
    * @syscap SystemCapability.MultimodalInput.Input.Pointer
-   * @since 10
+   * @since arkts {'1.1':'10', '1.2':'20'}
+   * @arkts 1.1&1.2
    */
-  function setPointerStyleSync(windowId: number, pointerStyle: PointerStyle): void;
+  function setPointerStyleSync(windowId: int, pointerStyle: PointerStyle): void;
 
   /**
    * Obtains the mouse pointer style. This API uses an asynchronous callback to return the result.
