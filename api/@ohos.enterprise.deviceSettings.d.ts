95680f37f1df036d333195d5f3a990cadc79c190
谢智宇 <xiezhiyu2@huawei.com>
2025-07-19 15:08:45 +0800

    修复设置壁纸接口描述
    Change-Id: I73d14c90cb6da4ef3a19a4408138ffe4d2e8060d
    Signed-off-by: 谢智宇 <xiezhiyu2@huawei.com>

--- api/@ohos.enterprise.deviceSettings.d.ts
+++ api/@ohos.enterprise.deviceSettings.d.ts
@@ -433,7 +433,7 @@ declare namespace deviceSettings {
    * @throws { BusinessError } 9200001 - The application is not an administrator application of the device.
    * @throws { BusinessError } 9200002 - The administrator application does not have permission to manage the device.
    * @throws { BusinessError } 9200012 - Parameter verification failed.
-   * @throws { BusinessError } 201 - Permission verification failed.The application does not have the permission required to call the API
+   * @throws { BusinessError } 201 - Permission verification failed.The application does not have the permission required to call the API.
    * @syscap SystemCapability.Customization.EnterpriseDeviceManager
    * @stagemodelonly
    * @since 20
@@ -452,7 +452,7 @@ declare namespace deviceSettings {
    * @throws { BusinessError } 9200001 - The application is not an administrator application of the device.
    * @throws { BusinessError } 9200002 - The administrator application does not have permission to manage the device.
    * @throws { BusinessError } 9200012 - Parameter verification failed.
-   * @throws { BusinessError } 201 - Permission verification failed.The application does not have the permission required to call the API
+   * @throws { BusinessError } 201 - Permission verification failed.The application does not have the permission required to call the API.
    * @syscap SystemCapability.Customization.EnterpriseDeviceManager
    * @stagemodelonly
    * @since 20
