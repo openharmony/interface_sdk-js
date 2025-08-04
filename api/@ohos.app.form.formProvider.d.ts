f9230a2b31fa4d1ae1737dca06f48ddbe8d01815
cxy251 <caixinyu9@h-partners.com>
2025-08-04 22:34:54 +0800

    overload1
    Signed-off-by: cxy251 <caixinyu9@h-partners.com>

--- api/@ohos.app.form.formProvider.d.ts
+++ api/@ohos.app.form.formProvider.d.ts
@@ -77,8 +77,7 @@ declare namespace formProvider {
    * @throws { BusinessError } 16501003 - The form cannot be operated by the current application.
    * @syscap SystemCapability.Ability.Form
    * @atomicservice
-   * @since arkts {'1.1':'11', '1.2':'20'}
-   * @arkts 1.1&1.2
+   * @since 11
    */
   function setFormNextRefreshTime(formId: string, minute: int, callback: AsyncCallback<void>): void;
 
@@ -117,10 +116,58 @@ declare namespace formProvider {
    * @throws { BusinessError } 16501003 - The form cannot be operated by the current application.
    * @syscap SystemCapability.Ability.Form
    * @atomicservice
-   * @since arkts {'1.1':'11', '1.2':'20'}
-   * @arkts 1.1&1.2
+   * @since 11
    */
   function setFormNextRefreshTime(formId: string, minute: int): Promise<void>;
+
+  /**
+   * Set next update time for a specified form.
+   *
+   * @param { string } formId - Indicates the form ID.
+   * @param { int } minute - Indicates duration minute before next update.
+   * @param { AsyncCallback<void> } callback - The callback of setFormNextRefreshTime.
+   * @throws { BusinessError } 401 - Parameter error. Possible causes:
+   *     1.Mandatory parameters are left unspecified; 2.Incorrect parameter types; 3.Parameter verification failed.
+   * @throws { BusinessError } 16500050 - IPC connection error.
+   * @throws { BusinessError } 16500060 - Service connection error.
+   * @throws { BusinessError } 16500100 - Failed to obtain the configuration information.
+   * @throws { BusinessError } 16501000 - An internal functional error occurred.
+   * @throws { BusinessError } 16501001 - The ID of the form to be operated does not exist.
+   * @throws { BusinessError } 16501002 - The number of forms exceeds the maximum allowed.
+   * @throws { BusinessError } 16501003 - The form cannot be operated by the current application.
+   * @syscap SystemCapability.Ability.Form
+   * @atomicservice
+   * @since 20
+   * @arkts 1.2
+   */
+    function setFormNextRefreshTimeWithCallback(formId: string, minute: int, callback: AsyncCallback<void>): void;
+
+  /**
+   * Set next update time for a specified form.
+   *
+   * @param { string } formId - Indicates the form ID.
+   * @param { int } minute - Indicates duration minute before next update.
+   * @returns { Promise<void> } The promise returned by the function.
+   * @throws { BusinessError } 401 - Parameter error. Possible causes:
+   *     1.Mandatory parameters are left unspecified; 2.Incorrect parameter types; 3.Parameter verification failed.
+   * @throws { BusinessError } 16500050 - IPC connection error.
+   * @throws { BusinessError } 16500060 - Service connection error.
+   * @throws { BusinessError } 16500100 - Failed to obtain the configuration information.
+   * @throws { BusinessError } 16501000 - An internal functional error occurred.
+   * @throws { BusinessError } 16501001 - The ID of the form to be operated does not exist.
+   * @throws { BusinessError } 16501002 - The number of forms exceeds the maximum allowed.
+   * @throws { BusinessError } 16501003 - The form cannot be operated by the current application.
+   * @syscap SystemCapability.Ability.Form
+   * @atomicservice
+   * @since 20
+   * @arkts 1.2
+   */
+  function setFormNextRefreshTimeReturnsPromise(formId: string, minute: int): Promise<void>;
+
+  /**
+   * @arkts 1.2
+   */
+  overload setFormNextRefreshTime { setFormNextRefreshTimeWithCallback, setFormNextRefreshTimeReturnsPromise };
 
   /**
    * Update a specified form.
