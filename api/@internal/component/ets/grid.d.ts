679d1c62fca4e720e78fd3db6946ef3485aed845
yeyinglong_admin <yeyinglong@hisilicon.com>
2025-08-05 21:22:37 +0800

    滚动组件API设计实现一致性整改
    Signed-off-by: yeyinglong_admin <yeyinglong@hisilicon.com>

--- api/@internal/component/ets/grid.d.ts
+++ api/@internal/component/ets/grid.d.ts
@@ -1440,7 +1440,18 @@ declare class GridAttribute extends ScrollableCommonMethod<GridAttribute> {
    * @atomicservice
    * @since 11
    */
-  onScrollFrameBegin(event: (offset: number, state: ScrollState) => { offsetRemain: number }): GridAttribute;
+  /**
+   * Called when scrolling begin each frame.
+   * Anonymous Object Rectification.
+   *
+   * @param { OnScrollFrameBeginCallback } event - callback function, triggered when the scrolling begin each frame.
+   * @returns { GridAttribute } Returns the instance of the GridAttribute.
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 20
+   */
+  onScrollFrameBegin(event: OnScrollFrameBeginCallback): GridAttribute;
 
   /**
    * Called when scrolling begin each frame.
