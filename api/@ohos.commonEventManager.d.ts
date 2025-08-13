62a60d9a77c541d9da45b7984310c9d3359d50e7
zeng <zengxi14@h-partners.com>
2025-08-13 10:36:35 +0800

    update share
    Signed-off-by: zeng <zengxi14@h-partners.com>

--- api/@ohos.commonEventManager.d.ts
+++ api/@ohos.commonEventManager.d.ts
@@ -2646,7 +2646,7 @@ declare namespace commonEventManager {
      * @since 20
      * @arkts 1.1&1.2
      */
-        COMMON_EVENT_SCREEN_CAPTURE = 'usual.event.SCREEN_CAPTURE',
+        COMMON_EVENT_SCREEN_SHARE = 'usual.event.SCREEN_SHARE',
   }
 
   /**
