17cbbfd64ee393b356b56f1caab91eb802a4718c
chuanstudy <xule37@h-partners.com>
2025-08-14 11:39:55 +0000

    update api/@ohos.window.d.ts.
    Signed-off-by: chuanstudy <xule37@h-partners.com>

--- api/@ohos.window.d.ts
+++ api/@ohos.window.d.ts
@@ -1261,7 +1261,7 @@ declare namespace window {
    * @syscap SystemCapability.Window.SessionManager
    * @since 20
    */
-  interface Position {
+  export interface Position {
 
     /**
      * The X-coordinate
