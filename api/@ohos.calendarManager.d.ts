664835ef16b2085ffa96a76cc91ec927eb0e10cb
渠超洋 <2656338986@qq.com>
2025-08-15 10:02:16 +0000

    update：deleteEvent、deleteEvents元服务接口打标
    Signed-off-by: 渠超洋 <2656338986@qq.com>

--- api/@ohos.calendarManager.d.ts
+++ api/@ohos.calendarManager.d.ts
@@ -351,14 +351,6 @@ declare namespace calendarManager {
      * @syscap SystemCapability.Applications.CalendarData
      * @since 10
      */
-    /**
-     * Delete a single event.
-     * @param { number } id - Indicates the ID of an event.
-     * @param {AsyncCallback<void>} callback - The callback of deleteEvent.
-     * @syscap SystemCapability.Applications.CalendarData
-     * @atomicservice
-     * @since 20
-     */
     deleteEvent(id: number, callback: AsyncCallback<void>): void;
 
     /**
@@ -384,14 +376,6 @@ declare namespace calendarManager {
      * @param {AsyncCallback<void>} callback - The callback of deleteEvents.
      * @syscap SystemCapability.Applications.CalendarData
      * @since 10
-     */
-    /**
-     * Delete multiple events.
-     * @param { number[] } ids - Indicates the IDs of multiple events.
-     * @param {AsyncCallback<void>} callback - The callback of deleteEvents.
-     * @syscap SystemCapability.Applications.CalendarData
-     * @atomicservice
-     * @since 20
      */
     deleteEvents(ids: number[], callback: AsyncCallback<void>): void;
 
