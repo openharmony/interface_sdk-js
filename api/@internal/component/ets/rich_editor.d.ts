60b3c14edd0cb968c17d3875328304ccb3032436
yao_qiaoming1 <yaoming14@h-partners.com>
2025-08-18 15:46:19 +0800

    [RichEditor]增加RichEditor滚动条颜色接口
    Signed-off-by: yao_qiaoming1 <yaoming14@h-partners.com>

--- api/@internal/component/ets/rich_editor.d.ts
+++ api/@internal/component/ets/rich_editor.d.ts
@@ -4402,6 +4402,18 @@ declare class RichEditorAttribute extends CommonMethod<RichEditorAttribute> {
    * @since 20
    */
   undoStyle(style: Optional<UndoStyle>): RichEditorAttribute;
+
+  /**
+   * Set scrollbar color of rich editor.
+   *
+   * @param { Optional<ColorMetrics> } color  - Color of the rich editor scrollbar.
+   * @returns { RichEditorAttribute } returns the instance of the RichEditorAttribute.
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 21
+   */
+  scrollBarColor(color: Optional<ColorMetrics>): RichEditorAttribute;
 }
 
 /**
