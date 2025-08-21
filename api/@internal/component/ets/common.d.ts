b76c643757ab9d09bdaa4c2074d1523d1630c707
liuhui <liuhui160@huawei.com>
2025-08-21 10:09:52 +0800

    无障碍1.2接口拆分
    Signed-off-by: liuhui <liuhui160@huawei.com>

--- api/@internal/component/ets/common.d.ts
+++ api/@internal/component/ets/common.d.ts
@@ -2068,7 +2068,7 @@ declare enum AccessibilitySamePageMode {
   * @atomicservice
   * @since 18
   */
-  FULL_SILENT = 1,
+  FULL_SILENT = 1
 }
 /**
  * Enum for accessibility component type
@@ -3204,7 +3204,7 @@ declare enum AccessibilityRoleType {
   * @atomicservice
   * @since 18
   */
-  ROLE_NONE = 124,
+  ROLE_NONE = 124
 }
 
 /**
