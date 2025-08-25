b00ee58db7b4b25b05f2b7710c35ae634e2a00ab
guxiang <guxiang11@huawei.com>
2025-08-25 21:43:53 +0800

    disableNotificationFeature增加错误码注释
    Signed-off-by: guxiang <guxiang11@huawei.com>

--- api/@ohos.notificationManager.d.ts
+++ api/@ohos.notificationManager.d.ts
@@ -3918,6 +3918,25 @@ declare namespace notificationManager {
    * @systemapi
    * @since 18
    */
+  /**
+   * Disabling notifications based on the application list.
+   *
+   * @permission ohos.permission.NOTIFICATION_CONTROLLER or ohos.permission.MANAGE_EDM_POLICY
+   * @param { boolean } disabled - The switch of disableNotification.
+   * @param { Array<string> } bundleList - The bundles of disableNotification.
+   * @returns { Promise<void> } The promise returned by the function.
+   * @throws { BusinessError } 201 - Permission denied.
+   * @throws { BusinessError } 202 - Not system application to call the interface.
+   * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified.
+   * <br>2. Incorrect parameter types. 3. Parameter verification failed.
+   * @throws { BusinessError } 801 - Capability not supported.
+   * @throws { BusinessError } 1600001 - Internal error.
+   * @throws { BusinessError } 1600002 - Marshalling or unmarshalling error.
+   * @syscap SystemCapability.Notification.Notification
+   * @systemapi
+   * @since arkts{ '1.1':'18','1.2':'20'}
+   * @arkts 1.1&1.2
+   */
   function disableNotificationFeature(disabled:boolean, bundleList: Array<string>): Promise<void>;
 
   /**
