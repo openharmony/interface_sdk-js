7c90ee75ea402f38eeaaef98bd165a216ed0b80d
潜秋宏 <qianqiuhong@h-partners.com>
2025-08-27 21:29:26 +0800

    一致性修改02
    Signed-off-by: 潜秋宏 <qianqiuhong@h-partners.com>
    Change-Id: Ibf52ebb68edb4922d9e79946209cb80bbb2912c0

--- kits/@kit.ArkUI.d.ts
+++ kits/@kit.ArkUI.d.ts
@@ -290,7 +290,7 @@ export {
   RefreshModifier, SearchModifier, TextAreaModifier, TextModifier, TextInputModifier, ImageSpanModifier,
   ImageAnimatorModifier, ImageModifier, VideoModifier, DataPanelModifier, GaugeModifier, LoadingProgressModifier,
   MarqueeModifier, ProgressModifier, QRCodeModifier, TextClockModifier, TextTimerModifier, LineModifier, PathModifier,
-  PolygonModifier, PolylineModifier, RectModifier, ShapeModifier, AlphabetIndexerModifier, FormComponentModifier,
+  PolygonModifier, PolylineModifier, RectModifier, ShapeModifier, AlphabetIndexerModifier,
   HyperlinkModifier, MenuModifier, MenuItemModifier, PanelModifier, SymbolGlyphModifier, AttributeUpdater, SymbolSpanModifier,
   ContainerSpanModifier, PixelMapDrawableDescriptor, AnimationOptions, AnimatedDrawableDescriptor,
   IDataSourcePrefetching, IPrefetcher, BasicPrefetcher, MarqueeDynamicSyncScene, MarqueeDynamicSyncSceneType,
