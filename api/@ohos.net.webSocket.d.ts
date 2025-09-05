23f4197aec16c3edd0a07d50b27325f448519c4b
177******89 <1225095037@qq.com>
2025-09-05 07:08:02 +0000

    update api/@ohos.net.webSocket.d.ts.
    Signed-off-by: 177******89 <1225095037@qq.com>

--- api/@ohos.net.webSocket.d.ts
+++ api/@ohos.net.webSocket.d.ts
@@ -176,23 +176,21 @@ declare namespace webSocket {
      * Self defined interval of ping frame.
      * default: 30. disable: 0. max: 30000. unit:second.
      * Ping is performed at every pingInterval.
-     * @type {?int}
+     * @type {?number}
      * @syscap SystemCapability.Communication.NetStack
      * @since 21
-     * @arkts 1.2
      */
-    pingInterval?: int;
+    pingInterval?: number;
 
     /**
      * Self defined timeout of pong frame.
      * default: 30. max: 30000. unit:second. The value must be less than or equal to pingInterval.
      * If no pong is received with the pongTimeout period, the websocket connection will be disconnected.
-     * @type {?int}
+     * @type {?number}
      * @syscap SystemCapability.Communication.NetStack
      * @since 21
-     * @arkts 1.2
      */
-    pongTimeout?: int;
+    pongTimeout?: number;
   }
 
   /**
