96051b58507cb77263152ecde324d6b20b295afb
chuanstudy <xule37@h-partners.com>
2025-09-16 09:46:26 +0800

    fix
    Signed-off-by: chuanstudy <xule37@h-partners.com>

--- api/@ohos.window.d.ts
+++ api/@ohos.window.d.ts
@@ -7693,7 +7693,8 @@ declare namespace window {
      * Unregister the callback of window highlight state change
      *
      * @param { 'windowHighlightChange' } type - The value is fixed at 'windowHighlightChange', indicating the window highlight change event.
-     * @param { Callback<boolean> } callback - Callback used to return the highlight status of the window.
+     * @param { Callback<boolean> } [callback] - Callback used to return the highlight status of the window.
+     *     if not provided, all callbacks for the given event type will be removed.
      * @throws { BusinessError } 401 - Parameter error. Possible cause: 1. Mandatory parameters are left unspecified; 
      *                                                                  2. Incorrect parameter types; 
      *                                                                  3. Parameter verification failed.
