f1c54c9896ae2e852592ee44f684071a867613f7
laiganlu <Laiganlu@huawei.com>
2025-09-22 20:11:05 +0800

    超行整改
    Signed-off-by: laiganlu <Laiganlu@huawei.com>

--- api/@ohos.window.d.ts
+++ api/@ohos.window.d.ts
@@ -12414,29 +12414,30 @@ declare namespace window {
     /**
      * Sets Image for recent.
      *
-     * @param { image.PixelMap } pixelMap - Recent image.
+     * @param { long | image.PixelMap } imageResource - imageResourceId or pixelMap for recent image.
      * @param { ImageFit } value - Sets the zoom type of an image.
      * @returns { Promise<void> } Promise that returns no value.
      * @throws { BusinessError } 202 - Permission verification failed. A non-system application calls a system API.
-     * @throws { BusinessError } 801 - Capability not supported. Failed to call the API due to limited device capabilities.
+     * @throws { BusinessError } 801 - Capability not supported.
+     *     Failed to call the API due to limited device capabilities.
      * @throws { BusinessError } 1300002 - This window state is abnormal.
      * @throws { BusinessError } 1300003 - This window manager service works abnormally.
-     * @throws { BusinessError } 1300016 - Parameter error. Possible cause: 1. Invalid parameter range.
-     *                                                                      2. Invalid parameter length.
-     *                                                                      3. Incorrect parameter format.
+     * @throws { BusinessError } 1300016 - Parameter error. Possible cause: 
+     *     1. Invalid parameter range. 2. Invalid parameter length. 3. Incorrect parameter format.
      * @syscap SystemCapability.Window.SessionManager
      * @systemapi Hide this for inner system use.
      * @stagemodelonly
      * @since 21
      */
-    setImageForRecent(pixelMap: image.PixelMap, value: ImageFit): Promise<void>;
+    setImageForRecent(imageResource: long | image.PixelMap, value: ImageFit): Promise<void>;
 
     /**
      * Remove Image for recent.
      *
      * @returns { Promise<void> } Promise that returns no value.
      * @throws { BusinessError } 202 - Permission verification failed. A non-system application calls a system API.
-     * @throws { BusinessError } 801 - Capability not supported. Failed to call the API due to limited device capabilities.
+     * @throws { BusinessError } 801 - Capability not supported.
+     *     Failed to call the API due to limited device capabilities.
      * @throws { BusinessError } 1300002 - This window state is abnormal.
      * @throws { BusinessError } 1300003 - This window manager service works abnormally.
      * @syscap SystemCapability.Window.SessionManager
