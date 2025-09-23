e91db80987320814a9a69ca255c77e7fbe77f825
SoftSquirrel <xuhao47@huawei.com>
2025-09-23 20:21:57 +0800

    Issue:#18424 Description:add setAbilityFileTypesForSelf Sig: SIG_ApplicaitonFramework Feature or Bugfix:Feature Binary Source: No
    Signed-off-by: SoftSquirrel <xuhao47@huawei.com>

--- api/@ohos.bundle.bundleManager.d.ts
+++ api/@ohos.bundle.bundleManager.d.ts
@@ -4344,6 +4344,11 @@ declare namespace bundleManager {
    * @throws { BusinessError } 202 - Permission denied, non-system app called system api.
    * @throws { BusinessError } 17700002 - The specified moduleName is not found.
    * @throws { BusinessError } 17700003 - The specified abilityName is not found.
+   * @throws { BusinessError } 17700351 - Invalid fileTypes. Possible causes:
+   *   1. The array length exceeds 1024;
+   *   2. The array contains an empty item;
+   *   3. An item exceeds 512 characters;
+   *   4. The array contains wildcard or general.object.
    * @syscap SystemCapability.BundleManager.BundleFramework.Core
    * @systemapi
    * @since 22 dynamic&static
