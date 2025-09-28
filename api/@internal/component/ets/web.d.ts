7dc5ca8ab0299a1643bcae0f6f36e3256fb0f24f
z30026177 <zhengyutao@huawei.com>
2025-09-28 09:40:08 +0800

    回退API22再废弃的接口
    Signed-off-by: z30026177 <zhengyutao@huawei.com>

--- api/@internal/component/ets/web.d.ts
+++ api/@internal/component/ets/web.d.ts
@@ -2155,9 +2155,6 @@ declare class ClientAuthenticationHandler {
    * @syscap SystemCapability.Web.Webview.Core
    * @atomicservice
    * @since 11
-   * @since20 static
-   * @deprecated since 22
-   * @useinstead ohos.web.ClientAuthenticationHandler#confirm
    */
   confirm(priKeyFile: string, certChainFile: string): void;
 
