c8abfbd78ae41953d1631ed331ce6debf5dd8434
小小熊睡觉觉 <linhu5@huawei.com>
2025-10-17 10:59:08 +0800

    离屏圆角
    Signed-off-by: 小小熊睡觉觉 <linhu5@huawei.com>

--- api/@internal/component/ets/common.d.ts
+++ api/@internal/component/ets/common.d.ts
@@ -22204,6 +22204,25 @@ declare class CommonMethod<T> {
    * @since 12
    */
   borderRadius(value: Length | BorderRadiuses | LocalizedBorderRadiuses): T;
+  /**
+   * Sets the radius of the border rounded corners.
+   *     The radius is restricted by the component size. The maximum value is half of the component width or height.
+   * NOTE
+   *    1. **CornerApplyType.FAST**: The current component and its child components will be drawn directly
+   *        onto the canvas with rounded corners applied.
+   *    2. **CornerApplyType.OFFSCREEN**: The current component and its child components will first be rendered onto
+   *        an off-screen canvas, then undergo a rounded corner clipping, and finally be drawn onto the main canvas.
+   * @param { Length | BorderRadiuses | LocalizedBorderRadiuses } value
+   * @param { CornerApplyType } [type] - Application types for drawing rounded corners.
+   *                                         Default value: **CornerApplyType.FAST**.
+   * @returns { T }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @form
+   * @atomicservice
+   * @since 22 dynamic
+   */
+  borderRadius(value: Length | BorderRadiuses | LocalizedBorderRadiuses, type?: CornerApplyType): T;
 
   /**
    * Border image
