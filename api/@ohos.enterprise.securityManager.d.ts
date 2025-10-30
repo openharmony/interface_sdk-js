3bced05b1382539abef980bbf590b3f1286d3708
ZhuAgile <zhumingyong1@h-partners.com>
2025-10-30 18:48:03 +0800

    补充二进制管控接口SDK
    Signed-off-by: ZhuAgile <zhumingyong1@h-partners.com>

--- api/@ohos.enterprise.securityManager.d.ts
+++ api/@ohos.enterprise.securityManager.d.ts
@@ -470,11 +470,12 @@ declare namespace securityManager {
    * @param { common.ManagedPolicy } policy - policy indicates the policy of extensions.
    * @throws { BusinessError } 9200001 - The application is not an administrator application of the device.
    * @throws { BusinessError } 9200002 - The administrator application does not have permission to manage the device.
+   * @throws { BusinessError } 9200010 - A conflict policy has been configured.
+   * @throws { BusinessError } 9200012 - Parameter verification failed.
    * @throws { BusinessError } 201 - Permission verification failed.
    *  The application does not have the permission required to call the API.
    * @syscap SystemCapability.Customization.EnterpriseDeviceManager
    * @stagemodelonly
-   * @since 22
    */
     function setExternalSourceExtensionsPolicy(admin: Want, policy: common.ManagedPolicy): void;
 
@@ -489,7 +490,6 @@ declare namespace securityManager {
    * @throws { BusinessError } 201 - Permission verification failed. The application does not have the permission required to call the API.
    * @syscap SystemCapability.Customization.EnterpriseDeviceManager
    * @stagemodelonly
-   * @since 22
    */
     function getExternalSourceExtensionsPolicy(admin: Want): common.ManagedPolicy;
 
