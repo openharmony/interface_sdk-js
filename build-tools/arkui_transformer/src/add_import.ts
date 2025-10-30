e190ce4874f816346cbe2b22f00c952089f346f1
Boren Zhang <bobo1993324@126.com>
2025-10-30 14:22:19 +0800

    Completely remove @memo.
    Signed-off-by: zhangboren<bobo1993324@126.com>

--- build-tools/arkui_transformer/src/add_import.ts
+++ build-tools/arkui_transformer/src/add_import.ts
@@ -113,12 +113,12 @@ function createTargetImport(sourceFile: ts.SourceFile, context: ts.Transformatio
             false,
             undefined,
             ts.factory.createNamedImports([
-                ts.factory.createImportSpecifier(false, undefined, ts.factory.createIdentifier("memo"))
+                ts.factory.createImportSpecifier(false, undefined, ts.factory.createIdentifier("Memo"))
             ])
         ),
-        ts.factory.createStringLiteral("./../stateManagement/runtime")
+        ts.factory.createStringLiteral("./../incremental/annotation")
     );
-    targetImport.push(memoImport)
+    targetImport.push(memoImport);
 
     const customBuilderImport = ts.factory.createImportDeclaration(
         undefined,
