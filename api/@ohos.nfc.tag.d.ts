476ab7b33ab05cef145804a6293cdfc39c0b5d00
y60072242 <yangpengwei5@h-partners.com>
2025-10-31 16:40:20 +0800

    修改api接口文件
    Signed-off-by: y60072242 <yangpengwei5@h-partners.com>

--- api/@ohos.nfc.tag.d.ts
+++ api/@ohos.nfc.tag.d.ts
@@ -1313,7 +1313,7 @@ declare namespace tag {
    * @throws { BusinessError } 201 - Permission denied.
    * @throws { BusinessError } 401 - The parameter check failed. Possible causes:
    * <br> 1. Mandatory parameters are left unspecified.
-   * <br> 2. Incorrect parameter types.
+   * <br> 2. Incorrect parameters types.
    * <br> 3. Parameter verification failed.
    * @throws { BusinessError } 801 - Capability not supported.
    * @throws { BusinessError } 3100201 - The tag running state is abnormal in the service.
@@ -1333,7 +1333,7 @@ declare namespace tag {
    * @throws { BusinessError } 201 - Permission denied.
    * @throws { BusinessError } 401 - Parameter error. Possible causes:
    *     1. Mandatory parameters are left unspecified.
-   *     2. Incorrect parameters types.
+   *     2. Incorrect parameter types.
    *     3. Parameter verification failed.
    * @throws { BusinessError } 801 - Capability not supported.
    * @throws { BusinessError } 3100201 - The tag running state is abnormal in the service.
