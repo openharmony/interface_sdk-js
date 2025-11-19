37dc63338076f80224541f60a7a05ca9334c4203
ZhangYuanhuai <zhangyuanhuai@huawei.com>
2025-11-19 12:02:48 +0800

    d.ts回合6.0
    Signed-off-by: ZhangYuanhuai <zhangyuanhuai@huawei.com>

--- api/@ohos.file.fileuri.d.ts
+++ api/@ohos.file.fileuri.d.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022-2023 Huawei Device Co., Ltd.
+ * Copyright (C) 2022-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -84,7 +84,8 @@ declare namespace fileUri {
     /**
      * Obtains the file name of uri.
      *
-     * @type { string }
+     * @returns { string }
+     * string: Return the file name of uri.
      * @readonly
      * @throws { BusinessError } 13900005 - I/O error
      * @throws { BusinessError } 13900042 - Unknown error
@@ -94,7 +95,8 @@ declare namespace fileUri {
     /**
      * Obtains the file name of uri.
      *
-     * @type { string }
+     * @returns { string }
+     * string: Return the file name of uri.
      * @readonly
      * @throws { BusinessError } 13900005 - I/O error
      * @throws { BusinessError } 13900042 - Unknown error
@@ -102,7 +104,7 @@ declare namespace fileUri {
      * @atomicservice
      * @since 15 dynamic
      */
-    readonly name: string;
+    get name(): string;
 
     /**
      * Get the full directory uri where the file URI is located
