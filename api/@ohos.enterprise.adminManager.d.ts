32193e331a1752ec6ac9e234f6c4f89ea36b9820
xie-zhiyu <xiezhiyu2@huawei.com>
2025-11-21 11:26:18 +0800

    update: 更新文件 @ohos.enterprise.adminManager.d.ts
    Signed-off-by: xie-zhiyu <xiezhiyu2@huawei.com>

--- api/@ohos.enterprise.adminManager.d.ts
+++ api/@ohos.enterprise.adminManager.d.ts
@@ -414,23 +414,6 @@ declare namespace adminManager {
    * @stagemodelonly
    * @since 20
    */
-  /**
-   * Disables a current administrator ability.
-   * Only apps with the ohos.permission.MANAGE_ENTERPRISE_DEVICE_ADMIN permission, 
-   *     ohos.permission.START_PROVISIONING_MESSAGE, ohos.permission.ENTERPRISE_MANAGE_DEVICE_ADMIN or the shell uid can call this method.
-   *
-   * @permission ohos.permission.MANAGE_ENTERPRISE_DEVICE_ADMIN or ohos.permission.START_PROVISIONING_MESSAGE or ohos.permission.ENTERPRISE_MANAGE_DEVICE_ADMIN
-   * @param { Want } admin - admin indicates the enterprise admin extension ability information.
-   *                         The admin must have the corresponding permission.
-   * @param { number } [userId] - userId indicates the user ID or do not pass user ID.
-   * @returns { Promise<void> } the promise returned by the disableAdmin.
-   * @throws { BusinessError } 9200005 - Failed to deactivate the administrator application of the device.
-   * @throws { BusinessError } 201 - Permission verification failed.
-   *     The application does not have the permission required to call the API.
-   * @syscap SystemCapability.Customization.EnterpriseDeviceManager
-   * @stagemodelonly
-   * @since 23
-   */
   function disableAdmin(admin: Want, userId?: number): Promise<void>;
 
   /**
