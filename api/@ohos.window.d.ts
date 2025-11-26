2ca71e3b5bb4dcfa46ab13c4dc8f39b2485b44a3
zhoulingxiao <zhoulingxiao@huawei.com>
2025-11-26 19:21:31 +0800

    窗口旋转/窗口动效接口错误信息调整 Signed-off-by: zhoulingxiao <zhoulingxiao@huawei.com>

--- api/@ohos.window.d.ts
+++ api/@ohos.window.d.ts
@@ -6865,8 +6865,8 @@ declare namespace window {
      * @throws { BusinessError } 401 - Parameter error. Possible cause: 1. Mandatory parameters are left unspecified;
      *                                                                  2. Failed to convert parameter to Orientation;
      *                                                                  3. Invalid parameter value range.
-     * @throws { BusinessError } 1300002 - This window state is abnormal. Possible cause:   1. Window is nullptr;
-     *                                                                                      2. Send event failed.
+     * @throws { BusinessError } 1300002 - This window state is abnormal. Possible cause:   1. The window is not created or destroyed;
+     *                                                                                      2. Internal napi error.
      * @syscap SystemCapability.WindowManager.WindowManager.Core
      * @crossplatform
      * @atomicservice
@@ -6911,8 +6911,8 @@ declare namespace window {
      * @throws { BusinessError } 401 - Parameter error. Possible cause: 1. Mandatory parameters are left unspecified;
      *                                                                  2. Failed to convert parameter to Orientation;
      *                                                                  3. Invalid parameter value range.
-     * @throws { BusinessError } 1300002 - This window state is abnormal. Possible cause:   1. Window is nullptr;
-     *                                                                                      2. Send event failed.
+     * @throws { BusinessError } 1300002 - This window state is abnormal. Possible cause:   1. The window is not created or destroyed;
+     *                                                                                      2. Internal napi error.
      * @syscap SystemCapability.WindowManager.WindowManager.Core
      * @crossplatform
      * @atomicservice
@@ -6926,7 +6926,7 @@ declare namespace window {
      * This API can be called only by the main window.
      *
      * @returns { Orientation } orientation - The orientation config of the window
-     * @throws { BusinessError } 1300002 - This window state is abnormal. Possible cause: Window is nullptr.
+     * @throws { BusinessError } 1300002 - This window state is abnormal. Possible cause: The window is not created or destroyed.
      * @syscap SystemCapability.WindowManager.WindowManager.Core
      * @atomicservice
      * @since 12 dynamic
@@ -10509,9 +10509,8 @@ declare namespace window {
      *                                                                  2. Napi get radius value failed;
      *                                                                  3. The shadow radius is less than zero.
      * @throws { BusinessError } 801 - Capability not supported. Failed to call the API due to limited device capabilities.
-     * @throws { BusinessError } 1300002 - This window state is abnormal. Possible cause:   1. Window is nullptr;
-     *                                                                                      2. Native window is nullptr;
-     *                                                                                      3. Send event failed.
+     * @throws { BusinessError } 1300002 - This window state is abnormal. Possible cause:   1. The window is not created or destroyed;
+     *                                                                                      2. Internal napi error.
      * @throws { BusinessError } 1300004 - Possible cause:  1. Unauthorized operation;
      *                                                      2. This is not sub window or float window.
      * @syscap SystemCapability.Window.SessionManager
@@ -10562,8 +10561,8 @@ declare namespace window {
                                                                         2. Failed to convert parameter to radius;
                                                                         3. The corner radius is less than zero.
      * @throws { BusinessError } 801 - Capability not supported. Failed to call the API due to limited device capabilities.
-     * @throws { BusinessError } 1300002 - This window state is abnormal. Possible cause:   1. Window is nullptr;
-     *                                                                                      2. Send event failed.
+     * @throws { BusinessError } 1300002 - This window state is abnormal. Possible cause:   1. The window is not created or destroyed;
+     *                                                                                      2. Internal napi error.
      * @throws { BusinessError } 1300003 - This window manager service works abnormally.
      * @throws { BusinessError } 1300004 - Unauthorized operation.
      * @syscap SystemCapability.Window.SessionManager
@@ -10578,8 +10577,8 @@ declare namespace window {
      *
      * @returns { double } - Radius of the rounded corner of the child window or floating window, measured in vp.
      * @throws { BusinessError } 801 - Capability not supported. Failed to call the API due to limited device capabilities.
-     * @throws { BusinessError } 1300002 - This window state is abnormal. Possible cause:   1. Window is nullptr;
-     *                                                                                      2. Send event failed.
+     * @throws { BusinessError } 1300002 - This window state is abnormal. Possible cause:   1. The window is not created or destroyed;
+     *                                                                                      2. Internal napi error.
      * @throws { BusinessError } 1300004 - Unauthorized operation.
      * @syscap SystemCapability.Window.SessionManager
      * @atomicservice
@@ -11722,10 +11721,8 @@ declare namespace window {
      * @param { TransitionAnimation } animation - Transition animation config.
      * @returns { Promise<void> } Promise that returns no value.
      * @throws { BusinessError } 801 - Capability not supported. Failed to call the API due to limited device capabilities.
-     * @throws { BusinessError } 1300002 - This window state is abnormal. Possible cause:   1. Window is nullptr;
-     *                                                                                      2. Native window is nullptr;
-     *                                                                                      3. Window instance not exist;
-     *                                                                                      4. Send event failed.
+     * @throws { BusinessError } 1300002 - This window state is abnormal. Possible cause:   1. The window is not created or destroyed;
+     *                                                                                      2. Internal napi error.
      * @throws { BusinessError } 1300003 - This window manager service works abnormally.
      * @throws { BusinessError } 1300004 - Unauthorized operation.
      * @throws { BusinessError } 1300016 - Parameter error. Possible cause: 1. Invalid parameter range;
@@ -11746,12 +11743,11 @@ declare namespace window {
      * @param { WindowTransitionType } transitionType - Transition animation type.
      * @returns { TransitionAnimation | undefined } Transition animation with transition type, or undefined if it has not been set.
      * @throws { BusinessError } 801 - Capability not supported. Failed to call the API due to limited device capabilities.
-     * @throws { BusinessError } 1300002 - This window state is abnormal. Possible cause:   1. Window is nullptr;
-     *                                                                                      2. Transition animation is not enable;
-     *                                                                                      3. Send event failed.
+     * @throws { BusinessError } 1300002 - This window state is abnormal. Possible cause:   1. The window is not created or destroyed;
+     *                                                                                      2. Internal napi error.
      * @throws { BusinessError } 1300003 - This window manager service works abnormally.
      * @throws { BusinessError } 1300004 - Unauthorized operation.
-     * @throws { BusinessError } 1300016 - Parameter error. Possible cause: 1. Invalid parameter range.
+     * @throws { BusinessError } 1300016 - Parameter error. Possible cause: Invalid parameter range.
      * @syscap SystemCapability.Window.SessionManager
      * @stagemodelonly
      * @atomicservice
