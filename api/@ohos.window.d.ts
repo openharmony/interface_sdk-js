e69b3d21209bdbf056c49cf3ec27b3b996239d1c
jjxxx123 <1720356274@qq.com>
2025-11-28 15:23:20 +0800

    api错误信息整改
    Signed-off-by: jjxxx123 <1720356274@qq.com>

--- api/@ohos.window.d.ts
+++ api/@ohos.window.d.ts
@@ -3866,10 +3866,11 @@ declare namespace window {
    * @param { int } sourceWindowId - ID of the source window. You are advised to call getWindowProperties() to obtain the window ID.
    * @param { int } targetWindowId - ID of the target window. You are advised to call getWindowProperties() to obtain the window ID.
    * @returns { Promise<void> } - Promise that returns no value.
-   * @throws { BusinessError } 401 - Parameter error. Possible cause: 1. Mandatory parameters are left unspecified;
-   *                                                                  2. Failed to convert parameter to sourceWindowId;
-   *                                                                  3. Failed to convert parameter to targetWindowId;
-   *                                                                  4. Invalid sourceWindowId or targetWindowId.
+   * @throws { BusinessError } 401 - Parameter error.
+   *     Possible cause: 1. Mandatory parameters are left unspecified;
+   *                     2. Failed to convert parameter to sourceWindowId;
+   *                     3. Failed to convert parameter to targetWindowId;
+   *                     4. Invalid sourceWindowId or targetWindowId.
    * @throws { BusinessError } 801 - Capability not supported. Failed to call the API due to limited device capabilities.
    * @throws { BusinessError } 1300002 - This window state is abnormal.
    * @throws { BusinessError } 1300003 - This window manager service works abnormally.
