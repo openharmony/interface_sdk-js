c9039390c3ce82b284d56d49b5641a7208cf49be
zhushuanghong <zhushuanghong5@huawei.com>
2025-12-02 09:55:53 +0800

    后台任务新增运动健康子类型
    Signed-off-by: zhushuanghong <zhushuanghong5@huawei.com>

--- api/@ohos.resourceschedule.backgroundTaskManager.d.ts
+++ api/@ohos.resourceschedule.backgroundTaskManager.d.ts
@@ -1696,6 +1696,15 @@ declare namespace backgroundTaskManager {
      * @since 22 dynamic
      */
     SUBMODE_VIDEO_BROADCAST_NORMAL_NOTIFICATION = 10,
+
+    /**
+     * submode of 'MODE_SPECIAL_SCENARIO_PROCESSING', used for workout scenarios.
+     *
+     * @syscap SystemCapability.ResourceSchedule.BackgroundTaskManager.ContinuousTask
+     * @stagemodeonly
+     * @since 23 dynamic
+     */
+    SUBMODE_WORK_OUT_NORMAL_NOTIFICATION = 11,
   }
 
   /**
