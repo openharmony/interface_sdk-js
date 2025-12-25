3f56f9cc0880d2009eb33362251c534219aaa59f
xuqian01311 <xuqian65@huawei.com>
2025-12-25 19:17:03 +0800

    fix error
    Signed-off-by: xuqian01311 <xuqian65@huawei.com>

--- api/@ohos.account.osAccount.d.ts
+++ api/@ohos.account.osAccount.d.ts
@@ -1896,7 +1896,7 @@ declare namespace osAccount {
      * <br> 2. Incorrect parameter types.
      * @throws { BusinessError } 12300001 - The system service works abnormally.
      * @throws { BusinessError } 12300002 - Invalid serialNumber.
-     * @throws { BusinessError } 12300003 - The account indicated by serialNumber dose not exist.
+     * @throws { BusinessError } 12300003 - The account indicated by serialNumber does not exist.
      * @syscap SystemCapability.Account.OsAccount
      * @since 9 dynamic
      * @since 23 static
@@ -1912,7 +1912,7 @@ declare namespace osAccount {
      * <br> 2. Incorrect parameter types.
      * @throws { BusinessError } 12300001 - The system service works abnormally.
      * @throws { BusinessError } 12300002 - Invalid serialNumber.
-     * @throws { BusinessError } 12300003 - The account indicated by serialNumber dose not exist.
+     * @throws { BusinessError } 12300003 - The account indicated by serialNumber does not exist.
      * @syscap SystemCapability.Account.OsAccount
      * @since 9 dynamic
      * @since 23 static
