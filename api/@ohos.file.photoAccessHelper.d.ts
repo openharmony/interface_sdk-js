9dc02cd6c5b030bdde5c4d5be61424b494a45dea
zhixiang_song <songzhixiang2@huawei.com>
2026-01-14 19:29:31 +0800

    add interface setAppLinkState and enum AppLinkState
    Signed-off-by: zhixiang_song <songzhixiang2@huawei.com>

--- api/@ohos.file.photoAccessHelper.d.ts
+++ api/@ohos.file.photoAccessHelper.d.ts
@@ -1008,7 +1008,7 @@ declare namespace photoAccessHelper {
     NO_HIDE_SENSITIVE_TYPE = 3,
 
     /**
-     * Refer MEIDA_LOCATION permission to hide location and shooting parameters.
+     * Refer MEDIA_LOCATION permission to hide location and shooting parameters.
      *
      * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
      * @systemapi
@@ -10952,6 +10952,21 @@ declare namespace photoAccessHelper {
     setHasAppLink(hasAppLink: int): void;
 
     /**
+     * Set the AppLink state of this asset.
+     *
+     * @param { AppLinkState } appLinkState - Boomerang status of assets
+     * @throws { BusinessError } 202 - Invoked by non-system applications
+     * @throws { BusinessError } 23800301 - Internal system error. It is recommended to retry and check the logs.
+     * @throws { BusinessError } 23800151 - The scenario parameter verification fails. Possible causes:
+     *     The input parameter is not within the valid range.
+     * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+     * @systemapi
+     * @stagemodelonly
+     * @since 24 dynamic&static
+     */
+    setAppLinkState(appLinkState: AppLinkState): void;
+
+    /**
      * Set the AppLink info of this asset.
      *
      * @param { string } appLink - AppLink info of the asset to set.
@@ -14320,6 +14335,47 @@ declare namespace photoAccessHelper {
      * @since 23 dynamic&static
      */
     RANDOM_VOTE = 0
+  }
+ 
+  /**
+   * App link state of an asset
+   *
+   * @enum { int } AppLinkState
+   * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+   * @systemapi
+   * @stagemodelonly
+   * @since 24 dynamic&static
+   */
+  enum AppLinkState {  
+    /**
+     * Unknow whether an asset has app link or not
+     *
+     * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+     * @systemapi
+     * @stagemodelonly
+     * @since 24 dynamic&static
+     */
+    DEFAULT = 0,
+    
+    /**
+     * An asset has no app link
+     *
+     * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+     * @systemapi
+     * @stagemodelonly
+     * @since 24 dynamic&static
+     */
+    HAS_NO_LINK = 1,
+
+    /**
+     * An asset has app link
+     *
+     * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+     * @systemapi
+     * @stagemodelonly
+     * @since 24 dynamic&static
+     */
+    HAS_LINK = 2
   }
  
   /**
