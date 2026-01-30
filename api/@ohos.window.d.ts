7a75d567ed9f67ba48d65ceca5141c12604e80d5
chuanqistudy <xule37@h-partners.com>
2026-01-30 10:48:51 +0800

    更新注释
    Signed-off-by: chuanqistudy <xule37@h-partners.com>

--- api/@ohos.window.d.ts
+++ api/@ohos.window.d.ts
@@ -7887,8 +7887,10 @@ declare namespace window {
     /**
      * Register the callback of keyboardHeightChange
      *
-     * @param { 'keyboardHeightChange' } type - The value is fixed at 'keyboardHeightChange', indicating the keyboard height change event.
-     * @param { Callback<int> } callback - Callback used to return the current keyboard height, which is an integer, in px.
+     * @param { 'keyboardHeightChange' } type - The value is fixed at 'keyboardHeightChange',
+     *     indicating the keyboard height change event.
+     * @param { Callback<int> } callback - Callback used to return the current keyboard height,
+     *     which is an integer, in px.
      * @throws { BusinessError } 401 - Parameter error. Possible cause: 1. Mandatory parameters are left unspecified; 
      *                                                                  2. Incorrect parameter types; 
      *                                                                  3. Parameter verification failed.
@@ -7898,8 +7900,10 @@ declare namespace window {
     /**
      * Register the callback of keyboardHeightChange
      *
-     * @param { 'keyboardHeightChange' } type - The value is fixed at 'keyboardHeightChange', indicating the keyboard height change event.
-     * @param { Callback<int> } callback - Callback used to return the current keyboard height, which is an integer, in px.
+     * @param { 'keyboardHeightChange' } type - The value is fixed at 'keyboardHeightChange',
+     *     indicating the keyboard height change event.
+     * @param { Callback<int> } callback - Callback used to return the current keyboard height,
+     *     which is an integer, in px.
      * @throws { BusinessError } 401 - Parameter error. Possible cause: 1. Mandatory parameters are left unspecified; 
      *                                                                  2. Incorrect parameter types; 
      *                                                                  3. Parameter verification failed.
@@ -7912,7 +7916,8 @@ declare namespace window {
     /**
      * Register the callback of keyboard height change
      *
-     * @param { Callback<int> } callback - Callback used to return the current keyboard height, which is an integer, in px.
+     * @param { Callback<int> } callback - Callback used to return the current keyboard height,
+     *     which is an integer, in px.
      * @syscap SystemCapability.WindowManager.WindowManager.Core
      * @since 23 static
      */
@@ -7921,8 +7926,10 @@ declare namespace window {
     /**
      * Unregister the callback of keyboardHeightChange
      *
-     * @param { 'keyboardHeightChange' } type - The value is fixed at 'keyboardHeightChange', indicating the keyboard height change event.
-     * @param { Callback<int> } callback - Callback used to return the current keyboard height, which is an integer, in px. 
+     * @param { 'keyboardHeightChange' } type - The value is fixed at 'keyboardHeightChange',
+     *     indicating the keyboard height change event.
+     * @param { Callback<int> } callback - Callback used to return the current keyboard height,
+     *     which is an integer, in px. 
      * If a value is passed in, the corresponding subscription is canceled. 
      * If no value is passed in, all subscriptions to the specified event are canceled.
      * @throws { BusinessError } 401 - Parameter error. Possible cause: 1. Incorrect parameter types; 
@@ -7933,8 +7940,10 @@ declare namespace window {
     /**
      * Unregister the callback of keyboardHeightChange
      *
-     * @param { 'keyboardHeightChange' } type - The value is fixed at 'keyboardHeightChange', indicating the keyboard height change event.
-     * @param { Callback<int> } callback - Callback used to return the current keyboard height, which is an integer, in px. 
+     * @param { 'keyboardHeightChange' } type - The value is fixed at 'keyboardHeightChange',
+     *     indicating the keyboard height change event.
+     * @param { Callback<int> } callback - Callback used to return the current keyboard height,
+     *     which is an integer, in px. 
      * If a value is passed in, the corresponding subscription is canceled. 
      * If no value is passed in, all subscriptions to the specified event are canceled.
      * @throws { BusinessError } 401 - Parameter error. Possible cause: 1. Incorrect parameter types; 
