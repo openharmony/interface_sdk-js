f4d994618d4532a47aac4330aad01ee3da4b037b
suger_yeezy <chenyugan1@huawei.com>
2026-02-24 14:56:48 +0800

    update: 更新文件 @ohos.multimedia.systemSoundManager.d.ts
    Signed-off-by: suger_yeezy <chenyugan1@huawei.com>

--- api/@ohos.multimedia.systemSoundManager.d.ts
+++ api/@ohos.multimedia.systemSoundManager.d.ts
@@ -1203,10 +1203,7 @@ declare namespace systemSoundManager {
      *                                 1.Mandatory parameters are left unspecified;
      *                                 2.Incorrect parameter types.
      * @throws { BusinessError } 5400102 - Operation is not allowed, e.g. ringtone to add is not customized.
-     * @throws { BusinessError } 5400103 - I/O error. Possible causes:
-     *     1. The target file exceeds the int32 upper limit;
-     *     2. Failed to find the specified file;
-     *     3. System sound manager service error.
+     * @throws { BusinessError } 5400103 - I/O error.
      * @syscap SystemCapability.Multimedia.SystemSound.Core
      * @systemapi
      * @since 12 dynamic
@@ -1225,11 +1222,11 @@ declare namespace systemSoundManager {
      *                                 2.Incorrect parameter types.
      * @throws { BusinessError } 5400102 - Operation is not allowed, e.g. ringtone to add is not customized.
      * @throws { BusinessError } 5400103 - I/O error. Possible causes:
-     *     1. The target file exceeds the int32 upper limit;
+     *     1. The target file size exceeds 2 GB;
      *     2. Failed to find the specified file;
      *     3. System sound manager service error.
      * @throws { BusinessError } 20700004 - Data size exceeds the limit. Note:
-     *     If the data size exceeds the int32 upper limit, the system will return error code 5400103.
+     *     This error is returned when the file size is between 200MB and 2GB.
      * @throws { BusinessError } 20700005 - The number of files exceeds the limit.
      * @throws { BusinessError } 20700006 - Insufficient ROM space.
      * @syscap SystemCapability.Multimedia.SystemSound.Core
@@ -1279,12 +1276,12 @@ declare namespace systemSoundManager {
      *     2.Incorrect parameter types.
      * @throws { BusinessError } 5400102 - Operation is not allowed, e.g. ringtone to add is not customized.
      * @throws { BusinessError } 5400103 - I/O error. Possible causes:
-     *     1. The target file exceeds the int32 upper limit;
+     *     1. The target file size exceeds 2 GB;
      *     2. Failed to find the specified file;
      *     3. Duplicate file name;
      *     4. System sound manager service error.
      * @throws { BusinessError } 20700004 - Data size exceeds the limit. Note:
-     *     If the data size exceeds the int32 upper limit, the system will return error code 5400103.
+     *     This error is returned when the file size is between 200MB and 2GB.
      * @throws { BusinessError } 20700005 - The number of files exceeds the limit.
      * @throws { BusinessError } 20700006 - Insufficient ROM space.
      * @syscap SystemCapability.Multimedia.SystemSound.Core
