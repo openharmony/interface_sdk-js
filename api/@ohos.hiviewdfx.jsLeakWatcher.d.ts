8ea6b06a01a4913419f5da5074f4848406ba67f5
gan-chuantao <ganchuantao1@h-partners.com>
2026-02-27 17:44:06 +0800

    update
    Signed-off-by: gan-chuantao <ganchuantao1@h-partners.com>

--- api/@ohos.hiviewdfx.jsLeakWatcher.d.ts
+++ api/@ohos.hiviewdfx.jsLeakWatcher.d.ts
@@ -87,102 +87,28 @@ declare namespace jsLeakWatcher {
    */
   function enableLeakWatcher(isEnabled: boolean, configs: Array<string>, callback: Callback<Array<string>>): void
 
-  interface LeakWatcherConfig {
-    /**
-     * Array of types of objects to watch. default: value is an empty string.
-     *
-     * @type { string }
-     * @syscap SystemCapability.HiviewDFX.HiChecker
-     * @FaAndStageModel
-     * @since 24 dynamic
-     */
-    objectWatcher: string;
-    /**
-     * List of monitored object IDs. default: value is an empty array.
-     *
-     * @type { Array<number> }
-     * @syscap SystemCapability.HiviewDFX.HiChecker
-     * @FaAndStageModel
-     * @since 24 dynamic
-     */
-    objectUniqueIDs: Array<number>;
-    /**
-     * Interval between each leak detection cycle. default: value is 30 seconds.
-     *
-     * @type { number }
-     * @syscap SystemCapability.HiviewDFX.HiChecker
-     * @FaAndStageModel
-     * @since 24 dynamic
-     */
-    checkInterval: number;
-    /**
-     * Triggering dump when the number of leaked applications in the foreground reaches the specified value. default: value is 5.
-     *
-     * @type { number }
-     * @syscap SystemCapability.HiviewDFX.HiChecker
-     * @FaAndStageModel
-     * @since 24 dynamic
-     */
-    retainedVisibleThreshold: number;
-    /**
-     * Triggering dump when the number of leaked apps in the background reaches the specified value. default: value is 1.
-     *
-     * @type { number }
-     * @syscap SystemCapability.HiviewDFX.HiChecker
-     * @FaAndStageModel
-     * @since 24 dynamic
-     */
-    retainedInvisibleThreshold: number;
-    /**
-     * Maximum number of dump files that can be saved to prevent the disk space from being used up. default: value is 10.
-     *
-     * @type { number }
-     * @syscap SystemCapability.HiviewDFX.HiChecker
-     * @FaAndStageModel
-     * @since 24 dynamic
-     */
-    maxStoredHeapDumps: number;
-    /**
-     * Delay the dump execution to ensure that the GC can be scheduled and executed before the dump is performed. default: value is 5 seconds.
-     *
-     * @type { number }
-     * @syscap SystemCapability.HiviewDFX.HiChecker
-     * @FaAndStageModel
-     * @since 24 dynamic
-     */
-    dumpHeapWaitTimeMs: number;
-    /**
-     * Filter out the class names of objects you do not want to monitor. default: value is an empty array.
-     *
-     * @type { Array<string> }
-     * @syscap SystemCapability.HiviewDFX.HiChecker
-     * @FaAndStageModel
-     * @since 24 dynamic
-     */
-    whiteList: Array<string>;
-  }
-
   /**
    * Enables or disables jsLeakWatcher.
-   * 
+   *
    * This interface can detect js object memory leaks in a single call, which is more concise than the previous method
    * requiring four function (enable,watch,check dump) calls.If there is a memory leak, the leak file will be
    * returned to the developer through the callback function.
-   * 
-   * @param { boolean } isEnabled - Whether to enable or disable jsLeankWatcher. The value true means to enable the feature, and false means the opposite.
+   *
+   * @param { boolean } isEnabled - Whether to enable or disable jsLeankWatcher. 
+   *                                The value true means to enable the feature, and false means the opposite.
    * @param { LeakWatcherConfig } configs - Configuration of object types whose structures need to be monitored.
    * @param { Callback<Array<string>> } callback - Callback invoked when an object-related memory leak is detected.
    * @throws { BusinessError } 10801001 - The parameter isEnabled is invalid.
    * @throws { BusinessError } 10801002 - The parameter config is invalid.
    * @throws { BusinessError } 10801003 - The parameter callback is invalid.
-   * Input parameter error. Possible causes:
-   *  1.Mandatory parameters are left unspecified;
-   *  2.Incorrect parameter types;
-   *  3.Parameter verification failed.
+   *     Input parameter error. Possible causes:
+   *     1.Mandatory parameters are left unspecified;
+   *     2.Incorrect parameter types;
+   *     3.Parameter verification failed.
    * @syscap SystemCapability.HiviewDFX.HiChecker
    * @FaAndStageModel
    * @since 24 dynamic
    */
-  function enableLeakWatcher(isEnabled: boolean, configs: LeakWatcherConfig, callback: Callback<Array<string>>): void
+  function enableLeakWatcher(isEnabled: boolean, configs: LeakWatcherConfig, callback: Callback<Array<string>>): void;
 }
 export default jsLeakWatcher;
