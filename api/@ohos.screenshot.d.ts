60d1f1f153035d1a9f0cc1ee499b11342a0f0f46
chenxinjie253 <chenxinjie11@h-partners.com>
2026-02-27 09:49:45 +0800

    大小
    Signed-off-by: chenxinjie253 <chenxinjie11@h-partners.com>

--- api/@ohos.screenshot.d.ts
+++ api/@ohos.screenshot.d.ts
@@ -199,6 +199,7 @@ declare namespace screenshot {
    * @throws { BusinessError } 401 - Parameter error. Possible causes:
    *     1.Mandatory parameters are left unspecified.
    *     2.Incorrect parameter types.
+   *     3.The window IDs passed in the options are invalid.
    * @throws { BusinessError } 801 - Capability not supported on this device.
    * @throws { BusinessError } 1400003 - This display manager service works abnormally.
    * @syscap SystemCapability.WindowManager.WindowManager.Core
@@ -213,6 +214,10 @@ declare namespace screenshot {
    * @returns { Promise<image.PixelMap> } Promise used to return a PixelMap object.
    * @throws { BusinessError } 201 - Permission verification failed.
    *     The application does not have the permission required to call the API.
+   * @throws { BusinessError } 401 - Parameter error. Possible causes:
+   *     1.Mandatory parameters are left unspecified.
+   *     2.Incorrect parameter types.
+   *     3.The window IDs passed in the options are invalid.
    * @throws { BusinessError } 801 - Capability not supported on this device.
    * @throws { BusinessError } 1400003 - This display manager service works abnormally.
    * @syscap SystemCapability.WindowManager.WindowManager.Core
