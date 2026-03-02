3fde3608cf02cc42be1b72bfd235eaf619af0226
gan-chuantao <ganchuantao1@h-partners.com>
2026-03-02 11:10:25 +0800

    update
    Signed-off-by: gan-chuantao <ganchuantao1@h-partners.com>

--- api/@ohos.hiviewdfx.jsLeakWatcher.d.ts
+++ api/@ohos.hiviewdfx.jsLeakWatcher.d.ts
@@ -52,7 +52,7 @@ declare namespace jsLeakWatcher {
    * @syscap SystemCapability.HiviewDFX.HiChecker
    * @since 12 dynamic
    */
-   function check(): string;
+  function check(): string;
 
   /**
    * Dump leak list and heap snapshot.
@@ -67,138 +67,139 @@ declare namespace jsLeakWatcher {
 
   /**
    * Enables or disables jsLeakWatcher.
-   * 
+   *
    * This interface can detect js object memory leaks in a single call, which is more concise than the previous method
    * requiring four function (enable,watch,check dump) calls.If there is a memory leak, the leak file will be
    * returned to the developer through the callback function.
-   * 
-   * @param { boolean } isEnabled - Whether to enable or disable jsLeankWatcher. The value true means to enable the feature, and false means the opposite.
+   *
+   * @param { boolean } isEnabled - Whether to enable or disable jsLeakWatcher.
+   *     The value true means to enable the feature, and false means the opposite.
    * @param { Array<string> } configs - Array of types of objects to watch.
    * @param { Callback<Array<string>> } callback - Callback invoked when an object-related memory leak is detected.
    * @throws { BusinessError } 10801001 - The parameter isEnabled is invalid.
    * @throws { BusinessError } 10801002 - The parameter config is invalid.
    * @throws { BusinessError } 10801003 - The parameter callback is invalid.
-   * Input parameter error. Possible causes:
-   *  1.Mandatory parameters are left unspecified;
-   *  2.Incorrect parameter types;
-   *  3.Parameter verification failed.
+   *     Input parameter error. Possible causes:
+   *     1.Mandatory parameters are left unspecified;
+   *     2.Incorrect parameter types;
+   *     3.Parameter verification failed.
    * @syscap SystemCapability.HiviewDFX.HiChecker
    * @since 20 dynamic
    */
-  function enableLeakWatcher(isEnabled: boolean, configs: Array<string>, callback: Callback<Array<string>>): void
+  function enableLeakWatcher(isEnabled: boolean, configs: Array<string>, callback: Callback<Array<string>>): void;
 
-    /**
+  /**
    * JSLeakWatcher Configurations.
    *
    * @interface LeakWatcherConfig
    * @syscap SystemCapability.HiviewDFX.HiChecker
    * @FaAndStageModel
-   * @since 24 dynamic&static
+   * @since 24 dynamic
    */
-    interface LeakWatcherConfig {  
-      /**
-       * Array of types of objects to watch.
-       *
-       * @type { Array<string> }
-       * @syscap SystemCapability.HiviewDFX.HiChecker
-       * @FaAndStageModel
-       * @since 24 dynamic&static
-       */
-      objectWatcher: Array<string>;
-      /**
-       * List of monitored object IDs. Default: value is an empty array.
-       *
-       * @type { ?Array<int> }
-       * @syscap SystemCapability.HiviewDFX.HiChecker
-       * @FaAndStageModel
-       * @since 24 dynamic&static
-       */
-      objectUniqueIDs?: Array<int>;
-      /**
-       * Interval between each leak detection cycle. Default: value is 30 seconds.
-       *
-       * @type { ?int }
-       * @syscap SystemCapability.HiviewDFX.HiChecker
-       * @FaAndStageModel
-       * @since 24 dynamic&static
-       */
-      checkInterval?: int;
-      /**
-       * Triggering dump when the number of leaked applications in the foreground reaches the specified value.
-       * Default: value is 5.
-       *
-       * @type { ?int }
-       * @syscap SystemCapability.HiviewDFX.HiChecker
-       * @FaAndStageModel
-       * @since 24 dynamic&static
-       */
-      retainedVisibleThreshold?: int;
-      /**
-       * Triggering dump when the number of leaked apps in the background reaches the specified value.
-       * Default: value is 1.
-       *
-       * @type { ?int }
-       * @syscap SystemCapability.HiviewDFX.HiChecker
-       * @FaAndStageModel
-       * @since 24 dynamic&static
-       */
-      retainedInvisibleThreshold?: int;
-      /**
-       * Maximum number of dump files that can be saved to prevent the disk space from being exhausted.
-       * Default: value is 10.
-       *
-       * @type { ?int }
-       * @syscap SystemCapability.HiviewDFX.HiChecker
-       * @FaAndStageModel
-       * @since 24 dynamic&static
-       */
-      maxStoredHeapDumps?: int;
-      /**
-       * Delay the dump execution to ensure that the GC can be scheduled and executed before the dump is performed.
-       * Default: value is 5 seconds.
-       *
-       * @type { ?int }
-       * @syscap SystemCapability.HiviewDFX.HiChecker
-       * @FaAndStageModel
-       * @since 24 dynamic&static
-       */
-      dumpHeapWaitTimeMs?: int;
-      /**
-       * Filter out component, ability, and window names of objects you do not want to monitor.
-       * Default: value is an empty array.
-       *
-       * @type { ?Array<string> }
-       * @syscap SystemCapability.HiviewDFX.HiChecker
-       * @FaAndStageModel
-       * @since 24 dynamic&static
-       */
-      whiteList?: Array<string>;
-    }
-  
+  interface LeakWatcherConfig {
     /**
-     * Enables or disables jsLeakWatcher.
+     * Array of types of objects to watch.
      *
-     * This interface can detect js object memory leaks in a single call, which is more concise than the previous method
-     * requiring four functions (enable, watch, check, dump) calls.If there is a memory leak, the leak file will be
-     * returned to the developer through the callback function.
-     * Furthermore, parameters such as object's unique IDs for monitored objects and check interval can be adjusted via
-     * the configs parameter.
-     *
-     * @param { boolean } isEnabled - Whether to enable or disable jsLeakWatcher.
-     *     The value true means to enable the feature, and false means the opposite.
-     * @param { LeakWatcherConfig } configs - Configuration of object types whose structures need to be monitored.
-     * @param { Callback<Array<string>> } callback - Callback invoked when an object-related memory leak is detected.
-     * @throws { BusinessError } 10801001 - The parameter isEnabled is invalid.
-     * @throws { BusinessError } 10801002 - The parameter config is invalid.
-     * @throws { BusinessError } 10801003 - The parameter callback is invalid.
-     *     Input parameter error. Possible causes:
-     *     1.Mandatory parameters are left unspecified;
-     *     2.Incorrect parameter types;
-     *     3.Parameter verification failed.
+     * @type { Array<string> }
      * @syscap SystemCapability.HiviewDFX.HiChecker
      * @FaAndStageModel
-     * @since 24 dynamic&static
+     * @since 24 dynamic
      */
-    function enableLeakWatcher(isEnabled: boolean, configs: LeakWatcherConfig, callback: Callback<Array<string>>): void;
+    objectWatcher: Array<string>;
+    /**
+     * List of monitored object IDs. Default: value is an empty array.
+     *
+     * @type { ?Array<number> }
+     * @syscap SystemCapability.HiviewDFX.HiChecker
+     * @FaAndStageModel
+     * @since 24 dynamic
+     */
+    objectUniqueIDs?: Array<number>;
+    /**
+     * Interval between each leak detection cycle. Default: value is 30 seconds.
+     *
+     * @type { ?number }
+     * @syscap SystemCapability.HiviewDFX.HiChecker
+     * @FaAndStageModel
+     * @since 24 dynamic
+     */
+    checkInterval?: number;
+    /**
+     * Triggering dump when the number of leaked applications in the foreground reaches the specified value.
+     * Default: value is 5.
+     *
+     * @type { ?number }
+     * @syscap SystemCapability.HiviewDFX.HiChecker
+     * @FaAndStageModel
+     * @since 24 dynamic
+     */
+    retainedVisibleThreshold?: number;
+    /**
+     * Triggering dump when the number of leaked apps in the background reaches the specified value.
+     * Default: value is 1.
+     *
+     * @type { ?number }
+     * @syscap SystemCapability.HiviewDFX.HiChecker
+     * @FaAndStageModel
+     * @since 24 dynamic
+     */
+    retainedInvisibleThreshold?: number;
+    /**
+     * Maximum number of dump files that can be saved to prevent the disk space from being exhausted.
+     * Default: value is 10.
+     *
+     * @type { ?number }
+     * @syscap SystemCapability.HiviewDFX.HiChecker
+     * @FaAndStageModel
+     * @since 24 dynamic
+     */
+    maxStoredHeapDumps?: number;
+    /**
+     * Delay the dump execution to ensure that the GC can be scheduled and executed before the dump is performed.
+     * Default: value is 5 seconds.
+     *
+     * @type { ?number }
+     * @syscap SystemCapability.HiviewDFX.HiChecker
+     * @FaAndStageModel
+     * @since 24 dynamic
+     */
+    dumpHeapWaitTimeMs?: number;
+    /**
+     * Filter out component, ability, and window names of objects you do not want to monitor.
+     * Default: value is an empty array.
+     *
+     * @type { ?Array<string> }
+     * @syscap SystemCapability.HiviewDFX.HiChecker
+     * @FaAndStageModel
+     * @since 24 dynamic
+     */
+    whiteList?: Array<string>;
   }
+
+  /**
+   * Enables or disables jsLeakWatcher.
+   *
+   * This interface can detect js object memory leaks in a single call, which is more concise than the previous method
+   * requiring four functions (enable, watch, check, dump) calls.If there is a memory leak, the leak file will be
+   * returned to the developer through the callback function.
+   * Furthermore, parameters such as object's unique IDs for monitored objects and check interval can be adjusted via
+   * the configs parameter.
+   *
+   * @param { boolean } isEnabled - Whether to enable or disable jsLeakWatcher.
+   *     The value true means to enable the feature, and false means the opposite.
+   * @param { LeakWatcherConfig } configs - Configuration of object types whose structures need to be monitored.
+   * @param { Callback<Array<string>> } callback - Callback invoked when an object-related memory leak is detected.
+   * @throws { BusinessError } 10801001 - The parameter isEnabled is invalid.
+   * @throws { BusinessError } 10801002 - The parameter config is invalid.
+   * @throws { BusinessError } 10801003 - The parameter callback is invalid.
+   *     Input parameter error. Possible causes:
+   *     1.Mandatory parameters are left unspecified;
+   *     2.Incorrect parameter types;
+   *     3.Parameter verification failed.
+   * @syscap SystemCapability.HiviewDFX.HiChecker
+   * @FaAndStageModel
+   * @since 24 dynamic
+   */
+  function enableLeakWatcher(isEnabled: boolean, configs: LeakWatcherConfig, callback: Callback<Array<string>>): void;
+}
 export default jsLeakWatcher;
