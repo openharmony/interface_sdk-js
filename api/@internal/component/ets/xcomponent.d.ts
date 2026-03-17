9ddadb745fc16a735831ed32c8d6312a8d17be0e
lisitao <lisitao3@h-partners.com>
2026-03-17 15:44:11 +0800

    [L] xc add EDR type Co-Authored-By:Agent
    Signed-off-by: lisitao <lisitao3@h-partners.com>

--- api/@internal/component/ets/xcomponent.d.ts
+++ api/@internal/component/ets/xcomponent.d.ts
@@ -609,6 +609,15 @@ declare enum HdrType {
    * @since 24 dynamic
    */
   AIHDR = 1,
+  /**
+   * EDR type.
+   *
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @stagemodelonly
+   * @atomicservice
+   * @since 24 dynamic
+   */
+  EDR = 2
 }
 
 /**
