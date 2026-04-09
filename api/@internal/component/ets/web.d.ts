e4c58f18f9e3bb1526a851b8edc0bf8cec2a83d4
liyongzhuang <liyongzhuang1@huawei.com>
2026-04-09 15:24:38 +0800

    fix sdk功能一致性
    Signed-off-by: liyongzhuang <liyongzhuang1@huawei.com>

--- api/@internal/component/ets/web.d.ts
+++ api/@internal/component/ets/web.d.ts
@@ -670,23 +670,7 @@ declare enum MessageLevel {
    * @atomicservice
    * @since 11 dynamic
    */
-  Debug,
-
-  /**
-   * Error level.
-   *
-   * @syscap SystemCapability.Web.Webview.Core
-   * @since 8
-   */
-  /**
-   * Error level.
-   *
-   * @syscap SystemCapability.Web.Webview.Core
-   * @crossplatform
-   * @atomicservice
-   * @since 11 dynamic
-   */
-  Error,
+  Debug = 1,
 
   /**
    * Info level.
@@ -702,23 +686,7 @@ declare enum MessageLevel {
    * @atomicservice
    * @since 11 dynamic
    */
-  Info,
-
-  /**
-   * Log level.
-   *
-   * @syscap SystemCapability.Web.Webview.Core
-   * @since 8
-   */
-  /**
-   * Log level.
-   *
-   * @syscap SystemCapability.Web.Webview.Core
-   * @crossplatform
-   * @atomicservice
-   * @since 11 dynamic
-   */
-  Log,
+  Info = 2,
 
   /**
    * Warn level.
@@ -734,7 +702,40 @@ declare enum MessageLevel {
    * @atomicservice
    * @since 11 dynamic
    */
-  Warn
+  Warn = 3,
+
+  /**
+   * Error level.
+   *
+   * @syscap SystemCapability.Web.Webview.Core
+   * @since 8
+   */
+  /**
+   * Error level.
+   *
+   * @syscap SystemCapability.Web.Webview.Core
+   * @crossplatform
+   * @atomicservice
+   * @since 11 dynamic
+   */
+  Error = 4,
+
+  /**
+   * Log level.
+   *
+   * @syscap SystemCapability.Web.Webview.Core
+   * @since 8
+   */
+  /**
+   * Log level.
+   *
+   * @syscap SystemCapability.Web.Webview.Core
+   * @crossplatform
+   * @atomicservice
+   * @since 11 dynamic
+   * @deprecated since 26.0.0
+   */
+  Log = 5
 }
 
 /**
