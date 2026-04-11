174e7e0b22cee34af73b9eadb1abbd702d6b42c6
zhang-leioy <zhanglei730@h-partners.com>
2026-04-11 16:03:55 +0800

    update: 更新文件 @ohos.file.picker.d.ts
    Signed-off-by: zhang-leioy <zhanglei730@h-partners.com>

--- api/@ohos.file.picker.d.ts
+++ api/@ohos.file.picker.d.ts
@@ -20,6 +20,7 @@
 
 import { AsyncCallback, Callback } from './@ohos.base';
 import Context from './application/Context';
+import window from './@ohos.window';
 import type { CustomColors } from './@ohos.arkui.theme';
 /**
  * The **Picker** module encapsulates APIs such as **DocumentViewPicker**, **AudioViewPicker**, and **PhotoViewPicker**
