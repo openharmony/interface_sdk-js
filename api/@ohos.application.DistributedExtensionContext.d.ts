e1d42a2c6481a8a62d97bef5c9f5c01f79f81c94
wei-guoqing1 <weiguoqing2@h-partners.com>
2026-04-17 10:50:38 +0800

    add DistributedExtensionContext interface
    Signed-off-by: wei-guoqing1 <weiguoqing2@h-partners.com>

--- api/@ohos.application.DistributedExtensionContext.d.ts
+++ api/@ohos.application.DistributedExtensionContext.d.ts
@@ -18,7 +18,10 @@
  * @kit DistributedServiceKit
  */
 
+import { ConnectOptions } from '../ability/connectOptions';
 import type ExtensionContext from './application/ExtensionContext';
+import Want from '../@ohos.app.ability.Want';
+
 /**
  * Class inherited for the distributed extension function.
  *
@@ -27,6 +30,40 @@ import type ExtensionContext from './application/ExtensionContext';
  * @since 20 dynamic
  * @since 23 static
  */
-declare class DistributedExtensionContext extends ExtensionContext {}
+declare class DistributedExtensionContext extends ExtensionContext {
+  /**
+   * Connects to a remote Service extension ability.
+   * <p>This method connects to a Service extension ability on a remote device.
+   * You must implement the {@link ConnectOptions} interface to obtain the proxy of the target
+   * service extension when connected.</p>
+   *
+   * @param { Want } want - Indicates the service extension to connect.
+   * @param { ConnectOptions } options - Indicates the callback of connection.
+   * @returns { number } Returns the connection id.
+   * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified;
+   * 2. Incorrect parameter types; 3. Parameter verification failed.
+   * @throws { BusinessError } 16000011 - The context does not exist.
+   * @throws { BusinessError } 16000050 - Internal error.
+   * @syscap SystemCapability.DistributedSched.AppCollaboration
+   * @stagemodelonly
+   * @since 26.0.0 dynamic&static
+   */
+  connectServiceExtensionAbility(want: Want, options: ConnectOptions): number;
+
+  /**
+   * Disconnects from a remote Service extension ability.
+   *
+   * @param { number } connection - the connection id returned from connectServiceExtensionAbility.
+   * @returns { Promise<void> } The promise returned by the function.
+   * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified;
+   * 2. Incorrect parameter types; 3. Parameter verification failed.
+   * @throws { BusinessError } 16000011 - The context does not exist.
+   * @throws { BusinessError } 16000050 - Internal error.
+   * @syscap SystemCapability.DistributedSched.AppCollaboration
+   * @stagemodelonly
+   * @since 26.0.0 dynamic&static
+   */
+  disconnectServiceExtensionAbility(connection: number): Promise<void>;
+}
 
 export default DistributedExtensionContext;
