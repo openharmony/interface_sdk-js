b8b136eb9bdbafb88fb40c58456dea33f42c6973
wei-guoqing1 <weiguoqing2@h-partners.com>
2026-04-22 12:13:49 +0800

    update: 更新文件 @ohos.application.DistributedExtensionContext.d.ts
    Signed-off-by: wei-guoqing1 <weiguoqing2@h-partners.com>

--- api/@ohos.application.DistributedExtensionContext.d.ts
+++ api/@ohos.application.DistributedExtensionContext.d.ts
@@ -14,7 +14,7 @@
  */
 
 /**
- * @file
+ * @file Distributed Extension Context
  * @kit DistributedServiceKit
  */
 
@@ -33,37 +33,49 @@ import Want from '../@ohos.app.ability.Want';
 declare class DistributedExtensionContext extends ExtensionContext {
   /**
    * Connects to a remote Service extension ability.
-   * <p>This method connects to a Service extension ability on a remote device.
+   * 
+   * This method connects to a Service extension ability on a remote device.
    * You must implement the {@link ConnectOptions} interface to obtain the proxy of the target
-   * service extension when connected.</p>
+   * service extension when connected.
    *
    * @param { Want } want - Indicates the service extension to connect.
    * @param { ConnectOptions } options - Indicates the callback of connection.
-   * @returns { number } Returns the connection id.
+   * @returns { long } Returns the connection id.
+   * @throws { BusinessError } 201 - The application does not have permission to call the interface.
    * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified;
-   * 2. Incorrect parameter types; 3. Parameter verification failed.
+   *     2. Incorrect parameter types; 3. Parameter verification failed.
+   * @throws { BusinessError } 16000001 - The specified ability does not exist.
+   * @throws { BusinessError } 16000002 - Incorrect ability type.
+   * @throws { BusinessError } 16000004 - Cannot start an invisible component.
+   * @throws { BusinessError } 16000005 - The specified process does not have the permission.
+   * @throws { BusinessError } 16000006 - Cross-user operations are not allowed.
+   * @throws { BusinessError } 16000008 - The crowdtesting application expires.
    * @throws { BusinessError } 16000011 - The context does not exist.
+   * @throws { BusinessError } 16000012 - The application is controlled.
+   * @throws { BusinessError } 16000013 - The application is controlled by EDM.
    * @throws { BusinessError } 16000050 - Internal error.
+   * @throws { BusinessError } 16000053 - The ability is not on the top of the UI.
+   * @throws { BusinessError } 16000055 - Installation-free timed out.
    * @syscap SystemCapability.DistributedSched.AppCollaboration
    * @stagemodelonly
    * @since 26.0.0 dynamic&static
    */
-  connectServiceExtensionAbility(want: Want, options: ConnectOptions): number;
+  connectServiceExtensionAbility(want: Want, options: ConnectOptions): long;
 
   /**
    * Disconnects from a remote Service extension ability.
    *
-   * @param { number } connection - the connection id returned from connectServiceExtensionAbility.
+   * @param { long } connection - the connection id returned from connectServiceExtensionAbility.
    * @returns { Promise<void> } The promise returned by the function.
    * @throws { BusinessError } 401 - Parameter error. Possible causes: 1. Mandatory parameters are left unspecified;
-   * 2. Incorrect parameter types; 3. Parameter verification failed.
+   *     2. Incorrect parameter types; 3. Parameter verification failed.
    * @throws { BusinessError } 16000011 - The context does not exist.
    * @throws { BusinessError } 16000050 - Internal error.
    * @syscap SystemCapability.DistributedSched.AppCollaboration
    * @stagemodelonly
    * @since 26.0.0 dynamic&static
    */
-  disconnectServiceExtensionAbility(connection: number): Promise<void>;
+  disconnectServiceExtensionAbility(connection: long): Promise<void>;
 }
 
 export default DistributedExtensionContext;
