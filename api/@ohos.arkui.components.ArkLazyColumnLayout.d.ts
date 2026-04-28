75e78acbc44f3a7faabf6376f892843e7f424cc6
yangcan18 <yangcan18@huawei.com>
2026-04-28 21:06:23 +0800

    新增LazyColumnLayout组件API
    Signed-off-by: yangcan18 <yangcan18@huawei.com>
    Change-Id: Ib2d353c1466a9219e39dbd4feeb3eb6df710b853

--- api/@ohos.arkui.components.ArkLazyColumnLayout.d.ts
+++ api/@ohos.arkui.components.ArkLazyColumnLayout.d.ts
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2026 Huawei Device Co., Ltd.
- * Licensed under the Apache License, Version 2.0 (the "License"),
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -30,7 +30,7 @@
  */
 export interface LazyColumnLayoutInterface {
   /**
-   * Construct the lazy column layout attribute.
+   * Construct the lazy column attribute.
    *
    * @returns { LazyColumnLayoutAttribute }
    * @syscap SystemCapability.ArkUI.ArkUI.Full
@@ -54,10 +54,10 @@ export interface LazyColumnLayoutInterface {
  */
 export declare class LazyColumnLayoutAttribute extends CommonMethod<LazyColumnLayoutAttribute> {
   /**
-   * The spacing between child components in the vertical direction.
+   * The spacing between rows.
    *
-   * @param { LengthMetrics | undefined } value - The spacing between child components in the vertical direction.
-   *     <br>Default value: LengthMetrics.vp(0)
+   * @param { LengthMetrics | undefined } space - the spacing between rows.
+   *     <br>Default value 0. <br>Range: [0, +∞).
    * @returns { LazyColumnLayoutAttribute }
    * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @stagemodelonly
@@ -65,13 +65,13 @@ export declare class LazyColumnLayoutAttribute extends CommonMethod<LazyColumnLa
    * @atomicservice
    * @since 26.0.0 dynamic
    */
-  space(value: LengthMetrics | undefined): LazyColumnLayoutAttribute;
+  space(space: LengthMetrics | undefined): LazyColumnLayoutAttribute;
 
   /**
-   * Sets the alignment mode of child components in the horizontal direction when the width of the LazyColumnLayout
-   * is greater than the width of the child components.
+   * Sets the horizontal alignment of the row content.
    *
-   * @param { ListItemAlign } value - Alignment mode of child components in the horizontal direction.<br>Default value: <em>ListItemAlign.Start</em>
+   * @param { HorizontalAlign | undefined } value - the horizontal alignment of the row content.
+   *     <br>Default value HorizontalAlign.Center.
    * @returns { LazyColumnLayoutAttribute }
    * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @stagemodelonly
@@ -79,13 +79,14 @@ export declare class LazyColumnLayoutAttribute extends CommonMethod<LazyColumnLa
    * @atomicservice
    * @since 26.0.0 dynamic
    */
-  alignListItem(value: ListItemAlign): LazyColumnLayoutAttribute;
+  alignItems(value: HorizontalAlign | undefined): LazyColumnLayoutAttribute;
 
   /**
-   * Set or reset the callback which is triggered when the start and end positions of the display change.
+   * Triggered when the index of child components in the visible area changes.
    *
-   * @param { OnScrollIndexCallback | undefined } callback - callback function, triggered when the start or
-   *     end positions of the display change.
+   * @param { onVisibleIndexesChangeCallback | undefined } callback - callback function, triggered
+   *     when the index of child components in the visible area changes.
+   *     <br>Passing undefined will unregister the callback.
    * @returns { LazyColumnLayoutAttribute }
    * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @stagemodelonly
@@ -93,27 +94,12 @@ export declare class LazyColumnLayoutAttribute extends CommonMethod<LazyColumnLa
    * @atomicservice
    * @since 26.0.0 dynamic
    */
-  onScrollIndex(callback: OnScrollIndexCallback | undefined): LazyColumnLayoutAttribute;
+  onVisibleIndexesChange(callback: onVisibleIndexesChangeCallback | undefined): LazyColumnLayoutAttribute;
 }
 
 /**
- * Defines the callback type used in onScrollIndex.
+ * Defines the lazy column layout component.
  *
- * @typedef {function} OnScrollIndexCallback
- * @param {number} start - the first index in visible content.
- * @param {number} end - the last index in visible content.
- * @syscap SystemCapability.ArkUI.ArkUI.Full
- * @stagemodelonly
- * @crossplatform
- * @atomicservice
- * @since 26.0.0 dynamic
- */
-declare type OnScrollIndexCallback = (start: number, end: number) => void;
-
-/**
- * Defines LazyColumnLayout Component.
- *
- * @type { LazyColumnLayoutInterface }
  * @syscap SystemCapability.ArkUI.ArkUI.Full
  * @stagemodelonly
  * @crossplatform
@@ -123,9 +109,8 @@ declare type OnScrollIndexCallback = (start: number, end: number) => void;
 export declare const LazyColumnLayout: LazyColumnLayoutInterface;
 
 /**
- * Defines LazyColumnLayout Component instance.
+ * Defines the lazy column layout component instance.
  *
- * @type { LazyColumnLayoutAttribute }
  * @syscap SystemCapability.ArkUI.ArkUI.Full
  * @stagemodelonly
  * @crossplatform
