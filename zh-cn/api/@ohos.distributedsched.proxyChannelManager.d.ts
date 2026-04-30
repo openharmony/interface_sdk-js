abf7d0080732dc14510ccdae34402115642c1110
crazy_hu <huxiusong1@huawei.com>
2026-04-30 14:21:27 +0800

    api与文档注释一致性整改
    Signed-off-by: crazy_hu <huxiusong1@huawei.com>

--- zh-cn/api/@ohos.distributedsched.proxyChannelManager.d.ts
+++ zh-cn/api/@ohos.distributedsched.proxyChannelManager.d.ts
@@ -20,30 +20,25 @@
 
 import { Callback } from './@ohos.base';
 /**
- * # Instructions
+ * # 使用说明
  *
- * Before calling the APIs of this module, complete the following configurations:
+ * 调用模块接口前，需要完成如下配置。
  *
- * 1. Apply for the **ohos.permission.ACCESS_BLUETOOTH** permission. For details about how to configure and apply for
- * permissions, see [Declaring Permissions](docroot://security/AccessToken/declare-permissions.md)
- * and [Requesting User Authorization](docroot://security/AccessToken/request-user-authorization.md).
- * 2. For application processes that need to be started by proxy, set the **action** field to
- * **action.ohos.pull.listener** in the **module.json5** file.
+ * 1. 需要申请ohos.permission.ACCESS_BLUETOOTH权限。如何配置和申请权限，具体操作请参考
+ *    [声明权限](docroot://security/AccessToken/declare-permissions.md)和
+ *    [向用户申请授权](docroot://security/AccessToken/request-user-authorization.md)。
+ * 2. 对于需要代理拉起的应用进程，需要在module.json5文件中配置action字段: "action.ohos.pull.listener"。
  */
 /**
- * DSoftBus provides stable and reliable underlying channels for cross-device communication. This module is developed
- * based on DSoftBus. It supports efficient data exchange between phones and wearables, providing users with a seamless
- * device interconnection experience. During collaboration between the phone application and watch application, if the
- * phone application is not running in the foreground, its downlink messages are forwarded to the notification server
- * and then sent to the watch through the proxy module. The core functions of this module include proxy channel
- * management, data route management, application state awareness and wakeup, and link state monitoring.
+ * 软总线具备常驻运行能力，可为跨设备通信提供稳定可靠的底层通道。本模块基于软总线进程开发，支持手机与穿戴设备间高效的数据互通，
+ * 可为用户提供无缝的设备互联体验。使用场景：手机侧APP与手表侧APP协同时，当手机APP不在前台被使用，手机应
+ * 用的下行消息经由通知服务器，通过代理模块发送给手表侧。模块核心功能包括：代理通道管理、数据路由管理、 应用状态感知和唤醒、
+ * 链路状态监听。
  *
- * - Proxy channel management: Manages bidirectional data channels established between phones and wearables via the
- * Bluetooth Basic Rate (BR) protocol.
- * - Data route management: Accurately forwards data of wearables based on the specified service UUID.
- * - Application state awareness and wakeup: After a proxy channel is enabled, dynamically analyzes and wakes up the
- * corresponding application process on the phone after receiving data sent by the wearable.
- * - Link state monitoring: Monitors the channel connection state in real time through callback.
+ * - 代理通道管理：通过蓝牙 BR 协议建立手机与穿戴设备的双向数据通道，支持的数据通道 ID 范围是[1,2147483647] 。
+ * - 数据路由管理：基于 UUID 服务识别机制，精准转发穿戴设备数据。
+ * - 应用状态感知和唤醒：代理通道使能后，收到穿戴设备发送的数据后，动态分析和唤醒手机端对应应用进程。
+ * - 全链路状态监控：通过回调实时感知通道连接状态。
  *
  * @syscap SystemCapability.DistributedSched.AppCollaboration
  * @stagemodelonly
@@ -52,14 +47,12 @@ import { Callback } from './@ohos.base';
  */
 declare namespace proxyChannelManager {
   /**
-   * Opens a proxy channel. This API uses a promise to return the result.
+   * 打开代理通道，使用Promise异步回调返回结果。
    *
    * @permission ohos.permission.ACCESS_BLUETOOTH
-   * @param { ChannelInfo } channelInfo - Channel information, including the MAC address and service UUID of the peer
-   *     device.
-   * @returns { Promise<int> } Proxy channel ID. The value range is [1, 2147483647]. The lifecycle of **channelId** is
-   *     the same as that of the proxy channel. If the proxy is not closed, the returned **channelId** is the same as
-   *     that passed in the API.
+   * @param { ChannelInfo } channelInfo - 对端设备及服务的MAC和UUID信息。
+   * @returns { Promise<int> } 返回代理通道的channelId，取值范围为1~2147483647。channelId的生命周期和代理通道生命周期相同，
+   *     不关闭代理时，传入相同入参将返回相同channelId。
    * @throws { BusinessError } 201 - Permission denied.
    * @throws { BusinessError } 801 - Capability not supported because bluetooth proxy function has
    *     been trimmed. [since 26.0.0]
@@ -77,10 +70,10 @@ declare namespace proxyChannelManager {
   function openProxyChannel(channelInfo: ChannelInfo): Promise<int>;
 
   /**
-   * Closes a proxy channel that has been opened.
+   * 关闭已打开的代理通道。
    *
    * @permission ohos.permission.ACCESS_BLUETOOTH
-   * @param { int } channelId - Channel ID obtained when the proxy channel is opened.
+   * @param { int } channelId - 打开代理通道时获取的channelId。
    * @throws { BusinessError } 201 - Permission denied.
    * @throws { BusinessError } 801 - Capability not supported because bluetooth proxy function has
    *     been trimmed. [since 26.0.0]
@@ -96,12 +89,12 @@ declare namespace proxyChannelManager {
   function closeProxyChannel(channelId: int): void;
 
   /**
-   * Send data to the peer device through proxy channel identification.
+   * 向对端发送数据，使用Promise异步回调。
    *
    * @permission ohos.permission.ACCESS_BLUETOOTH
-   * @param { int } channelId - Indicates the unique channelId.
-   * @param { ArrayBuffer } data - Indicates the message data to send.
-   * @returns { Promise<void> } The promise returned by the function.
+   * @param { int } channelId - 打开代理通道时获取的channelId。
+   * @param { ArrayBuffer } data - 向对端发送的字节消息，长度最大为4096个字节。
+   * @returns { Promise<void> } 无返回值的Promise的对象。
    * @throws { BusinessError } 201 - Permission denied.
    * @throws { BusinessError } 801 - Capability not supported.[since 26]
    * @throws { BusinessError } 32390004 - ChannelId is invalid or unavailable.
@@ -118,13 +111,12 @@ declare namespace proxyChannelManager {
   function sendData(channelId: int, data: ArrayBuffer): Promise<void>;
 
   /**
-   * Subscribes to data receiving events. This API returns the result asynchronously through a callback.
+   * 订阅数据接收事件，使用异步回调。
    *
    * @permission ohos.permission.ACCESS_BLUETOOTH
-   * @param { 'receiveData' } type - Event type. The value **receiveData** indicates the data receiving event.
-   * @param { number } channelId - Channel ID obtained when the proxy channel is opened.
-   * @param { Callback<DataInfo> } callback - Callback used to return the received data. If the callback function is
-   *     registered multiple times, only the last registered one takes effect.
+   * @param { 'receiveData' } type - 设置订阅类型，固定取值为'receiveData'。
+   * @param { number } channelId - 打开代理通道时获取的channelId。
+   * @param { Callback<DataInfo> } callback - 回调函数，返回接收到的数据。多次注册回调函数，最后一次注册的回调函数生效。
    * @throws { BusinessError } 201 - Permission denied.
    * @throws { BusinessError } 32390004 - ChannelId is invalid or unavailable.
    * @throws { BusinessError } 32390006 - Parameter error.
@@ -137,14 +129,13 @@ declare namespace proxyChannelManager {
   function on(type: 'receiveData', channelId: number, callback: Callback<DataInfo>): void;
 
   /**
-   * Unsubscribes from data receiving events.
+   * 取消订阅数据接收事件，停止接收数据。
    *
    * @permission ohos.permission.ACCESS_BLUETOOTH
-   * @param { 'receiveData' } type - Event type. The value **receiveData** indicates the data receiving event.
-   * @param { number } channelId - Channel ID obtained when the proxy channel is opened.
-   * @param { Callback<DataInfo> } [callback] - Registered callback, If the value is empty, **undefined**, or **null**,
-   *     all callbacks of data receiving events are unregistered. If the value is not empty, the last registered
-   *     callback is used.
+   * @param { 'receiveData' } type - 设置订阅类型，固定取值为'receiveData'。
+   * @param { number } channelId - 打开代理通道时获取的channelId。
+   * @param { Callback<DataInfo> } [callback] - 注册的回调函数。如果为空、undefined、null，则取消订阅所有的数据接收事件。
+   *     如果不为空，传入最后一次注册的回调函数。
    * @throws { BusinessError } 201 - Permission denied.
    * @throws { BusinessError } 32390004 - ChannelId is invalid or unavailable.
    * @throws { BusinessError } 32390006 - Parameter error.
@@ -157,12 +148,11 @@ declare namespace proxyChannelManager {
   function off(type: 'receiveData', channelId: number, callback?: Callback<DataInfo>): void;
 
   /**
-   * Subscribes to data receiving events. This API returns the result asynchronously through a callback.
+   * 订阅数据接收事件，使用异步回调。
    *
    * @permission ohos.permission.ACCESS_BLUETOOTH
-   * @param { int } channelId - Channel ID obtained when the proxy channel is opened.
-   * @param { Callback<DataInfo> } callback - Callback used to return the received data. If the callback function is
-   *     registered multiple times, only the last registered one takes effect.
+   * @param { int } channelId - 打开代理通道时获取的channelId。
+   * @param { Callback<DataInfo> } callback - 回调函数，返回接收到的数据。多次注册回调函数，最后一次注册的回调函数生效。
    * @throws { BusinessError } 201 - Permission denied.
    * @throws { BusinessError } 32390004 - ChannelId is invalid or unavailable.
    * @throws { BusinessError } 32390006 - Parameter error.
@@ -175,13 +165,12 @@ declare namespace proxyChannelManager {
   function onReceiveData(channelId: int, callback: Callback<DataInfo>): void;
 
   /**
-   * Unsubscribes from data receiving events.
+   * 取消订阅数据接收事件，停止接收数据。
    *
    * @permission ohos.permission.ACCESS_BLUETOOTH
-   * @param { int } channelId - Channel ID obtained when the proxy channel is opened.
-   * @param { Callback<DataInfo> } [callback] - Registered callback, If the value is empty, **undefined**, or **null**,
-   *     all callbacks of data receiving events are unregistered. If the value is not empty, the last registered
-   *     callback is used.
+   * @param { int } channelId - 打开代理通道时获取的channelId。
+   * @param { Callback<DataInfo> } [callback] - 注册的回调函数。如果为空、undefined、null，则取消订阅所有的数据接收事件。
+   *     如果不为空，传入最后一次注册的回调函数。
    * @throws { BusinessError } 201 - Permission denied.
    * @throws { BusinessError } 32390004 - ChannelId is invalid or unavailable.
    * @throws { BusinessError } 32390006 - Parameter error.
@@ -194,14 +183,13 @@ declare namespace proxyChannelManager {
   function offReceiveData(channelId: int, callback?: Callback<DataInfo>): void;
 
   /**
-   * Subscribes to channel state change events. This API returns the result asynchronously through a callback.
+   * 订阅通道状态事件，使用callback进行异步回调。
    *
    * @permission ohos.permission.ACCESS_BLUETOOTH
-   * @param { 'channelStateChange' } type - Event type. The value **channelStateChange** indicates the channel state
-   *     change event.
-   * @param { number } channelId - Channel ID obtained when the proxy channel is opened.
-   * @param { Callback<ChannelStateInfo> } callback - Callback used to return the received channel state. If the
-   *     callback function is registered multiple times, only the last registered one takes effect.
+   * @param { 'channelStateChange' } type - 设置订阅类型，固定取值为'channelStateChange'。
+   * @param { number } channelId - 打开代理通道时获取的channelId。
+   * @param { Callback<ChannelStateInfo> } callback - 回调函数，返回接收到的通道状态。多次注册callback，
+   *     最后一次注册的callback生效
    * @throws { BusinessError } 201 - Permission denied.
    * @throws { BusinessError } 32390004 - ChannelId is invalid or unavailable.
    * @throws { BusinessError } 32390006 - Parameter error.
@@ -214,15 +202,13 @@ declare namespace proxyChannelManager {
   function on(type: 'channelStateChange', channelId: number, callback: Callback<ChannelStateInfo>): void;
 
   /**
-   * Unsubscribes from channel state change events.
+   * 取消订阅通道状态事件。
    *
    * @permission ohos.permission.ACCESS_BLUETOOTH
-   * @param { 'channelStateChange' } type - Event type. The value **channelStateChange** indicates the channel state
-   *     change event.
-   * @param { number } channelId - Channel ID obtained when the proxy channel is opened.
-   * @param { Callback<ChannelStateInfo> } [callback] - Registered callback, If the value is empty, **undefined**, or
-   *     **null**, all callbacks of data receiving events are unregistered. If the value is not empty, the last
-   *     registered callback is used.
+   * @param { 'channelStateChange' } type - 设置订阅类型为'channelStateChange'。
+   * @param { number } channelId - 打开代理通道时获取的channelId。
+   * @param { Callback<ChannelStateInfo> } [callback] - 注册的回调函数。如果为空、undefined、null，
+   *     则取消订阅所有的数据接收事件。如果不为空，传入最后一次注册的回调函数。
    * @throws { BusinessError } 201 - Permission denied.
    * @throws { BusinessError } 32390004 - ChannelId is invalid or unavailable.
    * @throws { BusinessError } 32390006 - Parameter error.
@@ -235,12 +221,12 @@ declare namespace proxyChannelManager {
   function off(type: 'channelStateChange', channelId: number, callback?: Callback<ChannelStateInfo>): void;
 
   /**
-   * Subscribes to channel state change events. This API returns the result asynchronously through a callback.
+   * 订阅通道状态事件，使用callback进行异步回调。
    *
    * @permission ohos.permission.ACCESS_BLUETOOTH
-   * @param { int } channelId - Channel ID obtained when the proxy channel is opened.
-   * @param { Callback<ChannelStateInfo> } callback - Callback used to return the received channel state. If the
-   *     callback function is registered multiple times, only the last registered one takes effect.
+   * @param { int } channelId - 打开代理通道时获取的channelId。
+   * @param { Callback<ChannelStateInfo> } callback - 回调函数，返回接收到的通道状态。多次注册callback，
+   *     最后一次注册的callback生效
    * @throws { BusinessError } 201 - Permission denied.
    * @throws { BusinessError } 32390004 - ChannelId is invalid or unavailable.
    * @throws { BusinessError } 32390006 - Parameter error.
@@ -253,13 +239,12 @@ declare namespace proxyChannelManager {
   function onChannelStateChange(channelId: int, callback: Callback<ChannelStateInfo>): void;
 
   /**
-   * Unsubscribes from channel state change events.
+   * 取消订阅通道状态事件。
    *
    * @permission ohos.permission.ACCESS_BLUETOOTH
-   * @param { int } channelId - Channel ID obtained when the proxy channel is opened.
-   * @param { Callback<ChannelStateInfo> } [callback] - Registered callback, If the value is empty, **undefined**, or
-   *     **null**, all callbacks of data receiving events are unregistered. If the value is not empty, the last
-   *     registered callback is used.
+   * @param { int } channelId - 打开代理通道时获取的channelId。
+   * @param { Callback<ChannelStateInfo> } [callback] - 注册的回调函数。如果为空、undefined、null，
+   *     则取消订阅所有的数据接收事件。如果不为空，传入最后一次注册的回调函数。
    * @throws { BusinessError } 201 - Permission denied.
    * @throws { BusinessError } 32390004 - ChannelId is invalid or unavailable.
    * @throws { BusinessError } 32390006 - Parameter error.
@@ -272,7 +257,7 @@ declare namespace proxyChannelManager {
   function offChannelStateChange(channelId: int, callback?: Callback<ChannelStateInfo>): void;
 
   /**
-   * Represents the received data, including the channel ID and data.
+   * 存放接收的数据信息，包括通道Id和数据。
    *
    * @syscap SystemCapability.DistributedSched.AppCollaboration
    * @stagemodelonly
@@ -281,7 +266,7 @@ declare namespace proxyChannelManager {
    */
   interface DataInfo {
     /**
-     * Proxy channel ID.
+     * 代理通道的channelId。
      *
      * @syscap SystemCapability.DistributedSched.AppCollaboration
      * @stagemodelonly
@@ -291,7 +276,7 @@ declare namespace proxyChannelManager {
     channelId: int;
 
     /**
-     * Received byte array.
+     * 接收到的字节数据。
      *
      * @syscap SystemCapability.DistributedSched.AppCollaboration
      * @stagemodelonly
@@ -302,7 +287,7 @@ declare namespace proxyChannelManager {
   }
 
   /**
-   * Enumerates the link types.
+   * 链路类型。
    *
    * @syscap SystemCapability.DistributedSched.AppCollaboration
    * @stagemodelonly
@@ -311,7 +296,7 @@ declare namespace proxyChannelManager {
    */
   enum LinkType {
     /**
-     * Bluetooth.
+     * 蓝牙。
      *
      * @syscap SystemCapability.DistributedSched.AppCollaboration
      * @stagemodelonly
@@ -322,7 +307,7 @@ declare namespace proxyChannelManager {
   }
 
   /**
-   * Represents the proxy channel information, including the MAC address and service UUID of the peer device.
+   * 打开代理通道函数的入参，包括对端设备的MAC地址和监听服务的UUID。
    *
    * @syscap SystemCapability.DistributedSched.AppCollaboration
    * @stagemodelonly
@@ -331,7 +316,7 @@ declare namespace proxyChannelManager {
    */
   interface ChannelInfo {
     /**
-     * Link type of the proxy channel.
+     * 代理通道的链路类型。
      *
      * @syscap SystemCapability.DistributedSched.AppCollaboration
      * @stagemodelonly
@@ -341,7 +326,7 @@ declare namespace proxyChannelManager {
     linkType: LinkType;
 
     /**
-     * MAC address of the peer device.
+     * 对端设备的MAC地址。
      *
      * @syscap SystemCapability.DistributedSched.AppCollaboration
      * @stagemodelonly
@@ -351,7 +336,7 @@ declare namespace proxyChannelManager {
     peerDevAddr: string;
 
     /**
-     * Service UUID of the peer device.
+     * 对端监听的服务的UUID。
      *
      * @syscap SystemCapability.DistributedSched.AppCollaboration
      * @stagemodelonly
@@ -362,7 +347,7 @@ declare namespace proxyChannelManager {
   }
 
   /**
-   * Enumerates the connection states of the proxy channel.
+   * 通道状态发生变化时，代理通道上报的通道连接状态。
    *
    * @syscap SystemCapability.DistributedSched.AppCollaboration
    * @stagemodelonly
@@ -371,7 +356,7 @@ declare namespace proxyChannelManager {
    */
   enum ChannelState {
     /**
-     * The connection is disconnected, and the channel is unavailable.
+     * 连接已断开，通道不可用。
      *
      * @syscap SystemCapability.DistributedSched.AppCollaboration
      * @stagemodelonly
@@ -381,7 +366,7 @@ declare namespace proxyChannelManager {
     CHANNEL_WAIT_RESUME = 0,
 
     /**
-     * The connection is restored, and the channel is available.
+     * 连接已恢复，通道可用。
      *
      * @syscap SystemCapability.DistributedSched.AppCollaboration
      * @stagemodelonly
@@ -391,7 +376,7 @@ declare namespace proxyChannelManager {
     CHANNEL_RESUME = 1,
 
     /**
-     * The channel is unavailable due to other software errors.
+     * 其他软件错误导致通道不可用。
      *
      * @syscap SystemCapability.DistributedSched.AppCollaboration
      * @stagemodelonly
@@ -401,7 +386,7 @@ declare namespace proxyChannelManager {
     CHANNEL_EXCEPTION_SOFTWARE_FAILED = 2,
 
     /**
-     * The Bluetooth pairing relationship is deleted, and the channel is unavailable.
+     * 蓝牙配对关系被删除，通道不可用。
      *
      * @syscap SystemCapability.DistributedSched.AppCollaboration
      * @stagemodelonly
@@ -412,7 +397,7 @@ declare namespace proxyChannelManager {
   }
 
   /**
-   * Represents the connection state information of the proxy channel.
+   * 当代理通道状态变化时，用于表示代理通道的连接状态。
    *
    * @syscap SystemCapability.DistributedSched.AppCollaboration
    * @stagemodelonly
@@ -421,7 +406,7 @@ declare namespace proxyChannelManager {
    */
   interface ChannelStateInfo {
     /**
-     * Proxy channel ID.
+     * 代理通道的channelId。
      *
      * @syscap SystemCapability.DistributedSched.AppCollaboration
      * @stagemodelonly
@@ -431,7 +416,7 @@ declare namespace proxyChannelManager {
     channelId: int;
 
     /**
-     * Connection state of the proxy channel.
+     * 通道的连接状态。
      *
      * @syscap SystemCapability.DistributedSched.AppCollaboration
      * @stagemodelonly
