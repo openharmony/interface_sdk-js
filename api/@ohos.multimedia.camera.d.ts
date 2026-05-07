179b121cbf4ecc684d6f1c8f1ca3ed1d460cb836
zcr0917 <zhangchunrui1@h-partners.com>
2026-05-07 17:23:39 +0800

    add automotive camera position description js interface
    Signed-off-by: zcr0917 <zhangchunrui1@h-partners.com>

--- api/@ohos.multimedia.camera.d.ts
+++ api/@ohos.multimedia.camera.d.ts
@@ -2042,6 +2042,16 @@ declare namespace camera {
      * @since 24 dynamic&static
      */
     readonly sensorColorFilterArrangement?: SensorColorFilterArrangement;
+
+    /**
+     * Automotive camera position attribute.
+     *
+     * @syscap SystemCapability.Multimedia.Camera.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    readonly automotiveCameraPosition?: AutomotiveCameraPosition;
   }
 
   /**
@@ -14129,7 +14139,7 @@ declare namespace camera {
     enableOriginalImageGeneration(enabled: boolean): void;
 
     /**
-     * Confirm if auto extended gainmap delivery supporteed.
+     * Confirm if auto extended gainmap delivery supported.
      * 
      * @returns { boolean } TRUE if the auto extended gainmap delivery is supported.
      * @syscap SystemCapability.Multimedia.Camera.Core
@@ -14143,7 +14153,7 @@ declare namespace camera {
      * Enable auto extended gainmap delivery.
      * 
      * @param { boolean } enabled - enable auto extended gainmap delivery if TRUE.
-     * @throws { BusinessError } 7400102 - Operation not alloweed.
+     * @throws { BusinessError } 7400102 - Operation not allowed.
      * @throws { BusinessError } 7400103 - Session not config, only throw in session usage.
      * @throws { BusinessError } 7400201 - Camera service fatal error.
      * @syscap SystemCapability.Multimedia.Camera.Core
@@ -17433,6 +17443,166 @@ declare namespace camera {
    * @since 26.0.0 dynamic&static
    */
   type NotificationInfo = DefocusFromProximityNotificationInfo;
+
+    /**
+   * Enum for automotive camera position.
+   *
+   * @syscap SystemCapability.Multimedia.Camera.Core
+   * @stagemodelonly
+   * @atomicservice
+   * @since 26.0.0 dynamic&static
+   */
+  enum AutomotiveCameraPosition {
+    /**
+     * Exterior other position.
+     *
+     * @syscap SystemCapability.Multimedia.Camera.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    AUTOMOTIVE_CAMERA_POSITION_EXTERIOR_OTHER = 0,
+
+    /**
+     * Exterior front position.
+     *
+     * @syscap SystemCapability.Multimedia.Camera.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    AUTOMOTIVE_CAMERA_POSITION_EXTERIOR_FRONT = 1,
+
+    /**
+     * Exterior rear position.
+     *
+     * @syscap SystemCapability.Multimedia.Camera.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    AUTOMOTIVE_CAMERA_POSITION_EXTERIOR_REAR = 2,
+
+    /**
+     * Exterior left position.
+     *
+     * @syscap SystemCapability.Multimedia.Camera.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    AUTOMOTIVE_CAMERA_POSITION_EXTERIOR_LEFT = 3,
+
+    /**
+     * Exterior right position.
+     *
+     * @syscap SystemCapability.Multimedia.Camera.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    AUTOMOTIVE_CAMERA_POSITION_EXTERIOR_RIGHT = 4,
+
+    /**
+     * Interior other position.
+     *
+     * @syscap SystemCapability.Multimedia.Camera.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    AUTOMOTIVE_CAMERA_POSITION_INTERIOR_OTHER = 5,
+
+    /**
+     * Interior left side position of the first row.
+     *
+     * @syscap SystemCapability.Multimedia.Camera.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    AUTOMOTIVE_CAMERA_POSITION_INTERIOR_ROW_1_LEFT = 6,
+
+    /**
+     * Interior center side position of the first row.
+     *
+     * @syscap SystemCapability.Multimedia.Camera.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    AUTOMOTIVE_CAMERA_POSITION_INTERIOR_ROW_1_CENTER = 7,
+
+    /**
+     * Interior right side position of the first row.
+     *
+     * @syscap SystemCapability.Multimedia.Camera.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    AUTOMOTIVE_CAMERA_POSITION_INTERIOR_ROW_1_RIGHT = 8,
+
+    /**
+     * Interior left side position of the second row.
+     *
+     * @syscap SystemCapability.Multimedia.Camera.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    AUTOMOTIVE_CAMERA_POSITION_INTERIOR_ROW_2_LEFT = 9,
+
+    /**
+     * Interior center side position of the second row.
+     *
+     * @syscap SystemCapability.Multimedia.Camera.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    AUTOMOTIVE_CAMERA_POSITION_INTERIOR_ROW_2_CENTER = 10,
+
+    /**
+     * Interior right side position of the second row.
+     *
+     * @syscap SystemCapability.Multimedia.Camera.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    AUTOMOTIVE_CAMERA_POSITION_INTERIOR_ROW_2_RIGHT = 11,
+
+    /**
+     * Interior left side position of the third row.
+     *
+     * @syscap SystemCapability.Multimedia.Camera.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    AUTOMOTIVE_CAMERA_POSITION_INTERIOR_ROW_3_LEFT = 12,
+
+    /**
+     * Interior center side position of the third row.
+     *
+     * @syscap SystemCapability.Multimedia.Camera.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    AUTOMOTIVE_CAMERA_POSITION_INTERIOR_ROW_3_CENTER = 13,
+
+    /**
+     * Interior right side position of the third row.
+     *
+     * @syscap SystemCapability.Multimedia.Camera.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    AUTOMOTIVE_CAMERA_POSITION_INTERIOR_ROW_3_RIGHT = 14
+  }
 }
 
 export default camera;
