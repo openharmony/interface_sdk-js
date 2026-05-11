c487a601b909256e31ced5ed641ea31ab2c7c1ac
qianyang-yi <qianyangyi@h-partners.com>
2026-05-11 21:43:15 +0800

    update: 更新文件 @ohos.deviceInfo.d.ts
    Signed-off-by: qianyang-yi <qianyangyi@h-partners.com>

--- api/@ohos.deviceInfo.d.ts
+++ api/@ohos.deviceInfo.d.ts
@@ -23,6 +23,8 @@
  * 
  * > **NOTE**
  * >
+ * > The initial APIs of this module are supported since API version 6. Newly added APIs
+ * > will be marked with a superscript to indicate their earliest API version.
  * > Some parameters whose return value is the default value are not yet available.
  * > The APIs of this module return information about device constants. You are not expected to call these APIs 
  * > frequently.
