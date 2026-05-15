84f25e2c87d51fd7013c61925ca6c751231afab0
谢晨 <1194369083@qq.com>
2026-05-15 11:32:35 +0800

    Signed-off-by: 谢晨 <1194369083@qq.com>

--- zh-cn/api/@ohos.security.identifySensitiveContent.d.ts
+++ zh-cn/api/@ohos.security.identifySensitiveContent.d.ts
@@ -17,35 +17,37 @@
  * @kit DataProtectionKit
  */
 /**
- * This module identifies sensitive information in a specified file based on the input [scan policy]{@link identifySensitiveContent.policy}.
+ * 识别敏感内容功能的实现是通过输入的[扫描策略]{@link identifySensitiveContent.Policy}来检测指定文件中的敏感信息。
  *
  * @syscap SystemCapability.Security.DataLossPrevention
  * @since 21
  */
 declare namespace identifySensitiveContent {
     /**
-     * Defines the policy for sensitive content identification.
+     * 定义敏感内容识别策略。
      *
      * @syscap SystemCapability.Security.DataLossPrevention
      * @since 21
      */
     export interface Policy {
         /**
-         * Label of an identification policy. The value can contain a maximum of 30 bytes.
+         * 表示识别策略的标签。最大30字节。
          *
          * @syscap SystemCapability.Security.DataLossPrevention
          * @since 21
          */
         sensitiveLabel: string;
         /**
-         * Keyword set. The array can contain a maximum of 50 elements, and each element can contain a maximum of 30 bytes.
+         * 表示关键字集合。Array最大50，每个元素最大30字节。
          *
          * @syscap SystemCapability.Security.DataLossPrevention
          * @since 21
          */
         keywords: Array<string>;
         /**
-         * Regular expression. The value can contain a maximum of 512 bytes.
+         * 表示正则表达式内容。最大512字节。 
+         * 
+         * 在输入字符串时，需检查某些特殊字符（如反斜杠 \、双引号 "、换行符等），不会被自动转义，确保字符串的输入效果。
          *
          * @syscap SystemCapability.Security.DataLossPrevention
          * @since 21
@@ -53,28 +55,28 @@ declare namespace identifySensitiveContent {
         regex: string;
     }
     /**
-     * Displays the identification result of sensitive content.
+     * 显示敏感内容的识别结果。
      *
      * @syscap SystemCapability.Security.DataLossPrevention
      * @since 21
      */
     export interface MatchResult {
         /**
-         * Label of an identification policy.
+         * 表示识别策略的标签。
          *
          * @syscap SystemCapability.Security.DataLossPrevention
          * @since 21
          */
         readonly sensitiveLabel: string;
         /**
-         * Matched content.
+         * 表示匹配内容。
          *
          * @syscap SystemCapability.Security.DataLossPrevention
          * @since 21
          */
         readonly matchContent: string;
         /**
-         * Total number of matched items.
+         * 表示识别内容的总数。
          *
          * @syscap SystemCapability.Security.DataLossPrevention
          * @since 21
@@ -82,13 +84,12 @@ declare namespace identifySensitiveContent {
         readonly matchNumber: number;
     }
     /**
-     * Identifies sensitive content in a specified file based on the configured policy. This API uses a promise to return  
-     * the result.
+     * 根据设置的策略，识别指定文件中的敏感内容。结果通过Promise方式异步返回。
      *
      * @permission ohos.permission.ENTERPRISE_DATA_IDENTIFY_FILE
-     * @param { string } filePath - Path of the file to be identified.
-     * @param { Array<Policy> } identifyPolicies - Identification policy.
-     * @returns { Promise<Array<MatchResult>> } Promise used to return the identification result of sensitive content.
+     * @param { string } filePath - 识别的文件路径。
+     * @param { Array<Policy> } identifyPolicies - 识别的策略。
+     * @returns { Promise<Array<MatchResult>> } Promise对象。返回敏感内容识别的结果。
      * @throws { BusinessError } 201 - permission denied.
      * @throws { BusinessError } 801 - Capability not supported.
      * @throws { BusinessError } 19110001 - Parameter error. Possible causes:
