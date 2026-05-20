5f566480d5774b6caf82022fcbc987495012eccc
anchi119 <anchi1@huawei.com>
2026-05-20 09:19:16 +0800

    api私有化整改
    Signed-off-by: anchi119 <anchi1@huawei.com>

--- api/@ohos.resourceschedule.workScheduler.d.ts
+++ api/@ohos.resourceschedule.workScheduler.d.ts
@@ -221,7 +221,7 @@ declare namespace workScheduler {
    * @stagemodelonly
    * @since 26.0.0 dynamic&static
    */
-  const WORK_SCHEDULER_CONDITION = 'WORK_SCHEDULER_CONDITION';
+  const WORK_SCHEDULER_CONDITION: string = 'WORK_SCHEDULER_CONDITION';
 
   /**
    * Whether the requested task is executed immediately.
@@ -231,7 +231,7 @@ declare namespace workScheduler {
    * @stagemodelonly
    * @since 26.0.0 dynamic&static
    */
-  const EXECUTE_IMMEDIATE = 'executeImmediate';
+  const EXECUTE_IMMEDIATE: string = 'executeImmediate';
 
   /**
    * Requests a deferred task. Upon successful request, the deferred task is added to the execution queue and will be 
