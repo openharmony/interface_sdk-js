f8931b71458360da3bdc7b0c0af8ccc0954ec0b3
zhaoyong70 <zhaoyong70@h-partners.com>
2026-05-20 12:05:30 +0800

    modify default value of edgeLightMode
    Signed-off-by: zhaoyong70 <zhaoyong70@h-partners.com>

--- api/@internal/component/ets/alert_dialog.d.ts
+++ api/@internal/component/ets/alert_dialog.d.ts
@@ -1355,7 +1355,7 @@ declare interface AlertDialogParam {
   /**
    * Sets the edgeLight animation Mode of the dialog.
    *
-   * @default EdgeLightMode.EDGELIGHT_DISABLED
+   * @default EdgeLightMode.EDGELIGHT_AUTO
    * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @systemapi
    * @stagemodelonly
