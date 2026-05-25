6ce39814ba8aaa8d6cb668c0fe7e7503df18a28d
songxuelian <1546432050@qq.com>
2026-05-25 09:46:38 +0800

    bas
    Signed-off-by: songxuelian <1546432050@qq.com>

--- api/@ohos.bluetooth.bas.d.ts
+++ api/@ohos.bluetooth.bas.d.ts
@@ -13,3 +13,134 @@
  * limitations under the License.
  */
 
+/**
+ * @file
+ * @kit ConnectivityKit
+ */
+
+import type { AsyncCallback, Callback } from './@ohos.base';
+import type common from './@ohos.bluetooth.common';
+
+/**
+ * Provide methods to access BAS(Battery Service)-related capabilities.
+ *
+ * @syscap SystemCapability.Communication.Bluetooth.Core
+ * @systemapi
+ * @stagemodelonly
+ * @since 26.0.0 dynamic&static
+ */
+
+declare namespace bas {
+  /**
+   * Bluetooth device address.
+   *
+   * @syscap SystemCapability.Communication.Bluetooth.Core
+   * @systemapi
+   * @stagemodelonly
+   * @since 26.0.0 dynamic&static
+   */
+  type BluetoothAddress = common.BluetoothAddress;
+
+  /**
+   * Determine whether the local device can obtain the battery level of the remote device.
+   *
+   * @returns { boolean } Returns true if the battery service is enabled; returns false otherwise.
+   * @throws { BusinessError } 202 - Non-system applications are not allowed to use system APIs.
+   * @throws { BusinessError } 2900099 - Operation failed.
+   * @syscap SystemCapability.Communication.Bluetooth.Core
+   * @systemapi
+   * @stagemodelonly
+   * @since 26.0.0 dynamic&static
+   */
+  function isBasSupported(): boolean;
+
+  /**
+   * Get remote device battery information.
+   *
+   * @permission ohos.permission.ACCESS_BLUETOOTH
+   * @param { BluetoothAddress } deviceId - Indicates address of peer device.
+   * @returns { Promise<BatteryInfo> } Returns the battery info.
+   * @throws { BusinessError } 201 - Permission denied.
+   * @throws { BusinessError } 202 - Non-system applications are not allowed to use system APIs.
+   * @throws { BusinessError } 801 - Capability not supported.
+   *     Only can be called on phone, tablet, and 2in1 devices.
+   *     Failed to call the API when the short-range chip is not inserted on 2in1 device.
+   * @throws { BusinessError } 2900001 - Service stopped.
+   * @throws { BusinessError } 2900003 - Bluetooth disabled.
+   * @throws { BusinessError } 2900004 - Remote device profile not supported.
+   * @throws { BusinessError } 2900099 - Operation failed.
+   * @throws { BusinessError } 2901003 - Connection not established.
+   * @syscap SystemCapability.Communication.Bluetooth.Core
+   * @systemapi
+   * @stagemodelonly
+   * @since 26.0.0 dynamic&static
+   */
+  function getRemoteDeviceBatteryInfo(deviceId: BluetoothAddress): Promise<BatteryInfo>;
+
+  /**
+   * Subscribe the event of battery state changes from a remote device.
+   *
+   * @permission ohos.permission.ACCESS_BLUETOOTH
+   * @param { Callback<BatteryInfo> } callback - Callback used to listen.
+   * @throws { BusinessError } 201 - Permission denied.
+   * @throws { BusinessError } 202 - Non-system applications are not allowed to use system APIs.
+   * @throws { BusinessError } 801 - Capability not supported.
+   *     Only can be called on phone, tablet, and 2in1 devices.
+   *     Failed to call the API when the short-range chip is not inserted on 2in1 device.
+   * @throws { BusinessError } 2900099 - Operation failed.
+   * @syscap SystemCapability.Communication.Bluetooth.Core
+   * @systemapi
+   * @stagemodelonly
+   * @since 26.0.0 dynamic&static
+   */
+  function onBatteryChange(callback: Callback<BatteryInfo>): void;
+
+  /**
+   * Unsubscribe the event of battery state changed from a remote device.
+   *
+   * @permission ohos.permission.ACCESS_BLUETOOTH
+   * @param { Callback<BatteryInfo> } [callback] - Callback used to listen.
+   * @throws { BusinessError } 201 - Permission denied.
+   * @throws { BusinessError } 202 - Non-system applications are not allowed to use system APIs.
+   * @throws { BusinessError } 801 - Capability not supported.
+   *     Only can be called on phone, tablet, and 2in1 devices.
+   *     Failed to call the API when the short-range chip is not inserted on 2in1 device.
+   * @throws { BusinessError } 2900099 - Operation failed.
+   * @syscap SystemCapability.Communication.Bluetooth.Core
+   * @systemapi
+   * @stagemodelonly
+   * @since 26.0.0 dynamic&static
+   */
+  function offBatteryChange(callback?: Callback<BatteryInfo>): void;
+
+  /**
+   * Describe the contents of the battery information.
+   *
+   * @syscap SystemCapability.Communication.Bluetooth.Core
+   * @systemapi
+   * @stagemodelonly
+   * @since 26.0.0 dynamic&static
+   */
+  interface BatteryInfo {
+    /**
+     * Identify of the discovery device.
+     *
+     * @syscap SystemCapability.Communication.Bluetooth.Core
+     * @systemapi
+     * @stagemodelonly
+     * @since 26.0.0 dynamic&static
+     */
+    deviceId: BluetoothAddress;
+    /**
+     * battery value of the device. {@code -1} means no power information.
+     *
+     * @syscap SystemCapability.Communication.Bluetooth.Core
+     * @systemapi
+     * @stagemodelonly
+     * @since 26.0.0 dynamic&static
+     */
+    batteryLevel: int;
+  }
+}
+
+export default bas;
