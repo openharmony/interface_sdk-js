1555a0849a3aa9af341d200f2ae23912303fe81b
gcw_O7hu6ZQF <liujiayu33@huawei.com>
2026-06-13 09:53:04 +0800

    新增全局耳返API
    Signed-off-by: gcw_O7hu6ZQF <liujiayu33@huawei.com>

--- api/@ohos.multimedia.audio.d.ts
+++ api/@ohos.multimedia.audio.d.ts
@@ -502,6 +502,28 @@ declare namespace audio {
   function createAudioLoopback(mode: AudioLoopbackMode): Promise<AudioLoopback | null>;
 
   /**
+   * Creates a global audio loopback instance, which provides low-latency in-ear monitor function.
+   * Hardware audio loopback can only be created in supported platform, application can use
+   * {@link AudioStreamManager#isAudioLoopbackSupported} to check first.
+   * There should be only one main instance that own the global loopback in the system, the others
+   * are controllers. A controller can manage the global loopback by sending commands to the main
+   * instance, and listen status change from it.
+   *
+   * @param { AudioLoopbackMode } mode - Audio loopback mode to create.
+   * @param { boolean } isController - Create an object that own the audio loopback or only a controller.
+   * @returns { Promise<AudioLoopback | null> } Promise used to return the audio loopback instance,
+   *     or null when an error happens.
+   * @throws { BusinessError } 202 - Caller is not a system application.
+   * @throws { BusinessError } 6800101 - Parameter verification failed.
+   * @throws { BusinessError } 6800104 - Loopback mode is unsupported.
+   * @syscap SystemCapability.Multimedia.Audio.Capturer
+   * @systemapi
+   * @stagemodelonly
+   * @since 26.0.0 dynamic&static
+   */
+  function createGlobalAudioLoopback(mode: AudioLoopbackMode, isController: boolean): Promise<AudioLoopback | null>;
+
+  /**
    * Enumerates the audio states.
    * @enum { int }
    * @syscap SystemCapability.Multimedia.Audio.Core
