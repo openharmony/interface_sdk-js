af4e1a4aa6070d17637c648d9fca21ccb7a33ffb
chenyuchi <chenyuchi2@h-partners.com>
2026-06-17 19:38:12 +0800

    高级组件和绘制逻辑组件API一致性补充
    Signed-off-by: chenyuchi <chenyuchi2@h-partners.com>
    
    Co-Authored-By: Agent

--- zh-cn/api/@internal/component/ets/for_each.d.ts
+++ zh-cn/api/@internal/component/ets/for_each.d.ts
@@ -19,7 +19,7 @@
  */
 
 /**
-* declare ForEachAttribute
+* 支持[拖拽排序]{@link common}属性。
 *
  * @syscap SystemCapability.ArkUI.ArkUI.Full
  * @stagemodelonly
@@ -32,7 +32,11 @@
 declare class ForEachAttribute extends DynamicNode<ForEachAttribute> {}
 
 /**
-* looping function.
+* ForEach接口基于数组类型数据来进行循环渲染。
+*
+* > **说明：**
+*
+* 开发者指南见：[ForEach开发者指南](docroot://ui/rendering-control/arkts-rendering-control-foreach.md)。
 *
  * @syscap SystemCapability.ArkUI.ArkUI.Full
  * @FaAndStageModel
@@ -45,25 +49,18 @@ declare class ForEachAttribute extends DynamicNode<ForEachAttribute> {}
 interface ForEachInterface {
 
   /**
+   * ForEach接口基于数组类型数据来进行循环渲染，需要与容器组件配合使用，且接口返回的组件应当是允许包含在ForEach父容器组件中的子组件。例如，[ListItem]{@link list_item}组件要求ForEach的父容
+   * 器组件必须为[List]{@link list}组件或[ListItemGroup]{@link list_item_group}组件。
    *
-   * @param { Array<any> } arr - Data source, which is an array.<br>If this parameter is set to **undefined**, the
-   *     **ForEach** API does not take effect.<br>**NOTE**<br>- You can set this parameter to an empty array. In this
-   *     case, no child component is created.<br>- You can also set this parameter to a function whose return value is
-   *     an array, for example, **arr.slice (1, 3)**. However, the set function cannot change any state variables
-   *     including the array itself. For example, **Array.splice**, **Array.sort**, and **Array.reverse** functions are
-   *     not allowed, as they may change the array.
-   * @param { function } itemGenerator - Component generator.<br>- It generates a component for each data item in an
-   *     array. <br>- (Optional) **item**: data item in the **arr** array.<br>- (Optional) **index**: index of the data
-   *     item in the **arr** array.<br>**NOTE**<br>- The type of the created component must be the one allowed inside
-   *     the parent container component of **ForEach**. For example, a **ListItem** component is allowed only when the
-   *     parent container component of **ForEach** is **List**.
-   * @param { function } keyGenerator - Key generator.<br>- It generates a unique and persistent key for each array item
-   *     of the data source **arr**. You can customize the key generation rule using this function.<br>- (Optional)
-   *     **item**: data item in the **arr** array.<br>- (Optional) **index**: index of the data item in the **arr**
-   *     array.<br>**NOTE**<br>- If this function is not specified, the default key generator of the framework is used:
-   *     **(item: T, index: number) => { return index + '__' + JSON.stringify(item); }**.<br>- The key generator should
-   *     not change any component state.
-   * @returns { ForEachInterface }    * @returns { ForEachAttribute }
+   * @param { Array<any> } arr - 数据源，为`Array`类型的数组。<br/>设置为undefined时ForEach接口不生效。<br/>**说明：**<br/>- 可以设置为空数组，此时不会创建子组件。
+   *     <br/>- 可以设置返回值为数组类型的函数，例如`arr.slice(1, 3)`，但设置的函数不应改变包括数组本身在内的任何状态变量，例如不应使用`Array.splice()`,`Array.sort()`或
+   *     `Array.reverse()`这些会改变原数组的函数。
+   * @param { function } itemGenerator - 组件生成函数。<br/>- 为数组中的每个元素创建对应的组件。<br/>- `item`参数（可选）：`arr`数组中的数据项。<br/>- `index`参
+   *     数（可选）：`arr`数组中的数据项索引。<br/>**说明：**<br/>- 组件的类型必须是`ForEach`的父容器所允许的。例如，`ListItem`组件要求`ForEach`的父容器组件必须为`List`组件。
+   * @param { function } keyGenerator - 键值生成函数。<br/>- 为数据源`arr`的每个数组项生成唯一且持久的键值。开发者可以通过该函数自定义键值生成规则。<br/>- `item`参数（可选）：
+   *     `arr`数组中的数据项。<br/>- `index`参数（可选）：`arr`数组中的数据项索引。<br/>**说明：**<br/>- 如果函数缺省，框架默认的键值生成函数为
+   *     `(item: T, index: number) => { return index + '__' + JSON.stringify(item); }`<br/>- 键值生成函数不应改变任何组件状态。
+   * @returns { ForEachInterface } [since 7 - 11]
    * @returns { ForEachAttribute } [since 12]
    * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @FaAndStageModel
@@ -81,7 +78,11 @@ interface ForEachInterface {
 }
 
 /**
-* Defines ForEach Component.
+* ForEach接口基于数组类型数据来进行循环渲染。
+*
+* > **说明：**
+*
+* 开发者指南见：[ForEach开发者指南](docroot://ui/rendering-control/arkts-rendering-control-foreach.md)。
 *
  * @syscap SystemCapability.ArkUI.ArkUI.Full
  * @FaAndStageModel
@@ -90,4 +91,4 @@ interface ForEachInterface {
  * @atomicservice [since 11]
  * @since 7 dynamic
  */
-declare const ForEach: ForEachInterface;
+declare const ForEach: ForEachInterface;
