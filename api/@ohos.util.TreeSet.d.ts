f18feefa243904e70ec2d80fa42a96709186b504
王子睿 <wangzirui19@huawei.com>
2026-06-23 16:24:26 +0800

    @ohos.util.TreeSet.d.ts#foreach remove 401 errorcode
    Issue: https://gitcode.com/openharmony/interface_sdk-js/issues/25017
    
    Signed-off-by: 王子睿 <wangzirui19@huawei.com>

--- api/@ohos.util.TreeSet.d.ts
+++ api/@ohos.util.TreeSet.d.ts
@@ -255,9 +255,6 @@ declare class TreeSet<T> {
    *     thisArg (Optional) The value to be used as this value for when callbackFn is called.
    *     If thisArg is omitted, undefined is used as the this value.
    * @throws { BusinessError } 10200011 - The forEach method cannot be bound.
-   * @throws { BusinessError } 401 - Parameter error. Possible causes:
-   *     1.Mandatory parameters are left unspecified;
-   *     2.Incorrect parameter types.
    * @syscap SystemCapability.Utils.Lang
    * @crossplatform [since 10]
    * @atomicservice [since 12]
