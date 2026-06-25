4ba770e34878d15cda757085e5e344dfa4c20abb
ALTNAT <774082891@qq.com>
2026-06-25 21:03:02 +0800

    modify maximizeWithOptions
    Signed-off-by: ALTNAT <774082891@qq.com>

--- api/@ohos.window.d.ts
+++ api/@ohos.window.d.ts
@@ -11438,7 +11438,7 @@ declare namespace window {
      * @returns { Promise<void> } - Promise that returns no value.
      * @throws { BusinessError } 801 - Capability not supported.
      *     Failed to call the API due to limited device capabilities.
-     * @throws { BusinessError } 1300002 - This window state is abnormal.
+     * @throws { BusinessError } 1300002 - This window state is abnormal. Possible cause:
      *     1. The window is not created or destroyed;
      *     2. Internal task error.
      * @throws { BusinessError } 1300003 - This window manager service works abnormally.
