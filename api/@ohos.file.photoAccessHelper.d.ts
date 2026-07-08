017ca2d9e159f33545a5f90b5c07badda29ea963
qinquan5 <qinquan5@h-partners.com>
2026-07-08 11:06:19 +0800

    媒体库API一致性修改
    Co-Authored-By: Agent
    Signed-off-by: qinquan5 <qinquan5@h-partners.com>

--- api/@ohos.file.photoAccessHelper.d.ts
+++ api/@ohos.file.photoAccessHelper.d.ts
@@ -8021,28 +8021,27 @@ declare namespace photoAccessHelper {
     getClonedAssetUris(oldUris: Array<string>): Promise<Map<string, string>>;
   
     /**
-     * Start medialibrary database backup and wait for returning with backup information which only works on beta 
-     * device.
+     * Obtains the current URIs of cloned albums. This API uses a promise to return the result.
+     * 
+     * To control the size of the database table space, the system automatically deletes the previously stored clone 
+     * data during each clone operation. As a result, this API only keeps the mapping between the user's new and old 
+     * device URIs from the latest clone operation.
      *
-     * @param { string } betaIssueId - The beta issue id.
-     * @param { string } betaScenario - The beta scenario.
-     * @returns { Promise<Map<string, string>> } - The  returning with backup information,
-     *     which includes FILE_FD, FILE_NAME and FILE_SIZE.
-     * @throws { BusinessError } 202 - Called by non-system application
-     * @throws { BusinessError } 23800151 - The scenario parameter verification fails. Possible causes:
-     *     <br>1. The betaIssueId parameter is invalid, such as null, undefined or empty string.
-     *     <br>2. The betaScenario parameter is invalid, such as null, undefined or empty string.
-     *     <br>3. The same betaIssueId task is processing.
-     * @throws { BusinessError } 23800201 - Unsupported operation type, this api only works on beta device.
-     * @throws { BusinessError } 23800301 - Internal system error. You are advised to retry and check the logs.
-     *     Possible causes: 
-     *     <br>1. The database is corrupted. 
-     *     <br>2. The file system is abnormal. 
+     * @permission ohos.permission.READ_IMAGEVIDEO
+     * @param { Array<string> } oldUris - Array of old URIs before cloning.
+     * @returns { Promise<Map<string, string>> } Promise used to return a map of URIs.
+     * @throws { BusinessError } 201 - Permission denied.
+     * @throws { BusinessError } 202 - Called by non-system application.
+     * @throws { BusinessError } 23800151 - The scenario parameter verification fails.
+     *     Possible causes: The size of input parameter exceeds 100 or is 0.
+     * @throws { BusinessError } 23800301 - Internal system error.
+     *     It is recommended to retry and check the logs. Possible causes:
+     *     <br>1. Database corrupted;
+     *     <br>2. The file system is abnormal;
      *     <br>3. The IPC request timed out.
      * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
      * @systemapi
      * @since 22 dynamic
-     * @since 26.0.0 static
      */
     getClonedAlbumUris(oldUris: Array<string>): Promise<Map<string, string>>;
 
@@ -16330,6 +16329,127 @@ declare namespace photoAccessHelper {
   }
 
   /**
+   * Search Suggestion Type.
+   *
+   * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+   * @stagemodelonly
+   * @atomicservice
+   * @since 26.0.0 dynamic&static
+   */
+  enum SearchSuggestionType {  
+    /**
+     * Searching for recommended words by time, location, and label.
+     *
+     * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    TIME_GEO_LABEL = 0,
+	
+    /**
+     * Searching for recommended words by time and location.
+     *
+     * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    TIME_GEO = 1,
+	
+    /**
+     * Searching for recommended words by time and label.
+     *
+     * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    TIME_LABEL = 2,
+	
+    /**
+     * Searching for recommended words by time, people, location and label.
+     *
+     * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    TIME_PEOPLE_GEO_LABEL = 3,
+	
+    /**
+     * Searching for recommended words by time, people and label.
+     *
+     * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    TIME_PEOPLE_GEO = 4,
+	
+    /**
+     * Searching for recommended words by time, people and label.
+     *
+     * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    TIME_PEOPLE_LABEL = 5,
+	
+    /**
+     * Searching for recommended words by time and people.
+     *
+     * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+     * @stagemodelonly
+     * @atomicservice
+     * @since 26.0.0 dynamic&static
+     */
+    TIME_PEOPLE = 6
+  }  
+
+  /**
+   * Search suggestion result.
+   *
+   * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+   * @systemapi
+   * @stagemodelonly
+   * @since 26.0.0 dynamic&static
+   */
+  interface SearchSuggestionResult {  
+    /**
+     * Search Suggestion Type
+     *
+     * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+     * @systemapi
+     * @stagemodelonly
+     * @since 26.0.0 dynamic&static
+     */
+    type: SearchSuggestionType;
+	
+    /**
+     * Search suggestion
+     *
+     * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+     * @systemapi
+     * @stagemodelonly
+     * @since 26.0.0 dynamic&static
+     */
+    value: string;
+	
+    /**
+     * Number of photos in the scenario where a recommendation word is searched.
+     * The value range is all integers.
+     *
+     * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+     * @systemapi
+     * @stagemodelonly
+     * @since 26.0.0 dynamic&static
+     */
+    count: int;
+  }
+
+  /**
    * Preferred compatible mode.
    *
    * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
@@ -16400,6 +16520,30 @@ declare namespace photoAccessHelper {
      * @since 23 dynamic&static
      */
     static getRelatedEntity (topic: string, context: ContextMap, option?: Options): Promise<Entity[]>;
+
+    /**
+     * Get Search Suggestion.
+     *
+     * @permission ohos.permission.READ_IMAGEVIDEO
+     * @param { Array<SearchSuggestionType> } searchSuggestionTypes - Array of search suggestion types
+     *     <br>The maximum length is 7 and cannot be empty.
+     * @returns { Promise<Array<SearchSuggestionResult>> } Result of searching for recommended words
+     * @throws { BusinessError } 201 - Permission denied
+     * @throws { BusinessError } 202 - Called by nonsystem application
+     * @throws { BusinessError } 23800151 - Scenario parameters fail to pass the verification.Possible causes:
+     *     <br>1. The searchSuggestionTypes list is empty.
+     *     <br>2. The searchSuggestionTypes error.
+     * @throws { BusinessError } 23800301 - Internal system error. It is recommended to retry and check the logs.
+     *     <br>Possible causes:
+     *     <br>1. Database corrupted;
+     *     <br>2. The file system is abnormal;
+     *     <br>3. The IPC request timed out.
+     * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+     * @systemapi
+     * @stagemodelonly
+     * @since 26.0.0 dynamic&static
+     */
+    static getSearchSuggestion( searchSuggestionTypes: Array<SearchSuggestionType>): Promise<Array<SearchSuggestionResult>>;
   }
 
   /**
