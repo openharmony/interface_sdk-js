808744ced7d4a3430daab96e318ba3ec4b7fc2c6
cnhook <xueshenke@h-partners.com>
2026-07-14 17:03:45 +0800

    copy baseline to zh-cn
    Signed-off-by: cnhook <xueshenke@h-partners.com>

---
+++ zh-cn/api/@ohos.file.fileuri.d.ts
@@ -0,0 +1,190 @@
+/*
+ * Copyright (C) 2022-2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file
+ * @kit CoreFileKit
+ */
+
+import uri from './@ohos.uri';
+
+/**
+ * Provides fileUri APIS
+ *
+ * @namespace fileUri
+ * @syscap SystemCapability.FileManagement.AppFileService
+ * @since 9
+ */
+
+/**
+ * Provides fileUri APIS
+ *
+ * @namespace fileUri
+ * @syscap SystemCapability.FileManagement.AppFileService
+ * @atomicservice
+ * @since 15 dynamic
+ * @since 23 static
+ */
+declare namespace fileUri {
+  /**
+   * FileUri represents the uri of the file.
+   *
+   * @extends uri.URI
+   * @syscap SystemCapability.FileManagement.AppFileService
+   * @since 10
+   */
+  /**
+   * FileUri represents the uri of the file.
+   *
+   * @extends uri.URI
+   * @syscap SystemCapability.FileManagement.AppFileService
+   * @atomicservice
+   * @since 15 dynamic
+   * @since 23 static
+   */
+  class FileUri extends uri.URI {
+    /**
+     * Constructor for obtaining the instance of the FileUri class.
+     *
+     * @param { string } uriOrPath - Uri or Path.
+     * @throws { BusinessError } 13900005 - I/O error
+     * @throws { BusinessError } 13900020 - Invalid argument
+     * @throws { BusinessError } 13900042 - Unknown error
+     * @throws { BusinessError } 14300002 - Invalid uri
+     * @syscap SystemCapability.FileManagement.AppFileService
+     * @since 10
+     */
+    /**
+     * Constructor for obtaining the instance of the FileUri class.
+     *
+     * @param { string } uriOrPath - Uri or Path.
+     * @throws { BusinessError } 13900005 - I/O error
+     * @throws { BusinessError } 13900020 - Invalid argument
+     * @throws { BusinessError } 13900042 - Unknown error
+     * @throws { BusinessError } 14300002 - Invalid uri
+     * @syscap SystemCapability.FileManagement.AppFileService
+     * @atomicservice
+     * @since 15 dynamic
+     * @since 23 static
+     */
+    constructor(uriOrPath: string);
+
+    /**
+     * Obtains the file name of uri.
+     *
+     * @returns { string }
+     * string: Return the file name of uri
+     * @readonly
+     * @throws { BusinessError } 13900005 - I/O error
+     * @throws { BusinessError } 13900042 - Unknown error
+     * @syscap SystemCapability.FileManagement.AppFileService
+     * @since 10
+     */
+    /**
+     * Obtains the file name of uri.
+     *
+     * @returns { string }
+     * string: Return the file name of uri
+     * @readonly
+     * @throws { BusinessError } 13900005 - I/O error
+     * @throws { BusinessError } 13900042 - Unknown error
+     * @syscap SystemCapability.FileManagement.AppFileService
+     * @atomicservice
+     * @since 15 dynamic
+     * @since 23 static
+     */
+    get name(): string;
+
+    /**
+     * Get the full directory uri where the file URI is located
+     *
+     * @returns { string } Return the directory uri
+     * @throws { BusinessError } 13900002 - No such file or directory
+     * @throws { BusinessError } 13900012 - Permission denied
+     * @throws { BusinessError } 13900042 - Unknown error
+     * @syscap SystemCapability.FileManagement.AppFileService
+     * @since 11
+     */
+    /**
+     * Get the full directory uri where the file URI is located
+     *
+     * @returns { string } Return the directory uri
+     * @throws { BusinessError } 13900002 - No such file or directory
+     * @throws { BusinessError } 13900012 - Permission denied
+     * @throws { BusinessError } 13900042 - Unknown error
+     * @syscap SystemCapability.FileManagement.AppFileService
+     * @atomicservice
+     * @since 15 dynamic
+     * @since 23 static
+     */
+    getFullDirectoryUri(): string;
+
+    /**
+     * Check whether the incoming URI is a remote URI
+     *
+     * @returns { boolean } Return true or false
+     * @throws { BusinessError } 13900042 - Unknown error
+     * @syscap SystemCapability.FileManagement.AppFileService
+     * @since 12
+     */
+    /**
+     * Check whether the incoming URI is a remote URI
+     *
+     * @returns { boolean } Return true or false
+     * @throws { BusinessError } 13900042 - Unknown error
+     * @syscap SystemCapability.FileManagement.AppFileService
+     * @atomicservice
+     * @since 15 dynamic
+     * @since 23 static
+     */
+    isRemoteUri(): boolean;
+
+    /**
+     * Converts this URI into an encoded string.
+     *
+     * @returns { string } URI in a serialized string.
+     * @syscap SystemCapability.Utils.Lang
+     * @stagemodelonly   
+     * @since 23 static
+     */
+    toString(): string;
+  }
+  
+  /**
+   * Get the uri from the path of file in app sandbox
+   *
+   * @param { string } path the path of file in app sandbox
+   * @returns { string } Return the file uri
+   * @throws { BusinessError } 401 - The input parameter is invalidPossible causes:1.Mandatory parameters are left unspecified;
+   * <br>2.Incorrect parameter types.
+   * @syscap SystemCapability.FileManagement.AppFileService
+   * @since 9
+   */
+  /**
+   * Get the uri from the path of file in app sandbox
+   *
+   * @param { string } path the path of file in app sandbox
+   * @returns { string } Return the file uri
+   * @throws { BusinessError } 401 - The input parameter is invalidPossible causes:1.Mandatory parameters are left unspecified;
+   * <br>2.Incorrect parameter types.
+   * @syscap SystemCapability.FileManagement.AppFileService
+   * @atomicservice
+   * @since 15 dynamic
+   * @since 23 static
+   */
+  function getUriFromPath(path: string): string;
+}
+
+export default fileUri;
