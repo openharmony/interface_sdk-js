eeb2416eea716a595d3d32acfa578949c94e5368
zhangwentao <zhangwentao96@huawei.com>
2026-07-24 20:11:37 +0800

    布局回刷
    Signed-off-by: zhangwentao <zhangwentao96@huawei.com>
    Change-Id: I4e546c93c60283ff0e81e70073fda9b566da346f

--- zh-cn/api/@internal/component/ets/line.d.ts
+++ zh-cn/api/@internal/component/ets/line.d.ts
@@ -33,164 +33,183 @@
  * @since 18 dynamic
  */
 interface LineOptions {
-    /**
-     * 宽度。
-     * 
-     * 值为异常值或缺省时按照自身内容需要的宽度处理。
-     * 
-     * 默认单位：vp
-     *
-     * @type { ?(string | number) } [since 7 - 19]
-     * @type { ?Length } [since 20]
-     * @syscap SystemCapability.ArkUI.ArkUI.Full
-     * @FaAndStageModel
-     * @crossplatform [since 10]
-     * @form [since 9]
-     * @atomicservice [since 11]
-     * @since 7 dynamic
-     */
-    width?: Length;
-  
-    /**
-     * 高度。
-     * 
-     * 值为异常值或缺省时按照自身内容需要的高度处理。
-     * 
-     * 默认单位：vp
-     *
-     * @type { ?(string | number) } [since 7 - 19]
-     * @type { ?Length } [since 20]
-     * @syscap SystemCapability.ArkUI.ArkUI.Full
-     * @FaAndStageModel
-     * @crossplatform [since 10]
-     * @form [since 9]
-     * @atomicservice [since 11]
-     * @since 7 dynamic
-     */
-    height?: Length;
-  }
-  
   /**
-   * 直线绘制组件。
+   * 宽度。
    * 
-   * > **说明：**
-   * >
-   * > 该组件从API version 20开始支持使用[AttributeUpdater]{@link AttributeUpdater}类的
-   * > [updateConstructorParams](docroot://reference/apis-arkui/js-apis-arkui-AttributeUpdater.md#属性)接口更新构造参数。
-   *
-   * @syscap SystemCapability.ArkUI.ArkUI.Full
-   * @FaAndStageModel
-   * @crossplatform [since 10]
-   * @form [since 9]
-   * @atomicservice [since 11]
-   * @noninterop
-   * @since 7 dynamic
-   */
-  interface LineInterface {
-    /**
-     * Uses new to create the line.
-     * Anonymous Object Rectification.
-     *
-     * @param { object } value [since 7 - 17]
-     * @param { LineOptions } [options] - Line options [since 18]
-     * @returns { LineAttribute }
-     * @syscap SystemCapability.ArkUI.ArkUI.Full
-     * @FaAndStageModel
-     * @crossplatform [since 10]
-     * @form [since 9]
-     * @atomicservice [since 11]
-     * @since 7 dynamic
-     */
-    new (options?: LineOptions): LineAttribute;
-  
-    /**
-     * 用于绘制直线的构造函数。
-     *
-     * @param { object } value [since 7 - 17]
-     * @param { LineOptions } [options] - Line绘制区域。<br/>异常值undefined和null按照无效值处理，本次设置不生效。 [since 18]
-     * @returns { LineAttribute }
-     * @syscap SystemCapability.ArkUI.ArkUI.Full
-     * @FaAndStageModel
-     * @crossplatform [since 10]
-     * @form [since 9]
-     * @atomicservice [since 11]
-     * @since 7 dynamic
-     */
-    (options?: LineOptions): LineAttribute;
-  }
-  
-  /**
-   * 除支持[通用属性]{@link common}外，还支持以下属性：
-   *
-   * @syscap SystemCapability.ArkUI.ArkUI.Full
-   * @FaAndStageModel
-   * @crossplatform [since 10]
-   * @form [since 9]
-   * @atomicservice [since 11]
-   * @noninterop
-   * @since 7 dynamic
-   */
-  declare class LineAttribute extends CommonShapeMethod<LineAttribute> {
-    /**
-     * 设置直线起点坐标点（相对坐标），支持[attributeModifier]{@link CommonMethod#attributeModifier}动态设置属性方法，异常值按照默认值处理。
-     *
-     * @param { Array<any> } value - 直线起点坐标点（相对坐标），单位vp。<br/>默认值：[0, 0] <br/>异常值undefined和null按照默认值处理。
-     * @returns { LineAttribute }
-     * @syscap SystemCapability.ArkUI.ArkUI.Full
-     * @FaAndStageModel
-     * @crossplatform [since 10]
-     * @form [since 9]
-     * @atomicservice [since 11]
-     * @since 7 dynamic
-     */
-    startPoint(value: Array<any>): LineAttribute;
-  
-    /**
-     * 设置直线终点坐标点（相对坐标），支持[attributeModifier]{@link CommonMethod#attributeModifier}动态设置属性方法，异常值按照默认值处理。
-     *
-     * @param { Array<any> } value - 直线终点坐标点（相对坐标），单位vp。<br/>默认值：[0, 0] <br/>异常值undefined和null按照默认值处理。
-     * @returns { LineAttribute }
-     * @syscap SystemCapability.ArkUI.ArkUI.Full
-     * @FaAndStageModel
-     * @crossplatform [since 10]
-     * @form [since 9]
-     * @atomicservice [since 11]
-     * @since 7 dynamic
-     */
-    endPoint(value: Array<any>): LineAttribute;
-  }
-  
-  /**
-   * 直线绘制组件。
+   * 值为异常值或缺省时，根据startPoint和endPoint自动计算所需的绘制区域宽度。
    * 
-   * > **说明：**
-   * >
-   * > 该组件从API version 20开始支持使用[AttributeUpdater]{@link AttributeUpdater}类的
-   * > [updateConstructorParams](docroot://reference/apis-arkui/js-apis-arkui-AttributeUpdater.md#属性)接口更新构造参数。
-   * 
-   * ###### 子组件
-   * 
-   * 无
+   * 默认单位：vp
    *
-   * @syscap SystemCapability.ArkUI.ArkUI.Full
-   * @FaAndStageModel
-   * @crossplatform [since 10]
-   * @form [since 9]
-   * @atomicservice [since 11]
-   * @noninterop
-   * @since 7 dynamic
-   */
-  declare const Line: LineInterface;
-  
-  /**
-   * Defines Line Component instance.
-   *
+   * @type { ?(string | number) } [since 7 - 19]
+   * @type { ?Length } [since 20]
    * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @FaAndStageModel
    * @crossplatform [since 10]
    * @form [since 9]
    * @atomicservice [since 11]
    * @since 7 dynamic
-   * @noninterop [since 11]
    */
-  declare const LineInstance: LineAttribute;
+  width?: Length;
+
+  /**
+   * 高度。
+   * 
+   * 值为异常值或缺省时，根据startPoint和endPoint自动计算所需的绘制区域高度。
+   * 
+   * 默认单位：vp
+   *
+   * @type { ?(string | number) } [since 7 - 19]
+   * @type { ?Length } [since 20]
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @FaAndStageModel
+   * @crossplatform [since 10]
+   * @form [since 9]
+   * @atomicservice [since 11]
+   * @since 7 dynamic
+   */
+  height?: Length;
+}
+
+/**
+ * Line组件用于在应用界面中绘制直线，支持自定义直线的起点、终点、颜色、宽度、透明度、虚线样式、端点样式等属性。适用于绘制分隔线、装饰性线条、图表中的坐标轴或连接线、自定义图形边框等场景。
+ * 
+ * > **说明：**
+ * >
+ * > 该组件从API version 7开始支持。后续版本的新增接口，采用上角标单独标记接口的起始版本。
+ * >
+ * > 该组件从API version 20开始支持使用[AttributeUpdater]{@link ../../../arkui/AttributeUpdater}类的
+ * > [updateConstructorParams](docroot://reference/apis-arkui/js-apis-arkui-AttributeUpdater.md#属性)接口更新构造参数。
+ * >
+ * > - Line组件无法形成闭合区域，fill和fillOpacity属性设置无效。
+ * >
+ * > - Line组件不支持拐角，strokeLineJoin和strokeMiterLimit属性设置无效。
+ *
+ * @syscap SystemCapability.ArkUI.ArkUI.Full
+ * @FaAndStageModel
+ * @crossplatform [since 10]
+ * @form [since 9]
+ * @atomicservice [since 11]
+ * @since 7 dynamic
+ * @noninterop
+ */
+interface LineInterface {
+  /**
+   * Uses new to create the line.
+   * Anonymous Object Rectification.
+   *
+   * @param { object } value [since 7 - 17]
+   * @param { LineOptions } [options] - Line options [since 18]
+   * @returns { LineAttribute }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @FaAndStageModel
+   * @crossplatform [since 10]
+   * @form [since 9]
+   * @atomicservice [since 11]
+   * @since 7 dynamic
+   */
+  new (options?: LineOptions): LineAttribute;
+
+  /**
+   * 用于绘制直线的构造函数。Line组件在width和height定义的矩形区域内绘制直线，绘制区域的左上角为坐标原点(0,0)，x轴向右延伸，y轴向下延伸。
+   *
+   * @param { object } value - [since 7 - 17]
+   * @param { LineOptions } [options] - Line组件绘制区域，包含width和height属性，用于设置Line组件的宽高。不传递此参数时，Line组件的width和height属性将按照各自属性的缺
+   *     省逻辑处理（参见LineOptions对象说明）。
+   *     <br>异常值undefined和null按照无效值处理，本次设置不生效。 [since 18]
+   * @returns { LineAttribute }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @FaAndStageModel
+   * @crossplatform [since 10]
+   * @form [since 9]
+   * @atomicservice [since 11]
+   * @since 7 dynamic
+   */
+  (options?: LineOptions): LineAttribute;
+}
+
+/**
+ * 除支持[通用属性]{@link ./common}以及[图形绘制通用属性](docroot://reference/apis-arkui/arkui-ts/ts-drawing-components-common.md)外，还支持以下
+ * 属性：
+ *
+ * @syscap SystemCapability.ArkUI.ArkUI.Full
+ * @FaAndStageModel
+ * @crossplatform [since 10]
+ * @form [since 9]
+ * @atomicservice [since 11]
+ * @since 7 dynamic
+ * @noninterop
+ */
+declare class LineAttribute extends CommonShapeMethod<LineAttribute> {
+  /**
+   * 设置直线起点坐标点（相对于Line组件绘制区域的左上角原点），支持[attributeModifier]{@link CommonMethod#attributeModifier}动态设置属性方法，异常值按照默认值处理。
+   *
+   * @param { Array<any> } value - 直线起点坐标点（相对于Line组件绘制区域的左上角原点），单位vp。数组格式为[x坐标, y坐标]，数组长度必须为2，元素应为Length类型。
+   *     <br>默认值：[0, 0] 
+   *     <br>异常值undefined和null按照默认值处理。
+   * @returns { LineAttribute }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @FaAndStageModel
+   * @crossplatform [since 10]
+   * @form [since 9]
+   * @atomicservice [since 11]
+   * @since 7 dynamic
+   */
+  startPoint(value: Array<any>): LineAttribute;
+
+  /**
+   * 设置直线终点坐标点（相对于Line组件绘制区域的左上角原点），支持[attributeModifier]{@link CommonMethod#attributeModifier}动态设置属性方法，异常值按照默认值处理。
+   *
+   * @param { Array<any> } value - 直线终点坐标点（相对于Line组件绘制区域的左上角原点），单位vp。数组格式为[x坐标, y坐标]，数组长度必须为2，元素应为Length类型。
+   *     <br>默认值：[0, 0] 
+   *     <br>异常值undefined和null按照默认值处理。
+   * @returns { LineAttribute }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @FaAndStageModel
+   * @crossplatform [since 10]
+   * @form [since 9]
+   * @atomicservice [since 11]
+   * @since 7 dynamic
+   */
+  endPoint(value: Array<any>): LineAttribute;
+}
+
+/**
+ * Line组件用于在应用界面中绘制直线，支持自定义直线的起点、终点、颜色、宽度、透明度、虚线样式、端点样式等属性。适用于绘制分隔线、装饰性线条、图表中的坐标轴或连接线、自定义图形边框等场景。
+ * 
+ * > **说明：**
+ * >
+ * > 该组件从API version 7开始支持。后续版本的新增接口，采用上角标单独标记接口的起始版本。
+ * >
+ * > 该组件从API version 20开始支持使用[AttributeUpdater]{@link ../../../arkui/AttributeUpdater}类的
+ * > [updateConstructorParams](docroot://reference/apis-arkui/js-apis-arkui-AttributeUpdater.md#属性)接口更新构造参数。
+ * >
+ * > - Line组件无法形成闭合区域，fill和fillOpacity属性设置无效。
+ * >
+ * > - Line组件不支持拐角，strokeLineJoin和strokeMiterLimit属性设置无效。
+ * 
+ * ###### 子组件
+ * 
+ * 无
+ *
+ * @syscap SystemCapability.ArkUI.ArkUI.Full
+ * @FaAndStageModel
+ * @crossplatform [since 10]
+ * @form [since 9]
+ * @atomicservice [since 11]
+ * @since 7 dynamic
+ * @noninterop
+ */
+declare const Line: LineInterface;
+
+/**
+ * Defines Line Component instance.
+ *
+ * @syscap SystemCapability.ArkUI.ArkUI.Full
+ * @FaAndStageModel
+ * @crossplatform [since 10]
+ * @form [since 9]
+ * @atomicservice [since 11]
+ * @since 7 dynamic
+ * @noninterop [since 11]
+ */
+declare const LineInstance: LineAttribute;
--- zh-cn/api/@internal/component/ets/polygon.d.ts
+++ zh-cn/api/@internal/component/ets/polygon.d.ts
@@ -33,154 +33,164 @@
  * @since 18 dynamic
  */
 declare interface PolygonOptions {
-    /**
-     * 宽度，取值范围≥0。
-     * 
-     * 默认值：0
-     * 
-     * 默认单位：vp
-     * 
-     * 异常值undefined、null、NaN和Infinity按照默认值处理。
-     *
-     * @type { ?(string | number) } [since 7 - 19]
-     * @type { ?Length } [since 20]
-     * @syscap SystemCapability.ArkUI.ArkUI.Full
-     * @FaAndStageModel
-     * @crossplatform [since 10]
-     * @form [since 9]
-     * @atomicservice [since 11]
-     * @since 7 dynamic
-     */
-    width?: Length;
-  
-    /**
-     * 高度，取值范围≥0。
-     * 
-     * 默认值：0
-     * 
-     * 默认单位：vp
-     * 
-     * 异常值undefined、null、NaN和Infinity按照默认值处理。
-     *
-     * @type { ?(string | number) } [since 7 - 19]
-     * @type { ?Length } [since 20]
-     * @syscap SystemCapability.ArkUI.ArkUI.Full
-     * @FaAndStageModel
-     * @crossplatform [since 10]
-     * @form [since 9]
-     * @atomicservice [since 11]
-     * @since 7 dynamic
-     */
-    height?: Length;
-  }
-  
   /**
-   * 多边形绘制组件。
+   * 宽度，取值范围≥0。
    * 
-   * > **说明：**
+   * 默认值：0
    * 
-   * > 该组件从API version 20开始支持使用[AttributeUpdater]{@link AttributeUpdater}类的
-   * > [updateConstructorParams](docroot://reference/apis-arkui/js-apis-arkui-AttributeUpdater.md#属性)接口更新构造参数。
+   * 默认单位：vp
+   * 
+   * 若给定值小于0，则按默认值处理。异常值undefined、null、NaN和Infinity按照默认值处理。
    *
+   * @type { ?(string | number) } [since 18 - 19]
+   * @type { ?Length } [since 20]
    * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @FaAndStageModel
+   * @crossplatform
+   * @form
+   * @atomicservice
+   * @since 18 dynamic
+   */
+  width?: Length;
+
+  /**
+   * 高度，取值范围≥0。
+   * 
+   * 默认值：0
+   * 
+   * 默认单位：vp
+   * 
+   * 若给定值小于0，则按默认值处理。异常值undefined、null、NaN和Infinity按照默认值处理。
+   *
+   * @type { ?(string | number) } [since 18 - 19]
+   * @type { ?Length } [since 20]
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @FaAndStageModel
+   * @crossplatform
+   * @form
+   * @atomicservice
+   * @since 18 dynamic
+   */
+  height?: Length;
+}
+
+/**
+ * 多边形绘制组件。该组件通过设置顶点坐标列表来定义多边形的形状，支持填充颜色、边框样式等属性配置。组件采用二维坐标系统，按照顶点顺序依次连接形成封闭多边形区域。适用于绘制三角形、四边形、五边形等自定义多边形形状，以及实现图表、图标等需要
+ * 多边形元素的可视化场景。
+ * 
+ * > **说明：**
+ * >
+ * > 该组件从API version 7开始支持。后续版本的新增接口，采用上角标单独标记接口的起始版本。
+ * >
+ * > 该组件从API version 20开始支持使用[AttributeUpdater]{@link ../../../arkui/AttributeUpdater}类的
+ * > [updateConstructorParams](docroot://reference/apis-arkui/js-apis-arkui-AttributeUpdater.md#属性)接口更新构造参数。
+ *
+ * @syscap SystemCapability.ArkUI.ArkUI.Full
+ * @FaAndStageModel
+ * @crossplatform [since 10]
+ * @form [since 9]
+ * @atomicservice [since 11]
+ * @since 7 dynamic
+ * @noninterop
+ */
+interface PolygonInterface {
+  /**
+   * Uses new to create Polygon.
+   * Anonymous Object Rectification.
+   *
+   * @param { PolygonOptions } [options] - Polygon options [since 18]
+   * @returns { PolygonAttribute } [since 18]
+   * @syscap SystemCapability.ArkUI.ArkUI.Full [since 9]
    * @FaAndStageModel
    * @crossplatform [since 10]
    * @form [since 9]
    * @atomicservice [since 11]
-   * @noninterop
    * @since 7 dynamic
    */
-  interface PolygonInterface {
-    /**
-     * Uses new to create Polygon.
-     * Anonymous Object Rectification.
-     *
-     * @param { PolygonOptions } [options] - Polygon options [since 18]
-     * @returns { PolygonAttribute } [since 18]
-     * @syscap SystemCapability.ArkUI.ArkUI.Full [since 9]
-     * @FaAndStageModel
-     * @crossplatform [since 10]
-     * @form [since 9]
-     * @atomicservice [since 11]
-     * @since 7 dynamic
-     */
-    new (options?: PolygonOptions): PolygonAttribute;
-  
-    /**
-     * 用于绘制多边形的构造函数。
-     *
-     * @param { object } value [since 7 - 17]
-     * @param { PolygonOptions } [options] - Polygon绘制区域。<br/>异常值undefined和null按照无效值处理，本次设置不生效。 [since 18]
-     * @returns { PolygonAttribute }
-     * @syscap SystemCapability.ArkUI.ArkUI.Full
-     * @FaAndStageModel
-     * @crossplatform [since 10]
-     * @form [since 9]
-     * @atomicservice [since 11]
-     * @since 7 dynamic
-     */
-    (options?: PolygonOptions): PolygonAttribute;
-  }
-  
+  new (options?: PolygonOptions): PolygonAttribute;
+
   /**
-   * 除支持[通用属性]{@link common}外，还支持以下属性：
+   * 用于绘制多边形的构造函数。
    *
-   * @syscap SystemCapability.ArkUI.ArkUI.Full
-   * @FaAndStageModel
-   * @crossplatform [since 10]
-   * @form [since 9]
-   * @atomicservice [since 11]
-   * @noninterop
-   * @since 7 dynamic
-   */
-  declare class PolygonAttribute extends CommonShapeMethod<PolygonAttribute> {
-    /**
-     * 设置多边形的顶点坐标列表，支持[attributeModifier]{@link CommonMethod#attributeModifier}动态设置属性方法。异常值按照默认值处理。
-     *
-     * @param { Array<any> } value - 多边形的顶点坐标列表。使用时传入一个二维数组，每个子数组表示一个顶点的[x, y]坐标。<br/>默认值：[]（空数组）<br/>默认单位：vp <br/>异常值
-     *     undefined和null按照默认值处理。
-     * @returns { PolygonAttribute }
-     * @syscap SystemCapability.ArkUI.ArkUI.Full
-     * @FaAndStageModel
-     * @crossplatform [since 10]
-     * @form [since 9]
-     * @atomicservice [since 11]
-     * @since 7 dynamic
-     */
-    points(value: Array<any>): PolygonAttribute;
-  }
-  
-  /**
-   * 多边形绘制组件。
-   * 
-   * > **说明：**
-   * 
-   * > 该组件从API version 20开始支持使用[AttributeUpdater]{@link AttributeUpdater}类的
-   * > [updateConstructorParams](docroot://reference/apis-arkui/js-apis-arkui-AttributeUpdater.md#属性)接口更新构造参数。
-   * 
-   * ###### 子组件
-   * 
-   * 无
-   *
-   * @syscap SystemCapability.ArkUI.ArkUI.Full
-   * @FaAndStageModel
-   * @crossplatform [since 10]
-   * @form [since 9]
-   * @atomicservice [since 11]
-   * @noninterop
-   * @since 7 dynamic
-   */
-  declare const Polygon: PolygonInterface;
-  
-  /**
-   * Defines Polygon Component instance.
-   *
+   * @param { object } value - [since 7 - 17]
+   * @param { PolygonOptions } [options] - Polygon组件的配置选项，用于定义绘制区域的宽度和高度。需要指定多边形尺寸时传入此参数，不传入时使用默认宽度和高度（均为0）。当传入undefined
+   *     或null时，参数设置无效，组件属性维持原值。 [since 18]
+   * @returns { PolygonAttribute }
    * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @FaAndStageModel
    * @crossplatform [since 10]
    * @form [since 9]
    * @atomicservice [since 11]
    * @since 7 dynamic
-   * @noninterop [since 11]
    */
-  declare const PolygonInstance: PolygonAttribute;
+  (options?: PolygonOptions): PolygonAttribute;
+}
+
+/**
+ * 除支持[通用属性]{@link ./common}以及[图形绘制通用属性](docroot://reference/apis-arkui/arkui-ts/ts-drawing-components-common.md)外，还支持以下
+ * 属性：
+ *
+ * @syscap SystemCapability.ArkUI.ArkUI.Full
+ * @FaAndStageModel
+ * @crossplatform [since 10]
+ * @form [since 9]
+ * @atomicservice [since 11]
+ * @since 7 dynamic
+ * @noninterop
+ */
+declare class PolygonAttribute extends CommonShapeMethod<PolygonAttribute> {
+  /**
+   * 设置多边形的顶点坐标列表，支持[attributeModifier]{@link CommonMethod#attributeModifier}动态设置属性方法。异常值按照默认值处理。
+   *
+   * @param { Array<any> } value - 多边形的顶点坐标列表。使用时传入一个二维数组，每个子数组表示一个顶点的[x, y]坐标。
+   *     <br>默认值：[]（空数组）
+   *     <br>默认单位：vp 
+   *     <br>异常值undefined和null按照默认值处理。
+   * @returns { PolygonAttribute }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @FaAndStageModel
+   * @crossplatform [since 10]
+   * @form [since 9]
+   * @atomicservice [since 11]
+   * @since 7 dynamic
+   */
+  points(value: Array<any>): PolygonAttribute;
+}
+
+/**
+ * 多边形绘制组件。该组件通过设置顶点坐标列表来定义多边形的形状，支持填充颜色、边框样式等属性配置。组件采用二维坐标系统，按照顶点顺序依次连接形成封闭多边形区域。适用于绘制三角形、四边形、五边形等自定义多边形形状，以及实现图表、图标等需要
+ * 多边形元素的可视化场景。
+ * 
+ * > **说明：**
+ * >
+ * > 该组件从API version 7开始支持。后续版本的新增接口，采用上角标单独标记接口的起始版本。
+ * >
+ * > 该组件从API version 20开始支持使用[AttributeUpdater]{@link ../../../arkui/AttributeUpdater}类的
+ * > [updateConstructorParams](docroot://reference/apis-arkui/js-apis-arkui-AttributeUpdater.md#属性)接口更新构造参数。
+ * 
+ * ###### 子组件
+ * 
+ * 无
+ *
+ * @syscap SystemCapability.ArkUI.ArkUI.Full
+ * @FaAndStageModel
+ * @crossplatform [since 10]
+ * @form [since 9]
+ * @atomicservice [since 11]
+ * @since 7 dynamic
+ * @noninterop
+ */
+declare const Polygon: PolygonInterface;
+
+/**
+ * Defines Polygon Component instance.
+ *
+ * @syscap SystemCapability.ArkUI.ArkUI.Full
+ * @FaAndStageModel
+ * @crossplatform [since 10]
+ * @form [since 9]
+ * @atomicservice [since 11]
+ * @since 7 dynamic
+ * @noninterop [since 11]
+ */
+declare const PolygonInstance: PolygonAttribute;
