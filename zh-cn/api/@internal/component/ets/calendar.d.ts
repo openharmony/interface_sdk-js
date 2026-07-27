556c68bbb6e41ed9af14a5d05b2f7884bb5510fa
nickyhey <nizhiguang1@h-partners.com>
2026-07-27 19:09:37 +0800

    picker组件文档格式规范化，JSDoc注释重写及空行清理
    Signed-off-by: nickyhey <nizhiguang1@h-partners.com>

--- zh-cn/api/@internal/component/ets/calendar.d.ts
+++ zh-cn/api/@internal/component/ets/calendar.d.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -1008,11 +1008,11 @@ interface CalendarInterface {
    * @deprecated since 20
    */
   (value: {
-    date: { year: number; month: number; day: number };
-    currentData: MonthData;
-    preData: MonthData;
-    nextData: MonthData;
     controller?: CalendarController;
+    currentData: MonthData;
+    nextData: MonthData;
+    preData: MonthData;
+    date: { year: number; month: number; day: number };
   }): CalendarAttribute;
 }
 
