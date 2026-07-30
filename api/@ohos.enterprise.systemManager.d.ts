3dd2cfbf9aa2caccf16fc5e8389901aebcc8d634
fengxun1005 <fengxun@163.com>
2026-07-30 16:21:04 +0800

    isOtaUpdateNonceEnable接口实际不会抛出801 删除
    Signed-off-by: fengxun1005 <fengxun@163.com>

--- api/@ohos.enterprise.systemManager.d.ts
+++ api/@ohos.enterprise.systemManager.d.ts
@@ -1393,8 +1393,6 @@ declare namespace systemManager {
    * @returns { boolean } returns true indicates enabled OTA update nonce, otherwise disabled.
    * @throws { BusinessError } 201 - Permission verification failed.
    *     The application does not have the permission required to call the API.
-   * @throws { BusinessError } 801 - Capability not supported.
-   *     Failed to call the API due to limited device capabilities.
    * @throws { BusinessError } 9200001 - The application is not an administrator application of the device.
    * @throws { BusinessError } 9200002 - The administrator application does not have permission to manage the device.
    * @throws { BusinessError } 9200016 - Service timeout.
