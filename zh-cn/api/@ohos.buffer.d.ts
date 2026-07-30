476eb20d0f2f87f17fef959add8f98cc966d5780
jizaiyi <jizaiyi@h-partners.com>
2026-07-30 20:54:36 +0800

    Modify Buffer
    https://gitcode.com/openharmony/interface_sdk-js/issues/25883
    Test: Build
    
    Co-Authored-By: Agent
    Signed-off-by: jizaiyi <jizaiyi@h-partners.com>
    
    AI[0%] Human Fixed[0%] Human[100%] AI Adopted[0%]

--- zh-cn/api/@ohos.buffer.d.ts
+++ zh-cn/api/@ohos.buffer.d.ts
@@ -49,18 +49,7 @@ declare namespace buffer {
    * @since 9 dynamic
    * @since 23 static
    */
-  type BufferEncoding =
-    | 'ascii'
-    | 'utf8'
-    | 'utf-8'
-    | 'utf16le'
-    | 'ucs2'
-    | 'ucs-2'
-    | 'base64'
-    | 'base64url'
-    | 'latin1'
-    | 'binary'
-    | 'hex';
+  type BufferEncoding = 'ascii' | 'utf8' | 'utf-8' | 'utf16le' | 'ucs2' | 'ucs-2' | 'base64' | 'base64url' | 'latin1' | 'binary' | 'hex';
   /**
    * TypedArray继承Int8Array的特性与方法。
    *
