a7225c3a0f574c2ecefa0bcdbe83ec36177d70e1
fangyun <fangyun2@huawei.com>
2026-08-01 20:39:17 +0800

    update: 更新文件 @ohos.enterprise.restrictions.d.ts
    Signed-off-by: fangyun <fangyun2@huawei.com>

--- api/@ohos.enterprise.restrictions.d.ts
+++ api/@ohos.enterprise.restrictions.d.ts
@@ -58,15 +58,6 @@ declare namespace restrictions {
     WIFI_P2P = 0,
 
     /**
-     * X key.
-     *
-     * @syscap SystemCapability.Customization.EnterpriseDeviceManager
-     * @stagemodelonly
-     * @since 26.1.0
-     */
-    X_KEY = 1,
-
-    /**
      * Local input.
      *
      * @syscap SystemCapability.Customization.EnterpriseDeviceManager
@@ -74,15 +65,6 @@ declare namespace restrictions {
      * @since 26.0.0
      */
     LOCAL_INPUT = 2,
-
-    /**
-     * Network packet filtering.
-     *
-     * @syscap SystemCapability.Customization.EnterpriseDeviceManager
-     * @stagemodelonly
-     * @since 26.1.0
-     */
-    PACKET_FILTERING = 3,
 
     /**
      * Super user do.
