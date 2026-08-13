2c35ba5fbaccd676e2e90cd82da4e7bb7b2caa8b
shenshiyi2 <shenshiyi2@h-partners.com>
2026-08-13 19:41:40 +0800

    update: 更新文件 @ohos.graphics.scene.d.ts
    Signed-off-by: shenshiyi2 <shenshiyi2@h-partners.com>

--- api/@ohos.graphics.scene.d.ts
+++ api/@ohos.graphics.scene.d.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -19,7 +19,7 @@
  */
 
 /**
- * Export post processing settings
+ * provides image post-processing methods (for example, tone mapping) in 3D graphics.
  * @syscap SystemCapability.ArkUi.Graphics3D
  * @since 12 dynamic
  * @since 23 static
@@ -27,7 +27,7 @@
 export { ToneMappingType, ToneMappingSettings, PostProcessSettings, BloomSettings, VignetteSettings, ColorFringeSettings } from './graphics3d/ScenePostProcessSettings';
 
 /**
- * Export scene types
+ * provides the data types in 3D graphics, including vectors and quaternions.
  * @syscap SystemCapability.ArkUi.Graphics3D
  * @since 12 dynamic
  * @since 23 static
@@ -37,14 +37,14 @@ export { Vec2, Vec3, Vec4, Color, Rect, Quaternion, Aabb, Position3, Rotation3, 
   RenderingPipelineType } from './graphics3d/SceneTypes';
 
 /**
- * Export scene types
+ * provides the data types in 3D graphics, including vectors and quaternions.
  * @syscap SystemCapability.ArkUi.Graphics3D
  * @since 23 dynamic&static
  */
 export { Mat4x4, CylinderGeometry } from './graphics3d/SceneTypes';
 
 /**
- * Export scene resources
+ * provides basic resource types, such as materials, images, and shaders, which are commonly used in 3D graphics.
  * @syscap SystemCapability.ArkUi.Graphics3D
  * @since 12 dynamic
  * @since 23 static
@@ -55,7 +55,7 @@ export { SceneResourceType, SceneResource, Shader, MaterialType, Material, Shade
   Sampler, SamplerFilter, SamplerAddressMode, Effect } from './graphics3d/SceneResources';
 
 /**
- * Export scene resources
+ * provides basic resource types, such as materials, images, and shaders, which are commonly used in 3D graphics.
  * @syscap SystemCapability.ArkUi.Graphics3D
  * @systemapi
  * @stagemodelonly
@@ -64,14 +64,14 @@ export { SceneResourceType, SceneResource, Shader, MaterialType, Material, Shade
 export { UnlitShadowAlphaMaterial } from './graphics3d/SceneResources';
 
 /**
- * Export scene resources
+ * provides basic resource types, such as materials, images, and shaders, which are commonly used in 3D graphics.
  * @syscap SystemCapability.ArkUi.Graphics3D
  * @since 23 dynamic&static
  */
 export { PolygonMode, UnlitMaterial, OcclusionMaterial } from './graphics3d/SceneResources';
 
 /**
- * Export scene nodes
+ * 3D scenes adopt a tree structure. You can change the 3D scene by operating the node properties and the node tree structure. The module provides the types and operation methods of scene nodes in 3D graphics.
  * @syscap SystemCapability.ArkUi.Graphics3D
  * @since 12 dynamic
  * @since 23 static
@@ -80,7 +80,7 @@ export { LayerMask, NodeType, Container, Node, Geometry, LightType, Light, SpotL
   Camera } from './graphics3d/SceneNodes';
 
 /**
- * Export scene
+ * ArkGraphics 3D basic module, which provides common data types such as SceneResourceParameters and SceneNodeParameters. It also provides basic methods such as glTF model loading, scene creation, and resource creation.
  * @syscap SystemCapability.ArkUi.Graphics3D
  * @since 12 dynamic
  * @since 23 static
@@ -89,14 +89,14 @@ export { SceneResourceParameters, SceneNodeParameters, SceneResourceFactory, Sce
   RenderParameters, RenderResourceFactory, SceneComponent, RenderContext, CameraParameters, EffectParameters } from './graphics3d/Scene';
 
 /**
- * Export scene
+ * ArkGraphics 3D basic module, which provides common data types such as SceneResourceParameters and SceneNodeParameters. It also provides basic methods such as glTF model loading, scene creation, and resource creation.
  * @syscap SystemCapability.ArkUi.Graphics3D
  * @since 23 dynamic&static
  */
 export { RenderConfiguration } from './graphics3d/Scene';
 
 /**
- * Export BoidsSim
+ * This module provides types and operation methods for Boids simulation animation in 3D graphics. Boids simulation animation is a computer animation method that drives individuals in a group through rules such as separation, alignment, and cohesion, making them exhibit movement characteristics similar to flocks of birds or schools of fish in nature.
  * @syscap SystemCapability.ArkUi.Graphics3D
  * @systemapi
  * @stagemodelonly
@@ -107,7 +107,7 @@ export {
     BoidsSimRepulsionParameters } from './graphics3d/SceneBoidsSim';
 
 /**
- * Export SceneLoadParams
+ * ArkGraphics 3D basic module, which provides common data types such as SceneResourceParameters and SceneNodeParameters. It also provides basic methods such as glTF model loading, scene creation, and resource creation.
  * @syscap SystemCapability.ArkUi.Graphics3D
  * @systemapi
  * @stagemodelonly
