545af15b0d5c23b5eefc887504ceb23619654503
ZA2333 <zhuan2@h-partners.com>
2026-08-13 21:47:07 +0800

    update: 更新文件 @ohos.multimedia.media.d.ts
    Signed-off-by: ZA2333 <zhuan2@h-partners.com>

--- api/@ohos.multimedia.media.d.ts
+++ api/@ohos.multimedia.media.d.ts
@@ -4114,7 +4114,7 @@ declare namespace media {
 
     /**
      * Sets the playback rate. This API can be called only when the AVPlayer is in the prepared, playing, paused, or 
-     * completed state. The value range is [0.125, 8.0], on API 24 and below, the range is [0.125, 4.0].
+     * completed state. The value range is [0.125, 4.0]. You can check whether the setting takes effect through the 
      * You can check whether the setting takes effect through the 
      * [playbackRateDone]{@link media.AVPlayer.on(type: 'playbackRateDone', callback: OnPlaybackRateDone)} event.
      * 
@@ -4122,8 +4122,7 @@ declare namespace media {
      * >
      * > This API is not supported in live mode.
      *
-     * @param { double } rate - Playback rate, which is in the range [0.125, 8.0]
-     *     on API 24 and below, the range is [0.125, 4.0].
+     * @param { double } rate - Playback rate, which is in the range [0.125, 4.0].
      * @throws { BusinessError } 5400108 - The parameter check failed, parameter value out of range.
      * @throws { BusinessError } 5400102 - Operation not allowed, if invalid state or live stream.
      * @syscap SystemCapability.Multimedia.Media.AVPlayer
