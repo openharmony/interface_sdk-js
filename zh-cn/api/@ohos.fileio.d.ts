35ecbea5461485900934707a110f92ac328c6847
xingshenjie <xingshenjie@huawei.com>
2026-08-16 11:38:38 +0800

    新旧文件不一致问题修改
    Signed-off-by: xingshenjie <xingshenjie@huawei.com>

--- zh-cn/api/@ohos.fileio.d.ts
+++ zh-cn/api/@ohos.fileio.d.ts
@@ -14,14 +14,19 @@
  */
 
 /**
- * @file
+ * @file @ohos.fileio (文件管理)
  * @kit API10LessDeprecatedModules
  */
 
 import { AsyncCallback } from './@ohos.base';
 
 /**
- * fileio
+ * 该模块提供文件存储管理能力，包括文件基本管理、文件目录管理、文件信息统计、文件流式读写等常用功能。
+ *
+ * > **使用说明：**
+ *
+ * 使用该功能模块对文件/目录进行操作前，需要先获取其应用沙箱路径，获取沙箱路径的方式及其接口用法可参考：
+ * [应用上下文Context-获取应用文件路径](docroot://application-models/application-context-stage.md#获取应用文件路径)。
  *
  * @syscap SystemCapability.FileManagement.File.FileIO
  * @since 6
