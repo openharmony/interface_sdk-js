453ad28c67a24aa3d1581dccfdfb54478a5939d0
hearn <hearn.qk@gmail.com>
2026-08-24 10:28:59 +0800

    update: 更新文件 @ohos.PiPWindow.d.ts
    Signed-off-by: hearn <hearn.qk@gmail.com>

--- zh-cn/api/@ohos.PiPWindow.d.ts
+++ zh-cn/api/@ohos.PiPWindow.d.ts
@@ -389,7 +389,7 @@ declare namespace PiPWindow {
      *
      * @syscap SystemCapability.Window.SessionManager
      * @systemapi Hide this for inner system use
-     * @atomicservice
+     * @stagemodelonly
      * @since 26.1.0 dynamic&static
      */
     VIDEO_NAVIGATION = 5
