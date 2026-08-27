df50352804298bb7f511ca0fa604f5b3455e9f3b
allenlee6 <lishengyang5@huawei.com>
2026-08-27 20:32:51 +0800

    update: 更新文件 @ohos.app.form.formAgent.d.ts
    Signed-off-by: allenlee6 <lishengyang5@huawei.com>

--- api/@ohos.app.form.formAgent.d.ts
+++ api/@ohos.app.form.formAgent.d.ts
@@ -55,7 +55,7 @@ declare namespace formAgent {
    * @throws { BusinessError } 16501000 - An internal functional error occurred.
    * @throws { BusinessError } 16501002 - The number of forms exceeds the maximum allowed. [since 26.1.0]
    * @throws { BusinessError } 16501008 - Waiting for the form addition to the desktop timed out. [since 12]
-   * @throws { BusinessError } 16501017 - There is no space to publish form. [since 26.1.0]
+   * @throws { BusinessError } 16501017 - There is no space to the publish form. [since 26.1.0]
    * @throws { BusinessError } 16501018 - This form does not support publishing. [since 26.1.0]
    * @syscap SystemCapability.Ability.Form
    * @systemapi
@@ -85,7 +85,7 @@ declare namespace formAgent {
    * @throws { BusinessError } 16501000 - An internal functional error occurred.
    * @throws { BusinessError } 16501002 - The number of forms exceeds the maximum allowed. [since 26.1.0]
    * @throws { BusinessError } 16501008 - Waiting for the form addition to the desktop timed out. [since 12]
-   * @throws { BusinessError } 16501017 - There is no space to publish form. [since 26.1.0]
+   * @throws { BusinessError } 16501017 - There is no space to publish the form. [since 26.1.0]
    * @throws { BusinessError } 16501018 - This form does not support publishing. [since 26.1.0]
    * @syscap SystemCapability.Ability.Form
    * @systemapi
