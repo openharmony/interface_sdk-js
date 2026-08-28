f382faab4d5600a2e14ff31ecec5b42cd4efae71
weixin_43212802 <liuwenhao36@h-partners.com>
2026-08-28 11:38:18 +0800

    update: 更新文件 @ohos.hidebug.d.ts
    Signed-off-by: weixin_43212802 <liuwenhao36@h-partners.com>

--- zh-cn/api/@ohos.hidebug.d.ts
+++ zh-cn/api/@ohos.hidebug.d.ts
@@ -1429,5 +1429,16 @@ declare namespace hidebug {
    * @since 24 dynamic&static
    */
   function getRssInfo(): RssInfo;
+
+  /**
+ 	 * 获取应用程序的运行唯一标识符。
+ 	 *
+ 	 * @returns { string } 返回运行唯一标识ID字符串。失败时返回空字符串。
+ 	 * @syscap SystemCapability.HiviewDFX.HiProfiler.HiDebug
+ 	 * @FaAndStageModel
+ 	 * @atomicservice
+ 	 * @since 26.1.0 dynamic&static
+ 	 */
+  function getAppRunningUniqueId(): string;
 }
 export default hidebug;
