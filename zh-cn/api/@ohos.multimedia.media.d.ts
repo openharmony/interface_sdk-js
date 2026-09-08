a2210f7e938e7f639ceb0e624470933c4f4d855a
wangyongli66 <wangyongli7@h-partners.com>
2026-09-08 09:53:35 +0800

    fix
    Signed-off-by: wangyongli66 <wangyongli7@h-partners.com>

--- zh-cn/api/@ohos.multimedia.media.d.ts
+++ zh-cn/api/@ohos.multimedia.media.d.ts
@@ -11557,7 +11557,7 @@ declare namespace media {
     addWatermark(watermark: image.PixelMap, config: WatermarkConfiguration): Promise<int>;
 
     /**
-     * 注册转码完成事件，并通过注册的回调方法通知开发者。开发者只能注册一个进度更新事件的回调方法，当开发者重复注册时，以最后一次注册的回调接口为准。使用callback异步回调。
+     * 注册转码完成事件，并通过注册的回调方法通知开发者。开发者只能注册一个完成事件的回调方法，当开发者重复注册时，以最后一次注册的回调接口为准。使用callback异步回调。
      * 
      * 当AVTranscoder上报complete事件时，当前转码操作已完成，开发者需要通过[release()]{@link media.AVTranscoder.release}退出转码操作。
      *
