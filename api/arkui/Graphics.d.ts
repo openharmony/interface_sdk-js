5d764f8560e5540dd3b7eec4828baa6c5635c24d
wangmiaoliang <wangmiaoliang1@huawei.com>
2026-09-11 01:44:32 +0000

    fix
    Signed-off-by: wangmiaoliang <wangmiaoliang1@huawei.com>
    Change-Id: I1e175b1634e27845d502a642df94f2913b10a1fc

--- api/arkui/Graphics.d.ts
+++ api/arkui/Graphics.d.ts
@@ -1015,7 +1015,7 @@ declare class ColorMetrics {
    *
    * @returns { double } The red value.
    *     Valid range:
-   *     For SDR colors: [0, 1].
+   *     For SDR colors: [0, 1]. 
    *     Fro HDR colors: [0, +∞), values > 1.0 indicate HDR brightness.
    * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @systemapi
