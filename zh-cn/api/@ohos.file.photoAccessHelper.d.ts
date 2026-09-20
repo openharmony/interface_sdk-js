5657336d77fbe8cdc1c1744e3a911248ca2a474b
linyouhui <linyouhui2@huawei.com>
2026-09-20 19:32:03 +0800

    Add FEATURED_SINGLE_PET recommendation type
    Signed-off-by: linyouhui <linyouhui2@huawei.com>

--- zh-cn/api/@ohos.file.photoAccessHelper.d.ts
+++ zh-cn/api/@ohos.file.photoAccessHelper.d.ts
@@ -735,7 +735,17 @@ declare namespace photoAccessHelper {
      * @since 24 dynamic
      * @since 26.0.0 static
      */
-    GAUSSIAN_SPLAT_3D = 17
+    GAUSSIAN_SPLAT_3D = 17,
+
+    /**
+     * 推荐宠物。
+     *
+     * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+     * @atomicservice
+     * @since 24 dynamic
+     * @since 26.0.0 static
+     */
+    FEATURED_SINGLE_PET = 18
   }
 
   /**
